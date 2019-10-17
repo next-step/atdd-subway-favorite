6cc42489916b24e52921488af6890a579a736361
gracefulBrown <sunghyun.ryu@woowahan.com>
2019-10-17 14:28:17 +0900

    init

---
+++ src/main/java/camp/nextstep/atdd/AtddApplication.java
@@ -0,0 +1,13 @@
+package camp.nextstep.atdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/camp/nextstep/atdd/AtddApplicationTests.java
@@ -0,0 +1,13 @@
+package camp.nextstep.atdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
