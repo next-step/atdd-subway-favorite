b1ab7e8a5e37c95cf40e138aa69e1e38019109df
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-02-02 13:28:21 +0900

    feat(station): init station sample

--- src/test/java/camp/nextstep/atdd/station/StationAcceptanceTest.java
+++ src/test/java/camp/nextstep/atdd/station/StationAcceptanceTest.java
@@ -1,33 +1,5 @@
 package camp.nextstep.atdd.station;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
 public class StationAcceptanceTest {
-    public static final String STATION_NAME = "강남역";
-
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @Test
-    public void createStation() {
-        String inputJson = "{\"name\":\"" + STATION_NAME + "\"}";
-
-        webTestClient.post().uri("/stations")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(inputJson), String.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectHeader().exists("Location")
-                .expectBody().jsonPath("$.name").isEqualTo(STATION_NAME);
-    }
 
 }
