47416f9de8392437f36a825b52eb6c92bc31d474
yukeun <snoopy12oyk@gmail.com>
2020-02-17 23:51:32 +0900

    feat: Add MemberDao

---
+++ src/main/java/atdd/path/dao/MemberDao.java
@@ -0,0 +1,69 @@
+package atdd.path.dao;
+
+import atdd.path.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class MemberDao
+{
+    private final JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    @Autowired
+    public void setDataSource(final DataSource dataSource)
+    {
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("MEMBER")
+                .usingGeneratedKeyColumns("ID");
+    }
+
+    public MemberDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Member save(Member member)
+    {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("ID", member.getId());
+        parameters.put("EMAIL", member.getEmail());
+        parameters.put("NAME", member.getName());
+        parameters.put("PASSWORD", member.getPassword());
+
+        Long memberId = simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
+        return findById(memberId);
+    }
+
+    public Member findById(long id)
+    {
+        String sql = "SELECT ID " +
+                     "      ,EMAIL " +
+                     "      ,NAME " +
+                     "      ,PASSWORD " +
+                     "  FROM MEMBER M " +
+                     " WHERE M.ID = ?";
+
+        Member member = jdbcTemplate.queryForObject(sql, new Object[]{id}, (rs, rowNum) ->
+                new Member(
+                        rs.getLong("id"),
+                        rs.getString("email"),
+                        rs.getString("name"),
+                        rs.getString("password")
+                ));
+        return member;
+    }
+
+    public void deleteById(Long id)
+    {
+        String sql = "DELETE " +
+                     "  FROM MEMBER" +
+                     " WHERE ID = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
