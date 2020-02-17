9e0d29a6689da118b49d278c613064063a91e50e
Hyeonseok AHN <hyeonseok@pplink.net>
2020-02-17 22:33:06 +0900

    Update UserRepositoryTest with findAll method

--- src/test/java/atdd/path/repository/UserRepositoryTest.java
+++ src/test/java/atdd/path/repository/UserRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -19,5 +22,17 @@ public class UserRepositoryTest {
         User persistUser = userRepository.save(user);
 
         assertThat(persistUser.getName()).isEqualTo("브라운");
+    }
+
+    @Test
+    public void readUsers() {
+        createUser();
+
+        List<User> userList = new ArrayList<User>();
+
+        userRepository.findAll().forEach(userList::add);
+
+        assertThat(userList.size()).isEqualTo(1);
+        assertThat(userList.get(0).getName()).isEqualTo("브라운");
     }
 }
