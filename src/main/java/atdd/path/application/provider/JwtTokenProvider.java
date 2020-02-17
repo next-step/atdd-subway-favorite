fbb769caa85638933637b09d53e251153ae7d280
yhs <mvmaniaz@gmail.com>
2020-02-17 21:32:21 +0900

    feat(login): add jwt token for login test

---
+++ src/main/java/atdd/path/application/provider/JwtTokenProvider.java
@@ -0,0 +1,36 @@
+package atdd.path.application.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    public static final String TOKEN_TYPE = "bearer";
+
+    @Value("${spring.security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    public String createToken(String email) {
+        Claims claims = Jwts.claims().setSubject(email);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+}
--- src/main/java/atdd/path/web/LoginController.java
+++ src/main/java/atdd/path/web/LoginController.java
@@ -3,6 +3,7 @@ package atdd.path.web;
 import atdd.path.application.dto.LoginRequestView;
 import atdd.path.application.dto.LoginResponseView;
 import atdd.path.application.exception.UnauthorizedException;
+import atdd.path.application.provider.JwtTokenProvider;
 import atdd.path.dao.MemberDao;
 import atdd.path.domain.Member;
 import org.springframework.http.ResponseEntity;
@@ -11,25 +12,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static atdd.path.application.provider.JwtTokenProvider.TOKEN_TYPE;
+
 @RequestMapping("/login")
 @RestController
 public class LoginController {
 
     private final MemberDao memberDao;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public LoginController(MemberDao memberDao) {
+    public LoginController(MemberDao memberDao, JwtTokenProvider jwtTokenProvider) {
         this.memberDao = memberDao;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping
     public ResponseEntity<LoginResponseView> login(@RequestBody LoginRequestView view) {
-        final Member findMember = memberDao.findByEmail(view.getEmail()).orElseThrow(UnauthorizedException::new);
+        final String email = view.getEmail();
+        final Member findMember = memberDao.findByEmail(email).orElseThrow(UnauthorizedException::new);
 
         if (!findMember.isMatchPassword(view.getPassword())) {
             throw new UnauthorizedException();
         }
 
-        return ResponseEntity.ok(new LoginResponseView("bearer", ""));
+        final String accessToken = jwtTokenProvider.createToken(email);
+        return ResponseEntity.ok(new LoginResponseView(TOKEN_TYPE, accessToken));
     }
 
 }
