0a6682c3d7354386a7427e982fb7cebb4f5904ff
Hyeonseok AHN <hyeonseok@pplink.net>
2020-02-17 22:29:48 +0900

    Simplify UserController with UserRepository

--- src/main/java/atdd/path/web/UserController.java
+++ src/main/java/atdd/path/web/UserController.java
@@ -3,6 +3,7 @@ package atdd.path.web;
 import atdd.path.application.dto.CreateUserRequestView;
 import atdd.path.application.dto.UserResponseView;
 import atdd.path.domain.User;
+import atdd.path.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +14,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @PostMapping
     public ResponseEntity createUser(@RequestBody CreateUserRequestView view) {
-        Long id = 1L;
-
-        return ResponseEntity.created(URI.create("/lines/" + id)).body(UserResponseView.of(view.toUser()));
+        User persistUser = userRepository.save(view.toUser());
+        return ResponseEntity.created(URI.create("/lines/" + persistUser.getId())).body(UserResponseView.of(persistUser));
     }
 
     @GetMapping
