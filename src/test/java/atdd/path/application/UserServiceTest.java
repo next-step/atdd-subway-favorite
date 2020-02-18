3c03cbca0bc0461f265d98a8742fd83087835307
sanggu <tkdrn8578@naver.com>
2020-02-19 00:00:34 +0900

    [MODIFY] 회원 등록에 대한 Email 검증의 파라미터를 ArrayList 로 변경

--- src/test/java/atdd/path/application/UserServiceTest.java
+++ src/test/java/atdd/path/application/UserServiceTest.java
@@ -1,15 +1,16 @@
 package atdd.path.application;
 
+import atdd.path.application.dto.UserSighUpResponseView;
 import atdd.path.application.exception.ExistUserException;
-import atdd.path.application.exception.NoDataException;
 import atdd.path.dao.UserDao;
-import atdd.path.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
 
 import static atdd.path.fixture.UserFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,10 +42,10 @@ public class UserServiceTest {
     @DisplayName("회원이 정상적으로 등록되는지")
     @Test
     public void signUp() {
-        when(userDao.findByEmail(any())).thenReturn(null);
+        when(userDao.findByEmail(any())).thenReturn(new ArrayList<>());
         when(userDao.save(any())).thenReturn(NEW_USER);
 
-        User user = userService.singUp(USER_SIGH_UP_REQUEST_DTO);
+        UserSighUpResponseView user = userService.singUp(USER_SIGH_UP_REQUEST_DTO);
 
         assertThat(user.getId()).isNotNull();
     }
