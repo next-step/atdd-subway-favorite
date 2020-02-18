7baa9a8db34ffafa6b1493503d839f0f2cf7c043
sanggu <tkdrn8578@naver.com>
2020-02-18 23:22:52 +0900

    [MODIFY] user 가 존재하지 않을 떄 저장하도록 로직 수정

--- src/main/java/atdd/path/application/UserService.java
+++ src/main/java/atdd/path/application/UserService.java
@@ -1,12 +1,15 @@
 package atdd.path.application;
 
 import atdd.path.application.dto.UserSighUpRequestView;
+import atdd.path.application.exception.ExistUserException;
 import atdd.path.dao.UserDao;
 import atdd.path.domain.Station;
+import atdd.path.domain.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -16,10 +19,13 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public List<Station> singUp(UserSighUpRequestView user) {
-        userDao.findByEmail(user.getEmail());
+    public User singUp(UserSighUpRequestView newUser) {
+        List<Map<String, Object>> user = userDao.findByEmail(newUser.getEmail());
 
-        return new ArrayList<>();
+        if (user != null) {
+            throw new ExistUserException();
+        }
+
+        return userDao.save(UserSighUpRequestView.toEntity(newUser));
     }
-
 }
