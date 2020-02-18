621801ba779bf534a75a85619c9182c2936e5a67
yukeun <snoopy12oyk@gmail.com>
2020-02-18 22:59:46 +0900

    feat: Add InvalidJwtAuthenticationException

---
+++ src/main/java/atdd/path/application/exception/InvalidJwtAuthenticationException.java
@@ -0,0 +1,13 @@
+package atdd.path.application.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidJwtAuthenticationException extends RuntimeException
+{
+    public InvalidJwtAuthenticationException(String message)
+    {
+        super(message);
+    }
+}
