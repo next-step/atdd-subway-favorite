34c5968a052d9944e52e210983b8900894f84f6c
CptBong <cpt1bong@gmail.com>
2020-02-18 15:28:19 +0900

    회원가입시나리오테스트추가 - 현재404에러.

---
+++ src/test/java/atdd/path/web/UserManageTest.java
@@ -0,0 +1,46 @@
+package atdd.path.web;
+
+import atdd.path.AbstractAcceptanceTest;
+import atdd.path.application.dto.LineResponseView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
+
+public class UserManageTest extends AbstractAcceptanceTest {
+//    Feature: 회원 정보 관리
+//
+//    Scenario: 회원 가입
+//    Given -
+//    When 사용자는 자신의 정보 등록을 요청한다.
+//    Then 회원 등록이 되었다.
+//
+//    Scenario: 회원 탈퇴
+//    Given 사용자는 회원가입을 했다.
+//    When 사용자는 탈퇴 요청한다.
+//    Then 회원 탈퇴가 되었다.
+//
+    @BeforeEach
+    void setUp() {}
+
+    @DisplayName("회원 가입")
+    @Test
+    public void CreateMemeber() {
+        String memberName = "봉일균";
+        Integer age = 33;
+        String cellPhone = "010-6616-6609";
+        String inputJson = "{\"name\":\"" + memberName + "\"," +
+                "\"age\":\"" + age + "\"," +
+                "\"cellPhone\":\"" + cellPhone + "\"}";
+
+        webTestClient.post().uri("/user")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(Mono.just(inputJson), String.class)
+            .exchange()
+            .expectStatus().isCreated()
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectHeader().exists("Location")
+            .expectBody(LineResponseView.class);
+    }
+}
