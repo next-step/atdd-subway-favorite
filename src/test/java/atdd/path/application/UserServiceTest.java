0560432067027979e41b5032f43d7bd03340a2a0
sanggu <tkdrn8578@naver.com>
2020-02-18 23:23:18 +0900

    [ADD] findByEmail user 가 존재하지 않는 경우 저장하는 테스트 추가

--- src/test/java/atdd/path/application/UserServiceTest.java
+++ src/test/java/atdd/path/application/UserServiceTest.java
@@ -1,7 +1,9 @@
 package atdd.path.application;
 
+import atdd.path.application.exception.ExistUserException;
 import atdd.path.application.exception.NoDataException;
 import atdd.path.dao.UserDao;
+import atdd.path.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static atdd.path.fixture.UserFixture.USER_SIGH_UP_REQUEST_DTO;
+import static atdd.path.fixture.UserFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = UserService.class)
 public class UserServiceTest {
@@ -20,15 +25,27 @@ public class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-
+        this.userService = new UserService(userDao);
     }
 
     @DisplayName("이미 회원이 있는 유저인 경우 예외를 처리하는지")
     @Test
     public void sighUpWhenExistUser() {
-        Long existUserId = 0L;
-        Assertions.assertThrows(NoDataException.class, () -> {
+        when(userDao.findByEmail(any())).thenReturn(getDaoUsers());
+
+        Assertions.assertThrows(ExistUserException.class, () -> {
             userService.singUp(USER_SIGH_UP_REQUEST_DTO);
         });
+    }
+
+    @DisplayName("회원이 정상적으로 등록되는지")
+    @Test
+    public void signUp() {
+        when(userDao.findByEmail(any())).thenReturn(null);
+        when(userDao.save(any())).thenReturn(NEW_USER);
+
+        User user = userService.singUp(USER_SIGH_UP_REQUEST_DTO);
+
+        assertThat(user.getId()).isNotNull();
     }
 }
