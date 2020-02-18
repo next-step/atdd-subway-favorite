e2bd8643f36a1d7d6d9ca4a52d1435919c9c669e
sanggu <tkdrn8578@naver.com>
2020-02-18 23:42:48 +0900

    [ADD] 회원탈퇴에 대한 Accpetance Test 코드 추가 및 createUser 메서드 추가

--- src/test/java/atdd/path/web/UserAcceptanceTest.java
+++ src/test/java/atdd/path/web/UserAcceptanceTest.java
@@ -1,28 +1,55 @@
 package atdd.path.web;
 
 import atdd.path.AbstractAcceptanceTest;
+import atdd.path.application.dto.UserSighUpResponseView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
+import static atdd.path.fixture.UserFixture.*;
+
 public class UserAcceptanceTest extends AbstractAcceptanceTest {
+    public static final String KIM_INPUT_JSON = "{\"email\":\"" + KIM_EMAIL + "\",\"password\":\"" + KIM_PASSWORD + "\",\"name\":\"" + KIM_NAME + "\"}";;
+
     @DisplayName("유저_회원가입이_성공하는지")
     @Test
     public void userSighUp() {
-        String email = "sgkim94@github.com";
-        String name = "김상구";
-        String inputJson = "{\"email\":\"" + email + "\",\"name\":\"" + name + "\"}";
-
         webTestClient.post().uri("/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(inputJson), String.class)
+                .body(Mono.just(KIM_INPUT_JSON), String.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectHeader().exists("Location")
                 .expectBody()
-                .jsonPath("$.email").isEqualTo(email)
-                .jsonPath("$.name").isEqualTo(name);
+                .jsonPath("$.email").isEqualTo(KIM_EMAIL)
+                .jsonPath("$.name").isEqualTo(KIM_NAME);
+    }
+
+
+    @DisplayName("유저_회원_탈퇴가_성공하는지")
+    @Test
+    public void userDelete() {
+        String id = createUser();
+
+        webTestClient.delete().uri("/users/" + id)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+
+    public String createUser() {
+        return Objects.requireNonNull(webTestClient.post().uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(KIM_INPUT_JSON), String.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .returnResult(UserSighUpResponseView.class)
+                .getResponseHeaders()
+                .getLocation())
+                .getPath();
     }
 }
