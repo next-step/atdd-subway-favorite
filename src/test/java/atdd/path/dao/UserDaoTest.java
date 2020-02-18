5b3e00949c2bf82985a150d6918d6250afcbb2fc
sanggu <tkdrn8578@naver.com>
2020-02-18 22:08:50 +0900

    [ADD] List<Map<String, Object>> 를 User 로 변환해주는 메서드를 테스트하기 위해 추가

--- src/test/java/atdd/path/dao/UserDaoTest.java
+++ src/test/java/atdd/path/dao/UserDaoTest.java
@@ -10,6 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
+import java.util.*;
+
 import static atdd.path.fixture.UserFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,4 +49,20 @@ public class UserDaoTest {
         assertThat(persistUser.getId()).isNotNull();
         assertThat(persistUser.getName()).isEqualTo(KIM_NAME);
     }
+
+    @DisplayName("findById 로 나온 결과를 User 로 만들어주는지")
+    @Test
+    public void mapUser() {
+        Map<String, Object> savedUser = new HashMap<>();
+        savedUser.put("ID", 1);
+        savedUser.put("NAME", "김상구");
+        savedUser.put("EMAIL", "sgkim94@github.com");
+        List<Map<String, Object>> users = Collections.singletonList(savedUser);
+
+        User user = userDao.mapUser(users);
+
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getName()).isEqualTo(KIM_NAME);
+    }
+
 }
