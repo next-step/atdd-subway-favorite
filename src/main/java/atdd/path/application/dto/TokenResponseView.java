527b899f2daa9e048b6b92f1ee127cf2bf302fa5
yukeun <snoopy12oyk@gmail.com>
2020-02-18 22:49:33 +0900

    feat: Add TokenResponseView

---
+++ src/main/java/atdd/path/application/dto/TokenResponseView.java
@@ -0,0 +1,29 @@
+package atdd.path.application.dto;
+
+public class TokenResponseView
+{
+    private String accessToken;
+    private String type = "Bearer";
+
+    public TokenResponseView() {}
+
+    public TokenResponseView(String accessToken)
+    {
+        this.accessToken = accessToken;
+    }
+
+    public static TokenResponseView of(String accessToken)
+    {
+        return new TokenResponseView(accessToken);
+    }
+
+    public String getAccessToken()
+    {
+        return accessToken;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+}
