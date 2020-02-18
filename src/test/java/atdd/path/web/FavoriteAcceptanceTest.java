7cc391f9d6103a1e1e98821428ab92b3bc9e181b
yhs <mvmaniaz@gmail.com>
2020-02-18 23:12:03 +0900

    feat(favorite): update acceptance test for find favorite station test

--- src/test/java/atdd/path/web/FavoriteAcceptanceTest.java
+++ src/test/java/atdd/path/web/FavoriteAcceptanceTest.java
@@ -1,14 +1,16 @@
 package atdd.path.web;
 
 import atdd.path.AbstractAcceptanceTest;
+import atdd.path.application.dto.FavoriteStationResponseView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 
-import static atdd.path.TestConstant.STATION_NAME;
-import static atdd.path.TestConstant.TEST_MEMBER;
+import static atdd.path.TestConstant.*;
 import static atdd.path.application.provider.JwtTokenProvider.AUTHORIZATION;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
 
@@ -29,15 +31,47 @@ public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
         memberHttpTest.createMemberRequest(TEST_MEMBER);
         String token = memberHttpTest.loginMember(TEST_MEMBER);
 
-        webTestClient.post().uri("/favorites/stations/" + stationId)
+        EntityExchangeResult<FavoriteStationResponseView> result = createForStationRequest(stationId, token);
+        FavoriteStationResponseView view = result.getResponseBody();
+
+        assertThat(view).isNotNull();
+        assertThat(view.getId()).isEqualTo(FAVORITE_STATION_ID);
+        assertThat(view.getStation().getName()).isEqualTo(STATION_NAME);
+    }
+
+    @DisplayName("지하철역 즐겨찾기 목록을 조회 할 수 있다")
+    @Test
+    void beAbleToFindForStation() {
+        Long stationId = stationHttpTest.createStation(STATION_NAME);
+        Long stationId2 = stationHttpTest.createStation(STATION_NAME_2);
+        Long stationId3 = stationHttpTest.createStation(STATION_NAME_3);
+
+        memberHttpTest.createMemberRequest(TEST_MEMBER);
+        String token = memberHttpTest.loginMember(TEST_MEMBER);
+
+        createForStationRequest(stationId, token);
+        createForStationRequest(stationId2, token);
+        createForStationRequest(stationId3, token);
+
+        webTestClient.get().uri("/favorites/stations")
+                .header(AUTHORIZATION, token)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.count").isEqualTo(3)
+                .jsonPath("$.stations[0].name").isEqualTo(STATION_NAME);
+    }
+
+    public EntityExchangeResult<FavoriteStationResponseView> createForStationRequest(Long stationId, String token) {
+        return webTestClient.post().uri("/favorites/stations/" + stationId)
                 .header(AUTHORIZATION, token)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectHeader().exists("Location")
-                .expectBody()
-                .jsonPath("$.id").isNotEmpty()
-                .jsonPath("$.station.name").isEqualTo(STATION_NAME);
+                .expectBody(FavoriteStationResponseView.class)
+                .returnResult();
     }
 
 }
