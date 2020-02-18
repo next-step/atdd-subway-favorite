bbfe281abb9165239f86d61f5e89628198e32f4b
parkeeseul <eeseul.p@gmail.com>
2020-02-19 01:09:02 +0900

    feat : InvalidEmailException 추가

---
+++ src/main/java/atdd/path/application/exception/InvalidEmailException.java
@@ -0,0 +1,10 @@
+package atdd.path.application.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidEmailException extends RuntimeException {
+}
+
+
