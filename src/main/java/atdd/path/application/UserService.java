cf18455fad97a6f7fa7af767bc4dce134bdd6bb0
parkeeseul <eeseul.p@gmail.com>
2020-02-19 01:04:52 +0900

    fix : UserServiceTest삭제

--- src/main/java/atdd/path/application/UserService.java
+++ src/main/java/atdd/path/application/UserService.java
@@ -1,14 +1,26 @@
 package atdd.path.application;
 
 import atdd.path.application.dto.CreateUserRequestView;
+import atdd.path.application.exception.InvalidEmailException;
 import atdd.path.domain.entity.User;
+import atdd.path.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 
     public User createUser(CreateUserRequestView view) {
-        return null;
+        User newUser = view.toUSer();
+
+        boolean checkEmail = checkEmail(newUser.getEmail());
+
+        return userRepository.save(view.toUSer());
     }
 
+    private boolean checkEmail(String email){
+        throw new InvalidEmailException();
+    }
 }
