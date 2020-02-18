632e2583ff10d6aa00b5a78cd50df67a8ff2f086
sanggu <tkdrn8578@naver.com>
2020-02-18 23:26:40 +0900

    [MODIFY] user singUp service 의 리턴타입을 view 로 변경

--- src/main/java/atdd/path/application/UserService.java
+++ src/main/java/atdd/path/application/UserService.java
@@ -1,13 +1,11 @@
 package atdd.path.application;
 
 import atdd.path.application.dto.UserSighUpRequestView;
+import atdd.path.application.dto.UserSighUpResponseView;
 import atdd.path.application.exception.ExistUserException;
 import atdd.path.dao.UserDao;
-import atdd.path.domain.Station;
-import atdd.path.domain.User;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,13 +17,13 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public User singUp(UserSighUpRequestView newUser) {
+    public UserSighUpResponseView singUp(UserSighUpRequestView newUser) {
         List<Map<String, Object>> user = userDao.findByEmail(newUser.getEmail());
 
         if (user != null) {
             throw new ExistUserException();
         }
 
-        return userDao.save(UserSighUpRequestView.toEntity(newUser));
+        return UserSighUpResponseView.toDtoEntity(userDao.save(UserSighUpRequestView.toEntity(newUser)));
     }
 }
