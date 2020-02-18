7badedb8b9fe31ce978ace3d6014b62658f6c2a5
sanggu <tkdrn8578@naver.com>
2020-02-18 21:00:05 +0900

    [ADD] User 회원가입을 위한 Mock Api 추가

---
+++ src/main/java/atdd/path/web/UserController.java
@@ -0,0 +1,24 @@
+package atdd.path.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity signUp(@RequestBody UserSighUpRequestDto user) {
+        return ResponseEntity.created(URI.create("/users/" + 0L)).body(UserSighUpRequestDto.of(0L));
+    }
+}
