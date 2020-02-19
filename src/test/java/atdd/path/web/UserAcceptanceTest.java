cbae2190df6c7cacfb1f476be9fc301fba838b83
irrationnelle <drakkarverenis@gmail.com>
2020-02-20 01:35:39 +0900

    Refactor UserAcceptanceTest

--- src/test/java/atdd/path/web/UserAcceptanceTest.java
+++ src/test/java/atdd/path/web/UserAcceptanceTest.java
@@ -2,24 +2,25 @@ package atdd.path.web;
 
 import atdd.path.AbstractAcceptanceTest;
 import atdd.path.application.dto.UserResponseView;
-import atdd.path.auth.JwtTokenDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserAcceptanceTest extends AbstractAcceptanceTest {
-    public static final String USER_URL = "/users";
-    String firstInputJson = String.format("{\"email\": \"%s\", \"name\": \"%s\", \"password\": \"%s\"}", "boorwonie" +
-            "@email.com", "브라운", "subway");
-    String secondInputJson = String.format("{\"email\": \"%s\", \"name\": \"%s\", \"password\": \"%s\"}",
+    public static final String USER_URI = "/users";
+    public static final String SIGN_IN_URI = USER_URI + "/login";
+    public static final String FIRST_USER_INPUT_JSON = String.format("{\"email\": \"%s\", \"name\": \"%s\", \"password\": \"%s\"}",
+            "boorwonie@email.com", "브라운", "subway");
+    public static final String SECOND_USER_INPUT_JSON = String.format("{\"email\": \"%s\", \"name\": \"%s\", \"password\": \"%s\"}",
             "irrationnelle@email.com", "라세", "station");
+    public static final String SIGN_IN_JSON = String.format("{\"email\": \"%s\", \"password\" : \"%s\"}",
+            "boorwonie@email.com", "subway");
 
     private UserHttpTest userHttpTest;
 
@@ -32,106 +33,85 @@ public class UserAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     public void createUser() {
         // when
-        userHttpTest.createUserSuccess(USER_URL, firstInputJson);
+        userHttpTest.createUserSuccess(USER_URI, FIRST_USER_INPUT_JSON);
 
         // then
         EntityExchangeResult<List<UserResponseView>> userResponseAfterGet = userHttpTest
-                .showUsersAndSingleUserTest(USER_URL)
+                .showUsersAndSingleUserTest(USER_URI)
                 .expectBodyList(UserResponseView.class)
                 .returnResult();
 
-        assertThat(userResponseAfterGet.getResponseBody()
-                                       .get(0)
-                                       .getName()).isEqualTo("브라운");
+        assertThat(userResponseAfterGet.getResponseBody().get(0).getName()).isEqualTo("브라운");
     }
 
     @DisplayName("회원 탈퇴")
     @Test
     public void deleteUser() {
         // given
-        Long firstUserId = userHttpTest.createUserSuccess(USER_URL, firstInputJson);
-        Long secondUserId = userHttpTest.createUserSuccess(USER_URL, secondInputJson);
+        Long firstUserId = userHttpTest.createUserSuccess(USER_URI, FIRST_USER_INPUT_JSON);
+        Long secondUserId = userHttpTest.createUserSuccess(USER_URI, SECOND_USER_INPUT_JSON);
+
+        String reqUriForFirstUser = USER_URI + "/" + firstUserId;
 
         // when
         webTestClient.delete()
-                     .uri(USER_URL + "/" + firstUserId)
+                     .uri(reqUriForFirstUser)
                      .exchange()
-                     .expectStatus()
-                     .isNoContent();
+                     .expectStatus().isNoContent();
 
         // then
         webTestClient.get()
-                     .uri(USER_URL + "/" + firstUserId)
+                     .uri(reqUriForFirstUser)
                      .exchange()
-                     .expectStatus()
-                     .isNotFound();
+                     .expectStatus().isNotFound();
 
-        String reqUriForSingleUser = USER_URL + "/" + secondUserId;
-        EntityExchangeResult<UserResponseView> userResponseAfterGet = userHttpTest
-                .showUsersAndSingleUserTest(reqUriForSingleUser)
-                .expectBody(UserResponseView.class)
-                .returnResult();
+        String reqUriForSecondUser = USER_URI + "/" + secondUserId;
+        EntityExchangeResult<UserResponseView> userResponseAfterGet =
+                userHttpTest.showUsersAndSingleUserTest(reqUriForSecondUser)
+                            .expectBody(UserResponseView.class)
+                            .returnResult();
 
-        assertThat(userResponseAfterGet.getResponseBody()
-                                       .getName()).isEqualTo("라세");
+        assertThat(userResponseAfterGet.getResponseBody().getName()).isEqualTo("라세");
     }
 
     @DisplayName("로그인 요청 후 토큰 발급")
     @Test
     public void signInUser() {
         // given
-        userHttpTest.createUserSuccess(USER_URL, firstInputJson);
+        userHttpTest.createUserSuccess(USER_URI, FIRST_USER_INPUT_JSON);
 
-        String signInUri = USER_URL + "/login";
-        String inputJson = String.format("{\"email\": \"%s\", \"password\" : \"%s\"}", "boorwonie@email.com", "subway");
+        // when
+        String tokenInfo = userHttpTest.createAuthorizationTokenSuccess(SIGN_IN_URI, SIGN_IN_JSON);
 
-        EntityExchangeResult<JwtTokenDTO> getTokenResponse = webTestClient
-                .post()
-                .uri(signInUri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(inputJson), String.class)
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectHeader()
-                .contentType(MediaType.APPLICATION_JSON)
-                .expectBody(JwtTokenDTO.class)
-                .consumeWith(result -> {
-                    assertThat(result.getResponseBody()
-                                     .getAccessToken()).isNotEmpty();
-                    assertThat(result.getResponseBody()
-                                     .getTokenType()).isNotEmpty();
-                })
-                .returnResult();
+        // then
+        assertThat(tokenInfo).isNotEmpty();
 
+        // when
         String improperJson = String.format("{\"email\": \"%s\", \"password\" : \"%s\"}", "boorwonie@email.com", "station");
 
-        webTestClient
-                .post()
-                .uri(signInUri)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(improperJson), String.class)
-                .exchange()
-                .expectStatus()
-                .isUnauthorized();
+        // then
+        userHttpTest.createAuthorizationTokenTest(SIGN_IN_URI, improperJson).isUnauthorized();
+    }
 
-        String accessToken = getTokenResponse.getResponseBody()
-                                             .getAccessToken();
-        String tokenType = getTokenResponse.getResponseBody()
-                                           .getTokenType();
+    @DisplayName("발급받은 토큰으로 유저 정보 받아오기")
+    @Test
+    public void getUserInfoWithToken() {
+        // given
+        userHttpTest.createUserSuccess(USER_URI, FIRST_USER_INPUT_JSON);
+        String tokenInfo = userHttpTest.createAuthorizationTokenSuccess(SIGN_IN_URI, SIGN_IN_JSON);
 
+        // when and then
         webTestClient.get()
-                     .uri(USER_URL + "/me")
-                     .header("Authorization", tokenType + " " + accessToken)
+                     .uri(USER_URI + "/me")
+                     .header("Authorization", tokenInfo)
                      .exchange()
-                     .expectStatus()
-                     .isOk()
-                     .expectHeader()
-                     .contentType(MediaType.APPLICATION_JSON)
-                     .expectBody(UserResponseView.class)
-                     .consumeWith(result -> {
-                         assertThat(result.getResponseBody().getName()).isEqualTo("브라운");
-                         assertThat(result.getResponseBody().getEmail()).isEqualTo("boorwonie@email.com");
-                     });
+                     .expectStatus().isOk()
+                     .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                     .expectBody(UserResponseView.class).consumeWith(this::getUserInfoAssert);
+    }
+
+    private void getUserInfoAssert(EntityExchangeResult<UserResponseView> result) {
+        assertThat(result.getResponseBody().getName()).isEqualTo("브라운");
+        assertThat(result.getResponseBody().getEmail()).isEqualTo("boorwonie@email.com");
     }
 }
