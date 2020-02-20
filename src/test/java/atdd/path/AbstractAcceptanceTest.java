c3e023bae47c6dfb6d687a1141fe9c3022384bb1
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 20:16:35 +0900

    [MODIFY] 동시 검증을 위한 SoftAssertionsExtension 를 AbstractAcceptanceTest 에 추가

--- src/test/java/atdd/path/AbstractAcceptanceTest.java
+++ src/test/java/atdd/path/AbstractAcceptanceTest.java
@@ -1,5 +1,7 @@
 package atdd.path;
 
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +10,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SoftAssertionsExtension.class)
 @AutoConfigureWebTestClient(timeout = "100000")
 public class AbstractAcceptanceTest {
     @Autowired
