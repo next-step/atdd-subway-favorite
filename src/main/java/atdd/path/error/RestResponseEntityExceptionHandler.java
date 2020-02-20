ac384b2360919a7502237b5e613ac0a629a0e6aa
Gyumin Kim <starkying@gmail.com>
2020-02-21 00:20:27 +0900

    Add InvalidJwtAuthenticationException handler

--- src/main/java/atdd/path/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/atdd/path/error/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package atdd.path.error;
 
+import atdd.path.application.exception.InvalidJwtAuthenticationException;
 import atdd.path.exception.MemberNotFoundException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -25,5 +26,11 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 	public ResponseEntity<Object> handleIllegalArgumentException() {
 		final String bodyOfResponse = "해당 member가 존재하지 않습니다.";
 		return new ResponseEntity<>(bodyOfResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(value = {InvalidJwtAuthenticationException.class})
+	public ResponseEntity<Object> handleInvalidJwtAuthenticationException(InvalidJwtAuthenticationException e) {
+		final String bodyOfResponse = e.getLocalizedMessage();
+		return new ResponseEntity<>(bodyOfResponse, HttpStatus.UNAUTHORIZED);
 	}
 }
