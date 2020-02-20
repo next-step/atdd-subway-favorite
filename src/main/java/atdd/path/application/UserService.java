5778822903acbb8aa36b22700c3cfa15a902bf73
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 21:59:47 +0900

    [MODIFY] login 시 token 생성하여 UserLoginResponseView 에 담아 리턴하도록 로직 수정

--- src/main/java/atdd/path/application/UserService.java
+++ src/main/java/atdd/path/application/UserService.java
@@ -1,29 +1,40 @@
 package atdd.path.application;
 
-import atdd.path.application.dto.UserSighUpRequestView;
-import atdd.path.application.dto.UserSighUpResponseView;
+import atdd.path.application.dto.*;
 import atdd.path.application.exception.ExistUserException;
+import atdd.path.application.exception.NotExistUserException;
 import atdd.path.dao.UserDao;
+import atdd.path.security.TokenAuthenticationService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class UserService {
     private UserDao userDao;
+    private TokenAuthenticationService tokenAuthenticationService;
 
-    public UserService(UserDao userDao) {
+    public UserService(TokenAuthenticationService tokenAuthenticationService, UserDao userDao) {
+        this.tokenAuthenticationService = tokenAuthenticationService;
         this.userDao = userDao;
     }
 
     public UserSighUpResponseView singUp(UserSighUpRequestView newUser) {
-        List<Map<String, Object>> user = userDao.findByEmail(newUser.getEmail());
+        FindByEmailResponseView user = userDao.findByEmail(newUser.getEmail());
 
-        if (!user.isEmpty()) {
+        if (user != null) {
             throw new ExistUserException();
         }
 
         return UserSighUpResponseView.toDtoEntity(userDao.save(UserSighUpRequestView.toEntity(newUser)));
+    }
+
+    public UserLoginResponseView login(UserLoginRequestView newUser) {
+        FindByEmailResponseView user = userDao.findByEmail(newUser.getEmail());
+
+        if (user == null) {
+            throw new NotExistUserException();
+        }
+
+        String jwt = tokenAuthenticationService.toJwtByEmail(user.getEmail());
+        return UserLoginResponseView.toDtoEntity(jwt, tokenAuthenticationService.getTokenTypeByJwt(jwt));
     }
 }
