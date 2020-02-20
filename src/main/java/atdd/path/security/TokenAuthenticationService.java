d1841051783e2f25b84de0cccd7959869105fb9d
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 20:56:06 +0900

    [ADD] Email 로 JWT 를 생성해주는 toJwtEmail 메서드 추가

--- src/main/java/atdd/path/security/TokenAuthenticationService.java
+++ src/main/java/atdd/path/security/TokenAuthenticationService.java
@@ -1,5 +1,14 @@
 package atdd.path.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static atdd.path.dao.UserDao.EMAIL_KEY;
+
+@Component
 public class TokenAuthenticationService {
     private static final String SALT = "63B75D39E3F6BFE72263F7C1145AC22E";
     private static final String HEADER_STRING = "Authorization";
@@ -7,5 +16,15 @@ public class TokenAuthenticationService {
 
     public byte[] generateKey(String salt) {
         return salt.getBytes();
+    }
+
+    public String toJwtByEmail(String email) {
+        return Jwts.builder()
+                .claim(EMAIL_KEY, email)
+                .setHeaderParam("tokenType", "Bearer")
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .signWith(SignatureAlgorithm.HS256, this.generateKey(SALT))
+                .compact();
     }
 }
