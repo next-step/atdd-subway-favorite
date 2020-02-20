9411c9fe72f0b66753aa1547e1eff20fdc5d8787
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 21:11:46 +0900

    [ADD]  SoftAssertionsExtension 의 설정값을 가지는 SoftAssertionTest 클래스 추가

---
+++ src/test/java/atdd/path/SoftAssertionTest.java
@@ -0,0 +1,8 @@
+package atdd.path;
+
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SoftAssertionsExtension.class)
+public class SoftAssertionTest {
+}
