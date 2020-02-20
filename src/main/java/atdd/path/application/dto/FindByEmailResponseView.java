96a9b6e77690bc3b64c587420d14a9103b2df032
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 21:43:54 +0900

    [ADD] findByEmail response 값이 Map<String, Object> 을 변환하기 위한 클래스 추가

--- src/main/java/atdd/path/application/dto/FindByEmailResponseView.java
+++ src/main/java/atdd/path/application/dto/FindByEmailResponseView.java
@@ -1,32 +1,31 @@
 package atdd.path.application.dto;
 
-import atdd.path.domain.User;
 import lombok.Builder;
+import lombok.Getter;
 
-import java.util.List;
 import java.util.Map;
 
 import static atdd.path.dao.UserDao.*;
 
+@Getter
 @Builder
 public class FindByEmailResponseView {
-    private List<User> users;
+    private Long id;
+    private String email;
+    private String name;
 
     @Builder
-    public FindByEmailResponseView(List<Map<String, Object>> mapUsers) {
-        mapUsers.stream()
-                .map(mapUser -> this.users.stream()
-                        .map(user -> User.builder()
-                                .email(mapUser.get(EMAIL_KEY).toString())
-                                .name(mapUser.get(NAME_KEY).toString())
-                                .id(Long(mapUser.get(ID_KEY))))
-        this.users = mapUsers;
+    public FindByEmailResponseView(Long id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
     }
 
-    public FindByEmailResponseView toDtoEntity(List<Map<String, Object>> user) {
+    public FindByEmailResponseView toDtoEntity(Map<String, Object> user) {
         return FindByEmailResponseView.builder()
-                .users(user)
+                .id((Long)user.get(ID_KEY))
+                .email(user.get(EMAIL_KEY).toString())
+                .name(user.get(NAME_KEY).toString())
                 .build();
     }
-
 }
