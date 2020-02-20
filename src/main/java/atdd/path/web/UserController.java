8cc220c5dfd8b9d984412c2d17d6d81dafd89765
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 20:14:25 +0900

    [MODIFY] Login API 의 Acceptance Test 를 위한 Mock 로직 추가

--- src/main/java/atdd/path/web/UserController.java
+++ src/main/java/atdd/path/web/UserController.java
@@ -1,9 +1,11 @@
 package atdd.path.web;
 
 import atdd.path.application.UserService;
+import atdd.path.application.dto.UserLoginResponseView;
 import atdd.path.application.dto.UserSighUpRequestView;
 import atdd.path.application.dto.UserSighUpResponseView;
 import atdd.path.dao.UserDao;
+import atdd.path.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +32,10 @@ public class UserController {
     public ResponseEntity delete(@PathVariable Long id) {
         userDao.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody User loginUser) {
+        return ResponseEntity.ok().body(UserLoginResponseView.builder().accessToken("toekn").tokenType("Bearer").build());
     }
 }
