c2d2e3ec40464369fd986e8dff09fea7c1d26562
kimsanggu <tkdrn8578@paymint.co.kr>
2020-02-20 22:01:42 +0900

    [MODIFY] Login 시 request body 를 저장하는 dto 축

---
+++ src/main/java/atdd/path/application/dto/UserLoginRequestView.java
@@ -0,0 +1,29 @@
+package atdd.path.application.dto;
+
+import atdd.path.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserLoginRequestView {
+    private String email;
+    private String password;
+
+
+    @Builder
+    public UserLoginRequestView(String accessToken, String tokenType) {
+        this.email = accessToken;
+        this.password = tokenType;
+    }
+
+    public static User toEntity(UserSighUpRequestView user) {
+        return User.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+
+}
