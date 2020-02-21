f58686c14df139be3d3423291c5b5241421b37f3
Hyunjin Choi <hyeonisism@gmail.com>
2020-02-22 00:30:30 +0900

    Add user delete controller

--- src/main/java/atdd/path/web/UserController.java
+++ src/main/java/atdd/path/web/UserController.java
@@ -6,6 +6,8 @@ import atdd.path.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +30,16 @@ public class UserController {
                 .contentType(MediaType.APPLICATION_JSON)
                 .build();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable("id") User user) {
+
+        if (user == null)
+            return ResponseEntity.notFound().build();
+
+        userService.delete(user);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
