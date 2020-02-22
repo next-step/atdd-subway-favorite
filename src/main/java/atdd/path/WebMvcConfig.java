50eb2ea12ef19737f0fa02e0a643208f40cdcdf3
sanggu <tkdrn8578@naver.com>
2020-02-22 17:41:07 +0900

    [ADD] UserHandlerMethodArgumentResolver 를 ArgumentResolver 로 등록하기 위한 Override 메서드 추가

--- src/main/java/atdd/path/WebMvcConfig.java
+++ src/main/java/atdd/path/WebMvcConfig.java
@@ -3,17 +3,22 @@ package atdd.path;
 import atdd.path.dao.UserDao;
 import atdd.path.security.JwtAuthInterceptor;
 import atdd.path.security.TokenAuthenticationService;
+import atdd.path.security.UserHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private UserDao userDao;
 
     private static String[] INTERCEPTOR_WITHE_LIST = {
-            "/users/**"
+            "/users/login",
+            "/users/sigh-up",
     };
 
     public WebMvcConfig(UserDao userDao) {
@@ -30,5 +35,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public JwtAuthInterceptor jwtAuthInterceptor() {
         return new JwtAuthInterceptor(new TokenAuthenticationService(), userDao);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserHandlerMethodArgumentResolver());
     }
 }
