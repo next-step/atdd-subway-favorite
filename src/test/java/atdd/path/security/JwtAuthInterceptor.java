3ef599469fb4c52c6eb524553b3b5584596e4b1f
sanggu <tkdrn8578@naver.com>
2020-02-22 14:30:44 +0900

    [ADD] 사용자가 controller 에 접근 시 사용자에 대한 인증을 하기 위한 interceptor 추가

---
+++ src/test/java/atdd/path/security/JwtAuthInterceptor.java
@@ -0,0 +1,40 @@
+package atdd.path.security;
+
+import atdd.path.application.dto.FindByEmailResponseView;
+import atdd.path.dao.UserDao;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@AllArgsConstructor
+public class JwtAuthInterceptor extends HandlerInterceptorAdapter {
+    private TokenAuthenticationService tokenAuthenticationService;
+    private UserDao userDao;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authorization == null) {
+            throw new AuthenticationException("Header 에 token 이 존재하지 않습니다.");
+        }
+
+        if (!tokenAuthenticationService.isVerifyToken(authorization)) {
+            throw new AuthenticationException("Not invalid Token!");
+        }
+
+        String email = tokenAuthenticationService.getEmailByJwt(authorization);
+        if (email == null) {
+            return false;
+        }
+
+        FindByEmailResponseView user = userDao.findByEmail(email);
+        request.setAttribute("user", user);
+
+        return true;
+    }
+}
