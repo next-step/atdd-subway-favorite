5cabc517c5c53556dda7470ce1c8f30b5c4263e5
LeeSeungHee <palmseung@gmail.com>
2020-02-23 23:41:31 +0900

    Teat (favorite) : add Acceptance to show FavoriteStations

--- src/test/java/atdd/favorite/web/FavoriteStationAcceptanceTest.java
+++ src/test/java/atdd/favorite/web/FavoriteStationAcceptanceTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import reactor.core.publisher.Mono;
 
 import java.util.stream.Collectors;
@@ -19,8 +20,9 @@ import java.util.stream.Collectors;
 import static atdd.Constant.AUTH_SCHEME_BEARER;
 import static atdd.path.TestConstant.STATION_NAME;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 public class FavoriteStationAcceptanceTest extends AbstractAcceptanceTest {
@@ -89,12 +91,43 @@ public class FavoriteStationAcceptanceTest extends AbstractAcceptanceTest {
                 .collect(Collectors.toList())
                 .get(0);
 
+        //when, then
         mockMvc.perform(
                 delete(FAVORITE_STATION_BASE_URI + "/" + id)
                         .header("Authorization", AUTH_SCHEME_BEARER + token)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void showFavoriteStations() throws Exception{
+        //given
+        String token = jwtTokenProvider.createToken(EMAIL);
+        this.stationId = stationHttpTest.createStation(STATION_NAME);
+        CreateFavoriteStationRequestView request = new CreateFavoriteStationRequestView(stationId);
+        Long id = webTestClient.post().uri(FAVORITE_STATION_BASE_URI)
+                .header("Authorization", AUTH_SCHEME_BEARER + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CreateFavoriteStationRequestView.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .returnResult(FavoriteStationResponseView.class)
+                .getResponseBody()
+                .toStream()
+                .map(FavoriteStationResponseView::getId)
+                .collect(Collectors.toList())
+                .get(0);
+
+        mockMvc.perform(
+                        get(FAVORITE_STATION_BASE_URI+"/"+id)
+                        .header("Authorization", AUTH_SCHEME_BEARER + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.stationId").value(stationId))
                 .andDo(print());
     }
 }
