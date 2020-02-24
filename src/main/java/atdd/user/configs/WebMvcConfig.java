0a8035b825a6dd9479cb8407638941513de68cf7
cye <cye@vivans.net>
2020-02-24 11:23:01 +0900

    Fix: /users/me 인터셉터패턴 추가

--- src/main/java/atdd/user/configs/WebMvcConfig.java
+++ src/main/java/atdd/user/configs/WebMvcConfig.java
@@ -17,6 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/users/me")
                 .addPathPatterns("/favorites/**");
     }
 }
