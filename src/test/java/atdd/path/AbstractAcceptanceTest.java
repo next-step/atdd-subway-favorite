f6d8e01e6139de9a790529499b574af344104d22
sanggu <tkdrn8578@naver.com>
2020-02-24 21:59:33 +0900

    [MODIFY] DirtiesContext 로 BEFORE_EACH_TEST_METHOD 를 제거하고 구동 전 모든 테이블을 비우는 형태로 변경
     * DirtiesContext 는 ApplicationContext 가 매번 올라와 시간이 많이 듬.
     * DirtiesContext 로는 테스트 격리가 완벽하게 이뤄지지 않음

--- src/test/java/atdd/path/AbstractAcceptanceTest.java
+++ src/test/java/atdd/path/AbstractAcceptanceTest.java
@@ -3,13 +3,24 @@ package atdd.path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "100000")
 public class AbstractAcceptanceTest extends SoftAssertionTest {
     @Autowired
     public WebTestClient webTestClient;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    protected void cleanAllDatabases() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "STATION", "LINE", "EDGE", "USER");
+        jdbcTemplate.update("ALTER TABLE STATION ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE LINE ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE EDGE ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE USER ALTER COLUMN id RESTART WITH 1");
+    }
 }
