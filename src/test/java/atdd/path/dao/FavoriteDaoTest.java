64f2dfeb561e38d4bb367d8bb7caae52ca0bda40
sanggu <tkdrn8578@naver.com>
2020-02-25 21:39:34 +0900

    [ADD] Favorite 의 findById 에 대한 Test code 추가

--- src/test/java/atdd/path/dao/FavoriteDaoTest.java
+++ src/test/java/atdd/path/dao/FavoriteDaoTest.java
@@ -2,6 +2,7 @@ package atdd.path.dao;
 
 import atdd.path.domain.Favorite;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -27,11 +28,22 @@ public class FavoriteDaoTest {
         favoriteDao.setDataSource(dataSource);
     }
 
+    @DisplayName("Favorite 에 Station 을 저장이 성공하는지")
     @Test
     public void save() {
         Favorite favorite = favoriteDao.save(new Favorite(TEST_STATION));
 
         assertThat(favorite.getId()).isNotNull();
         assertThat(favorite.getStation()).isEqualTo(TEST_STATION);
+    }
+
+    @DisplayName("Id 로 Favorite 을 조회할 수 있는지")
+    @Test
+    public void findById() {
+        Favorite savedFavorite = favoriteDao.save(new Favorite(TEST_STATION));
+        Favorite favorite = favoriteDao.findById(savedFavorite.getId());
+
+        assertThat(favorite.getId()).isNotNull();
+        assertThat(favorite.getStation()).isEqualTo(savedFavorite.getStation());
     }
 }
