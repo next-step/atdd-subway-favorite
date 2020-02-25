243cfb88e47717b54f22450072905d3f303ab7b1
sanggu <tkdrn8578@naver.com>
2020-02-25 22:01:06 +0900

    [ADD] FavoriteDao 의 mapFavorite 에 대한 테스트코드 추가

--- src/test/java/atdd/path/dao/FavoriteDaoTest.java
+++ src/test/java/atdd/path/dao/FavoriteDaoTest.java
@@ -1,6 +1,9 @@
 package atdd.path.dao;
 
+import atdd.path.SoftAssertionTest;
 import atdd.path.domain.Favorite;
+import atdd.path.domain.Station;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,39 +14,65 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 
 import static atdd.path.TestConstant.TEST_STATION;
+import static atdd.path.fixture.FavoriteFixture.STATION_NAME;
+import static atdd.path.fixture.FavoriteFixture.getDaoFavorites;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
-public class FavoriteDaoTest {
+public class FavoriteDaoTest extends SoftAssertionTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     @Autowired
     private DataSource dataSource;
 
     private FavoriteDao favoriteDao;
+    private StationDao stationDao;
 
     @BeforeEach
     void setUp() {
         favoriteDao = new FavoriteDao(jdbcTemplate);
         favoriteDao.setDataSource(dataSource);
+        stationDao = new StationDao(jdbcTemplate);
+        stationDao.setDataSource(dataSource);
     }
 
     @DisplayName("Favorite 에 Station 을 저장이 성공하는지")
     @Test
     public void save() {
-        Favorite favorite = favoriteDao.save(new Favorite(TEST_STATION));
+        //given
+        Station station = stationDao.save(TEST_STATION);
 
+        //when
+        Favorite favorite = favoriteDao.save(new Favorite(station));
+
+        //then
         assertThat(favorite.getId()).isNotNull();
-        assertThat(favorite.getStation()).isEqualTo(TEST_STATION);
+        assertThat(favorite.getStation().getName()).isEqualTo(STATION_NAME);
     }
 
     @DisplayName("Id 로 Favorite 을 조회할 수 있는지")
     @Test
     public void findById() {
-        Favorite savedFavorite = favoriteDao.save(new Favorite(TEST_STATION));
+        //given
+        Station station = stationDao.save(TEST_STATION);
+        Favorite savedFavorite = favoriteDao.save(new Favorite(station));
+
+        //when
         Favorite favorite = favoriteDao.findById(savedFavorite.getId());
 
+        //then
         assertThat(favorite.getId()).isNotNull();
         assertThat(favorite.getStation()).isEqualTo(savedFavorite.getStation());
+    }
+
+    @DisplayName("findById 로 나온 결과를 Favorite 로 만들어주는지")
+    @Test
+    public void mapFavorite(SoftAssertions softly) {
+        //when
+        Favorite favorite = favoriteDao.mapFavorite(getDaoFavorites());
+
+        //then
+        softly.assertThat(favorite.getId()).isNotNull();
+        softly.assertThat(favorite.getStation().getName()).isEqualTo(STATION_NAME);
     }
 }
