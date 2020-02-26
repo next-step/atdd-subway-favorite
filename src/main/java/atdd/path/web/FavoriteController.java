14a2749d89abdd7049266ec3915b0e51f7e5f45a
sanggu <tkdrn8578@naver.com>
2020-02-26 22:04:00 +0900

    [MODIFY] 사용자의 정보에 favorite 를 추가하기 위해 parameter 로 추가

--- src/main/java/atdd/path/web/FavoriteController.java
+++ src/main/java/atdd/path/web/FavoriteController.java
@@ -3,6 +3,8 @@ package atdd.path.web;
 import atdd.path.application.dto.favorite.FavoriteCreateRequestView;
 import atdd.path.dao.FavoriteDao;
 import atdd.path.domain.Favorite;
+import atdd.path.domain.User;
+import atdd.path.security.LoginUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +23,7 @@ public class FavoriteController {
     }
 
     @PostMapping("")
-    public ResponseEntity create(@RequestBody FavoriteCreateRequestView favorite) {
+    public ResponseEntity create(@LoginUser User user, @RequestBody FavoriteCreateRequestView favorite) {
         Favorite savedFavorite = favoriteDao.save(favorite.toEntity());
         return ResponseEntity.created(URI.create("/favorites/" + savedFavorite.getId())).body(savedFavorite);
     }
