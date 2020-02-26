07bbb8e63bef5f6e005a1f9d1b95e0c8fcf1ebf1
sanggu <tkdrn8578@naver.com>
2020-02-26 22:54:48 +0900

    [ADD] FavoriteCreateRequestView 에 대한 fixture 추가

--- src/test/java/atdd/path/fixture/FavoriteFixture.java
+++ src/test/java/atdd/path/fixture/FavoriteFixture.java
@@ -1,16 +1,23 @@
 package atdd.path.fixture;
 
+import atdd.path.application.dto.favorite.FavoriteCreateRequestView;
+import atdd.path.domain.Favorite;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static atdd.path.TestConstant.TEST_STATION;
 import static atdd.path.dao.FavoriteDao.*;
+import static atdd.path.fixture.UserFixture.NEW_USER;
 
 public class FavoriteFixture {
     public static final Long FAVORITE_ID = 0L;
     public static final String STATION_NAME = "강남역";
     public static final Long STATION_ID = 0L;
+    public static final Favorite NEW_FAVORITE = new Favorite(NEW_USER, TEST_STATION);
+    public static final FavoriteCreateRequestView NEW_FAVORITE_CREATE_VIEW = new FavoriteCreateRequestView(NEW_USER, TEST_STATION);
     public static List<Map<String, Object>> getDaoFavorites() {
         return Collections.singletonList(getDaoFavorite());
     }
