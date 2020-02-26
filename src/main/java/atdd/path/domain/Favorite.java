b58cba2a0d3bd547d6aef0bd8a79613bbb6eb52a
sanggu <tkdrn8578@naver.com>
2020-02-26 22:56:33 +0900

    [MODIFY] Favorite domain 에 User 포함하도록 수정 및 saveParameter 에 user 추가

--- src/main/java/atdd/path/domain/Favorite.java
+++ src/main/java/atdd/path/domain/Favorite.java
@@ -7,34 +7,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static atdd.path.dao.FavoriteDao.STATION_ID_KEY;
+import static atdd.path.dao.FavoriteDao.USER_ID_KEY;
 import static atdd.path.dao.UserDao.ID_KEY;
 
 @NoArgsConstructor
 public class Favorite {
     private Long id;
+    private User user;
     private Station station;
     private Line line;
 
     @Builder
-    public Favorite(Long id, Station station) {
+    public Favorite(Long id, User user, Station station) {
         this.id = id;
+        this.user = user;
         this.station = station;
     }
 
-    public Favorite(Station station) {
+    public Favorite(User user, Station station) {
         this.id = 0L;
+        this.user = user;
         this.station = station;
     }
 
     public static Map<String, Object> getSaveParameterByFavorite(Favorite favorite) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put(ID_KEY, favorite.getId());
-        parameters.put(STATION_ID_KEY, getStation(favorite).getId());
+        parameters.put(USER_ID_KEY, favorite.getUserId());
+        parameters.put(STATION_ID_KEY, favorite.getStationId());
         return parameters;
     }
 
-    private static Station getStation(Favorite favorite) {
-        return favorite.getStation();
+    private Long getStationId() {
+        return getStation().getId();
+    }
+
+    private Long getUserId() {
+        return getUser().getId();
+    }
+
+    public User getUser() {
+        return user;
     }
 
     public Long getId() {
