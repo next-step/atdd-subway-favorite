f0c17aa962173f6d208febbaa7caa78bbfc8411e
sanggu <tkdrn8578@naver.com>
2020-02-27 21:41:57 +0900

    [MODIFY] Favorite 상세 조회에 대한 API mock 로직 추가

--- src/main/java/atdd/path/web/FavoriteController.java
+++ src/main/java/atdd/path/web/FavoriteController.java
@@ -1,31 +1,33 @@
 package atdd.path.web;
 
+import atdd.path.application.FavoriteService;
 import atdd.path.application.dto.favorite.FavoriteCreateRequestView;
-import atdd.path.dao.FavoriteDao;
 import atdd.path.domain.Favorite;
+import atdd.path.domain.Station;
 import atdd.path.domain.User;
 import atdd.path.security.LoginUser;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("/favorites")
 public class FavoriteController {
-    private FavoriteDao favoriteDao;
+    private FavoriteService favoriteService;
 
-    public FavoriteController(FavoriteDao favoriteDao) {
-        this.favoriteDao = favoriteDao;
+    public FavoriteController(FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
     }
 
     @PostMapping("")
     public ResponseEntity create(@LoginUser User user, @RequestBody FavoriteCreateRequestView favorite) {
-        Favorite savedFavorite = favoriteDao.save(favorite.toEntity());
+        Favorite savedFavorite = favoriteService.save(user, favorite);
         return ResponseEntity.created(URI.create("/favorites/" + savedFavorite.getId())).body(savedFavorite);
     }
 
+    @GetMapping("")
+    public ResponseEntity detail(@LoginUser User user) {
+        return ResponseEntity.ok(new Favorite(user, new Station("강남역")));
+    }
 }
