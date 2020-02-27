35be3e32001a1c78f739dcb7d6ae5f57d35ca004
sanggu <tkdrn8578@naver.com>
2020-02-27 22:14:32 +0900

    [MODIFY] List<Favorite> 에 대한 fixture 추가

--- src/test/java/atdd/path/fixture/FavoriteFixture.java
+++ src/test/java/atdd/path/fixture/FavoriteFixture.java
@@ -3,12 +3,10 @@ package atdd.path.fixture;
 import atdd.path.application.dto.favorite.FavoriteCreateRequestView;
 import atdd.path.domain.Favorite;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static atdd.path.TestConstant.TEST_STATION;
+import static atdd.path.TestConstant.TEST_STATION_2;
 import static atdd.path.dao.FavoriteDao.*;
 import static atdd.path.fixture.UserFixture.*;
 
@@ -17,6 +15,8 @@ public class FavoriteFixture {
     public static final String STATION_NAME = "강남역";
     public static final Long STATION_ID = 0L;
     public static final Favorite NEW_FAVORITE = new Favorite(NEW_USER, TEST_STATION);
+    public static final Favorite NEW_SECOND_FAVORITE = new Favorite(NEW_USER, TEST_STATION_2);
+    public static final List<Favorite> NEW_FAVORITES = Arrays.asList(NEW_FAVORITE, NEW_SECOND_FAVORITE);
     public static final FavoriteCreateRequestView NEW_FAVORITE_CREATE_VIEW = new FavoriteCreateRequestView(KIM_ID, STATION_ID, STATION_NAME);
     public static List<Map<String, Object>> getDaoFavorites() {
         return Collections.singletonList(getDaoFavorite());
