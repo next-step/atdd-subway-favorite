1bbacdabedddda3e67ad61f8ac4a6ce14ff83bcd
sanggu <tkdrn8578@naver.com>
2020-02-28 22:44:18 +0900

    [ADD] Favorite table 의 item 객체를 추가

---
+++ src/main/java/atdd/path/domain/Item.java
@@ -0,0 +1,42 @@
+package atdd.path.domain;
+
+import java.util.Objects;
+
+public class Item {
+    private Long id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item station = (Item) o;
+        return Objects.equals(id, station.id) &&
+                Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
