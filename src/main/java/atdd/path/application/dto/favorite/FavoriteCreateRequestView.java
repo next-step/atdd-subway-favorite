7215a3bff4ebf011e9c90f7c766b96ac61db3cd3
sanggu <tkdrn8578@naver.com>
2020-02-28 22:42:23 +0900

    [MODIFY] FavoriteCreateRequestView 의 멤버변수를 itemId, itemName, type 으로 변경

--- src/main/java/atdd/path/application/dto/favorite/FavoriteCreateRequestView.java
+++ src/main/java/atdd/path/application/dto/favorite/FavoriteCreateRequestView.java
@@ -11,21 +11,21 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 public class FavoriteCreateRequestView {
-    private Long userId;
-    private Long stationId;
-    private String stationName;
+    private Long itemId;
+    private String itemName;
+    private String type;
 
     @Builder
-    public FavoriteCreateRequestView(Long userId, Long stationId, String stationName) {
-        this.userId = userId;
-        this.stationId = stationId;
-        this.stationName = stationName;
+    public FavoriteCreateRequestView(Long itemId, String itemName, String type) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.type = type;
     }
 
     public Favorite toEntity(User user) {
         return Favorite.builder()
                 .user(user)
-                .station(new Station(stationId, stationName))
+                .item(new Station(itemId, itemName))
                 .build();
     }
 }
