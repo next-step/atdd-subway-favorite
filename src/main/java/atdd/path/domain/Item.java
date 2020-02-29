f3c9e64dc96b0f410e987c6f1fb43da68258aaae
sanggu <tkdrn8578@naver.com>
2020-02-29 19:50:12 +0900

    [REFACTORING] 불필요한 메서드 제거

--- src/main/java/atdd/path/domain/Item.java
+++ src/main/java/atdd/path/domain/Item.java
@@ -2,8 +2,6 @@ package atdd.path.domain;
 
 import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
 @NoArgsConstructor
 public class Item {
     private Long id;
@@ -14,18 +12,5 @@ public class Item {
 
     public Long getId() {
         return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item station = (Item) o;
-        return Objects.equals(id, station.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
