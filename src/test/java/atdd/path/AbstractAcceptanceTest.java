f958d3f735c761e0228937038812ebe2c38e7574
sanggu <tkdrn8578@naver.com>
2020-02-29 16:42:48 +0900

    [MODIFY] FAVORITE 가 추가됨에 따른 Favorite 테이블도 제거하는 로직 추가

--- src/test/java/atdd/path/AbstractAcceptanceTest.java
+++ src/test/java/atdd/path/AbstractAcceptanceTest.java
@@ -17,10 +17,11 @@ public class AbstractAcceptanceTest extends SoftAssertionTest {
     private JdbcTemplate jdbcTemplate;
 
     protected void cleanAllDatabases() {
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "STATION", "LINE", "EDGE", "USER");
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "STATION", "LINE", "EDGE", "USER", "FAVORITE");
         jdbcTemplate.update("ALTER TABLE STATION ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE LINE ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE EDGE ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE USER ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE FAVORITE ALTER COLUMN id RESTART WITH 1");
     }
 }
