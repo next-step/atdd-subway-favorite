9752ef023a78dd47d2f1065953d1f9ec7888d668
sanggu <tkdrn8578@naver.com>
2020-02-29 16:47:39 +0900

    [MODIFY] Item 으로 Favorite schema 가 변경에 따른 수정

--- src/test/java/atdd/path/application/FavoriteServiceTest.java
+++ src/test/java/atdd/path/application/FavoriteServiceTest.java
@@ -5,6 +5,7 @@ import atdd.path.application.dto.favorite.FavoriteCreateResponseView;
 import atdd.path.application.dto.favorite.FavoriteListResponseView;
 import atdd.path.dao.FavoriteDao;
 import atdd.path.domain.Favorite;
+import atdd.path.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static atdd.path.TestConstant.STATION_NAME;
+import static atdd.path.dao.FavoriteDao.STATION_TYPE;
 import static atdd.path.fixture.FavoriteFixture.*;
 import static atdd.path.fixture.UserFixture.KIM_NAME;
 import static atdd.path.fixture.UserFixture.NEW_USER;
@@ -37,10 +39,10 @@ public class FavoriteServiceTest extends SoftAssertionTest {
         when(favoriteDao.save(any(), STATION_TYPE)).thenReturn(NEW_FAVORITE);
 
         //when
-        FavoriteCreateResponseView favorite = favoriteService.save(NEW_USER, NEW_FAVORITE_CREATE_VIEW);
-
+        FavoriteCreateResponseView favorite = favoriteService.save(NEW_USER, STATION_FAVORITE_CREATE_REQUEST_VIEW);
+        Station station = (Station) favorite.getItem();
         //then
-        assertThat(favorite.getItem().getName()).isEqualTo(STATION_NAME);
+        assertThat(station.getName()).isEqualTo(STATION_NAME);
     }
 
     @DisplayName("사용자가 등록되어 있는 즐겨찾기 목록을 조회가능한지")
@@ -53,9 +55,10 @@ public class FavoriteServiceTest extends SoftAssertionTest {
         FavoriteListResponseView favorites = favoriteService.findByUser(NEW_USER);
 
         Favorite firstFavorite = favorites.getFirstIndex();
+        Station station = (Station) firstFavorite.getItem();
         //then
         assertThat(favorites.getSize()).isGreaterThan(1);
-        assertThat(firstFavorite.getItem().getName()).isEqualTo(STATION_NAME);
+        assertThat(station.getName()).isEqualTo(STATION_NAME);
         assertThat(firstFavorite.getUser().getName()).isEqualTo(KIM_NAME);
     }
 }
