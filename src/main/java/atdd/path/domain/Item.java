fba3ea9a6693dd1a9cba996493b6114974f19726
sanggu <tkdrn8578@naver.com>
2020-03-03 23:22:51 +0900

    [ADD] validateFavoriteEdge 메서드 추가

--- src/main/java/atdd/path/domain/Item.java
+++ src/main/java/atdd/path/domain/Item.java
@@ -2,6 +2,8 @@ package atdd.path.domain;
 
 import lombok.NoArgsConstructor;
 
+import static atdd.path.dao.FavoriteDao.EDGE_TYPE;
+
 @NoArgsConstructor
 public class Item {
     private Long id;
@@ -12,5 +14,13 @@ public class Item {
 
     public Long getId() {
         return id;
+    }
+
+    public void validateFavoriteEdge(String type) {
+        if (EDGE_TYPE.equals(type)) {
+            Edge edge = (Edge) this;
+            edge.checkConnectSourceAndTarget();
+            edge.checkSourceAndTargetStationIsSameWhenEdge();
+        }
     }
 }
