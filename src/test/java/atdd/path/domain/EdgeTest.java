86a9dad43f60a0b69937cf1b75bffafe48e58fb2
sanggu <tkdrn8578@naver.com>
2020-03-03 23:19:17 +0900

    [ADD] sourceStation 과 targetStation 이 연결되있는지와 같은지에 대한 validation 추가
     * item 에 있던 것을 edge 에 있는게 맞다고 판한다여 옮김

--- src/test/java/atdd/path/domain/EdgeTest.java
+++ src/test/java/atdd/path/domain/EdgeTest.java
@@ -1,10 +1,12 @@
 package atdd.path.domain;
 
 import atdd.path.SoftAssertionTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DuplicateKeyException;
 
-import static atdd.path.TestConstant.TEST_STATION;
+import static atdd.path.TestConstant.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -16,6 +18,22 @@ public class EdgeTest extends SoftAssertionTest {
         Edge edge = new Edge(0L, TEST_STATION, TEST_STATION, 10);
 
         assertThat(edge.isSameNameWithSourceAndTarget()).isTrue();
+    }
+
+    @DisplayName("시작역광 종착역이 같을 때 오류를 리턴하는지")
+    @Test
+    public void cannotSourceStationSameWithTargetStation() {
+        Edge favoriteEdge = new Edge(0L, TEST_STATION, TEST_STATION, 10);
+        Assertions.assertThrows(DuplicateKeyException.class,
+                favoriteEdge::checkSourceAndTargetStationIsSameWhenEdge);
+    }
+
+    @DisplayName("시작역과 종착역이 같을 때 연결되어 있지 않을 때 예외를 처리하는지")
+    @Test
+    public void cannotFavoriteEdgeWhenNotConnectSourceWithTarget() {
+        Edge favoriteEdge = new Edge(EDGE_ID_15, TEST_STATION_2, TEST_STATION_17, 10);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                favoriteEdge::checkConnectSourceAndTarget);
     }
 }
 
