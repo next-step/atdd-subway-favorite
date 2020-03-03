3d354899914de52929ca4fa35adc6d3b1c08b035
Heesun Jeong <heesunjeong@wemakeprice.com>
2020-03-03 23:48:07 +0900

    Add createFavoriteRoute acceptance test

--- src/test/java/atdd/path/web/FavoriteAcceptanceTest.java
+++ src/test/java/atdd/path/web/FavoriteAcceptanceTest.java
@@ -4,30 +4,42 @@ import atdd.path.AbstractAcceptanceTest;
 import atdd.path.TestConstant;
 import atdd.path.application.dto.FavoriteResponseView;
 import atdd.path.application.dto.LoginResponseView;
+import atdd.path.domain.Station;
 import atdd.user.web.UserHttpTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
 
     private final String FAVORITE_URI = "/favorites";
+
     private UserHttpTest userHttpTest;
     private FavoriteHttpTest favoriteHttpTest;
+    private StationHttpTest stationHttpTest;
+    private LineHttpTest lineHttpTest;
+
+    private LoginResponseView token;
 
     @BeforeEach
     void setUp() {
         this.userHttpTest = new UserHttpTest(webTestClient);
         this.favoriteHttpTest = new FavoriteHttpTest(webTestClient);
+        this.stationHttpTest = new StationHttpTest(webTestClient);
+        this.lineHttpTest = new LineHttpTest(webTestClient);
+
+        userHttpTest.createUser(TestConstant.EMAIL_BROWN, TestConstant.NAME_BROWN, TestConstant.PASSWORD_BROWN);
+        this.token = userHttpTest.loginUser(TestConstant.EMAIL_BROWN, TestConstant.PASSWORD_BROWN).getResponseBody();
     }
 
     @DisplayName("지하철역 즐겨찾기 등록")
     @Test
     void createFavoriteStation() {
-        userHttpTest.createUser(TestConstant.EMAIL_BROWN, TestConstant.NAME_BROWN, TestConstant.PASSWORD_BROWN);
-        LoginResponseView token = userHttpTest.loginUser(TestConstant.EMAIL_BROWN, TestConstant.PASSWORD_BROWN).getResponseBody();
+        stationHttpTest.createStation("삼성역");
 
         FavoriteResponseView response = favoriteHttpTest.createFavoriteStation(1L, token).getResponseBody();
 
@@ -38,8 +50,7 @@ public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     void findFavoriteStation() {
         // given
-        userHttpTest.createUser(TestConstant.EMAIL_BROWN, TestConstant.NAME_BROWN, TestConstant.PASSWORD_BROWN);
-        LoginResponseView token = userHttpTest.loginUser(TestConstant.EMAIL_BROWN, TestConstant.PASSWORD_BROWN).getResponseBody();
+        stationHttpTest.createStation("삼성역");
         FavoriteResponseView response = favoriteHttpTest.createFavoriteStation(1L, token).getResponseBody();
 
         webTestClient.get().uri(FAVORITE_URI + "/station")
@@ -52,14 +63,31 @@ public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     void deleteFavoriteStation() {
         // given
-        userHttpTest.createUser(TestConstant.EMAIL_BROWN, TestConstant.NAME_BROWN, TestConstant.PASSWORD_BROWN);
-        LoginResponseView token = userHttpTest.loginUser(TestConstant.EMAIL_BROWN, TestConstant.PASSWORD_BROWN).getResponseBody();
+        stationHttpTest.createStation("삼성역");
         FavoriteResponseView response = favoriteHttpTest.createFavoriteStation(1L, token).getResponseBody();
 
         webTestClient.delete().uri(FAVORITE_URI + "/station/" + response.getId())
                 .header("Authorization", String.format("%s %s", token.getTokenType(), token.getAccessToken()))
                 .exchange()
                 .expectStatus().isNoContent();
+    }
 
+    @DisplayName("경로 즐겨찾기 등록")
+    @Test
+    void createFavoriteRoute() {
+        //given
+        stationHttpTest.createStation("삼성역");
+        lineHttpTest.createEdgeRequest(1L, 2L, 2L);
+        String request = "{" +
+                "\"sourceStationId\":" + 1 + ", " +
+                "\"targetStationId\":" + 2 + "}";
+
+        webTestClient.post().uri(FAVORITE_URI + "/route")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), String.class)
+                .header("Authorization", String.format("%s %s", token.getTokenType(), token.getAccessToken()))
+                .exchange()
+                .expectHeader().exists("Location")
+                .expectStatus().isCreated();
     }
 }
