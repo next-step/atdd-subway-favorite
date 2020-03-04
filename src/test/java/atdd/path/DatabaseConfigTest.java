2ccc31a847bc35f51899265a9e263252ef6096cd
sanggu <tkdrn8578@naver.com>
2020-03-05 00:22:26 +0900

    [MODIFY] @sql 로 대체함에 따른 cleanAllDatabase() 제거

--- src/test/java/atdd/path/DatabaseConfigTest.java
+++ src/test/java/atdd/path/DatabaseConfigTest.java
@@ -3,7 +3,6 @@ package atdd.path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.jdbc.JdbcTestUtils;
 
 
 
@@ -11,14 +10,5 @@ import org.springframework.test.jdbc.JdbcTestUtils;
 public class DatabaseConfigTest extends SoftAssertionTest {
     @Autowired
     protected JdbcTemplate jdbcTemplate;
-
-    public void cleanAllDatabases() {
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "STATION", "LINE", "EDGE", "USER", "FAVORITE");
-        jdbcTemplate.update("ALTER TABLE STATION AUTO_INCREMENT = 1");
-        jdbcTemplate.update("ALTER TABLE LINE AUTO_INCREMENT = 1");
-        jdbcTemplate.update("ALTER TABLE EDGE AUTO_INCREMENT = 1");
-        jdbcTemplate.update("ALTER TABLE USER AUTO_INCREMENT = 1");
-        jdbcTemplate.update("ALTER TABLE FAVORITE AUTO_INCREMENT = 1");
-    }
 }
 
