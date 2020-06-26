db64b8d6639c13bc8fabe276e58da2090963d2a2
boorownie <probitanima11@gmail.com>
2020-06-26 15:36:54 +0900

    init
    (cherry picked from commit 444ec63f9d6eaf9d3d096bd4955f756f44f53eb0)

---
+++ src/test/java/nextstep/subway/SubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package nextstep.subway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
