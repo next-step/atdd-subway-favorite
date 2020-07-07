25a9260750d779496779014ce9edfb4f521431db
boorownie <probitanima11@gmail.com>
2020-07-07 23:18:06 +0900

    feat(map): complete map

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -71,10 +71,19 @@ public class LineService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public List<LineResponse> findAllLinesWithStations() {
         List<Line> lines = lineRepository.findAll();
+        List<Long> lineStationIds = lines.stream()
+                .flatMap(it -> it.getStationInOrder().stream())
+                .map(it -> it.getStationId())
+                .collect(Collectors.toList());
+
+        Map<Long, Station> stations = stationRepository.findAllById(lineStationIds).stream()
+                .collect(Collectors.toMap(it -> it.getId(), Function.identity()));
+
         return lines.stream()
-                .map(it -> LineResponse.of(it))
+                .map(it -> LineResponse.of(it, extractLineStationResponses(it, stations)))
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -2,28 +2,62 @@ package nextstep.subway.line.application;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.LineStation;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LineServiceTest {
+    private List<Line> lines;
+    private List<Station> stations;
+
+    @BeforeEach
+    void setUp() {
+        Station station1 = new Station("강남역");
+        ReflectionTestUtils.setField(station1, "id", 1L);
+        Station station2 = new Station("역삼역");
+        ReflectionTestUtils.setField(station2, "id", 2L);
+        Station station3 = new Station("양재역");
+        ReflectionTestUtils.setField(station3, "id", 3L);
+        stations = Lists.newArrayList(station1, station2, station3);
+
+        Line line1 = new Line("신분당선", "RED", LocalTime.now(), LocalTime.now(), 10);
+        ReflectionTestUtils.setField(line1, "id", 1L);
+        Line line2 = new Line("2호선", "GREEN", LocalTime.now(), LocalTime.now(), 10);
+        ReflectionTestUtils.setField(line2, "id", 2L);
+        lines = Lists.newArrayList(line1, line2);
+
+        line1.addLineStation(new LineStation(1L, null, 10, 10));
+        line1.addLineStation(new LineStation(3L, 1L, 10, 10));
+        line2.addLineStation(new LineStation(1L, null, 10, 10));
+        line2.addLineStation(new LineStation(2L, 1L, 10, 10));
+    }
+
     @Test
     void findAllLinesWithStations() {
         LineRepository lineRepository = mock(LineRepository.class);
         StationRepository stationRepository = mock(StationRepository.class);
         LineService lineService = new LineService(lineRepository, stationRepository);
 
-        when(lineRepository.findAll()).thenReturn(Lists.newArrayList(new Line(), new Line()));
+        when(lineRepository.findAll()).thenReturn(lines);
+        when(stationRepository.findAllById(anyList())).thenReturn(stations);
 
         List<LineResponse> result = lineService.findAllLinesWithStations();
 
         assertThat(result).hasSize(2);
+        assertThat(result.get(0).getStations()).hasSize(2);
+        assertThat(result.get(1).getStations()).hasSize(2);
     }
 }
