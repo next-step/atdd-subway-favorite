82e1e45e42e373983f341d707667336ad7ba5cf8
boorownie <probitanima11@gmail.com>
2020-07-15 11:25:07 +0900

    feat(auth): step3 half

--- src/main/java/nextstep/subway/auth/application/converter/AuthenticationTokenConverter.java
+++
@@ -1,9 +0,0 @@
-package nextstep.subway.auth.application.converter;
-
-import nextstep.subway.auth.domain.AuthenticationToken;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface AuthenticationTokenConverter {
-    AuthenticationToken convert(HttpServletRequest request);
-}
