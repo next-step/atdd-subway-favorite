193ae4e9018e2d9ee3dac6a00f5339e3247cea3a
devson <sonyc5720@gmail.com>
2020-07-18 01:03:02 +0900

    feature(auth): Interceptor를 사용하여 JwtToken 생성하는 인증 로직 구현

---
+++ src/main/java/nextstep/subway/auth/ui/interceptor/authentication/TokenAuthenticationInterceptor.java
@@ -0,0 +1,81 @@
+package nextstep.subway.auth.ui.interceptor.authentication;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.auth.domain.Authentication;
+import nextstep.subway.auth.domain.AuthenticationToken;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.auth.infrastructure.AuthorizationExtractor;
+import nextstep.subway.auth.infrastructure.AuthorizationType;
+import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.member.application.CustomUserDetailsService;
+import nextstep.subway.member.domain.LoginMember;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Base64;
+
+public class TokenAuthenticationInterceptor implements HandlerInterceptor {
+    private CustomUserDetailsService userDetailsService;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public TokenAuthenticationInterceptor(CustomUserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
+        this.userDetailsService = userDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws JsonProcessingException {
+        AuthenticationToken token = convert(request);
+        Authentication authentication = authenticate(token);
+
+        if (authentication == null) {
+            throw new RuntimeException();
+        }
+
+        final String payload = new ObjectMapper().writeValueAsString(authentication.getPrincipal());
+        final String jwtToken = this.jwtTokenProvider.createToken(payload);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        try (PrintWriter writer = response.getWriter()) {
+            TokenResponse tokenResponse = new TokenResponse(jwtToken);
+            writer.write(new ObjectMapper().writeValueAsString(tokenResponse));
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+
+    public AuthenticationToken convert(HttpServletRequest request) {
+        String payload = AuthorizationExtractor.extract(request, AuthorizationType.BASIC);
+        String[] emailAndPassword = new String(Base64.getDecoder().decode(payload)).split(":");
+
+        return new AuthenticationToken(emailAndPassword[0], emailAndPassword[1]);
+    }
+
+    public Authentication authenticate(AuthenticationToken token) {
+        String principal = token.getPrincipal();
+        LoginMember userDetails = userDetailsService.loadUserByUsername(principal);
+        checkAuthentication(userDetails, token);
+
+        return new Authentication(userDetails);
+    }
+
+    private void checkAuthentication(LoginMember userDetails, AuthenticationToken token) {
+        if (userDetails == null) {
+            throw new RuntimeException();
+        }
+
+        if (!userDetails.checkPassword(token.getCredentials())) {
+            throw new RuntimeException();
+        }
+    }
+}
