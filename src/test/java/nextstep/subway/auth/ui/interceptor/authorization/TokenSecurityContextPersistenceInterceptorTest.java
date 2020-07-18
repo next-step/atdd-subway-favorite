78fcd32ca71dca12502182d0376c7350bc2e427b
devson <sonyc5720@gmail.com>
2020-07-18 14:49:23 +0900

    test(auth): Interceptor기반 JwtToken을 사용한 인증 기능 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptorTest.java
@@ -0,0 +1,79 @@
+package nextstep.subway.auth.ui.interceptor.authorization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.auth.infrastructure.SecurityContextHolder;
+import nextstep.subway.member.domain.LoginMember;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class TokenSecurityContextPersistenceInterceptorTest {
+    private JwtTokenProvider jwtTokenProvider;
+    private TokenSecurityContextPersistenceInterceptor interceptor;
+
+    @BeforeEach
+    void setUp() {
+        jwtTokenProvider = mock(JwtTokenProvider.class);
+        interceptor = new TokenSecurityContextPersistenceInterceptor(jwtTokenProvider);
+    }
+
+    @DisplayName("JwtToken이 유효하면 SecurityContext를 SecurityContextHolder에 담는다")
+    @Test
+    void whenJwtTokenIsValid() throws Exception {
+        // given
+        LoginMember loginMember = new LoginMember(1L, "email@email.com", "password", 20);
+        when(jwtTokenProvider.validateToken(anyString())).thenReturn(true);
+        when(jwtTokenProvider.getPayload(anyString())).thenReturn(new ObjectMapper().writeValueAsString(loginMember));
+
+        // when
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        boolean authenticated = interceptor.preHandle(request, response, new Object());
+
+        // then
+        assertThat(authenticated).isTrue();
+        LoginMember principal = (LoginMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        assertThat(principal.getId()).isEqualTo(loginMember.getId());
+    }
+
+    @DisplayName("JwtToken이 유효하지 않으면 FORBIDDEN 응답을 한다")
+    @Test
+    void whenJwtTokenIsNotValidForbidden() throws Exception {
+        // given
+        when(jwtTokenProvider.validateToken(anyString())).thenReturn(false);
+
+        // when
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        boolean authenticated = interceptor.preHandle(request, response, new Object());
+
+        // then
+        assertThat(authenticated).isFalse();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    @DisplayName("JwtToken이 유효하지 않으면 SecurityContext를 SecurityContextHolder에 담지 않는다")
+    @Test
+    void whenJwtTokenIsNotValidSecurityContext() throws Exception {
+        // given
+        when(jwtTokenProvider.validateToken(anyString())).thenReturn(false);
+
+        // when
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        boolean authenticated = interceptor.preHandle(request, response, new Object());
+
+        // then
+        assertThat(authenticated).isFalse();
+        assertThat(SecurityContextHolder.getContext().getAuthentication()).isNull();
+    }
+}
