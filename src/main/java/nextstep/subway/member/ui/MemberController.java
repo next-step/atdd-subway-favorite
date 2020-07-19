b05e2838ef346a56b55d710141e6ff3e11bdb89c
혜윰 <31656707+hyeyoom@users.noreply.github.com>
2020-07-19 20:15:12 +0900

    0️⃣ 로그인 인증 프로세스 실습 & 1️⃣ 로그인 토큰 발급 (#44)
    * chore(gitignore): added new untracked file
    
    * refactor(member-acceptance-test): refactored MemberAcceptanceTest. merged into manageMember method.
    
    * feat(member-controller): implemented MemberController#findMemberOfMine to make AuthAcceptanceTest#myInfoWithSession success.
    
    * test(convert-utils-test): writes tests for stringify and b64decode.
    
    * feat(convert-utils): implemented ConvertUtils that decodes base64-encoded string and converts given objects to json string.
    
    * test(http-response-utils): test HttpResponseUtils that writes body of http response easily.
    
    * feat(http-response-utils): implemented HttpResponseUtils.
    
    * test(token-auth-interceptor): added test for TokenAuthenticationInterceptor that handles authentication and issues jwt token for users.
    
    * feat(token-auth-interceptor): implemented TokenAuthenticationInterceptor. 뚝딱 'ㅅ'
    
    * test: 테스트 통과를 확인하였다. 결과는 훌륭했다. 'ㅅ'

--- src/main/java/nextstep/subway/member/ui/MemberController.java
+++ src/main/java/nextstep/subway/member/ui/MemberController.java
@@ -1,6 +1,8 @@
 package nextstep.subway.member.ui;
 
+import nextstep.subway.auth.infrastructure.SecurityContextHolder;
 import nextstep.subway.member.application.MemberService;
+import nextstep.subway.member.domain.LoginMember;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +32,8 @@ public class MemberController {
 
     @GetMapping("/members/me")
     public ResponseEntity<MemberResponse> findMemberOfMine() {
-        return ResponseEntity.ok().build();
+        final LoginMember loginMember = (LoginMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(new MemberResponse(loginMember.getId(), loginMember.getEmail(), loginMember.getAge()));
     }
 
     @PutMapping("/members/{id}")
