0db55e6c0bd1712d77a786c04b233aacdbe49b0a
devson <sonyc5720@gmail.com>
2020-07-19 09:47:10 +0900

    feature(member): Token을 사용한 회원 정보 수정/삭제 기능 구현

--- src/main/java/nextstep/subway/member/ui/MemberController.java
+++ src/main/java/nextstep/subway/member/ui/MemberController.java
@@ -6,10 +6,12 @@ import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.domain.LoginMember;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Objects;
 
 @RestController
 public class MemberController {
@@ -38,15 +40,43 @@ public class MemberController {
         return ResponseEntity.ok().body(memberService.findMember(loginMember.getId()));
     }
 
+    @Deprecated
     @PutMapping("/members/{id}")
     public ResponseEntity<MemberResponse> updateMember(@PathVariable Long id, @RequestBody MemberRequest param) {
         memberService.updateMember(id, param);
         return ResponseEntity.ok().build();
     }
 
+    @Deprecated
     @DeleteMapping("/members/{id}")
     public ResponseEntity<MemberResponse> deleteMember(@PathVariable Long id) {
         memberService.deleteMember(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/members/{id}/token")
+    public ResponseEntity<MemberResponse> updateMemberWithToken(@PathVariable Long id, @RequestBody MemberRequest param) {
+        // TODO parameter로 변경
+        LoginMember loginMember = (LoginMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!Objects.equals(id, loginMember.getId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        memberService.updateMember(loginMember.getId(), param);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/members/{id}/token")
+    public ResponseEntity<MemberResponse> deleteMemberWithToken(@PathVariable Long id) {
+        // TODO parameter로 변경
+        LoginMember loginMember = (LoginMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!Objects.equals(id, loginMember.getId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        memberService.deleteMember(loginMember.getId());
         return ResponseEntity.noContent().build();
     }
 }
