a34f72ae2927d9ad5f1f6afe1096989802630f40
koamania <koamania2@gmail.com>
2020-07-21 23:27:15 +0900

    bugfix(token): step2 AuthAcceptanceTest 성공하도록 코드 수정

--- src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
@@ -1,5 +1,6 @@
 package nextstep.subway.auth.ui.interceptor.authorization;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.auth.infrastructure.*;
@@ -9,10 +10,16 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public class TokenSecurityContextPersistenceInterceptor implements HandlerInterceptor {
 
+    private static final String ID = "id";
+    private static final String EMAIL = "email";
+    private static final String AGE = "age";
+
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     private final JwtTokenProvider jwtTokenProvider;
 
     public TokenSecurityContextPersistenceInterceptor(JwtTokenProvider jwtTokenProvider) {
@@ -29,10 +36,10 @@ public class TokenSecurityContextPersistenceInterceptor implements HandlerInterc
 
         String payload = jwtTokenProvider.getPayload(token);
 
-        MemberResponse memberResponse = OBJECT_MAPPER.readValue(payload, MemberResponse.class);
+        Map<String, String> payloadMap = OBJECT_MAPPER.readValue(payload, new TypeReference<Map<String, String>>() {});
 
         // TODO LoginMember로 들어온다고 가정하다보니까 payload가 유연하지 않음. Session Interceptor에서도 무조건 LoginMember를 넣고 있는데 일괄적으로 변경 필요
-        LoginMember principal = new LoginMember(memberResponse.getId(), memberResponse.getEmail(), null, memberResponse.getAge());
+        LoginMember principal = new LoginMember(Long.parseLong(payloadMap.get(ID)), payloadMap.get(EMAIL), null, Integer.parseInt(payloadMap.get(AGE)));
 
         SecurityContextHolder.setContext(new SecurityContext(new Authentication(principal)));
 
