f69a108fc58d0a4fadd63bd01c9189480600ac05
koamania <koamania2@gmail.com>
2020-07-22 00:04:59 +0900

    refactoring(token): step2 리팩토링

--- src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
@@ -1,24 +1,15 @@
 package nextstep.subway.auth.ui.interceptor.authorization;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.auth.infrastructure.*;
 import nextstep.subway.member.domain.LoginMember;
 import nextstep.subway.member.dto.MemberResponse;
-import org.springframework.web.servlet.HandlerInterceptor;
+import nextstep.subway.utils.ObjectMapperUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
-public class TokenSecurityContextPersistenceInterceptor implements HandlerInterceptor {
-
-    private static final String ID = "id";
-    private static final String EMAIL = "email";
-    private static final String AGE = "age";
-
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+public class TokenSecurityContextPersistenceInterceptor extends AbstractSecurityContextPersistenceInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -28,6 +19,7 @@ public class TokenSecurityContextPersistenceInterceptor implements HandlerInterc
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // TODO 얘랑 session persistence랑 하나로 합칠 수 있을 듯 한데...
         String token = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
 
         if (!isValidToken(token)) {
@@ -36,22 +28,23 @@ public class TokenSecurityContextPersistenceInterceptor implements HandlerInterc
 
         String payload = jwtTokenProvider.getPayload(token);
 
-        Map<String, String> payloadMap = OBJECT_MAPPER.readValue(payload, new TypeReference<Map<String, String>>() {});
+        LoginMember principal = extractPrincipal(payload);
 
-        // TODO LoginMember로 들어온다고 가정하다보니까 payload가 유연하지 않음. Session Interceptor에서도 무조건 LoginMember를 넣고 있는데 일괄적으로 변경 필요
-        LoginMember principal = new LoginMember(Long.parseLong(payloadMap.get(ID)), payloadMap.get(EMAIL), null, Integer.parseInt(payloadMap.get(AGE)));
-
+        // TODO 직접 참조하지 말고 인증 성공 실패에 대한 핸들러를 제공해보자
         SecurityContextHolder.setContext(new SecurityContext(new Authentication(principal)));
 
         return true;
     }
 
-    private boolean isValidToken(String token) {
-        return jwtTokenProvider.validateToken(token);
+    private LoginMember extractPrincipal(String payload) throws Exception {
+        MemberResponse memberResponse = ObjectMapperUtils.convert(payload, MemberResponse.class);
+
+        // TODO LoginMember로 들어온다고 가정하다보니까 payload가 유연하지 않음. Session Interceptor에서도 무조건 LoginMember를 넣고 있는데 일괄적으로 변경 필요
+        return new LoginMember(memberResponse.getId(), memberResponse.getEmail(), null, memberResponse.getAge());
     }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        SecurityContextHolder.clearContext();
+    private boolean isValidToken(String token) {
+        // TODO 라이브러리에 너무 의존하고 있는 느낌. Provider로 빼보자
+        return jwtTokenProvider.validateToken(token);
     }
 }
