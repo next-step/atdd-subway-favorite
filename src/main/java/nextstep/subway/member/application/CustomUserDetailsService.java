1f2dbac9777baa086ce4e9221f78273748e68814
hyeyoom <neoul_chw@icloud.com>
2020-07-22 02:09:30 +0900

    refactor: converted existing classes to subclass of interface in order to remove circular dependencies.

--- src/main/java/nextstep/subway/member/application/CustomUserDetailsService.java
+++ src/main/java/nextstep/subway/member/application/CustomUserDetailsService.java
@@ -1,18 +1,20 @@
 package nextstep.subway.member.application;
 
+import nextstep.subway.auth.application.UserDetailsService;
 import nextstep.subway.member.domain.LoginMember;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private MemberRepository memberRepository;
 
     public CustomUserDetailsService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Override
     public LoginMember loadUserByUsername(String email) {
         Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
         return LoginMember.of(member);
