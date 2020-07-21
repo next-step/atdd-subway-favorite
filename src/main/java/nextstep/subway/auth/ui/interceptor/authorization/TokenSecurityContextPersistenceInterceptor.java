57c7a17c78eb80c0452d91729e7f6f8af7cd5c84
etff <wish2rich@gmail.com>
2020-07-21 23:45:20 +0900

    feature(jwt): add token extract method

--- src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
@@ -1,7 +1,9 @@
 package nextstep.subway.auth.ui.interceptor.authorization;
 
-import nextstep.subway.auth.infrastructure.SecurityContext;
-import nextstep.subway.auth.infrastructure.SecurityContextHolder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.auth.infrastructure.*;
+import nextstep.subway.member.domain.LoginMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,8 +13,19 @@ import static nextstep.subway.auth.infrastructure.SecurityContextHolder.SPRING_S
 
 public class TokenSecurityContextPersistenceInterceptor  implements HandlerInterceptor {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    public TokenSecurityContextPersistenceInterceptor(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.objectMapper = new ObjectMapper();
+    }
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws JsonProcessingException {
+        String token = extractToken(request);
+        LoginMember loginMember = getLoginMemberFromToken(token);
+
         SecurityContext securityContext = (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
         if (securityContext != null) {
             SecurityContextHolder.setContext(securityContext);
@@ -24,4 +37,13 @@ public class TokenSecurityContextPersistenceInterceptor  implements HandlerInter
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         SecurityContextHolder.clearContext();
     }
+
+    private String extractToken(HttpServletRequest request) {
+        return AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
+    }
+
+    private LoginMember getLoginMemberFromToken(String token) throws JsonProcessingException {
+        return objectMapper.readValue(jwtTokenProvider.getPayload(token), LoginMember.class);
+    }
+
 }
--- src/test/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptorTest.java
+++ src/test/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptorTest.java
@@ -1,7 +1,57 @@
 package nextstep.subway.auth.ui.interceptor.authorization;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.auth.ui.interceptor.authentication.TokenAuthenticationInterceptor;
+import nextstep.subway.member.application.CustomUserDetailsService;
+import nextstep.subway.member.domain.LoginMember;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class TokenSecurityContextPersistenceInterceptorTest {
+    private static final String EMAIL = "email@email.com";
+    private static final String PASSWORD = "password";
+    private static final Integer AGE = 20;
+    private static final String REGEX = ":";
+    private static final String JWT = "accessToken";
+    private static final long ID = 1L;
 
+    @Mock
+    private CustomUserDetailsService customUserDetailsService;
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    private TokenSecurityContextPersistenceInterceptor interceptor;
+    private ObjectMapper objectMapper;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private LoginMember loginMember;
+
+    @BeforeEach
+    void setUp() {
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        objectMapper = new ObjectMapper();
+
+        request.addHeader("Authorization","Bearer NEXTSTEP");
+        loginMember = new LoginMember(ID, EMAIL, PASSWORD, AGE);
+        interceptor = new TokenSecurityContextPersistenceInterceptor(jwtTokenProvider);
+    }
+
+    @Test
+    void preHandler() throws Exception {
+
+    }
 }
