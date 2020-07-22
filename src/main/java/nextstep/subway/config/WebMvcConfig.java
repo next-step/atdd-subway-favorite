10ae3b253f34802d8023793a13e57e9cfd38943d
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-22 17:17:12 +0900

    feat: WebMvcConfig to be added with TokenAuthenticationInterceptor

--- src/main/java/nextstep/subway/config/WebMvcConfig.java
+++ src/main/java/nextstep/subway/config/WebMvcConfig.java
@@ -1,22 +1,29 @@
 package nextstep.subway.config;
 
-import nextstep.subway.auth.ui.interceptor.authentication.SessionAuthenticationInterceptor;
-import nextstep.subway.auth.ui.interceptor.authorization.SessionSecurityContextPersistenceInterceptor;
-import nextstep.subway.member.application.CustomUserDetailsService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.auth.ui.interceptor.authentication.SessionAuthenticationInterceptor;
+import nextstep.subway.auth.ui.interceptor.authorization.SessionSecurityContextPersistenceInterceptor;
+import nextstep.subway.auth.ui.interceptor.jwt.TokenAuthenticationInterceptor;
+import nextstep.subway.member.application.CustomUserDetailsService;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private CustomUserDetailsService userDetailsService;
+    private JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfig(CustomUserDetailsService userDetailsService) {
+    public WebMvcConfig(CustomUserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
         this.userDetailsService = userDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SessionAuthenticationInterceptor(userDetailsService)).addPathPatterns("/login/session");
         registry.addInterceptor(new SessionSecurityContextPersistenceInterceptor());
+        registry.addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider))
+            .addPathPatterns("/login/token");
     }
 }
