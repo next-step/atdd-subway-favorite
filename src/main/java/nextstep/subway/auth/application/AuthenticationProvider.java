06d886525f949c33062cdd9669ca8c9a185dcc16
koamania <koamania2@gmail.com>
2020-07-23 00:03:14 +0900

    feature(auth): step3 인증 토큰으로 사용자 인증 정보를 제공하는 클래스 기능 추가

--- src/main/java/nextstep/subway/auth/application/AuthenticationProvider.java
+++ src/main/java/nextstep/subway/auth/application/AuthenticationProvider.java
@@ -2,6 +2,8 @@ package nextstep.subway.auth.application;
 
 import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.auth.domain.AuthenticationToken;
+import nextstep.subway.auth.domain.exception.InvalidAuthenticationTokenException;
+import nextstep.subway.member.domain.LoginMember;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +15,21 @@ public class AuthenticationProvider {
         this.userDetailsService = userDetailsService;
     }
 
-    public Authentication authenticate(AuthenticationToken authenticationToken) {
-        return null;
+    public Authentication authenticate(AuthenticationToken token) {
+        String principal = token.getPrincipal();
+        LoginMember userDetails = userDetailsService.loadUserByUsername(principal);
+        checkAuthentication(userDetails, token);
+
+        return new Authentication(userDetails);
+    }
+
+    public void checkAuthentication(LoginMember userDetails, AuthenticationToken token) {
+        if (userDetails == null) {
+            throw new InvalidAuthenticationTokenException("unknown user");
+        }
+
+        if (!userDetails.checkPassword(token.getCredentials())) {
+            throw new InvalidAuthenticationTokenException("password does not match");
+        }
     }
 }
