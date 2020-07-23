6a30236e459d7bc75344f1815bdc540b97224b50
devson <sonyc5720@gmail.com>
2020-07-23 23:15:13 +0900

    test(favorite): FavoriteService에 대한 너무 상세한 정보를 알고있는 FavoriteController 단위 테스트 단순화

--- src/test/java/nextstep/subway/favorite/ui/FavoriteControllerTest.java
+++ src/test/java/nextstep/subway/favorite/ui/FavoriteControllerTest.java
@@ -4,11 +4,10 @@ import com.google.common.collect.Lists;
 import nextstep.subway.auth.domain.UserDetails;
 import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.domain.Favorite;
-import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +18,8 @@ import org.springframework.http.ResponseEntity;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,14 +28,10 @@ class FavoriteControllerTest {
 
     private FavoriteController favoriteController;
     private FavoriteService favoriteService;
-    private FavoriteRepository favoriteRepository;
-    private StationRepository stationRepository;
 
     @BeforeEach
     void setUp() {
-        favoriteRepository = mock(FavoriteRepository.class);
-        stationRepository = mock(StationRepository.class);
-        favoriteService = new FavoriteService(favoriteRepository, stationRepository);
+        favoriteService = mock(FavoriteService.class);
         favoriteController = new FavoriteController(favoriteService);
     }
 
@@ -48,13 +41,13 @@ class FavoriteControllerTest {
         // given
         FavoriteRequest request = new FavoriteRequest(1L, 2L);
         Favorite favorite = new Favorite(1L, LOGIN_USER.getId(), 1L, 2L);
-        when(favoriteRepository.save(any())).thenReturn(favorite);
+
+        when(favoriteService.createFavorite(LOGIN_USER.getId(), request)).thenReturn(favorite);
 
         // when
         ResponseEntity response = favoriteController.createFavorite(LOGIN_USER, request);
 
         // then
-        verify(favoriteRepository).save(any());
 
         String path = response.getHeaders().getLocation().getPath();
         long responsedFavoriteId = Long.parseLong(path.split("favorites/")[1]);
@@ -65,28 +58,15 @@ class FavoriteControllerTest {
     @Test
     void getFavoritesWhenFavoritesAreEmpty() {
         // given
-        when(favoriteRepository.findAllByMemberId(LOGIN_USER.getId())).thenReturn(Lists.newArrayList());
-
-        // when
-        ResponseEntity<List<FavoriteResponse>> response = favoriteController.getFavorites(LOGIN_USER);
-
-        // then
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        List<FavoriteResponse> body = response.getBody();
-        assertThat(body).isEmpty();
-    }
-
-    @DisplayName("getFavorites 즐겨찾기가 없는 경우")
-    @Test
-    void getFavoritesWhenFavoritesAreNotEmpty() {
-        // given
         List<Station> stations = Lists.newArrayList(new Station(1L, "강남역"), new Station(2L, "역삼역"));
         Favorite favorite = new Favorite(1L, LOGIN_USER.getId(), stations.get(0).getId(), stations.get(1).getId());
 
-        when(favoriteRepository.findAllByMemberId(LOGIN_USER.getId())).thenReturn(Lists.newArrayList(favorite));
-        when(stationRepository.findAllById(stations.stream().map(Station::getId).collect(Collectors.toSet())))
-                .thenReturn(stations);
+        when(favoriteService.findFavorites(LOGIN_USER.getId()))
+                .thenReturn(Lists.newArrayList(FavoriteResponse.of(
+                        favorite,
+                        StationResponse.of(stations.get(0)),
+                        StationResponse.of(stations.get(1))
+                )));
 
         // when
         ResponseEntity<List<FavoriteResponse>> response = favoriteController.getFavorites(LOGIN_USER);
@@ -99,17 +79,29 @@ class FavoriteControllerTest {
         assertThat(body.get(0).getId()).isEqualTo(favorite.getId());
     }
 
+    @DisplayName("getFavorites 즐겨찾기가 없는 경우")
+    @Test
+    void getFavoritesWhenFavoritesAreNotEmpty() {
+        // given
+        when(favoriteService.findFavorites(LOGIN_USER.getId())).thenReturn(Lists.newArrayList());
+
+        // when
+        ResponseEntity<List<FavoriteResponse>> response = favoriteController.getFavorites(LOGIN_USER);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        List<FavoriteResponse> body = response.getBody();
+        assertThat(body).isEmpty();
+    }
+
     @DisplayName("deleteFavorite 즐겨찾기가 있는 경우")
     @Test
     void deleteFavoriteWhenFavoriteFound() {
-        // given
-        when(favoriteRepository.existsByIdAndMemberId(1L, LOGIN_USER.getId())).thenReturn(true);
-
         // when
         ResponseEntity responseEntity = favoriteController.deleteFavorite(LOGIN_USER, 1L);
 
         // then
-        verify(favoriteRepository).existsByIdAndMemberId(1L, LOGIN_USER.getId());
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 
@@ -117,11 +109,14 @@ class FavoriteControllerTest {
     @Test
     void deleteFavoriteWhenFavoriteNotFound() {
         // given
-        when(favoriteRepository.existsByIdAndMemberId(1L, LOGIN_USER.getId())).thenReturn(false);
+        Long favoriteId = 1L;
+        doThrow(EntityNotFoundException.class)
+                .when(favoriteService).deleteFavorite(LOGIN_USER.getId(), favoriteId);
 
         // when
-        assertThrows(EntityNotFoundException.class, () ->
-                favoriteController.deleteFavorite(LOGIN_USER, 1L));
-        verify(favoriteRepository).existsByIdAndMemberId(1L, LOGIN_USER.getId());
+        ResponseEntity responseEntity = favoriteController.deleteFavorite(LOGIN_USER, favoriteId);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
