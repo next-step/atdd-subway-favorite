6e59e6ae10ceb42d9c2ea8f0e9f3ef8939a7d271
showpointer <wanho92@gmail.com>
2020-07-23 20:27:56 +0900

    feat: 토큰 인증 인터셉터 등록

--- src/main/java/nextstep/subway/config/WebMvcConfig.java
+++ src/main/java/nextstep/subway/config/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package nextstep.subway.config;
 
+import nextstep.subway.auth.acceptance.ui.authentication.TokenAuthenticationInterceptor;
+import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.auth.ui.interceptor.authentication.SessionAuthenticationInterceptor;
 import nextstep.subway.auth.ui.interceptor.authorization.SessionSecurityContextPersistenceInterceptor;
 import nextstep.subway.member.application.CustomUserDetailsService;
@@ -10,13 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private CustomUserDetailsService userDetailsService;
+    private JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfig(CustomUserDetailsService userDetailsService) {
+    public WebMvcConfig(CustomUserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
         this.userDetailsService = userDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SessionAuthenticationInterceptor(userDetailsService)).addPathPatterns("/login/session");
         registry.addInterceptor(new SessionSecurityContextPersistenceInterceptor());
+        registry.addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider)).addPathPatterns("/login/token");
     }
 }
