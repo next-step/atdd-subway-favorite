27bc88e1eaaba5f3e09673c3b41a3378712193b6
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-23 18:37:32 +0900

    test: invalid token to check my info handling exception

--- src/test/java/nextstep/subway/auth/infrastructure/TokenSecurityContextPersistenceInterceptorTest.java
+++ src/test/java/nextstep/subway/auth/infrastructure/TokenSecurityContextPersistenceInterceptorTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.member.application.CustomUserDetailsService;
 import nextstep.subway.member.domain.LoginMember;
@@ -74,6 +73,7 @@ public class TokenSecurityContextPersistenceInterceptorTest {
     void 잘못된_토큰이_들어오는_경우_예외처리를_진행한다() {
         // given: 로그인되지않음
         when(jwtTokenProvider.validateToken(anyString())).thenReturn(false);
+        when(jwtTokenProvider.getPayload(anyString())).thenReturn(null);
 
         // when: 내 회원 정보 요청
         interceptor.preHandle(request, response, mock(Object.class));
