7b32ff0d84643896550df98313e2840107ce9b1d
etff <wish2rich@gmail.com>
2020-07-24 17:08:18 +0900

    refactor(auth): add user details interface

---
+++ src/main/java/nextstep/subway/member/application/UserDetailsService.java
@@ -0,0 +1,7 @@
+package nextstep.subway.member.application;
+
+import nextstep.subway.member.domain.LoginMember;
+
+public interface UserDetailsService {
+    LoginMember loadUserByUsername(String username);
+}
