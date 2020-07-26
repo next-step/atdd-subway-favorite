bc98f3e197263adc049e2d349f4acef12b110979
showpointer <wanho92@gmail.com>
2020-07-26 16:27:49 +0900

    test(token): jwt payload에서 SecurityContext 추출

--- src/test/java/nextstep/subway/auth/acceptance/ui/authentication/TokenSecurityContextPersistenceInterceptorTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/ui/authentication/TokenSecurityContextPersistenceInterceptorTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.auth.infrastructure.SecurityContext;
 import nextstep.subway.auth.infrastructure.SecurityContextHolder;
 import nextstep.subway.auth.ui.interceptor.authorization.TokenSecurityContextPersistenceInterceptor;
 import nextstep.subway.member.application.CustomUserDetailsService;
@@ -25,6 +26,7 @@ import static org.mockito.Mockito.when;
 public class TokenSecurityContextPersistenceInterceptorTest {
     private static final String EMAIL = "test@test.com";
     private static final String PASSWORD = "test";
+    private static final Integer AGE = 10;
 
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
@@ -32,8 +34,9 @@ public class TokenSecurityContextPersistenceInterceptorTest {
     private JwtTokenProvider jwtTokenProvider;
 
     private TokenSecurityContextPersistenceInterceptor interceptor;
-    ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
 
+    private LoginMember loginMember;
     @BeforeEach
     void setUp() {
         request = new MockHttpServletRequest();
@@ -46,6 +49,8 @@ public class TokenSecurityContextPersistenceInterceptorTest {
         objectMapper = new ObjectMapper();
 
         request.addHeader("Authorization", "Bearer jwtToken");
+
+        loginMember = new LoginMember(1L, EMAIL, PASSWORD, AGE);
     }
 
     @DisplayName("토큰 헤더 값 검증")
@@ -57,6 +62,28 @@ public class TokenSecurityContextPersistenceInterceptorTest {
         // when
         assertThatThrownBy(() -> interceptor.preHandle(request, response, new Object()))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("토큰 SecurityContext 추출")
+    @Test
+    void extractSecurityContext() throws JsonProcessingException {
+        // given
+        String payload = objectMapper.writeValueAsString(loginMember);
+        when(jwtTokenProvider.getPayload(anyString())).thenReturn(payload);
+
+        String extractedPayload = jwtTokenProvider.getPayload("jwtToken");
+        LoginMember userDetails = objectMapper.readValue(extractedPayload, LoginMember.class);
+
+        SecurityContext context = new SecurityContext(new Authentication(userDetails));
+        LoginMember member = (LoginMember)context.getAuthentication().getPrincipal();
+
+        // then
+        assertAll(
+                () -> assertThat(member).isNotNull(),
+                () -> assertThat(member.getId()).isEqualTo(1L),
+                () -> assertThat(member.getEmail()).isEqualTo(EMAIL),
+                () -> assertThat(member.getAge()).isEqualTo(AGE)
+        );
     }
 
     @DisplayName("SecurityContextHolder 값 검증")
@@ -78,7 +105,7 @@ public class TokenSecurityContextPersistenceInterceptorTest {
             () -> assertThat(logged).isNotNull(),
             () -> assertThat(logged.getId()).isEqualTo(1L),
             () -> assertThat(logged.getEmail()).isEqualTo(EMAIL),
-            () -> assertThat(logged.getAge()).isEqualTo(PASSWORD)
+            () -> assertThat(logged.getAge()).isEqualTo(AGE)
         );
     }
 
