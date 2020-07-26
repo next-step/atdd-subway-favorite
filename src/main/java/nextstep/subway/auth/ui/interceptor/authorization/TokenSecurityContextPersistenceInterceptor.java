890fd95d4b755e1ed5789ba38a2fff6514825efc
showpointer <wanho92@gmail.com>
2020-07-26 16:30:52 +0900

    feat(token): jwt payload에서 SecurityContext 추출

--- src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/interceptor/authorization/TokenSecurityContextPersistenceInterceptor.java
@@ -1,8 +1,13 @@
 package nextstep.subway.auth.ui.interceptor.authorization;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.auth.infrastructure.AuthorizationExtractor;
 import nextstep.subway.auth.infrastructure.AuthorizationType;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.auth.infrastructure.SecurityContext;
+import nextstep.subway.member.domain.LoginMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,17 +15,23 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TokenSecurityContextPersistenceInterceptor implements HandlerInterceptor {
     JwtTokenProvider jwtTokenProvider;
+    ObjectMapper objectMapper;
 
     public TokenSecurityContextPersistenceInterceptor(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws JsonProcessingException {
         String token = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
 
         if (jwtTokenProvider.validateToken(token)) {
+            String payload = jwtTokenProvider.getPayload(token);
 
+            LoginMember userDetails = objectMapper.readValue(payload, LoginMember.class);
+
+            SecurityContext context = new SecurityContext(new Authentication(userDetails));
             return true;
         }
         throw new RuntimeException();
