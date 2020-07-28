5a91d1e8610bdee43fef12878ae4a53c66c8939a
etff <wish2rich@gmail.com>
2020-07-28 13:06:35 +0900

    test(favorite): remove duplicate code

--- src/test/java/nextstep/subway/favorite/ui/FavoriteControllerTest.java
+++ src/test/java/nextstep/subway/favorite/ui/FavoriteControllerTest.java
@@ -3,41 +3,53 @@ package nextstep.subway.favorite.ui;
 import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.member.domain.LoginMember;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 
 class FavoriteControllerTest {
+    private static final String EMAIL = "email@email.com";
+    private static final String PASSWORD = "password";
+    private static final Integer AGE = 20;
+    private static final long ID = 1L;
+
+    private LoginMember loginMember;
+    private FavoriteService favoriteService;
+    private FavoriteController favoriteController;
+
+    @BeforeEach
+    void setUp() {
+        loginMember = new LoginMember(ID, EMAIL, PASSWORD, AGE);
+        favoriteService = mock(FavoriteService.class);
+        favoriteController = new FavoriteController(favoriteService);
+    }
 
     @Test
     void create() {
-        LoginMember loginMember = new LoginMember(1L, "email@email.com", "password", 20);
-        FavoriteRequest favoriteRequest = new FavoriteRequest(1L, 2L);
+        // given
+        FavoriteRequest favoriteRequest = new FavoriteRequest(anyLong(), anyLong());
 
-        FavoriteService favoriteService = mock(FavoriteService.class);
-        FavoriteController favoriteController = new FavoriteController(favoriteService);
-
+        // when
         ResponseEntity entity = favoriteController.createFavorite(loginMember, favoriteRequest);
 
+        // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         verify(favoriteService).createFavorite(loginMember.getId(), favoriteRequest);
     }
 
-
     @Test
     void get() {
-        LoginMember loginMember = new LoginMember(1L, "email@email.com", "password", 20);
-
-        FavoriteService favoriteService = mock(FavoriteService.class);
-        FavoriteController favoriteController = new FavoriteController(favoriteService);
-
+        // when
         ResponseEntity entity = favoriteController.getFavorites(loginMember);
 
+        // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(favoriteService).findFavorites(loginMember.getId());
     }
@@ -45,13 +57,11 @@ class FavoriteControllerTest {
 
     @Test
     void delete() {
-        LoginMember loginMember = new LoginMember(1L, "email@email.com", "password", 20);
-        FavoriteService favoriteService = mock(FavoriteService.class);
-        FavoriteController favoriteController = new FavoriteController(favoriteService);
+        // when
+        ResponseEntity entity = favoriteController.deleteFavorite(loginMember, ID);
 
-        ResponseEntity entity = favoriteController.deleteFavorite(loginMember, 1L);
-
+        // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-        verify(favoriteService).deleteFavorite(loginMember.getId(), 1L);
+        verify(favoriteService).deleteFavorite(loginMember.getId(), ID);
     }
 }
