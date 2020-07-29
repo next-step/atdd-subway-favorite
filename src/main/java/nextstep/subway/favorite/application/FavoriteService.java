423e9c2415821390024e8f208981fb25e499f9ac
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-29 12:25:32 +0900

    refactor: FavoriteController to accept loginMember entity and its id

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,20 +1,22 @@
 package nextstep.subway.favorite.application;
 
-import nextstep.subway.favorite.domain.Favorite;
-import nextstep.subway.favorite.domain.FavoriteRepository;
-import nextstep.subway.favorite.dto.FavoriteRequest;
-import nextstep.subway.favorite.dto.FavoriteResponse;
-import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
-import nextstep.subway.station.dto.StationResponse;
-import org.springframework.stereotype.Service;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.domain.LoginMember;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
 
 @Service
 public class FavoriteService {
@@ -31,20 +33,20 @@ public class FavoriteService {
         favoriteRepository.save(favorite);
     }
 
-    public void deleteFavorite(Long id) {
+    public void deleteFavorite(Long loginMemberId, Long id) {
         favoriteRepository.deleteById(id);
     }
 
-    public List<FavoriteResponse> findFavorites() {
+    public List<FavoriteResponse> findFavorites(LoginMember loginMember) {
         List<Favorite> favorites = favoriteRepository.findAll();
         Map<Long, Station> stations = extractStations(favorites);
 
         return favorites.stream()
-                .map(it -> FavoriteResponse.of(
-                        it,
-                        StationResponse.of(stations.get(it.getSourceStationId())),
-                        StationResponse.of(stations.get(it.getTargetStationId()))))
-                .collect(Collectors.toList());
+            .map(it -> FavoriteResponse.of(
+                it,
+                StationResponse.of(stations.get(it.getSourceStationId())),
+                StationResponse.of(stations.get(it.getTargetStationId()))))
+            .collect(Collectors.toList());
     }
 
     private Map<Long, Station> extractStations(List<Favorite> favorites) {
--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -1,13 +1,20 @@
 package nextstep.subway.favorite.ui;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.List;
+import nextstep.subway.member.domain.LoginMember;
 
 @RestController
 public class FavoriteController {
@@ -18,22 +25,22 @@ public class FavoriteController {
     }
 
     @PostMapping("/favorites")
-    public ResponseEntity createFavorite(@RequestBody FavoriteRequest request) {
+    public ResponseEntity createFavorite(LoginMember loginMember, @RequestBody FavoriteRequest request) {
         favoriteService.createFavorite(request);
         return ResponseEntity
-                .created(URI.create("/favorites/" + 1L))
-                .build();
+            .created(URI.create("/favorites/" + loginMember.getId()))
+            .build();
     }
 
     @GetMapping("/favorites")
-    public ResponseEntity<List<FavoriteResponse>> getFavorites() {
-        List<FavoriteResponse> favorites = favoriteService.findFavorites();
+    public ResponseEntity<List<FavoriteResponse>> getFavorites(LoginMember loginMember) {
+        List<FavoriteResponse> favorites = favoriteService.findFavorites(loginMember);
         return ResponseEntity.ok().body(favorites);
     }
 
     @DeleteMapping("/favorites/{id}")
-    public ResponseEntity deleteFavorite(@PathVariable Long id) {
-        favoriteService.deleteFavorite(id);
+    public ResponseEntity deleteFavorite(LoginMember loginMember, @PathVariable Long id) {
+        favoriteService.deleteFavorite(loginMember.getId(), id);
         return ResponseEntity.noContent().build();
     }
 }
