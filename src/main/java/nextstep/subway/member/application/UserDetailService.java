c0c8ff02b5f5fd66e309fabb906cc87a034d667e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-29 19:10:33 +0900

    refactor: remove duplicate service

--- src/main/java/nextstep/subway/member/application/UserDetailService.java
+++
@@ -1,7 +0,0 @@
-package nextstep.subway.member.application;
-
-import nextstep.subway.member.domain.LoginMember;
-
-public interface UserDetailService {
-    LoginMember loadUserByUserName(String userName);
-}
