938906e19960470b9ddff51240b0f2fc6874aebf
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-29 19:03:47 +0900

    test: FavoriteControllerTest to fix create favorite error

--- src/test/java/nextstep/subway/favorite/ui/FavoriteControllerTest.java
+++ src/test/java/nextstep/subway/favorite/ui/FavoriteControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import nextstep.subway.favorite.application.FavoriteService;
+import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
 import nextstep.subway.member.domain.LoginMember;
@@ -21,10 +22,22 @@ public class FavoriteControllerTest {
     private LoginMember loginMember;
     private FavoriteService favoriteService;
     private FavoriteController favoriteController;
+    private Long SOURCE;
+    private Long TARGET;
+    private Long ID;
+    private String EMAIL;
+    private String PASSWORD;
+    private Integer AGE;
 
     @BeforeEach
     void setUp() {
-        loginMember = new LoginMember(1L, "crongro@email.com", "ilovejavascript", 20);
+        SOURCE = 1L;
+        TARGET = 2L;
+        ID = 1L;
+        EMAIL = "crongro@gmail.com";
+        PASSWORD = "ilovejavascript";
+        AGE = 20;
+        loginMember = new LoginMember(ID, EMAIL, PASSWORD, AGE);
         favoriteService = mock(FavoriteService.class);
         favoriteController = new FavoriteController(favoriteService);
     }
@@ -32,9 +45,15 @@ public class FavoriteControllerTest {
     @DisplayName("즐겨찾기를 추가한다.")
     @Test
     void 즐겨찾기를_추가한다() {
-        FavoriteRequest favoriteRequest = new FavoriteRequest(1L, 2L);
+        // given
+        FavoriteRequest favoriteRequest = new FavoriteRequest(SOURCE, TARGET);
+        Favorite favorite = new Favorite(ID, loginMember.getId(), SOURCE, TARGET);
+        when(favoriteService.createFavorite(loginMember.getId(), favoriteRequest)).thenReturn(favorite);
+
+        // when
         ResponseEntity<Void> entity = favoriteController.createFavorite(loginMember, favoriteRequest);
 
+        // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         verify(favoriteService).createFavorite(loginMember.getId(), favoriteRequest);
     }
@@ -42,8 +61,10 @@ public class FavoriteControllerTest {
     @DisplayName("즐겨찾기 목록을 조회한다.")
     @Test
     void 즐겨찾기_목록을_조회한다() {
+        // when
         ResponseEntity<List<FavoriteResponse>> entity = favoriteController.getFavorites(loginMember);
 
+        // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(favoriteService).findFavorites(loginMember.getId());
     }
@@ -51,8 +72,10 @@ public class FavoriteControllerTest {
     @DisplayName("즐겨찾기를 삭제한다.")
     @Test
     void 즐겨찾기를_삭제한다() {
+        // when
         ResponseEntity<Void> entity = favoriteController.deleteFavorite(loginMember, 1L);
 
+        // then
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
         verify(favoriteService).deleteFavorite(loginMember.getId(), 1L);
     }
