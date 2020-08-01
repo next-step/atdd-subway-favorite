431148004d59a252b05674aa13357dfbddd4bfd3
showpointer <wanho92@gmail.com>
2020-08-01 13:49:40 +0900

    test(converter): session converter 테스트 추가

--- src/test/java/nextstep/subway/auth/ui/interceptor/convertor/AuthenticationConverterTest.java
+++ src/test/java/nextstep/subway/auth/ui/interceptor/convertor/AuthenticationConverterTest.java
@@ -1,26 +1,62 @@
 package nextstep.subway.auth.ui.interceptor.convertor;
 
+import nextstep.subway.auth.domain.AuthenticationToken;
 import nextstep.subway.auth.ui.interceptor.converter.AuthenticationConverter;
 import nextstep.subway.auth.ui.interceptor.converter.SessionAuthenticationConverter;
 import nextstep.subway.auth.ui.interceptor.converter.TokenAuthenticationConverter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthenticationConverterTest {
-    AuthenticationConverter sessionConverter;
-    AuthenticationConverter tokenConverter;
+    private static final String EMAIL = "test@test.com";
+    private static final String REGEX = ":";
+    private static final String PASSWORD = "test";
+
+    private AuthenticationConverter sessionConverter;
+    private AuthenticationConverter tokenConverter;
+
+    private MockHttpServletRequest sessionRequest;
+    private MockHttpServletRequest tokenRequest;
 
     @BeforeEach
     void setUp() {
+        sessionRequest = new MockHttpServletRequest();
         sessionConverter = new SessionAuthenticationConverter();
         tokenConverter = new TokenAuthenticationConverter();
+
+        setSessionRequestHeader();
     }
 
+    void setSessionRequestHeader() {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", EMAIL);
+        params.put("password", PASSWORD);
+
+        sessionRequest.addParameters(params);
+    }
+
+    @DisplayName("Session, token Converter 생성")
     @Test
     void createConverter() {
         assertThat(sessionConverter).isNotNull();
         assertThat(tokenConverter).isNotNull();
+    }
+
+    @DisplayName("Session -> AuthenticationToken 변환")
+    @Test
+    void convertSessionToAuthenticationToken() {
+        // when
+        AuthenticationToken token = sessionConverter.convert(sessionRequest);
+
+        // then
+        assertThat(token.getPrincipal()).isEqualTo(EMAIL);
+        assertThat(token.getCredentials()).isEqualTo(PASSWORD);
     }
 }
