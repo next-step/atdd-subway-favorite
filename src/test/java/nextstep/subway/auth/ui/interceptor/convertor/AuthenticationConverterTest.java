7634e8c1fd304b161b24586ca20606d8424a62ab
showpointer <wanho92@gmail.com>
2020-08-04 22:39:06 +0900

    test(converter): token converter 테스트 작성

--- src/test/java/nextstep/subway/auth/ui/interceptor/convertor/AuthenticationConverterTest.java
+++ src/test/java/nextstep/subway/auth/ui/interceptor/convertor/AuthenticationConverterTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +33,7 @@ public class AuthenticationConverterTest {
         tokenConverter = new TokenAuthenticationConverter();
 
         setSessionRequestHeader();
+        setBasicAuthHeader();
     }
 
     void setSessionRequestHeader() {
@@ -40,6 +42,13 @@ public class AuthenticationConverterTest {
         params.put("password", PASSWORD);
 
         sessionRequest.addParameters(params);
+    }
+
+    private void setBasicAuthHeader() {
+        byte[] targetBytes = (EMAIL + REGEX + PASSWORD).getBytes();
+        byte[] encodedBytes = Base64.getEncoder().encode(targetBytes);
+        String credentials = new String(encodedBytes);
+        tokenRequest.addHeader("Authorization", "Basic " + credentials);
     }
 
     @DisplayName("Session, token Converter 생성")
@@ -54,6 +63,17 @@ public class AuthenticationConverterTest {
     void convertSessionToAuthenticationToken() {
         // when
         AuthenticationToken token = sessionConverter.convert(sessionRequest);
+
+        // then
+        assertThat(token.getPrincipal()).isEqualTo(EMAIL);
+        assertThat(token.getCredentials()).isEqualTo(PASSWORD);
+    }
+
+    @DisplayName("Token -> AuthenticationToken 변환")
+    @Test
+    void convertTokenToAuthenticationToken() {
+        // when
+        AuthenticationToken token = tokenConverter.convert(sessionRequest);
 
         // then
         assertThat(token.getPrincipal()).isEqualTo(EMAIL);
