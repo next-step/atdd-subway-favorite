07f3f835dc84f7d218b721978cee83e404de557f
showpointer <wanho92@gmail.com>
2020-08-06 01:45:08 +0900

    test(member): 내 회원정보 수정 테스트 작성

--- src/test/java/nextstep/subway/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/acceptance/MemberAcceptanceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.member.acceptance;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -39,5 +40,21 @@ public class MemberAcceptanceTest extends AcceptanceTest {
 
         // then
         회원_삭제됨(deleteResponse);
+    }
+
+    @DisplayName("회원 정보 수정")
+    @Test
+    void updateMember() {
+        // when
+        회원_등록되어_있음(EMAIL, PASSWORD, AGE);
+
+        TokenResponse tokenResponse = 로그인_되어_있음(EMAIL, PASSWORD);
+
+        ExtractableResponse<Response> response = 내_회원_정보_조회_요청(tokenResponse);
+        회원_정보_조회됨(response, EMAIL, AGE);
+
+        ExtractableResponse<Response> updated = 내_회원_정보_수정_요청(tokenResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+
+        회원_정보_수정됨(updated);
     }
 }
