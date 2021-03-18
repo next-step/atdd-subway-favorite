bba0f5f94b633781a3fd56e6371b1a9f7585bfc5
HyunSung-Na <oceana57@naver.com>
2021-03-18 22:14:53 +0900

    MemberAcceptanceTest의 인수 테스트를 통합하기

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -16,59 +16,32 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     public static final int AGE = 20;
     public static final int NEW_AGE = 21;
 
-    @DisplayName("회원가입을 한다.")
-    @Test
-    void createMember() {
-        // when
-        ExtractableResponse<Response> response = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // then
-        회원_생성됨(response);
-    }
-
-    @DisplayName("회원 정보를 조회한다.")
-    @Test
-    void getMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_정보_조회_요청(createResponse);
-
-        // then
-        회원_정보_조회됨(response, EMAIL, AGE);
-
-    }
-
-    @DisplayName("회원 정보를 수정한다.")
-    @Test
-    void updateMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
-
-        // then
-        회원_정보_수정됨(response);
-    }
-
-    @DisplayName("회원 정보를 삭제한다.")
-    @Test
-    void deleteMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_삭제_요청(createResponse);
-
-        // then
-        회원_삭제됨(response);
-    }
-
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // When 회원 생성을 요청
+        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // Then 회원 생성됨
+        회원_생성됨(createResponse);
+
+        // When 회원 정보 조회 요청
+        ExtractableResponse<Response> searchResponse = 회원_정보_조회_요청(createResponse);
+
+        // Then 회원 정보 조회됨
+        회원_정보_조회됨(searchResponse, EMAIL, AGE);
+
+        // When 회원 정보 수정 요청
+        ExtractableResponse<Response> updateResponse = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+
+        // Then 회원 정보 수정됨
+        회원_정보_수정됨(updateResponse);
+
+        // When 회원 삭제 요청
+        ExtractableResponse<Response> deleteResponse = 회원_삭제_요청(createResponse);
+
+        // Then 회원 삭제됨
+        회원_삭제됨(deleteResponse);
     }
 
     @DisplayName("나의 정보를 관리한다.")
