55c5dd85c786197bba2fe4ddf07197d9d06d6873
임한결 <dlagksruf19@naver.com>
2021-03-19 22:16:45 +0900

    feat : MemberAcceptanceTest 인수 테스트 통합 (실습)

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Test;
 
 import static nextstep.subway.member.MemberSteps.*;
 
+@DisplayName("회원 정보 관리 관련 기능 인수 테스트")
 public class MemberAcceptanceTest extends AcceptanceTest {
+
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
     public static final String NEW_EMAIL = "newemail@email.com";
@@ -47,7 +49,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
 
         // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
 
         // then
         회원_정보_수정됨(response);
@@ -69,6 +71,29 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // when
+        ExtractableResponse<Response> createdMemberResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        회원_생성됨(createdMemberResponse);
+
+        // when
+        ExtractableResponse<Response> foundMemberResponse = 회원_정보_조회_요청(createdMemberResponse);
+
+        // then
+        회원_정보_조회됨(foundMemberResponse, EMAIL, AGE);
+
+        // when
+        ExtractableResponse<Response> updatedMemberResponse = 회원_정보_수정_요청(createdMemberResponse, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
+
+        // then
+        회원_정보_수정됨(updatedMemberResponse);
+
+        // when
+        ExtractableResponse<Response> deletedMemberResponse = 회원_삭제_요청(createdMemberResponse);
+
+        // then
+        회원_삭제됨(deletedMemberResponse);
     }
 
     @DisplayName("나의 정보를 관리한다.")
