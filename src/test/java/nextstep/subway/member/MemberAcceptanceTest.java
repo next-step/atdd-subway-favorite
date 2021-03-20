337152e22ebecb0f266da0e2a6afa71fee7bc2d2
Jeongsu Yang <mycat83@gmail.com>
2021-03-20 22:09:13 +0900

    test: 회원 정보 관리 인수 테스트 통합

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -6,9 +6,18 @@ import nextstep.subway.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static nextstep.subway.member.MemberSteps.*;
+import static nextstep.subway.member.MemberSteps.회원_삭제_요청;
+import static nextstep.subway.member.MemberSteps.회원_삭제됨;
+import static nextstep.subway.member.MemberSteps.회원_생성_요청;
+import static nextstep.subway.member.MemberSteps.회원_생성됨;
+import static nextstep.subway.member.MemberSteps.회원_정보_수정_요청;
+import static nextstep.subway.member.MemberSteps.회원_정보_수정됨;
+import static nextstep.subway.member.MemberSteps.회원_정보_조회_요청;
+import static nextstep.subway.member.MemberSteps.회원_정보_조회됨;
 
-public class MemberAcceptanceTest extends AcceptanceTest {
+@DisplayName("회원 정보 관리")
+class MemberAcceptanceTest extends AcceptanceTest {
+
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
     public static final String NEW_EMAIL = "newemail@email.com";
@@ -47,7 +56,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
 
         // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
 
         // then
         회원_정보_수정됨(response);
@@ -69,6 +78,29 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // when
+        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        회원_생성됨(createResponse);
+
+        // when
+        ExtractableResponse<Response> findResponse = 회원_정보_조회_요청(createResponse);
+
+        // then
+        회원_정보_조회됨(findResponse, EMAIL, AGE);
+
+        // when
+        ExtractableResponse<Response> updateResponse = 회원_정보_수정_요청(createResponse, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
+
+        // then
+        회원_정보_수정됨(updateResponse);
+
+        // when
+        ExtractableResponse<Response> deleteResponse = 회원_삭제_요청(createResponse);
+
+        // then
+        회원_삭제됨(deleteResponse);
     }
 
     @DisplayName("나의 정보를 관리한다.")
