923ad3e98f0fbc0381b2b33a9c1c8571c103e610
SC <pch838811@gmail.com>
2021-03-20 21:24:22 +0900

    test: 회원정보 관리 인수테스트 통합

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import static nextstep.subway.member.MemberSteps.*;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 public class MemberAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
@@ -47,7 +49,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
 
         // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
 
         // then
         회원_정보_수정됨(response);
@@ -69,6 +71,34 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // when
+        final ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        회원_생성됨(createResponse);
+
+        // when
+        final ExtractableResponse<Response> infoResponse = 회원_정보_조회_요청(createResponse);
+
+        // then
+        회원_정보_조회됨(infoResponse, EMAIL, AGE);
+
+        // when
+        // 회원 정보 수정 요청
+        final ExtractableResponse<Response> modifyResponse =
+            회원_정보_수정_요청(createResponse, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
+
+        // then
+        // 회원 정보 수정됨
+        회원_정보_수정됨(modifyResponse);
+
+        // when
+        // 회원 정보 삭제 요청
+        final ExtractableResponse<Response> deleteResponse = 회원_삭제_요청(createResponse);
+
+        // then
+        // 회원 정보 삭제됨
+        회원_삭제됨(deleteResponse);
     }
 
     @DisplayName("나의 정보를 관리한다.")
