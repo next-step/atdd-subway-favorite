a954d59c6e47e5f4ff82b8310b13269a0216f941
pa324 <ms66225@nate.com>
2021-03-21 09:47:40 +0900

    feature : SessionAuthenticaionConverter 구현 및 테스트 오타 수정

--- src/test/java/nextstep/subway/auth/SessionAuthenticationConverterTest.java
+++ src/test/java/nextstep/subway/auth/SessionAuthenticationConverterTest.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import nextstep.subway.auth.domain.AuthenticationToken;
 import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.ui.AuthenticationConverter;
+import nextstep.subway.auth.ui.session.SessionAuthenticationConverter;
 import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +17,7 @@ import org.springframework.mock.web.MockHttpServletRequest;
 @DisplayName("세션인증 컨버터 테스트")
 public class SessionAuthenticationConverterTest {
 
-  private AuthenticaionConverter sessionAuthenticaionConverter;
+  private AuthenticationConverter sessionAuthenticationConverter;
 
   private static final String EMAIL = "email@email.com";
   private static final String PASSWORD = "password";
@@ -25,10 +27,10 @@ public class SessionAuthenticationConverterTest {
   @Test
   void convert() throws IOException, JSONException {
     // given
-    sessionAuthenticaionConverter = new SessionAuthenticationConverter();
+    sessionAuthenticationConverter = new SessionAuthenticationConverter();
     MockHttpServletRequest request = createMockRequest();
     // when
-    AuthenticationToken authenticationToken = sessionAuthenticaionConverter.convert(request);
+    AuthenticationToken authenticationToken = sessionAuthenticationConverter.convert(request);
     // then
     assertThat(authenticationToken.getPrincipal()).isEqualTo(EMAIL);
     assertThat(authenticationToken.getCredentials()).isEqualTo(PASSWORD);
