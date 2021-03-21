78f7889430e7c8b3097870c100b923273737f9fe
pa324 <ms66225@nate.com>
2021-03-21 09:54:00 +0900

    feature : SessionAuthenticaionInterceptor에서 SessionAuthenticaionConverter 의존하도록 수정

--- src/main/java/nextstep/subway/auth/infrastructure/AuthConfig.java
+++ src/main/java/nextstep/subway/auth/infrastructure/AuthConfig.java
@@ -1,6 +1,8 @@
 package nextstep.subway.auth.infrastructure;
 
-import nextstep.subway.auth.ui.*;
+import java.util.List;
+import nextstep.subway.auth.ui.AuthenticationPrincipalArgumentResolver;
+import nextstep.subway.auth.ui.session.SessionAuthenticationConverter;
 import nextstep.subway.auth.ui.session.SessionAuthenticationInterceptor;
 import nextstep.subway.auth.ui.session.SessionSecurityContextPersistenceInterceptor;
 import nextstep.subway.auth.ui.token.TokenAuthenticationInterceptor;
@@ -10,27 +12,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class AuthConfig implements WebMvcConfigurer {
-    private CustomUserDetailsService userDetailsService;
-    private JwtTokenProvider jwtTokenProvider;
 
-    public AuthConfig(CustomUserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
-        this.userDetailsService = userDetailsService;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+  private CustomUserDetailsService userDetailsService;
+  private JwtTokenProvider jwtTokenProvider;
 
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SessionAuthenticationInterceptor(userDetailsService)).addPathPatterns("/login/session");
-        registry.addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider)).addPathPatterns("/login/token");
-        registry.addInterceptor(new SessionSecurityContextPersistenceInterceptor());
-        registry.addInterceptor(new TokenSecurityContextPersistenceInterceptor(jwtTokenProvider));
-    }
+  public AuthConfig(CustomUserDetailsService userDetailsService,
+      JwtTokenProvider jwtTokenProvider) {
+    this.userDetailsService = userDetailsService;
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
 
-    @Override
-    public void addArgumentResolvers(List argumentResolvers) {
-        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
-    }
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new SessionAuthenticationInterceptor(userDetailsService,
+        new SessionAuthenticationConverter())).addPathPatterns("/login/session");
+    registry
+        .addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider))
+        .addPathPatterns("/login/token");
+    registry.addInterceptor(new SessionSecurityContextPersistenceInterceptor());
+    registry.addInterceptor(new TokenSecurityContextPersistenceInterceptor(jwtTokenProvider));
+  }
+
+  @Override
+  public void addArgumentResolvers(List argumentResolvers) {
+    argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+  }
 }
--- src/main/java/nextstep/subway/auth/ui/session/SessionAuthenticationInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/session/SessionAuthenticationInterceptor.java
@@ -3,6 +3,7 @@ package nextstep.subway.auth.ui.session;
 import nextstep.subway.auth.domain.Authentication;
 import nextstep.subway.auth.domain.AuthenticationToken;
 import nextstep.subway.auth.infrastructure.SecurityContext;
+import nextstep.subway.auth.ui.AuthenticationConverter;
 import nextstep.subway.member.application.CustomUserDetailsService;
 import nextstep.subway.member.domain.LoginMember;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -19,14 +20,16 @@ public class SessionAuthenticationInterceptor implements HandlerInterceptor {
     public static final String PASSWORD_FIELD = "password";
 
     private CustomUserDetailsService userDetailsService;
+    private AuthenticationConverter converter;
 
-    public SessionAuthenticationInterceptor(CustomUserDetailsService userDetailsService) {
+    public SessionAuthenticationInterceptor(CustomUserDetailsService userDetailsService,AuthenticationConverter authenticationConverter) {
         this.userDetailsService = userDetailsService;
+        this.converter = authenticationConverter;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        AuthenticationToken token = convert(request);
+        AuthenticationToken token = converter.convert(request);
         Authentication authentication = authenticate(token);
 
         if (authentication == null) {
@@ -37,14 +40,6 @@ public class SessionAuthenticationInterceptor implements HandlerInterceptor {
         httpSession.setAttribute(SPRING_SECURITY_CONTEXT_KEY, new SecurityContext(authentication));
         response.setStatus(HttpServletResponse.SC_OK);
         return false;
-    }
-
-    public AuthenticationToken convert(HttpServletRequest request) {
-        Map<String, String[]> paramMap = request.getParameterMap();
-        String principal = paramMap.get(USERNAME_FIELD)[0];
-        String credentials = paramMap.get(PASSWORD_FIELD)[0];
-
-        return new AuthenticationToken(principal, credentials);
     }
 
     public Authentication authenticate(AuthenticationToken token) {
