1cd61189c138fa465ae3567c2ed909f08b83dc92
HyunSung-Na <oceana57@naver.com>
2021-03-22 23:53:09 +0900

    AuthenticationInterceptor add

---
+++ src/main/java/nextstep/subway/auth/ui/authentication/AuthenticationInterceptor.java
@@ -0,0 +1,58 @@
+package nextstep.subway.auth.ui.authentication;
+
+import nextstep.subway.auth.domain.Authentication;
+import nextstep.subway.auth.domain.AuthenticationToken;
+import nextstep.subway.auth.ui.convert.AuthenticationConverter;
+import nextstep.subway.member.application.CustomUserDetailsService;
+import nextstep.subway.member.domain.LoginMember;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private final AuthenticationConverter authenticationConverter;
+    private final CustomUserDetailsService userDetailService;
+
+    public AuthenticationInterceptor(AuthenticationConverter authenticationConverter, CustomUserDetailsService userDetailService) {
+        this.authenticationConverter = authenticationConverter;
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        AuthenticationToken authenticationToken = authenticationConverter.convert(request);
+        Authentication authentication = authenticate(authenticationToken);
+
+        if (authentication == null) {
+            throw new RuntimeException();
+        }
+
+        afterAuthentication(request, response, authentication);
+        return false;
+    }
+
+    public abstract void afterAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                             Authentication authentication) throws IOException;
+
+
+    public Authentication authenticate(AuthenticationToken token) {
+        String principal = token.getPrincipal();
+        LoginMember userDetails = userDetailService.loadUserByUsername(principal);
+        checkAuthentication(userDetails, token);
+
+        return new Authentication(userDetails);
+    }
+
+    private void checkAuthentication(LoginMember userDetails, AuthenticationToken token) {
+        if (userDetails == null) {
+            throw new RuntimeException();
+        }
+
+        if (!userDetails.checkPassword(token.getCredentials())) {
+            throw new RuntimeException();
+        }
+    }
+}
