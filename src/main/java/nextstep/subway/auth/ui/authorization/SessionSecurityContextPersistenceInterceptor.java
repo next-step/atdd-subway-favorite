36ee21ec4b1ff5b6064fd8cb0ed1e2af0c3fc7e9
HyunSung-Na <oceana57@naver.com>
2021-03-23 00:08:03 +0900

    SecurityContextInterceptor 추상화

---
+++ src/main/java/nextstep/subway/auth/ui/authorization/SessionSecurityContextPersistenceInterceptor.java
@@ -0,0 +1,25 @@
+package nextstep.subway.auth.ui.authorization;
+
+import nextstep.subway.auth.infrastructure.SecurityContext;
+import nextstep.subway.auth.infrastructure.SecurityContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static nextstep.subway.auth.infrastructure.SecurityContextHolder.SPRING_SECURITY_CONTEXT_KEY;
+
+public class SessionSecurityContextPersistenceInterceptor extends SecurityContextInterceptor{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            return true;
+        }
+
+        SecurityContext securityContext = (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
+        if (securityContext != null) {
+            SecurityContextHolder.setContext(securityContext);
+        }
+        return true;
+    }
+}
