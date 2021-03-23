2efacc741ad79bee4a1b87b1ec961d6d3898f168
bbbnam <bbbnam@naver.com>
2021-03-23 21:19:09 +0900

    refactor : SecurityContextInterceptor 추상화

---
+++ src/main/java/nextstep/subway/auth/ui/common/SecurityContextInterceptor.java
@@ -0,0 +1,25 @@
+package nextstep.subway.auth.ui.common;
+
+import nextstep.subway.auth.infrastructure.SecurityContext;
+import nextstep.subway.auth.infrastructure.SecurityContextHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class SecurityContextInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            return true;
+        }
+        SecurityContext securityContext = initContext(request);
+        if (securityContext != null) {
+            SecurityContextHolder.setContext(securityContext);
+        }
+        return true;
+    }
+
+    public abstract SecurityContext initContext(HttpServletRequest request);
+}
--- src/main/java/nextstep/subway/auth/ui/session/SessionSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/session/SessionSecurityContextPersistenceInterceptor.java
@@ -2,25 +2,18 @@ package nextstep.subway.auth.ui.session;
 
 import nextstep.subway.auth.infrastructure.SecurityContext;
 import nextstep.subway.auth.infrastructure.SecurityContextHolder;
-import org.springframework.web.servlet.HandlerInterceptor;
+import nextstep.subway.auth.ui.common.SecurityContextInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static nextstep.subway.auth.infrastructure.SecurityContextHolder.SPRING_SECURITY_CONTEXT_KEY;
 
-public class SessionSecurityContextPersistenceInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            return true;
-        }
+public class SessionSecurityContextPersistenceInterceptor extends SecurityContextInterceptor {
 
-        SecurityContext securityContext = (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
-        if (securityContext != null) {
-            SecurityContextHolder.setContext(securityContext);
-        }
-        return true;
+    @Override
+    public SecurityContext initContext(HttpServletRequest request) {
+        return (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
     }
 
     @Override
