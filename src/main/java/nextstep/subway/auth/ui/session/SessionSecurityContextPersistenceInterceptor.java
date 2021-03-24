e48944444f288d72ca35a091e476b4134729ab16
SC <pch838811@gmail.com>
2021-03-25 05:52:13 +0900

    feat: SecurityContextInterceptor 추상화

--- src/main/java/nextstep/subway/auth/ui/session/SessionSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/session/SessionSecurityContextPersistenceInterceptor.java
@@ -1,30 +1,15 @@
 package nextstep.subway.auth.ui.session;
 
-import nextstep.subway.auth.infrastructure.SecurityContext;
-import nextstep.subway.auth.infrastructure.SecurityContextHolder;
-import org.springframework.web.servlet.HandlerInterceptor;
+import static nextstep.subway.auth.infrastructure.SecurityContextHolder.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import static nextstep.subway.auth.infrastructure.SecurityContextHolder.SPRING_SECURITY_CONTEXT_KEY;
+import nextstep.subway.auth.infrastructure.SecurityContext;
+import nextstep.subway.auth.ui.SecurityContextInterceptor;
 
-public class SessionSecurityContextPersistenceInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            return true;
-        }
-
-        SecurityContext securityContext = (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
-        if (securityContext != null) {
-            SecurityContextHolder.setContext(securityContext);
-        }
-        return true;
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        SecurityContextHolder.clearContext();
-    }
+public class SessionSecurityContextPersistenceInterceptor extends SecurityContextInterceptor {
+	@Override
+	protected SecurityContext extractSecurityContext(HttpServletRequest request) {
+		return (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
+	}
 }
--- src/main/java/nextstep/subway/auth/ui/token/TokenSecurityContextPersistenceInterceptor.java
+++ src/main/java/nextstep/subway/auth/ui/token/TokenSecurityContextPersistenceInterceptor.java
@@ -3,9 +3,6 @@ package nextstep.subway.auth.ui.token;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,47 +12,35 @@ import nextstep.subway.auth.infrastructure.AuthorizationExtractor;
 import nextstep.subway.auth.infrastructure.AuthorizationType;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.auth.infrastructure.SecurityContext;
-import nextstep.subway.auth.infrastructure.SecurityContextHolder;
+import nextstep.subway.auth.ui.SecurityContextInterceptor;
 
-public class TokenSecurityContextPersistenceInterceptor implements HandlerInterceptor {
+public class TokenSecurityContextPersistenceInterceptor extends SecurityContextInterceptor {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final ObjectMapper objectMapper;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final ObjectMapper objectMapper;
 
-    public TokenSecurityContextPersistenceInterceptor(
-        JwtTokenProvider jwtTokenProvider, ObjectMapper objectMapper) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.objectMapper = objectMapper;
-    }
+	public TokenSecurityContextPersistenceInterceptor(JwtTokenProvider jwtTokenProvider,
+		ObjectMapper objectMapper) {
+		this.jwtTokenProvider = jwtTokenProvider;
+		this.objectMapper = objectMapper;
+	}
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            return true;
-        }
+	@Override
+	protected SecurityContext extractSecurityContext(HttpServletRequest request) {
+		String credentials = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
+		if (!jwtTokenProvider.validateToken(credentials)) {
+			return null;
+		}
 
-        String credentials = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
-        if (!jwtTokenProvider.validateToken(credentials)) {
-            return true;
-        }
+		try {
+			String payload = jwtTokenProvider.getPayload(credentials);
+			TypeReference<Map<String, String>> typeRef = new TypeReference<Map<String, String>>() {
+			};
 
-        SecurityContext securityContext = extractSecurityContext(credentials);
-        if (securityContext != null) {
-            SecurityContextHolder.setContext(securityContext);
-        }
-        return true;
-    }
-
-    private SecurityContext extractSecurityContext(String credentials) {
-        try {
-            String payload = jwtTokenProvider.getPayload(credentials);
-            TypeReference<Map<String, String>> typeRef = new TypeReference<Map<String, String>>() {
-            };
-
-            Map principal = objectMapper.readValue(payload, typeRef);
-            return new SecurityContext(new Authentication(principal));
-        } catch (Exception e) {
-            return null;
-        }
-    }
+			Map principal = objectMapper.readValue(payload, typeRef);
+			return new SecurityContext(new Authentication(principal));
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
