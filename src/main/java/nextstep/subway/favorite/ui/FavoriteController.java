76310fdab8c1078dde1e0df3d1c68fbed92f7d43
bbbnam <bbbnam@naver.com>
2021-03-25 22:55:45 +0900

    feat : 즐겨찾기 생성 기능 구현 완료 & 테스트 수정

--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -5,6 +5,7 @@ import nextstep.subway.auth.domain.UserDetail;
 import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.domain.LoginMember;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +22,9 @@ public class FavoriteController {
     }
 
     @PostMapping("/favorites")
-    public ResponseEntity createFavorites(@AuthenticationPrincipal UserDetail userDetail,
+    public ResponseEntity createFavorites(@AuthenticationPrincipal LoginMember loginMember,
                                           @RequestBody FavoriteRequest request) {
-        FavoriteResponse favoriteResponse = favoriteService.saveFavorites(userDetail, request);
+        FavoriteResponse favoriteResponse = favoriteService.saveFavorites(loginMember, request);
         return ResponseEntity.created(URI.create("/favorites" + favoriteResponse.getId())).build();
     }
 }
--- src/test/java/nextstep/subway/favorite/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/acceptance/FavoriteAcceptanceTest.java
@@ -4,6 +4,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -12,29 +15,64 @@ import org.springframework.http.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
+import static nextstep.subway.member.MemberSteps.*;
+import static nextstep.subway.station.StationSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("즐겨찾기 기능을 관리한다.")
 public class FavoriteAcceptanceTest extends AcceptanceTest {
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final int AGE = 20;
+    private StationResponse 강남역;
+    private StationResponse 광교역;
+    private StationResponse 판교역;
+    private StationResponse 모란역;
+    private Map<String, String> param;
+    private Map<String, String> param2;
+    private TokenResponse loginResponse;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        // given
+        강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        광교역 = 지하철역_등록되어_있음("광교역").as(StationResponse.class);
+        판교역 = 지하철역_등록되어_있음("판교역").as(StationResponse.class);
+        모란역 = 지하철역_등록되어_있음("모란역").as(StationResponse.class);
+        param = new HashMap<>();
+        param.put("source",강남역.getId() + "");
+        param.put("target", 광교역.getId() + "");
+
+        param2 = new HashMap<>();
+        param2.put("source",판교역.getId() + "");
+        param2.put("target", 모란역.getId() + "");
+
+        // when
+        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        회원_생성됨(createResponse);
+
+        // when
+        loginResponse = 로그인_되어_있음(EMAIL, PASSWORD);
+    }
 
     @DisplayName("즐겨찾기 생성")
     @Test
     void create() {
-        // given
-        Map<String, String> param = new HashMap<>();
-        param.put("source", "1");
-        param.put("target", "3");
-
         // when
-        ExtractableResponse<Response> response = 즐겨찾기_생성_요청(param);
+        ExtractableResponse<Response> response = 즐겨찾기_생성_요청(param, loginResponse);
 
         // then
         즐겨찾기_생성됨(response);
     }
 
-    private ExtractableResponse<Response> 즐겨찾기_생성_요청(Map<String, String> param) {
+    private ExtractableResponse<Response> 즐겨찾기_생성_요청(Map<String, String> param, TokenResponse loginResponse) {
         String uri = "/favorites";
         return RestAssured.given().log().all()
+                          .auth().oauth2(loginResponse.getAccessToken())
                           .contentType(MediaType.APPLICATION_JSON_VALUE)
                           .body(param)
                           .when()
