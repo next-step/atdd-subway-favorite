a0f13791747bc31217a5b1b1ca8c38a0749cde68
Dong Hwan <bkuekany@gmail.com>
2021-03-26 00:36:58 +0900

    test : 토큰인증인터셉터 테스트 케이스 추가

--- src/test/java/nextstep/subway/auth/TokenAuthenticationInterceptorTest.java
+++ src/test/java/nextstep/subway/auth/TokenAuthenticationInterceptorTest.java
@@ -9,8 +9,8 @@ import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.auth.ui.token.TokenAuthenticationInterceptor;
 import nextstep.subway.member.application.CustomUserDetailsService;
 import nextstep.subway.member.domain.LoginMember;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -18,25 +18,75 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class TokenAuthenticationInterceptorTest {
+
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
     public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
 
+    private CustomUserDetailsService userDetailsService;
+    private JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    public void setup(){
+        userDetailsService = mock(CustomUserDetailsService.class);
+        jwtTokenProvider =mock(JwtTokenProvider.class);
+    }
+
+
     @Test
     void convert() throws IOException {
+        //Given
+        TokenAuthenticationInterceptor interceptor = new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider);
+        MockHttpServletRequest request = createMockRequest();
+
+        //When
+        AuthenticationToken authenticationToken = interceptor.convert(request);
+
+        //Then
+        assertAll(
+                () -> assertThat(authenticationToken.getPrincipal()).isEqualTo(EMAIL),
+                () -> assertThat(authenticationToken.getCredentials()).isEqualTo(PASSWORD)
+        );
     }
 
     @Test
     void authenticate() {
+        //Given
+        TokenAuthenticationInterceptor interceptor = new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider);
+        AuthenticationToken token = new AuthenticationToken(EMAIL, PASSWORD);
+
+        //When
+        when(userDetailsService.loadUserByUsername(EMAIL)).thenReturn(new LoginMember(1L, EMAIL, PASSWORD, 15));
+        Authentication authentication = interceptor.authenticate(token);
+
+        //Then
+        assertThat(authentication.getPrincipal()).isNotNull();
     }
 
     @Test
     void preHandle() throws IOException {
+        //Given
+        TokenAuthenticationInterceptor interceptor = new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        //When
+        when(userDetailsService.loadUserByUsername(EMAIL)).thenReturn(new LoginMember(1L, EMAIL, PASSWORD, 15));
+        when(jwtTokenProvider.createToken(anyString())).thenReturn(JWT_TOKEN);
+        boolean expected = interceptor.preHandle(createMockRequest(), response, this);
+
+        //Then
+        assertAll(
+                () -> assertThat(expected).isFalse(),
+                () -> assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE),
+                () -> assertThat(response.getContentAsString()).isEqualTo(new ObjectMapper().writeValueAsString(new TokenResponse(JWT_TOKEN)))
+
+        );
     }
 
     private MockHttpServletRequest createMockRequest() throws IOException {
@@ -45,5 +95,4 @@ class TokenAuthenticationInterceptorTest {
         request.setContent(new ObjectMapper().writeValueAsString(tokenRequest).getBytes());
         return request;
     }
-
 }
