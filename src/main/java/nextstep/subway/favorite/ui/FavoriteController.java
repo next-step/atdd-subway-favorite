7b360478822c1530996132f6a5356140fb2132fb
임한결 <dlagksruf19@naver.com>
2021-03-25 16:38:39 +0900

    feat : 즐겨찾기 생성 요청 Controller 구현

---
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -0,0 +1,27 @@
+package nextstep.subway.favorite.ui;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/favorites")
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<URI> createFavorite(@RequestBody FavoriteRequest favoriteRequest) {
+        Long id = favoriteService.createFavorite(favoriteRequest);
+        URI createdURI = URI.create(String.format("/favorites/%d", id));
+        return ResponseEntity.created(createdURI)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
