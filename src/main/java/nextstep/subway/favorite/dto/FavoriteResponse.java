32774522c045b888a059da49358376b18bfb492f
임한결 <dlagksruf19@naver.com>
2021-03-26 00:16:33 +0900

    feat : favoriteDTO 구현

---
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
@@ -0,0 +1,53 @@
+package nextstep.subway.favorite.dto;
+
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.station.dto.StationResponse;
+
+import java.util.Objects;
+
+public class FavoriteResponse {
+
+    private Long id;
+    private StationResponse source;
+    private StationResponse target;
+
+    public FavoriteResponse() {
+    }
+
+    public FavoriteResponse(Long id, StationResponse source, StationResponse target) {
+        this.id = id;
+        this.source = source;
+        this.target = target;
+    }
+
+    public static FavoriteResponse of(Favorite favorite) {
+        StationResponse source = StationResponse.of(favorite.getSource());
+        StationResponse target = StationResponse.of(favorite.getTarget());
+        return new FavoriteResponse(favorite.getId(), source, target);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public StationResponse getSource() {
+        return source;
+    }
+
+    public StationResponse getTarget() {
+        return target;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FavoriteResponse that = (FavoriteResponse) o;
+        return id.equals(that.id) && source.equals(that.source) && target.equals(that.target);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, source, target);
+    }
+}
