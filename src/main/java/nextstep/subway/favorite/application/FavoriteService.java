8650c3458d3c4d6da22049e8fb2f8099f4098000
bbbnam <bbbnam@naver.com>
2021-03-26 19:08:05 +0900

    refactor : 리뷰받은 내용 수정#1 - UserDetail -> LoginMember로 변경

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,9 +1,9 @@
 package nextstep.subway.favorite.application;
 
-import nextstep.subway.auth.domain.UserDetail;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.domain.LoginMember;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
@@ -21,14 +21,14 @@ public class FavoriteService {
         this.stationRepository = stationRepository;
     }
 
-    public FavoriteResponse saveFavorites(UserDetail userDetail, FavoriteRequest request) {
+    public FavoriteResponse saveFavorites(LoginMember loginMember, FavoriteRequest request) {
         Station source = stationRepository.findById(request.getSource()).orElseThrow(RuntimeException::new);
         Station target = stationRepository.findById(request.getTarget()).orElseThrow(RuntimeException::new);
-        Favorite favorite = favoriteRepository.save(new Favorite(userDetail.getId(), source, target));
+        Favorite favorite = favoriteRepository.save(new Favorite(loginMember.getId(), source, target));
         return FavoriteResponse.of(favorite);
     }
 
-    public List<FavoriteResponse> findFavorites(UserDetail loginMember) {
+    public List<FavoriteResponse> findFavorites(LoginMember loginMember) {
         List<Favorite> favorites = favoriteRepository.findAll();
 
         return favorites.stream()
