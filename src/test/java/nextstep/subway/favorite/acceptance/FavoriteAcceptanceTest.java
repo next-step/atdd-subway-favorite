11318fe1355d1e54c5b0b16a8d1e8851211514a7
SC <pch838811@gmail.com>
2021-03-27 20:10:23 +0900

    test: 즐겨찾기 인수테스트 시나리오 작성

---
+++ src/test/java/nextstep/subway/favorite/acceptance/FavoriteAcceptanceTest.java
@@ -0,0 +1,34 @@
+package nextstep.subway.favorite.acceptance;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.subway.AcceptanceTest;
+
+@DisplayName("즐겨찾기를 관리한다")
+public class FavoriteAcceptanceTest extends AcceptanceTest {
+
+	@DisplayName("즐겨찾기 생성을 요청한다")
+	@Test
+	void createFavorites() {
+
+	}
+
+	@DisplayName("즐겨찾기 목록을 조회한다")
+	@Test
+	void getFavorites() {
+
+	}
+
+	@DisplayName("즐겨찾기 삭제를 요청한다")
+	@Test
+	void deleteFavorites() {
+
+	}
+
+	@DisplayName("즐겨찾기 정보를 관리한다")
+	@Test
+	void manageFavorites() {
+
+	}
+}
