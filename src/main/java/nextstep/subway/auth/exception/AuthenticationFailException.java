69e67a130ace037366aaf9c7874821cc0367bbf0
dbeod2 <darung.0211@gmail.com>
2021-03-28 12:39:48 +0900

    refactor : 인증관련 exception ResponseStatus추가

--- src/main/java/nextstep/subway/auth/exception/AuthenticationFailException.java
+++ src/main/java/nextstep/subway/auth/exception/AuthenticationFailException.java
@@ -1,5 +1,10 @@
 package nextstep.subway.auth.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class AuthenticationFailException extends RuntimeException {
     public AuthenticationFailException() {
         super("인증이 올바르지 않습니다.");
