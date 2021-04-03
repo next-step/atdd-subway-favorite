980d2e6779c50baf5b80c946c0d6dff80ffd8759
Admin <admin@BR-MacBook.local>
2021-04-03 23:06:47 +0900

    Step3:피드백 반영

--- src/main/java/nextstep/subway/favorite/exception/NoContentsException.java
+++
@@ -1,7 +0,0 @@
-package nextstep.subway.favorite.exception;
-
-public class NoContentsException extends RuntimeException{
-    public NoContentsException() {
-        super("삭제할 즐겨찾기가 없습니다.");
-    }
-}
