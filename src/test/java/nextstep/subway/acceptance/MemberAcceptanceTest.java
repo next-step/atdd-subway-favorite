6f7d8a8a7c92786db8938c6a5e944bcb6c3fa965
00hongjun <00hongjun@gmail.com>
2022-02-05 23:32:52 +0900

    <test> 회원 정보 관리 테스트 코드 추가

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -1,15 +1,23 @@
 package nextstep.subway.acceptance;
 
+import static nextstep.subway.acceptance.MemberSteps.회원_삭제_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_삭제됨;
+import static nextstep.subway.acceptance.MemberSteps.회원_생성_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_생성됨;
+import static nextstep.subway.acceptance.MemberSteps.회원_수정됨;
+import static nextstep.subway.acceptance.MemberSteps.회원_정보_수정_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회됨;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import static nextstep.subway.acceptance.MemberSteps.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
 class MemberAcceptanceTest extends AcceptanceTest {
+
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
     public static final int AGE = 20;
@@ -64,13 +72,43 @@ class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
+    /**
+     * when 회원을 생성 요청한다
+     * then 회원 생성 응답을 받는다
+     * when 회원을 조회 요청한다
+     * then 회원 정보를 응답을 받는다
+     * when 회원 정보를 수정 요청한다
+     * then 회원 수정 완료 응답을 받는다
+     * when 회원 삭제를 요청한다
+     * then 회원 삭제 완료 응답을 받는다
+     */
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // when
+        ExtractableResponse<Response> 회원_생성_응답 = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+        // then
+        회원_생성됨(회원_생성_응답);
+
+        // when
+        ExtractableResponse<Response> 회원_조회_응답 = 회원_정보_조회_요청(회원_생성_응답);
+        // then
+        회원_정보_조회됨(회원_조회_응답, EMAIL, AGE);
+
+        // when
+        ExtractableResponse<Response> 회원_수정_응답 = 회원_정보_수정_요청(회원_생성_응답, "new" + EMAIL, "new" + PASSWORD, AGE);
+        // then
+        회원_수정됨(회원_수정_응답);
+
+        // when
+        ExtractableResponse<Response> 회원_삭제_응답 = 회원_삭제_요청(회원_생성_응답);
+        // then
+        회원_삭제됨(회원_삭제_응답);
     }
 
     @DisplayName("나의 정보를 관리한다.")
     @Test
     void manageMyInfo() {
     }
+
 }
