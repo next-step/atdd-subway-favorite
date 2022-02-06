d235197492e79ad1c2607fe078259a560baa195a
00hongjun <00hongjun@gmail.com>
2022-02-06 17:50:22 +0900

    <feat> TokenAuthenticationInterceptor2 이용하여 토큰 컨버터 로직 리팩토링 진행

--- src/main/java/nextstep/auth/AuthConfig.java
+++ src/main/java/nextstep/auth/AuthConfig.java
@@ -2,7 +2,8 @@ package nextstep.auth;
 
 import nextstep.auth.authentication.SessionAuthenticationConverter;
 import nextstep.auth.authentication.SessionAuthenticationInterceptor2;
-import nextstep.auth.authentication.TokenAuthenticationInterceptor;
+import nextstep.auth.authentication.TokenAuthenticationConverter;
+import nextstep.auth.authentication.TokenAuthenticationInterceptor2;
 import nextstep.auth.authorization.AuthenticationPrincipalArgumentResolver;
 import nextstep.auth.authorization.SessionSecurityContextPersistenceInterceptor;
 import nextstep.auth.authorization.TokenSecurityContextPersistenceInterceptor;
@@ -29,7 +30,8 @@ public class AuthConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new SessionAuthenticationInterceptor(userDetailsService)).addPathPatterns("/login/session");
         registry.addInterceptor(new SessionAuthenticationInterceptor2(userDetailsService, new SessionAuthenticationConverter())).addPathPatterns("/login/session");
-        registry.addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider)).addPathPatterns("/login/token");
+//        registry.addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider)).addPathPatterns("/login/token");
+        registry.addInterceptor(new TokenAuthenticationInterceptor2(userDetailsService, jwtTokenProvider, new TokenAuthenticationConverter())).addPathPatterns("/login/token");
         registry.addInterceptor(new SessionSecurityContextPersistenceInterceptor());
         registry.addInterceptor(new TokenSecurityContextPersistenceInterceptor(jwtTokenProvider));
     }
---
+++ src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor2.java
@@ -0,0 +1,71 @@
+package nextstep.auth.authentication;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.token.JwtTokenProvider;
+import nextstep.auth.token.TokenResponse;
+import nextstep.member.application.CustomUserDetailsService;
+import nextstep.member.domain.LoginMember;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TokenAuthenticationInterceptor2 implements HandlerInterceptor {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationConverter authenticationConverter;
+
+    public TokenAuthenticationInterceptor2(CustomUserDetailsService customUserDetailsService,
+                                           JwtTokenProvider jwtTokenProvider,
+                                           AuthenticationConverter authenticationConverter) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationConverter = authenticationConverter;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        AuthenticationToken authenticationToken = authenticationConverter.convert(request);
+        Authentication authentication = authenticate(authenticationToken);
+
+        String token = token(authentication);
+        TokenResponse tokenResponse = new TokenResponse(token);
+
+        String responseToClient = new ObjectMapper().writeValueAsString(tokenResponse);
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getOutputStream().print(responseToClient);
+
+        return false;
+    }
+    public Authentication authenticate(AuthenticationToken authenticationToken) {
+        String principal = authenticationToken.getPrincipal();
+        LoginMember loginMember = customUserDetailsService.loadUserByUsername(principal);
+        checkAuthentication(loginMember, authenticationToken);
+
+        return new Authentication(loginMember);
+    }
+
+    private String token(Authentication authentication) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String payload = mapper.writeValueAsString(authentication.getPrincipal());
+
+        return jwtTokenProvider.createToken(payload);
+    }
+
+    private void checkAuthentication(LoginMember userDetails, AuthenticationToken token) {
+        if (userDetails == null) {
+            throw new AuthenticationException();
+        }
+
+        if (!userDetails.checkPassword(token.getCredentials())) {
+            throw new AuthenticationException();
+        }
+    }
+
+}
