ae741a53741fef25207f53d550c88ed3519b0928
JoJo <buyongjo@gmail.com>
2022-02-07 11:32:19 +0900

    feat: 세션 기반 내 정보 조회 구현

--- src/main/java/nextstep/member/ui/MemberController.java
+++ src/main/java/nextstep/member/ui/MemberController.java
@@ -1,8 +1,10 @@
 package nextstep.member.ui;
 
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.MemberService;
 import nextstep.member.application.dto.MemberRequest;
 import nextstep.member.application.dto.MemberResponse;
+import nextstep.member.domain.LoginMember;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,7 +44,9 @@ public class MemberController {
 
     @GetMapping("/members/me")
     public ResponseEntity<MemberResponse> findMemberOfMine() {
-        return ResponseEntity.ok().build();
+        LoginMember loginMember = (LoginMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        MemberResponse member = memberService.findMember(loginMember.getId());
+        return ResponseEntity.ok().body(member);
     }
 
     @PutMapping("/members/me")
