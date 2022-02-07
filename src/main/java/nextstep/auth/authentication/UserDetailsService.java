0686d47a37a01cd65ff6aefa97c36838ed4f029d
Hero <icraft2170@gmail.com>
2022-02-07 22:22:33 +0900

    (양방향 연관관계 삭제)  피드백 반영 및 레거시 코드 삭제

--- src/main/java/nextstep/auth/authentication/UserDetailsService.java
+++ src/main/java/nextstep/auth/authentication/UserDetailsService.java
@@ -1,7 +1,5 @@
 package nextstep.auth.authentication;
 
-import nextstep.domain.member.domain.LoginMemberImpl;
-
 public interface UserDetailsService {
-    LoginMemberImpl loadUserByUsername(String email);
+    LoginMember loadUserByUsername(String email);
 }
