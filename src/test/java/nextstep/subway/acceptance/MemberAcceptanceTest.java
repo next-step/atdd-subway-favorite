8d8bdb9ef2a25b4b49ed175a862368bc1b74fd46
soo <yunsoo0510@daum.net>
2022-02-08 05:19:04 +0900

    [Test] manageMyInfo 인수 통합 테스트 작성

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -4,10 +4,8 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "email@email.com";
@@ -96,5 +94,29 @@ class MemberAcceptanceTest extends AcceptanceTest {
     @DisplayName("나의 정보를 관리한다.")
     @Test
     void manageMyInfo() {
+        // when
+        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        회원_생성됨(createResponse);
+
+        // when
+        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
+        ExtractableResponse<Response> response = 내_회원_정보_조회_요청(accessToken);
+
+        // then
+        회원_정보_조회됨(response, EMAIL, AGE);
+
+        // when
+        ExtractableResponse<Response> updatedResponse = 내_회원_정보_수정_요청(accessToken, NEW + EMAIL, NEW + PASSWORD, AGE);
+
+        // then
+        회원_정보_수정됨(updatedResponse);
+
+        // when
+        ExtractableResponse<Response> deletedResponse = 내_회원_정보_삭제_요청(accessToken);
+
+        // then
+        회원_삭제됨(deletedResponse);
     }
 }
