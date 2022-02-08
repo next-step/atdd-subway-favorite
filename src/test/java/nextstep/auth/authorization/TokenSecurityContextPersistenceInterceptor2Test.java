3d3a6ecadfe2fee2641963a7490100e79ebad97c
00hongjun <00hongjun@gmail.com>
2022-02-08 22:55:29 +0900

    <refactor> TokenSecurityContextPersistenceInterceptor2 추상화, 테스트 코드 추가

--- src/test/java/nextstep/auth/authorization/TokenSecurityContextPersistenceInterceptor2Test.java
+++ src/test/java/nextstep/auth/authorization/TokenSecurityContextPersistenceInterceptor2Test.java
@@ -2,30 +2,44 @@ package nextstep.auth.authorization;
 
 import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContextHolder;
+import nextstep.auth.token.JwtTokenProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import static nextstep.auth.authorization.step.SecurityContextStep.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 
-@DisplayName("session SecurityContext 테스트")
-class SessionSecurityContextPersistenceInterceptorTest {
+@DisplayName("token SecurityContext 테스트")
+@ExtendWith(MockitoExtension.class)
+class TokenSecurityContextPersistenceInterceptor2Test {
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
 
     @BeforeEach
     void setUp() {
         SecurityContextHolder.clearContext();
     }
 
-    @DisplayName("session을 이용한 인증, SecurityContextHolder 정보를 세팅한다")
+    @DisplayName("token 이용한 인증, SecurityContextHolder 정보를 세팅한다")
     @Test
     void preHandle() throws Exception {
         // given
-        MockHttpServletRequest 요청 = session_인증_요청_mock();
+        MockHttpServletRequest 요청 = token_인증_요청_mock();
         MockHttpServletResponse 응답 = 인증_응답_mock();
-        SessionSecurityContextPersistenceInterceptor interceptor = new SessionSecurityContextPersistenceInterceptor();
+        TokenSecurityContextPersistenceInterceptor2 interceptor = new TokenSecurityContextPersistenceInterceptor2(jwtTokenProvider);
+        Mockito.when(jwtTokenProvider.validateToken(anyString()))
+                .thenReturn(true);
+        Mockito.when(jwtTokenProvider.getPayload(anyString()))
+                .thenReturn(로그인멤버_json());
 
         // when
         boolean preHandle = interceptor.preHandle(요청, 응답, new Object());
@@ -37,13 +51,13 @@ class SessionSecurityContextPersistenceInterceptorTest {
         assertThat(preHandle).isTrue();
     }
 
-    @DisplayName("session에 인증 정보가 없는경우 ")
+    @DisplayName("token에 인증 정보가 없는 경우")
     @Test
     void preHandle_fail() throws Exception {
         // given
-        MockHttpServletRequest 요청 = session_인증정보없음_요청_mock();
+        MockHttpServletRequest 요청 = token_인증정보없음_요청_mock();
         MockHttpServletResponse 응답 = 인증_응답_mock();
-        SessionSecurityContextPersistenceInterceptor interceptor = new SessionSecurityContextPersistenceInterceptor();
+        TokenSecurityContextPersistenceInterceptor2 interceptor = new TokenSecurityContextPersistenceInterceptor2(jwtTokenProvider);
 
         // when
         boolean preHandle = interceptor.preHandle(요청, 응답, new Object());
--- src/test/java/nextstep/auth/authorization/step/SecurityContextStep.java
+++ src/test/java/nextstep/auth/authorization/step/SecurityContextStep.java
@@ -1,9 +1,9 @@
 package nextstep.auth.authorization.step;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContext;
-import nextstep.auth.token.TokenRequest;
 import nextstep.member.domain.LoginMember;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -13,18 +13,23 @@ import java.io.IOException;
 
 public class SecurityContextStep {
 
+    public static final String AUTHORIZATION = "Authorization";
     public static final String SPRING_SECURITY_CONTEXT_KEY = "SECURITY_CONTEXT";
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
     public static final int AGE = 20;
-    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
+    public static final String JWT_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
     private static final LoginMember LOGIN_MEMBER = new LoginMember(1L, EMAIL, PASSWORD, AGE);
 
     public static MockHttpServletRequest token_인증_요청_mock() throws IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        request.setContent(new ObjectMapper().writeValueAsString(tokenRequest).getBytes());
+        request.addHeader(AUTHORIZATION, JWT_TOKEN);
 
+        return request;
+    }
+
+    public static MockHttpServletRequest token_인증정보없음_요청_mock() throws IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
         return request;
     }
 
@@ -46,6 +51,11 @@ public class SecurityContextStep {
 
     public static MockHttpServletResponse 인증_응답_mock() {
         return new MockHttpServletResponse();
+    }
+
+    public static String 로그인멤버_json() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(LOGIN_MEMBER);
     }
 
     private static SecurityContext securityContext() {
