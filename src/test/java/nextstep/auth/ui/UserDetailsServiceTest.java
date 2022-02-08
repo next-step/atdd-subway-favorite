ef025160d8940e18f1bb91eedaccb5156653aacf
김우재 <kwj1270@naver.com>
2022-02-08 21:27:04 +0900

    test: UserDetailsServiceTest, 식별자를 통해 유저 정보를 반환 받는다

--- src/test/java/nextstep/auth/ui/UserDetailsServiceTest.java
+++ src/test/java/nextstep/auth/ui/UserDetailsServiceTest.java
@@ -1,11 +1,27 @@
 package nextstep.auth.ui;
 
+import nextstep.member.domain.LoginMember;
 import org.junit.jupiter.api.DisplayName;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @DisplayName("유저 정보 조회 서비스(UserDetailsService)")
 class UserDetailsServiceTest {
 
-    @DisplayName("")
-    void tset() {
+    @DisplayName("식별자를 통해 유저 정보를 반환 받는다")
+    void loadUserByUsername() {
+        final Long id = 1L;
+        final String email = "email@email.com";
+        final String password = "password";
+        final int age = 1;
+
+        final UserDetails userDetails = new LoginMember(id, email, password, age);
+        final UserDetailsService userDetailsService = mock(UserDetailsService.class);
+
+        given(userDetailsService.loadUserByUsername(email)).willReturn(userDetails);
+
+        final UserDetails actual = userDetailsService.loadUserByUsername(email);
+        assertThat(autual).isEqualTo(userDetails);
     }
 }
