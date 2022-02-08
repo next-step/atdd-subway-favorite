a2523fbff160c4672dc40ea4d308facf5582ce7d
김우재 <kwj1270@naver.com>
2022-02-08 21:30:30 +0900

    refactor: CustomUserDetailsService, UserDetailsService 추상화로 auth -> member 의존성 방향 제거

--- src/main/java/nextstep/member/application/CustomUserDetailsService.java
+++ src/main/java/nextstep/member/application/CustomUserDetailsService.java
@@ -1,20 +1,24 @@
 package nextstep.member.application;
 
+import nextstep.auth.ui.UserDetailsService;
 import nextstep.member.domain.LoginMember;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
+@Transactional(readOnly = true)
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private MemberRepository memberRepository;
 
     public CustomUserDetailsService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Override
     public LoginMember loadUserByUsername(String email) {
         Member member = memberRepository.findByEmail(email).orElseThrow(EntityNotFoundException::new);
         return LoginMember.of(member);
