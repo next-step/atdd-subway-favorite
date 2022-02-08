9b0746cac1e84e327c327abdf50610251b655666
kimuno89 <kimunoya@gmail.com>
2022-02-09 00:06:48 +0900

    feat: AuthenticationToken convert 구현

--- src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor.java
+++ src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor.java
@@ -3,6 +3,7 @@ package nextstep.auth.authentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.token.JwtTokenProvider;
+import nextstep.auth.token.TokenRequest;
 import nextstep.auth.token.TokenResponse;
 import nextstep.member.application.CustomUserDetailsService;
 import org.springframework.http.MediaType;
@@ -11,8 +12,13 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TokenAuthenticationInterceptor implements HandlerInterceptor {
+    private static final String USERNAME_FIELD = "email";
+    private static final String PASSWORD_FIELD = "password";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private CustomUserDetailsService customUserDetailsService;
     private JwtTokenProvider jwtTokenProvider;
@@ -30,7 +36,7 @@ public class TokenAuthenticationInterceptor implements HandlerInterceptor {
         // TODO: authentication으로 TokenResponse 추출하기
         TokenResponse tokenResponse = null;
 
-        String responseToClient = new ObjectMapper().writeValueAsString(tokenResponse);
+        String responseToClient = OBJECT_MAPPER.writeValueAsString(tokenResponse);
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getOutputStream().print(responseToClient);
@@ -39,11 +45,17 @@ public class TokenAuthenticationInterceptor implements HandlerInterceptor {
     }
 
     public AuthenticationToken convert(HttpServletRequest request) throws IOException {
-        // TODO: request에서 AuthenticationToken 객체 생성하기
-        String principal = "";
-        String credentials = "";
+        TokenRequest tokenRequest = OBJECT_MAPPER.readValue(getRequestBody(request), TokenRequest.class);
+        String principal = tokenRequest.getEmail();
+        String credentials = tokenRequest.getPassword();
 
         return new AuthenticationToken(principal, credentials);
+    }
+
+    private String getRequestBody(HttpServletRequest request) throws IOException {
+        return request.getReader()
+            .lines()
+            .collect(Collectors.joining(System.lineSeparator()));
     }
 
     public Authentication authenticate(AuthenticationToken authenticationToken) {
