b779079c52d3a81e08d12555bbb71346e4c6ebbe
sombrero104 <sombrero.khy@gmail.com>
2022-02-09 19:00:51 +0900

    feat: AuthenticationConverter 구현

--- src/main/java/nextstep/auth/authentication/converter/TokenAuthenticationConverter.java
+++ src/main/java/nextstep/auth/authentication/converter/TokenAuthenticationConverter.java
@@ -1,14 +1,47 @@
 package nextstep.auth.authentication.converter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.auth.authentication.AuthenticationToken;
+import nextstep.auth.token.TokenRequest;
+import nextstep.exception.JsonDeserializeFailed;
+import org.springframework.http.HttpMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class TokenAuthenticationConverter implements AuthenticationConverter {
 
+    private ObjectMapper objectMapper;
+
+    public TokenAuthenticationConverter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
-    public AuthenticationToken convert(HttpServletRequest request) {
-        return null;
+    public AuthenticationToken convert(HttpServletRequest request) throws IOException {
+        String principal, credentials;
+        try {
+            TokenRequest tokenRequest = objectMapper.readValue(extractPostRequestBody(request), TokenRequest.class);
+            principal = tokenRequest.getEmail();
+            credentials = tokenRequest.getPassword();
+        } catch (IOException ioe) {
+            throw new JsonDeserializeFailed(ioe.getMessage());
+        }
+        return new AuthenticationToken(principal, credentials);
+    }
+
+    private String extractPostRequestBody(HttpServletRequest request) {
+        if (HttpMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
+            Scanner s = null;
+            try {
+                s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s.hasNext() ? s.next() : "";
+        }
+        return "";
     }
 
 }
--- src/test/java/nextstep/subway/unit/auth/TokenAuthenticationConverterTest.java
+++ src/test/java/nextstep/subway/unit/auth/TokenAuthenticationConverterTest.java
@@ -6,21 +6,33 @@ import nextstep.auth.authentication.converter.AuthenticationConverter;
 import nextstep.auth.authentication.converter.TokenAuthenticationConverter;
 import nextstep.auth.token.TokenRequest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpMethod;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class TokenAuthenticationConverterTest {
+
+    @Mock
+    private ObjectMapper objectMapper;
 
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
 
     @Test
     void convert() throws IOException {
-        AuthenticationConverter converter = new TokenAuthenticationConverter();
+        TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
+        when(objectMapper.readValue(anyString(), eq(TokenRequest.class))).thenReturn(tokenRequest);
+        AuthenticationConverter converter = new TokenAuthenticationConverter(objectMapper);
         MockHttpServletRequest request = createMockRequest();
         AuthenticationToken authenticationToken = converter.convert(request);
 
