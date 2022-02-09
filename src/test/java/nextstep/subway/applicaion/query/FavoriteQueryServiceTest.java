c72eebf2ea244cd66d02d3387df4d653a9882548
00hongjun <00hongjun@gmail.com>
2022-02-09 22:18:02 +0900

    <test> 즐겨찾기 조회 서비스 테스트 코드 추가

---
+++ src/test/java/nextstep/subway/applicaion/query/FavoriteQueryServiceTest.java
@@ -0,0 +1,57 @@
+package nextstep.subway.applicaion.query;
+
+import nextstep.subway.applicaion.dto.FavoriteResponse;
+import nextstep.subway.domain.Favorite;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.repository.FavoriteRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("즐겨찾기 조회 서비스")
+@SpringBootTest
+@Transactional
+class FavoriteQueryServiceTest {
+
+    @Autowired
+    private StationRepository stationRepository;
+    @Autowired
+    private FavoriteRepository favoriteRepository;
+
+    private FavoriteQueryService service;
+    private Station 강남역;
+    private Station 판교역;
+    private long memberId = 1L;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        판교역 = new Station("판교역");
+
+        stationRepository.save(강남역);
+        stationRepository.save(판교역);
+        favoriteRepository.save(Favorite.of(memberId, 강남역, 판교역));
+
+        service = new FavoriteQueryService(favoriteRepository);
+    }
+
+    @DisplayName("즐겨찾기 조회")
+    @Test
+    void findFavorite() {
+        // when
+        List<FavoriteResponse> favorite = service.findFavorite(memberId);
+
+        // then
+        assertThat(favorite.get(0).getSource().getName()).isEqualTo(강남역.getName());
+        assertThat(favorite.get(0).getTarget().getName()).isEqualTo(판교역.getName());
+    }
+
+}
