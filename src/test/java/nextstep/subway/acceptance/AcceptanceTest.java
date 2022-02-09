3e5a97b319f7b4ab5f911d8d7fed4064c3ac89df
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-02-09 23:48:50 +0900

    feat : 이전 코드 옮기기

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -1,11 +1,16 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
@@ -19,5 +24,24 @@ public class AcceptanceTest {
     public void setUp() {
         RestAssured.port = port;
         databaseCleanup.execute();
+    }
+
+    public void 상태_값_검사(ExtractableResponse<Response> 응답_값, HttpStatus 상태_코드) {
+        assertThat(응답_값.statusCode()).isEqualTo(상태_코드.value());
+    }
+
+    public void 리스트_값_검사(ExtractableResponse<Response> 응답_값, String 예상_키, Object... 예상_값) {
+        for (Object 값 : 예상_값) {
+            assertThat(응답_값.jsonPath().getList(예상_키)).contains(값);
+        }
+    }
+
+    public void 단일_값_검사(ExtractableResponse<Response> 응답_값, String 예상_키, String 예상_값) {
+        Object result = 응답_값.jsonPath().get(예상_키);
+        assertThat(String.valueOf(result)).isEqualTo(예상_값);
+    }
+
+    public void 예외_검사(ExtractableResponse<Response> 응답_값, String 예외_메시지) {
+        assertThat(응답_값.jsonPath().getString("message")).isEqualTo(예외_메시지);
     }
 }
