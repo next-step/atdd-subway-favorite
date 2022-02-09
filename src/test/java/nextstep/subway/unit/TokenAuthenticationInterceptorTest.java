57e7901a83833bdf07fe3ecc4e628031e06fdba5
YisooKim <gashe7@gmail.com>
2022-02-09 23:18:04 +0900

    Feat:Implement tokenAuthenticationInterceptor

--- src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
+++ src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
@@ -1,27 +1,99 @@
 package nextstep.subway.unit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.auth.authentication.AuthenticationToken;
+import nextstep.auth.authentication.TokenAuthenticationInterceptor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.token.JwtTokenProvider;
 import nextstep.auth.token.TokenRequest;
+import nextstep.auth.token.TokenResponse;
+import nextstep.member.application.CustomUserDetailsService;
+import nextstep.member.domain.LoginMember;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TokenAuthenticationInterceptorTest {
+    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
-    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
+    @Mock
+    private CustomUserDetailsService customUserDetailsService;
 
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @InjectMocks
+    private TokenAuthenticationInterceptor tokenAuthenticationInterceptor;
+
+    @DisplayName("이메일/비밀번호가 담긴 토큰을 반환한다.")
     @Test
     void convert() throws IOException {
+        // given
+        MockHttpServletRequest request = createMockRequest();
+
+        // when
+        AuthenticationToken token = tokenAuthenticationInterceptor.convert(request);
+
+        // then
+        assertThat(token.getPrincipal()).isEqualTo(EMAIL);
+        assertThat(token.getCredentials()).isEqualTo(PASSWORD);
     }
 
+    @DisplayName("회원 인증한다.")
     @Test
     void authenticate() {
+        // given
+        Member member = new Member(EMAIL, PASSWORD, 10);
+        LoginMember loginMember = LoginMember.of(member);
+        when(customUserDetailsService.loadUserByUsername(EMAIL)).thenReturn(loginMember);
+        AuthenticationToken token = new AuthenticationToken(EMAIL, PASSWORD);
+
+        // when
+        Authentication authentication = tokenAuthenticationInterceptor.authenticate(token);
+
+        // then
+        assertThat(authentication.getPrincipal()).isEqualTo(loginMember);
     }
 
+    @DisplayName("이메일/비밀번호로 인증토큰을 얻는다.")
     @Test
     void preHandle() throws IOException {
+        // given
+        MockHttpServletRequest request = createMockRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        LoginMember loginMember = LoginMember.of(new Member(EMAIL, PASSWORD, 10));
+        when(customUserDetailsService.loadUserByUsername(EMAIL)).thenReturn(loginMember);
+        when(jwtTokenProvider.createToken(any())).thenReturn(JWT_TOKEN);
+
+        // when
+        tokenAuthenticationInterceptor.preHandle(request, response, null);
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+        assertThat(response.getContentAsString()).isEqualTo(new ObjectMapper().writeValueAsString(new TokenResponse(JWT_TOKEN)));
     }
 
     private MockHttpServletRequest createMockRequest() throws IOException {
