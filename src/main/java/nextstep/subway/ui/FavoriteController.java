cb6cfc6a2f53223d527648ff1b0cb8f67584f396
00hongjun <00hongjun@gmail.com>
2022-02-09 21:37:28 +0900

    <feat> 즐겨찾기 조회 controller 추가

--- src/main/java/nextstep/subway/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/ui/FavoriteController.java
@@ -3,7 +3,9 @@ package nextstep.subway.ui;
 
 import nextstep.auth.authorization.AuthenticationPrincipal;
 import nextstep.member.domain.LoginMember;
+import nextstep.subway.applicaion.dto.FavoriteResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,12 @@ public class FavoriteController {
     public ResponseEntity<Void> createFavorite(@AuthenticationPrincipal LoginMember loginMember) {
         return ResponseEntity.created(URI.create("/favorites"))
                 .build();
+    }
+
+    @GetMapping
+    public ResponseEntity<FavoriteResponse> findFavorite(@AuthenticationPrincipal LoginMember loginMember) {
+        FavoriteResponse response = null;
+        return ResponseEntity.ok(response);
     }
 
 }
