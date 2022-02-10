fc23daae29bc1e202ea1e2207bc374b08235a6cc
yongju <odetsuo@gmail.com>
2022-02-10 21:04:31 +0900

    feat: 토큰 기반 내 정보 수정

--- src/test/java/nextstep/subway/acceptance/MemberSteps.java
+++ src/test/java/nextstep/subway/acceptance/MemberSteps.java
@@ -102,6 +102,24 @@ public class MemberSteps {
                 .extract();
     }
 
+    public static ExtractableResponse<Response> 내_회원_정보_수정_요청(String accessToken, String email, String password, Integer age) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        params.put("age", age + "");
+
+        return RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().put("/members/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+
     public static void 회원_생성됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
