40e7a691fc0700975f21ff36e802d60c8bbe43c0
yongju <odetsuo@gmail.com>
2022-02-11 16:04:50 +0900

    refactor: SecurityContextInterceptor 추상화

---
+++ src/main/java/nextstep/auth/authorization/SecurityContextInterceptor.java
@@ -0,0 +1,36 @@
+package nextstep.auth.authorization;
+
+import nextstep.auth.context.SecurityContext;
+import nextstep.auth.context.SecurityContextHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class SecurityContextInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            return true;
+        }
+
+        SecurityContext securityContext = getSecurityContext(request);
+
+        if (securityContext != null) {
+            SecurityContextHolder.setContext(securityContext);
+        }
+
+        return true;
+    }
+
+    protected abstract SecurityContext getSecurityContext(HttpServletRequest request);
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) {
+        SecurityContextHolder.clearContext();
+    }
+}
