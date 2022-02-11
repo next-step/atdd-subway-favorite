a0a0a24b132eb220baa88a74555ccc74bb261e98
mandykr <hongseokii92@gmail.com>
2022-02-11 18:09:14 +0900

    test: SecurityContextInterceptor 추상화 세션 기반 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/unit/SessionSecurityContextPersistenceInterceptorTest.java
@@ -0,0 +1,57 @@
+package nextstep.subway.unit;
+
+import nextstep.auth.authorization.SecurityContextInterceptor;
+import nextstep.auth.authorization.SessionSecurityContextPersistenceInterceptor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.IOException;
+
+import static nextstep.auth.context.SecurityContextHolder.*;
+import static nextstep.subway.unit.AuthFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SessionSecurityContextPersistenceInterceptorTest {
+    SecurityContextInterceptor interceptor;
+    MockHttpServletRequest request;
+    MockHttpServletResponse response;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        interceptor = new SessionSecurityContextPersistenceInterceptor();
+        request = createSessionMockRequest();
+        response = createMockResponse();
+    }
+
+    @Test
+    void afterCompletion() throws Exception {
+        // given
+        SecurityContext securityContext = new SecurityContext(createAuthentication());
+        setContext(securityContext);
+
+        // when
+        interceptor.afterCompletion(request, response, new Object(), new RuntimeException());
+
+        // then
+        Authentication authentication = getContext().getAuthentication();
+        assertThat(authentication).isNull();
+    }
+
+    @Test
+    void preHandle() throws Exception {
+        // given
+        SecurityContext securityContext = new SecurityContext(createAuthentication());
+        request.getSession().setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
+
+        // when
+        interceptor.preHandle(request, response, new Object());
+
+        // then
+        Authentication authentication = getContext().getAuthentication();
+        assertThat(authentication).isNotNull();
+    }
+}
