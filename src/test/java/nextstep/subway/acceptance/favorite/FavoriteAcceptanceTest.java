6771f9c79183705608b62ad7f089062385c0ab02
darkant99 <darkant99@naver.com>
2022-02-11 19:39:06 +0900

    [TEST] 즐겨찾기 목록 조회

--- src/test/java/nextstep/subway/acceptance/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/favorite/FavoriteAcceptanceTest.java
@@ -1,59 +1,57 @@
 package nextstep.subway.acceptance.favorite;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static nextstep.subway.acceptance.favorite.FavoriteStep.*;
+import static nextstep.subway.acceptance.member.MemberSteps.*;
+import static nextstep.subway.acceptance.station.StationSteps.*;
+import static org.assertj.core.api.Assertions.*;
 
-import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.favorite.application.dto.FavoriteRequest;
 import nextstep.subway.acceptance.AcceptanceTest;
 import nextstep.subway.acceptance.line.LineSteps;
-import nextstep.subway.acceptance.member.MemberSteps;
-import nextstep.subway.acceptance.station.StationSteps;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 @DisplayName("즐겨찾기 관리 기능")
 public class FavoriteAcceptanceTest extends AcceptanceTest {
     /**
-     * Given 로그인이 되어있고
-     * And   출발, 도착 지하철 역이 등록 되어있고
-     * And   지하철 노선이 등록되어 있고
-     * And   지하철 노선에 지하철 역이 등록 되어있을때
-     * When  즐겨찾기를 추가 하면
-     * Then  즐겨찾기가 추가 된다.
+     *
+     * Feature: 즐겨찾기를 관리 한다.
+     *
+     *   Scenario: 즐겨찾기 관리
+     *      Given 로그인이 되어있고
+     *      And 출발, 도착 지하철 역이 등록 되어있고
+     *      And 지하철 노선이 등록되어 있고
+     *      And 지하철 노선에 지하철 역이 등록 되어있다.
+     *
+     *      When 즐겨찾기 추가를 요청
+     *      Then 즐겨찾기 추가됨
+     *      When 즐겨찾기 목록 조회 요청
+     *      Then 즐겨찾기 목록 조회됨
+     *
+     *
      * */
-    @DisplayName("즐겨찾기 추가")
+    @DisplayName("즐겨찾기 관리")
     @Test
     void addFavorite() {
         // given
-        String accessToken = MemberSteps.회원_생성_하고_로그인_됨("username@username.com", "password1234", 10);
-        Long 교대역 = StationSteps.지하철역_생성_요청_하고_ID_반환("교대역");
-        Long 양재역 = StationSteps.지하철역_생성_요청_하고_ID_반환("양재역");
+        String accessToken = 회원_생성_하고_로그인_됨("username@username.com", "password1234", 10);
+        Long 교대역 = 지하철역_생성_요청_하고_ID_반환("교대역");
+        Long 양재역 = 지하철역_생성_요청_하고_ID_반환("양재역");
         LineSteps.지하철_노선_생성_요청(LineSteps.createLineCreateParams(교대역, 양재역));
 
-        // when
-        FavoriteRequest request = new FavoriteRequest(교대역, 양재역);
-        ExtractableResponse<Response> response = 즐겨찾기_추가_요청(accessToken, request);
+        // 추가
+        ExtractableResponse<Response> createResponse = 즐겨찾기_추가_요청(accessToken, new FavoriteRequest(교대역, 양재역));
+        즐겨찾기_추가_요청_됨(createResponse);
 
-        // then
-        즐겨찾기_추가_요청_됨(response);
-    }
+        // 목록 조회
+        ExtractableResponse<Response> getResponse = 즐겨찾기_목록_조회_요청(accessToken);
+        assertThat(getResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(getResponse.jsonPath().getList("id")).containsExactly(교대역, 양재역);
 
-    private ExtractableResponse<Response> 즐겨찾기_추가_요청(String accessToken, FavoriteRequest request) {
-        return RestAssured.given().log().all()
-                          .auth().oauth2(accessToken)
-                          .body(request)
-                          .contentType(MediaType.APPLICATION_JSON_VALUE)
-                          .when().post("/favorites")
-                          .then().log().all()
-                          .extract();
-    }
 
-    private void 즐겨찾기_추가_요청_됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("location")).isNotBlank();
     }
 }
--- src/test/java/nextstep/subway/acceptance/favorite/FavoriteStep.java
+++ src/test/java/nextstep/subway/acceptance/favorite/FavoriteStep.java
@@ -1,5 +1,38 @@
 package nextstep.subway.acceptance.favorite;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.favorite.application.dto.FavoriteRequest;
+
 public class FavoriteStep {
     private FavoriteStep() {}
+
+    public static ExtractableResponse<Response> 즐겨찾기_추가_요청(String accessToken, FavoriteRequest request) {
+        return RestAssured.given().log().all()
+                          .auth().oauth2(accessToken)
+                          .body(request)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .when().post("/favorites")
+                          .then().log().all()
+                          .extract();
+    }
+
+    public static void 즐겨찾기_추가_요청_됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("location")).isNotBlank();
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_목록_조회_요청(String accessToken) {
+        return RestAssured.given().log().all()
+                          .auth().oauth2(accessToken)
+                          .when().get("/favorites")
+                          .then().log().all()
+                          .extract();
+    }
 }
