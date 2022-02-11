bca6de726bd4bb728980a176679e41c5cdbd252c
yongju <odetsuo@gmail.com>
2022-02-11 23:38:56 +0900

    feat: 비 로그인 상태에서 내 정보 관리 예외

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -97,4 +97,21 @@ class MemberAcceptanceTest extends AcceptanceTest {
         회원_정보_삭제됨(내_회원_삭제_응답);
 
     }
+
+    @DisplayName("비로그인 상태에서 내 정보 관리 예외")
+    @Test
+    void manageMyInfoUnauthorizedException() {
+        //given
+        String 유효하지_않은_토큰 = "invalid token";
+
+        ExtractableResponse<Response> 내_회원_정보_조회_응답 = 내_회원_정보_조회_요청(유효하지_않은_토큰);
+        권한_없음(내_회원_정보_조회_응답);
+
+        ExtractableResponse<Response> 내_회원_정보_수정_응답 = 내_회원_정보_수정_요청(유효하지_않은_토큰, "e@mail.com", PASSWORD, AGE);
+        권한_없음(내_회원_정보_수정_응답);
+
+        ExtractableResponse<Response> 내_회원_삭제_응답 = 내_회원_삭제_요청(유효하지_않은_토큰);
+        권한_없음(내_회원_삭제_응답);
+
+    }
 }
