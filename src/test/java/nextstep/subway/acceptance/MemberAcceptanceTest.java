300779285110668a5c48fd88f66ff5cea9fa4628
YisooKim <45457001+gashe-soo@users.noreply.github.com>
2022-02-12 16:09:08 +0900

    Step1 (#169)
    * Refactor:Refactor member acceptance test
    
    * Test:Pass myInfoWithSession test
    
    * Refactor:Extract common request uri
    
    * Feat:Implement tokenAuthenticationInterceptor
    
    * Feat:Implement manage member
    
    * Test:Fix test assertion
    
    * Maintain:Add object mapper
    
    * Test:Rename test

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -4,73 +4,96 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("회원 정보 관리")
 class MemberAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
     public static final int AGE = 20;
 
-    @DisplayName("회원가입을 한다.")
-    @Test
-    void createMember() {
-        // when
-        ExtractableResponse<Response> response = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    @DisplayName("회원 정보를 조회한다.")
-    @Test
-    void getMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_정보_조회_요청(createResponse);
-
-        // then
-        회원_정보_조회됨(response, EMAIL, AGE);
-
-    }
-
-    @DisplayName("회원 정보를 수정한다.")
-    @Test
-    void updateMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    @DisplayName("회원 정보를 삭제한다.")
-    @Test
-    void deleteMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_삭제_요청(createResponse);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-    }
-
+    /**
+     * When 회원 생성을 요청
+     * Then 회원 생성됨
+     * When 회원 정보 조회 요청
+     * Then 회원 정보 조회됨
+     * When 회원 정보 수정 요청
+     * Then 회원 정보 수정됨
+     * When 회원 삭제 요청
+     * Then 회원 삭제됨
+     */
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // when
+        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        회원_생성됨(createResponse);
+
+        // when
+        ExtractableResponse<Response> findResponse = 회원_정보_조회_요청(createResponse);
+
+        // then
+        회원_정보_조회됨(findResponse, EMAIL, AGE);
+
+        // given
+        String newEmail = "new" + EMAIL;
+        String newPassword = "new" + PASSWORD;
+
+        // when
+        ExtractableResponse<Response> editResponse = 회원_정보_수정_요청(createResponse, newEmail, newPassword, AGE);
+
+        // then
+        회원_정보_수정됨(editResponse, newEmail);
+
+        // when
+        ExtractableResponse<Response> deleteResponse = 회원_삭제_요청(createResponse);
+
+        // then
+        회원_정보_삭제됨(deleteResponse);
     }
 
-    @DisplayName("나의 정보를 관리한다.")
+    /**
+     * When 회원 생성을 요청
+     * Then 회원 생성됨
+     * When 내 정보 조회 요청
+     * Then 내 정보 조회됨
+     * When 내 정보 수정 요청
+     * Then 내 정보 수정됨
+     * When 회원 삭제 요청
+     * Then 회원 삭제됨
+     */
+    @DisplayName("나의 정보를 생성, 조회, 수정, 삭제한다.")
     @Test
     void manageMyInfo() {
+        // when
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // then
+        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
+
+        // when
+        ExtractableResponse<Response> findResponse = 내_회원_정보_조회_요청(accessToken);
+
+        // then
+        내_회원_정보_조회됨(findResponse, EMAIL, AGE);
+
+        // given
+        String newEmail = "new" + EMAIL;
+        String newPassword = "new" + PASSWORD;
+
+        // when
+        ExtractableResponse<Response> editResponse = 내_회원_정보_수정_요청(accessToken, newEmail, newPassword, AGE);
+
+        // then
+        내_회원_정보_수정됨(editResponse, newEmail);
+
+        // when
+        ExtractableResponse<Response> deleteResponse = 내_회원_정보_삭제_요청(accessToken);
+
+        // then
+        내_회원_정보_삭제됨(deleteResponse);
     }
 }
