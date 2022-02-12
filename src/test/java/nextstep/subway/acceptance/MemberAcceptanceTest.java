678f67c4378ed08d2641cc9e40dc768fcc4001f8
apdkeb <apdkeb@naver.com>
2022-02-12 15:35:47 +0900

    refactor : 인수테스트 통합
    2022. 02. 12
    MemberAcceptanceTest.java
    - 회원 생성, 조회, 수정, 삭제를 하나로 통합
    
    Resolver : loop-study
    See also : heesunjeong

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -14,59 +14,42 @@ class MemberAcceptanceTest extends AcceptanceTest {
     public static final String PASSWORD = "password";
     public static final int AGE = 20;
 
-    @DisplayName("회원가입을 한다.")
-    @Test
-    void createMember() {
-        // when
-        ExtractableResponse<Response> response = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    @DisplayName("회원 정보를 조회한다.")
-    @Test
-    void getMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_정보_조회_요청(createResponse);
-
-        // then
-        회원_정보_조회됨(response, EMAIL, AGE);
-
-    }
-
-    @DisplayName("회원 정보를 수정한다.")
-    @Test
-    void updateMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    @DisplayName("회원 정보를 삭제한다.")
-    @Test
-    void deleteMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
-
-        // when
-        ExtractableResponse<Response> response = 회원_삭제_요청(createResponse);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-    }
-
+    /**
+     * When 회원 생성을 요청합
+     * Then 회원 생성됨
+     * When 회원 정보 조회 요청
+     * Then 회원 정보 조회됨
+     * When 회원 정보 수정 요청
+     * Then 회원 정보 수정됨
+     * When 회원 삭제 요청
+     * Then 회원 삭제됨
+     */
     @DisplayName("회원 정보를 관리한다.")
     @Test
     void manageMember() {
+        // When
+        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        // Then
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // When
+        ExtractableResponse<Response> readResponse = 회원_정보_조회_요청(createResponse);
+
+        // Then
+        assertThat(readResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // When
+        ExtractableResponse<Response> updateResponse = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+
+        // Then
+        assertThat(updateResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // When
+        ExtractableResponse<Response> deleteResponse = 회원_삭제_요청(createResponse);
+
+        // Then
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
     @DisplayName("나의 정보를 관리한다.")
