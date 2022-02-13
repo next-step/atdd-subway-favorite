209df8ee7316a294d2a984e4d2a0f91c1749b472
misudev <jungmisu801@gmail.com>
2022-02-13 19:24:34 +0900

    test: 내 정보 관리 테스트(manageMyInfo) 작성

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -91,5 +91,25 @@ class MemberAcceptanceTest extends AcceptanceTest {
     @DisplayName("나의 정보를 관리한다.")
     @Test
     void manageMyInfo() {
+        // When 회원 생성을 요청
+        ExtractableResponse<Response> 회원_생성_응답 = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+        // Then 회원 생성됨
+        회원_생성됨(회원_생성_응답);
+
+        // When 회원 로그인 후 내 회원 정보 조회 요청
+        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
+        ExtractableResponse<Response> 내_회원_정보_조회_응답 = 내_회원_정보_조회_요청(accessToken);
+        // Then 내 회원 정보 정보 조회됨
+        회원_정보_조회됨(내_회원_정보_조회_응답, EMAIL, AGE);
+
+        // When 내 회원 정보 수정 요청
+        ExtractableResponse<Response> 냬_회원_정보_수정_응답 = 내_회원_정보_수정_요청(accessToken, "new" + EMAIL, "new" + PASSWORD, AGE);
+        // Then 내 회원 정보 수정됨
+        회원_정보_수정됨(냬_회원_정보_수정_응답);
+
+        // When 내 회원 정보 삭제 요청
+        ExtractableResponse<Response> 회원_정보_삭제_응답 = 내_회원_삭제_요청(accessToken);
+        // Then 내 회원 정보 삭제됨
+        회원_삭제됨(회원_정보_삭제_응답);
     }
 }
