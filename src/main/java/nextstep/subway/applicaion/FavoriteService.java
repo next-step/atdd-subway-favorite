81c168deb9f51a1a072ea3cae8b78eb4b1457a3f
kwj1270 <kwj1270@naver.com>
2022-02-13 17:55:56 +0900

    feat: FavoriteServiceTest, 즐겨찾기를 생성한다.

--- src/main/java/nextstep/subway/applicaion/FavoriteService.java
+++ src/main/java/nextstep/subway/applicaion/FavoriteService.java
@@ -1,5 +1,10 @@
 package nextstep.subway.applicaion;
 
+import nextstep.member.application.MemberService;
+import nextstep.member.application.dto.MemberResponse;
+import nextstep.subway.domain.Favorite;
+import nextstep.subway.domain.FavoriteRepository;
+import nextstep.subway.domain.Station;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -7,4 +12,22 @@ import javax.transaction.Transactional;
 @Transactional
 @Service
 public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final MemberService memberService;
+    private final StationService stationService;
+
+    public FavoriteService(final FavoriteRepository favoriteRepository, final MemberService memberService, final StationService stationService) {
+        this.favoriteRepository = favoriteRepository;
+        this.memberService = memberService;
+        this.stationService = stationService;
+    }
+
+    public Long createFavorite(final Long memberId, final Long sourceId, final Long targetId) {
+        final MemberResponse member = memberService.findMember(memberId);
+        final Station source = stationService.findById(sourceId);
+        final Station target = stationService.findById(targetId);
+        final Favorite favorite = new Favorite(member.getId(), source.getId(), target.getId());
+        return favoriteRepository.save(favorite).getId();
+    }
 }
--- src/test/java/nextstep/subway/applicaion/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/applicaion/FavoriteServiceTest.java
@@ -8,9 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
+@SpringBootTest
 class FavoriteServiceTest {
 
     @Autowired
