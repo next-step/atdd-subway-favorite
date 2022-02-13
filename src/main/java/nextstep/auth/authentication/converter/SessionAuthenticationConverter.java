3647fca66c3f108f8b4071d88c71bcb9546c456a
YisooKim <gashe7@gmail.com>
2022-02-13 22:38:55 +0900

    Refactor:Abstract AuthenticationConverter

---
+++ src/main/java/nextstep/auth/authentication/converter/SessionAuthenticationConverter.java
@@ -0,0 +1,18 @@
+package nextstep.auth.authentication.converter;
+
+import nextstep.auth.authentication.AuthenticationToken;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SessionAuthenticationConverter implements AuthenticationConverter{
+    public static final String USERNAME_FIELD = "username";
+    public static final String PASSWORD_FIELD = "password";
+
+    @Override
+    public AuthenticationToken convert(HttpServletRequest request) {
+        String principal = request.getParameterValues(USERNAME_FIELD)[0];
+        String credentials = request.getParameterValues(PASSWORD_FIELD)[0];
+
+        return new AuthenticationToken(principal, credentials);
+    }
+}
--- src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
+++ src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
@@ -5,22 +5,17 @@ import nextstep.auth.authentication.AuthenticationToken;
 import nextstep.auth.authentication.TokenAuthenticationInterceptor;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.token.JwtTokenProvider;
-import nextstep.auth.token.TokenRequest;
 import nextstep.auth.token.TokenResponse;
 import nextstep.member.application.CustomUserDetailsService;
 import nextstep.member.domain.LoginMember;
 import nextstep.member.domain.Member;
-import nextstep.member.domain.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -29,6 +24,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.io.IOException;
 
+import static nextstep.subway.utils.MockRequest.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -36,8 +32,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class TokenAuthenticationInterceptorTest {
     public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
-    private static final String EMAIL = "email@email.com";
-    private static final String PASSWORD = "password";
+
     @Mock
     private CustomUserDetailsService customUserDetailsService;
 
@@ -54,7 +49,7 @@ class TokenAuthenticationInterceptorTest {
     @Test
     void convert() throws IOException {
         // given
-        MockHttpServletRequest request = createMockRequest();
+        MockHttpServletRequest request = createMockTokenRequest();
 
         // when
         AuthenticationToken token = tokenAuthenticationInterceptor.convert(request);
@@ -84,7 +79,7 @@ class TokenAuthenticationInterceptorTest {
     @Test
     void preHandle() throws IOException {
         // given
-        MockHttpServletRequest request = createMockRequest();
+        MockHttpServletRequest request = createMockTokenRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
 
         LoginMember loginMember = LoginMember.of(new Member(EMAIL, PASSWORD, 10));
@@ -100,11 +95,5 @@ class TokenAuthenticationInterceptorTest {
         assertThat(response.getContentAsString()).isEqualTo(new ObjectMapper().writeValueAsString(new TokenResponse(JWT_TOKEN)));
     }
 
-    private MockHttpServletRequest createMockRequest() throws IOException {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        request.setContent(new ObjectMapper().writeValueAsString(tokenRequest).getBytes());
-        return request;
-    }
 
 }
