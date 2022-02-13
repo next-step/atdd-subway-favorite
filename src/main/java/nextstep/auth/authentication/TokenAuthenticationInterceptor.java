eff7ec33e438bbdbda3e9c18be7eac4463c5eaa5
원스톤 <onestone@socar.kr>
2022-02-14 00:10:53 +0900

    refactor: 불필요한 import 제거

--- src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor.java
+++ src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor.java
@@ -6,7 +6,6 @@ import nextstep.auth.authentication.converter.AuthenticationConverter;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.token.JwtTokenProvider;
 import nextstep.auth.token.TokenResponse;
-import nextstep.member.application.CustomUserDetailsService;
 import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
