63d7d6aa2b49af61361ed953d748c6b0f66c73cc
kimuno89 <kimunoya@gmail.com>
2022-02-16 23:05:17 +0900

    feat: 즐겨찾기 기능 추가

---
+++ src/main/java/nextstep/subway/applicaion/FavoriteService.java
@@ -0,0 +1,37 @@
+package nextstep.subway.applicaion;
+
+import nextstep.auth.authentication.UserDetails;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.subway.applicaion.dto.FavoriteRequest;
+import nextstep.subway.applicaion.dto.FavoriteResponse;
+import nextstep.subway.domain.Favorite;
+import nextstep.subway.domain.FavoriteRepository;
+import nextstep.subway.domain.Station;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class FavoriteService {
+    private FavoriteRepository favoriteRepository;
+    private StationService stationService;
+    private MemberRepository memberRepository;
+
+    public FavoriteService(FavoriteRepository favoriteRepository, StationService stationService, MemberRepository memberRepository) {
+        this.favoriteRepository = favoriteRepository;
+        this.stationService = stationService;
+        this.memberRepository = memberRepository;
+    }
+
+    public FavoriteResponse saveFavorite(FavoriteRequest favoriteRequest, UserDetails userDetails) {
+        Station source = stationService.findById(favoriteRequest.getSource());
+        Station target = stationService.findById(favoriteRequest.getTarget());
+        Member member = memberRepository.findById(userDetails.getId())
+            .orElseThrow(IllegalArgumentException::new);
+
+        Favorite favorite = favoriteRepository.save(new Favorite(source, target, member));
+
+        return FavoriteResponse.of(favorite);
+    }
+}
---
+++ src/main/java/nextstep/subway/ui/FavoriteController.java
@@ -0,0 +1,30 @@
+package nextstep.subway.ui;
+
+import nextstep.auth.authentication.UserDetails;
+import nextstep.auth.authorization.AuthenticationPrincipal;
+import nextstep.subway.applicaion.FavoriteService;
+import nextstep.subway.applicaion.dto.FavoriteRequest;
+import nextstep.subway.applicaion.dto.FavoriteResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/favorites")
+public class FavoriteController {
+    private FavoriteService favoriteService;
+
+    public FavoriteController(FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FavoriteResponse> createFavorite(@RequestBody FavoriteRequest favoriteRequest, @AuthenticationPrincipal UserDetails userDetails) {
+        FavoriteResponse favoriteResponse = favoriteService.saveFavorite(favoriteRequest, userDetails);
+        return ResponseEntity.created(URI.create("/favorites/" + favoriteResponse.getId())).body(favoriteResponse);
+    }
+}
