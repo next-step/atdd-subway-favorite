d4b89d315411f664c1eec36db53f47f5a3006c70
songsimo <chy.song89@gmail.com>
2022-02-16 22:43:51 +0900

    Test: TockenAuthenticationInterceptor convert 단위테스트

--- src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor.java
+++ src/main/java/nextstep/auth/authentication/TokenAuthenticationInterceptor.java
@@ -1,18 +1,25 @@
 package nextstep.auth.authentication;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import nextstep.auth.context.Authentication;
 import nextstep.auth.token.JwtTokenProvider;
 import nextstep.auth.token.TokenResponse;
 import nextstep.member.application.CustomUserDetailsService;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import nextstep.member.domain.LoginMember;
 
 public class TokenAuthenticationInterceptor implements HandlerInterceptor {
+    private static final String EMAIL = "email";
+    private static final String PASSWORD = "password";
 
     private CustomUserDetailsService customUserDetailsService;
     private JwtTokenProvider jwtTokenProvider;
@@ -40,14 +47,20 @@ public class TokenAuthenticationInterceptor implements HandlerInterceptor {
 
     public AuthenticationToken convert(HttpServletRequest request) throws IOException {
         // TODO: request에서 AuthenticationToken 객체 생성하기
-        String principal = "";
-        String credentials = "";
+        String stringJosn = request.getReader()
+            .lines()
+            .findFirst()
+            .orElseThrow(RuntimeException::new);
+        JSONObject jsonObject = new JSONObject(stringJosn);
 
-        return new AuthenticationToken(principal, credentials);
+        return new AuthenticationToken(jsonObject.getString(EMAIL), jsonObject.getString(PASSWORD));
     }
 
     public Authentication authenticate(AuthenticationToken authenticationToken) {
         // TODO: AuthenticationToken에서 AuthenticationToken 객체 생성하기
+        String principal = authenticationToken.getPrincipal();
+
+        LoginMember userDetails = customUserDetailsService.loadUserByUsername(principal);
         return new Authentication(null);
     }
 }
--- src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
+++ src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
@@ -1,19 +1,48 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import nextstep.auth.authentication.AuthenticationToken;
+import nextstep.auth.authentication.TokenAuthenticationInterceptor;
+import nextstep.auth.token.JwtTokenProvider;
 import nextstep.auth.token.TokenRequest;
+import nextstep.member.application.CustomUserDetailsService;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import java.io.IOException;
 
+@ExtendWith(MockitoExtension.class)
 class TokenAuthenticationInterceptorTest {
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
     public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
 
+    @Mock
+    CustomUserDetailsService userDetailsService;
+
+    @Mock
+    JwtTokenProvider jwtTokenProvider;
+
     @Test
     void convert() throws IOException {
+        // given
+        TokenAuthenticationInterceptor interceptor = new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider);
+        MockHttpServletRequest request = createMockRequest();
+
+        // when
+        AuthenticationToken authenticationToken = interceptor.convert(request);
+
+        // then
+        assertThat(authenticationToken.getPrincipal()).isEqualTo(EMAIL);
+        assertThat(authenticationToken.getCredentials()).isEqualTo(PASSWORD);
     }
 
     @Test
