90ef515a1578213c31f7f860aa2e6eec5fa9952a
abcdsds <choiyurim2@naver.com>
2022-02-19 17:46:59 +0900

    refactor: 사용안하는 코드 제거

--- src/main/java/nextstep/auth/User.java
+++ src/main/java/nextstep/auth/User.java
@@ -2,12 +2,4 @@ package nextstep.auth;
 
 public interface User {
     boolean checkPassword(String password);
-
-    Long getId();
-
-    String getEmail();
-
-    Integer getAge();
-
-    String getPassword();
 }
