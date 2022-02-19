fe1bd5645576e4f35e3656c6d00b63a80c968622
minjoon_lee <minjoon1995@naver.com>
2022-02-19 13:19:50 +0900

    refacotring : request에서 직접 parameter 꺼내도록 수정

--- src/main/java/nextstep/auth/authentication/converter/SessionAuthenticationConverter.java
+++ src/main/java/nextstep/auth/authentication/converter/SessionAuthenticationConverter.java
@@ -16,10 +16,8 @@ public class SessionAuthenticationConverter implements AuthenticationConverter {
 
 	@Override
 	public AuthenticationToken convert(HttpServletRequest request) throws IOException {
-		Map<String, String[]> paramMap = request.getParameterMap();
-		String principal = paramMap.get(USERNAME_FIELD)[0];
-		String credentials = paramMap.get(PASSWORD_FIELD)[0];
-
+		String principal = request.getParameter(USERNAME_FIELD);
+		String credentials = request.getParameter(PASSWORD_FIELD);
 		return new AuthenticationToken(principal, credentials);
 	}
 }
