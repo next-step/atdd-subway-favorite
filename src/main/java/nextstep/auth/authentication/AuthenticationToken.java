f72550a861499cfe4625e13d6e86f9724e7be3a6
mintheon <mintheon@mintheon.com>
2022-02-19 21:56:17 +0900

    feat: token 생성 기능 추가

--- src/main/java/nextstep/auth/authentication/AuthenticationToken.java
+++ src/main/java/nextstep/auth/authentication/AuthenticationToken.java
@@ -4,9 +4,13 @@ public class AuthenticationToken {
     private String principal;
     private String credentials;
 
-    public AuthenticationToken(String principal, String credentials) {
+    protected AuthenticationToken(String principal, String credentials) {
         this.principal = principal;
         this.credentials = credentials;
+    }
+
+    public static AuthenticationToken of(String principal, String credentials) {
+        new AuthenticationToken(principal, credentials);
     }
 
     public String getPrincipal() {
