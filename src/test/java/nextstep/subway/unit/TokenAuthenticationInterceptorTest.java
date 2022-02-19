693a8a8607d1fa47e3ea54704553f3fa308dec29
misudev <jungmisu801@gmail.com>
2022-02-19 16:27:35 +0900

    test: TokenAuthenticationConverterTest 작성

--- src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
+++ src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
@@ -5,7 +5,6 @@ import nextstep.auth.authentication.AuthenticationToken;
 import nextstep.auth.authentication.TokenAuthenticationInterceptor;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.token.JwtTokenProvider;
-import nextstep.auth.token.TokenRequest;
 import nextstep.auth.token.TokenResponse;
 import nextstep.member.application.CustomUserDetailsService;
 import nextstep.member.domain.LoginMember;
@@ -16,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import static nextstep.subway.unit.TokenFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -25,10 +25,6 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 @ExtendWith(MockitoExtension.class)
 class TokenAuthenticationInterceptorTest {
-    private static final String EMAIL = "email@email.com";
-    private static final String PASSWORD = "password";
-    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
-
     @Mock
     private CustomUserDetailsService customUserDetailsService;
 
@@ -78,13 +74,6 @@ class TokenAuthenticationInterceptorTest {
         // then
         TokenResponse expectedTokenResponse = new TokenResponse(JWT_TOKEN);
         assertThat(response.getContentAsString()).isEqualTo(new ObjectMapper().writeValueAsString(expectedTokenResponse));
-    }
-
-    private MockHttpServletRequest createMockRequest() throws IOException {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        request.setContent(new ObjectMapper().writeValueAsString(tokenRequest).getBytes());
-        return request;
     }
 
 }
---
+++ src/test/java/nextstep/subway/unit/TokenFixture.java
@@ -0,0 +1,20 @@
+package nextstep.subway.unit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import nextstep.auth.token.TokenRequest;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class TokenFixture {
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
+
+
+    public static MockHttpServletRequest createMockRequest() throws IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
+        request.setContent(new ObjectMapper().writeValueAsString(tokenRequest).getBytes());
+        return request;
+    }
+}
