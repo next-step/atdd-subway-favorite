218beadc8b457feffcfe41882defc5977294bb6b
minjoon_lee <minjoon1995@naver.com>
2022-02-20 19:56:25 +0900

    feature : generatevalue 추가

--- src/main/java/nextstep/subway/domain/Favorite.java
+++ src/main/java/nextstep/subway/domain/Favorite.java
@@ -1,6 +1,8 @@
 package nextstep.subway.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -8,6 +10,7 @@ import javax.persistence.OneToOne;
 @Entity
 public class Favorite {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@OneToOne
