0dd1c3624257324dc95590137e75dab8c6b938c9
hiro032 <kiryuinh32@gmail.com>
2022-02-26 00:25:47 +0900

    feat: AuthenticationConverter 추상화
    Session 과 Token 기반 Token converter 를 추상화한 인터페이스를 각각 구현합니다

--- src/main/java/nextstep/auth/authentication/converter/AuthenticationConverter.java
+++ src/main/java/nextstep/auth/authentication/converter/AuthenticationConverter.java
@@ -1,7 +1,8 @@
-package nextstep.auth.authentication;
+package nextstep.auth.authentication.converter;
 
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import nextstep.auth.authentication.AuthenticationToken;
 
 public interface AuthenticationConverter {
     AuthenticationToken convert(HttpServletRequest request) throws IOException;
---
+++ src/main/java/nextstep/auth/authentication/converter/TokenAuthenticationConverter.java
@@ -0,0 +1,24 @@
+package nextstep.auth.authentication.converter;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import nextstep.auth.authentication.AuthenticationToken;
+import org.json.JSONObject;
+
+public class TokenAuthenticationConverter implements AuthenticationConverter {
+
+    @Override
+    public AuthenticationToken convert(HttpServletRequest request) throws IOException {
+        String requestInfo = request.getReader()
+                .lines()
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        JSONObject jsonObject = new JSONObject(requestInfo);
+
+        String principal = jsonObject.getString("email");
+        String credentials = jsonObject.getString("password");
+
+        return new AuthenticationToken(principal, credentials);
+    }
+}
