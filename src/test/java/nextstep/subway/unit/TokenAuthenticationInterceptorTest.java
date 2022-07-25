e6679fb289cce6f3fd1b53aafc612f3704021d18
flamme <flamme1004@gmail.com>
2022-07-25 21:56:41 +0900

    chore: 사용하지 않는 파라미터를 정리하라

--- src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
+++ src/test/java/nextstep/subway/unit/TokenAuthenticationInterceptorTest.java
@@ -26,7 +26,6 @@ import static org.mockito.BDDMockito.given;
 class TokenAuthenticationInterceptorTest {
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
-    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLWiB8Kt59Lno";
 
     private TokenAuthenticationInterceptor interceptor;
     @Mock
