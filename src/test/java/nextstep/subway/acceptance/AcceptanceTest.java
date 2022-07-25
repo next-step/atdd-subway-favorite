3bef1f9c7020ff476d1270354172452a3ee83b9d
giyeon95 <giyeon15@gmail.com>
2022-07-25 21:04:47 +0900

    test: 테스트 수행시 mock 계정 생성로직 추가

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -1,6 +1,10 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import java.util.List;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +19,19 @@ public class AcceptanceTest {
     int port;
 
     @Autowired
+    private MemberRepository memberRepository;
+
+    protected static final String EMAIL = "admin@email.com";
+    protected static final String PASSWORD = "password";
+    protected static final Integer AGE = 20;
+
+    @Autowired
     private DatabaseCleanup databaseCleanup;
 
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
         databaseCleanup.execute();
+        memberRepository.save(new Member(EMAIL, PASSWORD, AGE, List.of(RoleType.ROLE_ADMIN.name())));
     }
 }
