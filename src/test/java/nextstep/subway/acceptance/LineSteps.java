71e783dd00cfa19ee09e325271fdfa53c048743a
DahamLeee <dlek1011@gmail.com>
2022-07-27 17:22:35 +0900

    style: static final vairable to uppercase

--- src/test/java/nextstep/subway/acceptance/LineSteps.java
+++ src/test/java/nextstep/subway/acceptance/LineSteps.java
@@ -13,15 +13,15 @@ import static nextstep.subway.acceptance.MemberSteps.로그인_되어_있음;
 
 public class LineSteps {
 
-    private static final String email = "admin@email.com";
-    private static final String password = "password";
+    private static final String ADMIN_EMAIL = "admin@email.com";
+    private static final String ADMIN_PASSWORD = "password";
 
     public static ExtractableResponse<Response> 지하철_노선_생성_요청(String name, String color) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
         params.put("color", color);
 
-        return given(로그인_되어_있음(email, password))
+        return given(로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD))
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
@@ -36,7 +36,7 @@ public class LineSteps {
     }
 
     public static ExtractableResponse<Response> 지하철_노선_수정_요청(String location, Map<String, String> params) {
-        return given(로그인_되어_있음(email, password))
+        return given(로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD))
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().put(location)
@@ -44,7 +44,7 @@ public class LineSteps {
     }
 
     public static ExtractableResponse<Response> 지하철_노선_삭제_요청(String location) {
-        return given(로그인_되어_있음(email, password))
+        return given(로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD))
                 .when().delete(location)
                 .then().log().all().extract();
     }
@@ -64,7 +64,7 @@ public class LineSteps {
     }
 
     public static ExtractableResponse<Response> 지하철_노선_생성_요청(Map<String, String> params) {
-        return given(로그인_되어_있음(email, password))
+        return given(로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD))
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
@@ -72,7 +72,7 @@ public class LineSteps {
     }
 
     public static ExtractableResponse<Response> 지하철_노선에_지하철_구간_생성_요청(Long lineId, Map<String, String> params) {
-        return given(로그인_되어_있음(email, password))
+        return given(로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(params)
                 .when().post("/lines/{lineId}/sections", lineId)
@@ -80,7 +80,7 @@ public class LineSteps {
     }
 
     public static ExtractableResponse<Response> 지하철_노선에_지하철_구간_제거_요청(Long lineId, Long stationId) {
-        return given(로그인_되어_있음(email, password))
+        return given(로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD))
                 .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, stationId)
                 .then().log().all().extract();
     }
