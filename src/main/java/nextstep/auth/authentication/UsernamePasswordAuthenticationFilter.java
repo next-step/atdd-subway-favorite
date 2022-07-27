f610ef879e2e4f3372d651238754f9fd64b63477
giyeon95 <giyeon15@gmail.com>
2022-07-27 19:27:04 +0900

    feat: UsernamePasswordAuthenticationFilter 구현 및 테스트 코드 작성

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,21 +1,44 @@
 package nextstep.auth.authentication;
 
-import nextstep.member.application.LoginMemberService;
-import org.springframework.web.servlet.HandlerInterceptor;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
+import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+@RequiredArgsConstructor
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
-    private LoginMemberService loginMemberService;
 
-    public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
-        this.loginMemberService = loginMemberService;
-    }
+    private final LoginMemberService loginMemberService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
+        LoginMember loginMember = findLoginMember(request);
+        setAuthentication(loginMember);
+
         return true;
+    }
+
+    private LoginMember findLoginMember(HttpServletRequest request) {
+        final String username = request.getParameter("username");
+        final String password = request.getParameter("password");
+
+        LoginMember loginMember = loginMemberService.loadUserByUsername(username);
+        if (isNotMatchPassword(password, loginMember)) {
+            throw new AuthenticationException();
+        }
+        return loginMember;
+    }
+
+    private boolean isNotMatchPassword(String password, LoginMember loginMember) {
+        return !loginMember.checkPassword(password);
+    }
+
+    private void setAuthentication(LoginMember loginMember) {
+        Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
