efdafad70e61759a67cbeba5b6b6e7031603cf1f
giyeon95 <giyeon15@gmail.com>
2022-07-27 21:17:06 +0900

    feat: BearerTokenAuthenticationFilter 구현 및 테스트 코드 작성

--- src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
@@ -1,21 +1,47 @@
 package nextstep.auth.authentication;
 
-import nextstep.auth.token.JwtTokenProvider;
-import org.springframework.web.servlet.HandlerInterceptor;
+import static org.apache.logging.log4j.util.Strings.EMPTY;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
+import nextstep.auth.token.JwtTokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+@RequiredArgsConstructor
 public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
-    private JwtTokenProvider jwtTokenProvider;
 
-    public BearerTokenAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private static final String TOKEN_TYPE = "Bearer ";
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
+        String token = getValidToken(request);
+        setAuthentication(token);
+
         return true;
     }
+
+    private String getValidToken(HttpServletRequest request) {
+        final String requestTokenHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (!requestTokenHeader.startsWith(TOKEN_TYPE)) {
+            throw new AuthenticationException();
+        }
+        String token = requestTokenHeader.replaceFirst(TOKEN_TYPE, EMPTY);
+
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new AuthenticationException();
+        }
+
+        return token;
+    }
+
+    private void setAuthentication(String token) {
+        Authentication authentication = new Authentication(jwtTokenProvider.getPrincipal(token), jwtTokenProvider.getRoles(token));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
 }
