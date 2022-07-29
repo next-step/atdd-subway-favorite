195c46aae4dde4f264d90317041e1d82fb325bc9
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-30 01:24:13 +0900

    Feat : Form 인증 기능 구현

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,12 +1,19 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContext;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(UsernamePasswordAuthenticationFilter.class);
     private LoginMemberService loginMemberService;
 
     public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
@@ -15,7 +22,22 @@ public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        if (securityContext.getAuthentication() != null) {
+            log.info("User information is already saved. User principal is {}", securityContext.getAuthentication().getPrincipal());
+            return true;
+        }
+
+        LoginMember loginMember = loginMemberService.loadUserByUsername(username);
+        if (!loginMember.checkPassword(password)) {
+            log.info("The password is not correct. User principal is {}", username);
+            return true;
+        }
+
+        securityContext.setAuthentication(new Authentication(loginMember.getEmail(), loginMember.getAuthorities()));
         return true;
     }
 }
--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -44,7 +44,13 @@ class AuthAcceptanceTest extends AcceptanceTest {
     }
 
     private ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
-        return null;
+        return RestAssured.given().log().all()
+            .auth().form(email, password, new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/members/me")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 
     private ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
