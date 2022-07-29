f37d09187f18309e9bc4c9fb6681549740c4294c
giyeon95 <giyeon15@gmail.com>
2022-07-30 00:51:23 +0900

    refactor: AppConfig 내 member 의존성 제거
    ```
    interface가 있으니 auth -> member 의존 관계를 제거할 수 있을 것 같아요 😄
    
    _Originally posted by @testrace in https://github.com/next-step/atdd-subway-favorite/pull/257#discussion_r933071819_
    ```

--- src/main/java/nextstep/config/AppConfig.java
+++ src/main/java/nextstep/config/AppConfig.java
@@ -7,7 +7,7 @@ import nextstep.auth.handler.BearerTokenAuthVerificationHandler;
 import nextstep.auth.handler.TokenAuthenticationHandler;
 import nextstep.auth.handler.UsernamePasswordAuthenticationHandler;
 import nextstep.auth.token.JwtTokenProvider;
-import nextstep.member.application.LoginMemberService;
+import nextstep.auth.user.UserDetailsService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,20 +16,20 @@ import java.util.List;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-    private LoginMemberService loginMemberService;
+    private UserDetailsService userDetailsService;
     private JwtTokenProvider jwtTokenProvider;
 
-    public AppConfig(LoginMemberService loginMemberService, JwtTokenProvider jwtTokenProvider) {
-        this.loginMemberService = loginMemberService;
+    public AppConfig(UserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
+        this.userDetailsService = userDetailsService;
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecurityContextPersistenceFilter());
-        registry.addInterceptor(new UsernamePasswordAuthenticationHandler(loginMemberService)).addPathPatterns("/login/form");
-        registry.addInterceptor(new TokenAuthenticationHandler(loginMemberService, jwtTokenProvider)).addPathPatterns("/login/token");
-        registry.addInterceptor(new BasicAuthVerificationHandler(loginMemberService));
+        registry.addInterceptor(new UsernamePasswordAuthenticationHandler(userDetailsService)).addPathPatterns("/login/form");
+        registry.addInterceptor(new TokenAuthenticationHandler(userDetailsService, jwtTokenProvider)).addPathPatterns("/login/token");
+        registry.addInterceptor(new BasicAuthVerificationHandler(userDetailsService));
         registry.addInterceptor(new BearerTokenAuthVerificationHandler(jwtTokenProvider));
     }
 
