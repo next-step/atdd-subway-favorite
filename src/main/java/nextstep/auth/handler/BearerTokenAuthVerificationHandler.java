24b9209c43813b333ec37da5a324d5ce149b6681
giyeon95 <giyeon15@gmail.com>
2022-07-30 00:41:56 +0900

    refactor: 클래스 변수, 멤버변수 구분 개행 추가
    ```
    클래스 변수와 멤버 변수를 개행을 통해 구분해 주면 가독성에 도움이 될 것 같아요 😃
    
    _Originally posted by @testrace in https://github.com/next-step/atdd-subway-favorite/pull/257#discussion_r933070806_
    ```

--- src/main/java/nextstep/auth/handler/BearerTokenAuthVerificationHandler.java
+++ src/main/java/nextstep/auth/handler/BearerTokenAuthVerificationHandler.java
@@ -14,6 +14,7 @@ import static org.apache.logging.log4j.util.Strings.EMPTY;
 public class BearerTokenAuthVerificationHandler extends AuthVerificationHandler {
 
     private static final String TOKEN_TYPE = "Bearer ";
+
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
