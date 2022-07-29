a3814b3b515a683f49852f2bd5043005965cca75
임명섭 <lms@elonsoft.co.kr>
2022-07-29 13:42:21 +0900

    feat Form 기반 로그인 필터 기능 구현

--- src/main/java/nextstep/member/application/LoginMemberService.java
+++ src/main/java/nextstep/member/application/LoginMemberService.java
@@ -5,16 +5,19 @@ import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LoginMemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     public LoginMemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     public LoginMember loadUserByUsername(String email) {
-        Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(RuntimeException::new);
         return LoginMember.of(member);
     }
 }
