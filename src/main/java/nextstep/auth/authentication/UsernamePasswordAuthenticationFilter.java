66d4307c63d1a5022a8c26277009ecdea9d164da
charlie.k <charlie.k@kakaocorp.com>
2022-07-30 11:22:38 +0900

    feat: session 로그인 인수테스트 및 기능 구현

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,12 +1,19 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContext;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
+    private final Logger log = LoggerFactory.getLogger(UsernamePasswordAuthenticationFilter.class);
     private LoginMemberService loginMemberService;
 
     public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
@@ -15,7 +22,21 @@ public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (context.getAuthentication() != null) {
+            log.info("로그인이 이미 되어있습니다. userName : {}", context.getAuthentication().getPrincipal());
+            return true;
+        }
+
+        LoginMember loginMember = loginMemberService.loadUserByUsername(request.getParameter("userName"));
+        if (!loginMember.checkPassword(request.getParameter("password"))){
+            log.info("패스워드가 틀렸습니다. userName : {}", loginMember.getEmail());
+            return false;
+        }
+
+        Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+        context.setAuthentication(authentication);
+
         return true;
     }
 }
