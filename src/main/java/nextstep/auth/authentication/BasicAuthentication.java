ebdb20db357c184f4da8a2a4c00e55a5d322c061
DahamLeee <dlek1011@gmail.com>
2022-07-31 00:38:39 +0900

    feat: authenticate basic implements strategy interface

---
+++ src/main/java/nextstep/auth/authentication/BasicAuthentication.java
@@ -0,0 +1,43 @@
+package nextstep.auth.authentication;
+
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
+import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class BasicAuthentication implements AuthenticationStrategy {
+
+    private LoginMemberService loginMemberService;
+
+    public BasicAuthentication(LoginMemberService loginMemberService) {
+        this.loginMemberService = loginMemberService;
+    }
+
+    @Override
+    public void authenticate(HttpServletRequest request) {
+        String authCredentials = AuthorizationExtractor.extract(request, AuthorizationType.BASIC);
+        String authHeader = new String(Base64.decodeBase64(authCredentials));
+
+        String[] splits = authHeader.split(":");
+        String principal = splits[0];
+        String credentials = splits[1];
+
+        AuthenticationToken token = new AuthenticationToken(principal, credentials);
+
+        LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+        if (loginMember == null) {
+            throw new AuthenticationException();
+        }
+
+        if (!loginMember.checkPassword(token.getCredentials())) {
+            throw new AuthenticationException();
+        }
+
+        Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
