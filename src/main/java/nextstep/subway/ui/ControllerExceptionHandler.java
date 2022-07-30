ae12acf3926b860cb5a5bd20341af770fb363690
JoMinKyu <minkyu.jo@worksmobile.com>
2022-07-31 01:47:57 +0900

    ControllerAdvice 로깅 수정

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -9,25 +9,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    private final Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
+    private final Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
-        logger.error("Error: ", e);
+        log.error("handleIllegalArgsException: ", e);
         return ResponseEntity.badRequest().build();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Void> handleIllegalArgsException(IllegalArgumentException e) {
-        logger.error("Error: ", e);
+        log.error("handleIllegalArgsException: ", e);
         return ResponseEntity.badRequest().build();
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> handleIllegalArgsException(Exception e) {
-        logger.error("Error: ", e);
+        log.error("handleIllegalArgsException: ", e);
         return ResponseEntity.badRequest().build();
     }
-
 
 }
