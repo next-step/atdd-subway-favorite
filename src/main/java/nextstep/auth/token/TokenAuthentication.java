8815e9a77ecea73f02d4a89d77e7aaebede01790
DahamLeee <dlek1011@gmail.com>
2022-07-31 01:07:22 +0900

    feat: create TokenAuthentication [strategy]

---
+++ src/main/java/nextstep/auth/token/TokenAuthentication.java
@@ -0,0 +1,50 @@
+package nextstep.auth.token;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.auth.authentication.AuthenticationException;
+import nextstep.auth.authentication.AuthenticationStrategy;
+import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.stream.Collectors;
+
+public class TokenAuthentication implements AuthenticationStrategy {
+
+    private LoginMemberService loginMemberService;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public TokenAuthentication(LoginMemberService loginMemberService, JwtTokenProvider jwtTokenProvider) {
+        this.loginMemberService = loginMemberService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void authenticate(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        TokenRequest tokenRequest = new ObjectMapper().readValue(content, TokenRequest.class);
+
+        String principal = tokenRequest.getEmail();
+        String credentials = tokenRequest.getPassword();
+
+        LoginMember loginMember = loginMemberService.loadUserByUsername(principal);
+
+        if (loginMember == null) {
+            throw new AuthenticationException();
+        }
+
+        if (!loginMember.checkPassword(credentials)) {
+            throw new AuthenticationException();
+        }
+
+        String token = jwtTokenProvider.createToken(loginMember.getEmail(), loginMember.getAuthorities());
+        TokenResponse tokenResponse = new TokenResponse(token);
+
+        String responseToClient = new ObjectMapper().writeValueAsString(tokenResponse);
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getOutputStream().print(responseToClient);
+    }
+}
