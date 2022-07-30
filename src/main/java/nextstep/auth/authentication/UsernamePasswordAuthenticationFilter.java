14c361c01b27fced3b9c5eb829747d8c29a40391
김민수 <minsu@sk.com>
2022-07-30 15:16:32 +0900

    test : Basic Auth, Session 로그인 테스트 성공하도록 수정. feat : UsernamePasswordAuthenticationFilter 작성

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,13 +1,20 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
-    private LoginMemberService loginMemberService;
+
+    public static final String USERNAME_FIELD = "username";
+    public static final String PASSWORD_FIELD = "password";
+
+    private final LoginMemberService loginMemberService;
 
     public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
         this.loginMemberService = loginMemberService;
@@ -16,6 +23,29 @@ public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         // TODO: 구현하세요.
-        return true;
+        try {
+            String username = request.getParameter(USERNAME_FIELD);
+            String password = request.getParameter(PASSWORD_FIELD);
+
+            AuthenticationToken token = new AuthenticationToken(username, password);
+
+            LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+
+            if (loginMember == null) {
+                throw new AuthenticationException();
+            }
+
+            if (!loginMember.checkPassword(token.getCredentials())) {
+                throw new AuthenticationException();
+            }
+
+            Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+
     }
 }
--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,9 +1,13 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.authentication.FormAuthConfig;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
 
@@ -12,6 +16,11 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
     private static final Integer AGE = 20;
+
+    @BeforeEach
+    void authSetUp(){
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
+    }
 
     @DisplayName("Basic Auth")
     @Test
@@ -40,7 +49,14 @@ class AuthAcceptanceTest extends AcceptanceTest {
     }
 
     private ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
-        return null;
+        return RestAssured.given().log().all()
+                .auth().form(email, password
+                        , new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/members/me")
+                .then().log().all()
+                .extract();
     }
 
     private ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
