f9441d4a2e3ee83c64b4cf4d2fd0a441a958b3f3
wlroh <wlroh.paul@gmail.com>
2022-07-31 22:36:57 +0900

    feat: 초기 데이터 구성

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,20 @@
 package nextstep;
 
+import lombok.RequiredArgsConstructor;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class DataLoader {
+
+    private final MemberRepository memberRepository;
+
     public void loadData() {
+        memberRepository.save(new Member("admin@email.com", "password", 20, List.of(RoleType.ROLE_ADMIN.name())));
     }
 }
