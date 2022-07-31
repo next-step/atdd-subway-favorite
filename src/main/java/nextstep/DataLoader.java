e52b81110595784de486b4a861803d934d7d0e52
YoungHo Choi <0505zxc@gmail.com>
2022-08-01 01:41:07 +0900

    test: 일반 유저권한을 가진 테스트 데이터 추가

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader {
     private final MemberRepository memberRepository;
-    public static final String ADMIN_EMAIL = "superAdmin@email.com";
-    public static final String ADMIN_PASSWORD = "test";
+    public static final String ADMIN_EMAIL = "admin@email.com";
+    public static final String ADMIN_PASSWORD = "adminPassword";
     public static final int ADMIN_AGE = 20;
+
+    public static final String MEMBER_EMAIL = "member@email.com";
+    public static final String MEMBER_PASSWORD = "memberPassword";
+    public static final int MEMBER_AGE = 25;
 
     public DataLoader(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -19,5 +23,6 @@ public class DataLoader {
 
     public void loadData() {
         memberRepository.save(new Member(ADMIN_EMAIL, ADMIN_PASSWORD, ADMIN_AGE, List.of(RoleType.ROLE_ADMIN.name())));
+        memberRepository.save(new Member(MEMBER_EMAIL, MEMBER_PASSWORD, MEMBER_AGE, List.of(RoleType.ROLE_MEMBER.name())));
     }
 }
