fa0d7d8b05246b7187a5b5ea5bfc33d44d216dc7
wlroh <wlroh.paul@gmail.com>
2022-07-31 23:48:33 +0900

    feat: Form 기반 로그인 인증 구현

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,7 +1,10 @@
 package nextstep.auth.authentication;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +12,30 @@ import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
+
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
     private final LoginMemberService loginMemberService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            String username = request.getParameter(USERNAME);
+            String password = request.getParameter(PASSWORD);
+
+            AuthenticationToken token = new AuthenticationToken(username, password);
+
+            LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+            if (!loginMember.checkPassword(token.getCredentials())) {
+                throw new AuthenticationException();
+            }
+
+            Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
