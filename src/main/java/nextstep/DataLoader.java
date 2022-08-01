32fd20a38e1c553f6bdf51896255b748cd337148
chkim <cndghks0213@gmail.com>
2022-08-02 07:14:31 +0900

    feat: 관리자 저장 기능 구현

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,26 @@
 package nextstep;
 
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static nextstep.member.domain.RoleType.ROLE_ADMIN;
 
 @Component
 public class DataLoader {
+    private static final String EMAIL = "admin@email.com";
+    private static final String PASSWORD = "password";
+    private static final Integer AGE = 20;
+
+    private final MemberRepository memberRepository;
+
+    public DataLoader(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public void loadData() {
+        memberRepository.save(new Member(EMAIL, PASSWORD, AGE, List.of(ROLE_ADMIN.name())));
     }
 }
