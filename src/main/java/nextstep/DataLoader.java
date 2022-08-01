026e03ec685afa06f791e0f679b1c0c73a412d5a
kookiedncream <kim76744@naver.com>
2022-08-01 21:20:17 +0900

    feat: DataLoader 데이터 생성 기능 추가

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,21 @@
 package nextstep;
 
+import lombok.RequiredArgsConstructor;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class DataLoader {
+
+    private final MemberRepository memberRepository;
+
     public void loadData() {
+        List<String> roles = List.of(RoleType.ROLE_MEMBER.name(), RoleType.ROLE_ADMIN.name());
+        memberRepository.save(new Member("admin@email.com", "password", 20, roles));
     }
 }
