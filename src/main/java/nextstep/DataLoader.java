7dfbe41ffc8fdc0ac105c5667e8332dc17383754
kookiedncream <kim76744@naver.com>
2022-08-02 00:18:42 +0900

    feat: 지하철 노선, 구간, 역의 생성, 수정, 삭제는 관리자만 하도록 변경

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -3,10 +3,12 @@ package nextstep;
 import lombok.RequiredArgsConstructor;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
-import nextstep.member.domain.RoleType;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+import static nextstep.member.domain.RoleType.ROLE_ADMIN;
+import static nextstep.member.domain.RoleType.ROLE_MEMBER;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +17,7 @@ public class DataLoader {
     private final MemberRepository memberRepository;
 
     public void loadData() {
-        List<String> roles = List.of(RoleType.ROLE_MEMBER.name(), RoleType.ROLE_ADMIN.name());
-        memberRepository.save(new Member("admin@email.com", "password", 20, roles));
+        memberRepository.save(new Member("admin@email.com", "password", 20, List.of(ROLE_ADMIN.name())));
+        memberRepository.save(new Member("member@email.com", "password", 20, List.of(ROLE_MEMBER.name())));
     }
 }
