b0a880a2b0e727e88251c973ba85a823a022a1a7
Shin seongsu <tlstjdtn321@naver.com>
2022-08-02 01:24:32 +0900

    feat: 테스트를 쉽게 하기위해 DataLoader 추가

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,20 @@
 package nextstep;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import nextstep.member.application.MemberService;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DataLoader {
+
+    private final MemberRepository memberRepository;
+
     public void loadData() {
+        Member member = new Member("admin", "password", 30, List.of("ROLE_ADMIN", "ROLE_MEMBER"));
+        memberRepository.save(member);
     }
 }
