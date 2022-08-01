24f30bafac733f040d3b8d728781e1a01906a350
jehyeong <jehyeong@finda.co.kr>
2022-08-02 08:01:08 +0900

    feat : UsernamePasswordAuthenticationFilter preHandle 구현

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,21 +1,49 @@
 package nextstep.auth.authentication;
 
-import nextstep.member.application.LoginMemberService;
-import org.springframework.web.servlet.HandlerInterceptor;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
+import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
-    private LoginMemberService loginMemberService;
+	private static final String PRINCIPAL_NAME = "username";
+	private static final String CREDENTIAL_NAME = "password";
+	private LoginMemberService loginMemberService;
 
-    public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
-        this.loginMemberService = loginMemberService;
-    }
+	public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
+		this.loginMemberService = loginMemberService;
+	}
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
-    }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+		Map<String, String[]> parameterMap = request.getParameterMap();
+		String userName = parameterMap.get(PRINCIPAL_NAME)[0];
+		String password = parameterMap.get(CREDENTIAL_NAME)[0];
+
+		AuthenticationToken token = new AuthenticationToken(userName, password);
+
+		LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+
+		if (loginMember == null) {
+			throw new AuthenticationException();
+		}
+
+		if (!loginMember.checkPassword(token.getCredentials())) {
+			throw new AuthenticationException();
+		}
+
+		Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		return true;
+
+	}
 }
