bb64b6eb324c820272d1ec1fc1a91c7d4c3e28f2
Shin seongsu <tlstjdtn321@naver.com>
2022-08-02 01:25:37 +0900

    refactor: 메소드 분리

--- src/main/java/nextstep/auth/token/TokenAuthenticationInterceptor.java
+++ src/main/java/nextstep/auth/token/TokenAuthenticationInterceptor.java
@@ -1,6 +1,8 @@
 package nextstep.auth.token;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import nextstep.auth.authentication.AuthenticationException;
 import nextstep.member.application.LoginMemberService;
 import nextstep.member.domain.LoginMember;
@@ -11,41 +13,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 public class TokenAuthenticationInterceptor implements HandlerInterceptor {
-    private LoginMemberService loginMemberService;
-    private JwtTokenProvider jwtTokenProvider;
-
-    public TokenAuthenticationInterceptor(LoginMemberService loginMemberService, JwtTokenProvider jwtTokenProvider) {
-        this.loginMemberService = loginMemberService;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final LoginMemberService loginMemberService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        TokenRequest tokenRequest = new ObjectMapper().readValue(content, TokenRequest.class);
-
-        String principal = tokenRequest.getEmail();
-        String credentials = tokenRequest.getPassword();
-
-        LoginMember loginMember = loginMemberService.loadUserByUsername(principal);
-
-        if (loginMember == null) {
-            throw new AuthenticationException();
-        }
-
-        if (!loginMember.checkPassword(credentials)) {
-            throw new AuthenticationException();
-        }
+        TokenRequest tokenRequest = convert(request);
+        LoginMember loginMember = authenticate(tokenRequest);
 
         String token = jwtTokenProvider.createToken(loginMember.getEmail(), loginMember.getAuthorities());
         TokenResponse tokenResponse = new TokenResponse(token);
 
         String responseToClient = new ObjectMapper().writeValueAsString(tokenResponse);
         response.setStatus(HttpServletResponse.SC_OK);
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getOutputStream().print(responseToClient);
 
         return false;
     }
+
+    public LoginMember authenticate(TokenRequest tokenRequest) {
+        String principal = tokenRequest.getEmail();
+        String credentials = tokenRequest.getPassword();
+        LoginMember loginMember = loginMemberService.loadUserByUsername(principal);
+
+        validation(credentials, loginMember);
+        return loginMember;
+    }
+
+    public TokenRequest convert(HttpServletRequest request) throws IOException {
+        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        return objectMapper.readValue(content, TokenRequest.class);
+    }
+
+    private void validation(String crredentials, LoginMember loginMember) {
+        if (loginMember == null) {
+            throw new AuthenticationException();
+        }
+
+        if(!loginMember.checkPassword(crredentials)) {
+            throw new AuthenticationException();
+        }
+    }
+
 }
