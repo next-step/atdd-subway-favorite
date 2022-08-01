11269c1258a02f8777ac50aefeea752fe6621b31
Shin seongsu <tlstjdtn321@naver.com>
2022-08-02 01:25:11 +0900

    feat: Authorization 보안 추가

--- src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
@@ -1,21 +1,44 @@
 package nextstep.auth.authentication;
 
+import lombok.RequiredArgsConstructor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.auth.token.JwtTokenProvider;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@RequiredArgsConstructor
 public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
-    private JwtTokenProvider jwtTokenProvider;
 
-    public BearerTokenAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) {
+        setAuthentication(request);
         return true;
     }
+
+    public void setAuthentication(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            String bearerToken = authorization.substring("Bearer ".length());
+            validatation(bearerToken);
+
+            Authentication authentication = new Authentication(
+                jwtTokenProvider.getPrincipal(bearerToken), jwtTokenProvider.getRoles(bearerToken));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+    private void validatation(String bearerToek) {
+        if (!jwtTokenProvider.validateToken(bearerToek)) {
+            throw new AuthenticationException();
+        }
+    }
+
 }
