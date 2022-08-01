f92406a4500ac7ba18b49fe87170f10c6cedb804
chkim <cndghks0213@gmail.com>
2022-08-02 07:13:55 +0900

    feat: BearerTokenAuthenticationFilter 구현

--- src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
@@ -1,10 +1,15 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.auth.token.JwtTokenProvider;
+import nextstep.member.domain.LoginMember;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
     private JwtTokenProvider jwtTokenProvider;
@@ -15,7 +20,23 @@ public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            String token = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
+
+            if (!jwtTokenProvider.validateToken(token)) {
+                throw new AuthenticationException();
+            }
+
+            String principal = jwtTokenProvider.getPrincipal(token);
+            List<String> authorities = jwtTokenProvider.getRoles(token);
+
+            Authentication authentication = new Authentication(principal, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return true;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
