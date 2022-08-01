5465ac2ea6d89e8d785b28fc3b2ce91ea1e52719
lsj8367 <lsj8367@naver.com>
2022-08-01 23:01:32 +0900

    :sparkles: 조건별 관리자 토큰 추가

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -25,7 +25,7 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @Test
     void createStation() {
         // when
-        ExtractableResponse<Response> response = 지하철역_생성_요청("강남역");
+        ExtractableResponse<Response> response = 지하철역_생성_요청(관리자_토큰, "강남역");
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -48,8 +48,8 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @Test
     void getStations() {
         // given
-        지하철역_생성_요청("강남역");
-        지하철역_생성_요청("역삼역");
+        지하철역_생성_요청(관리자_토큰, "강남역");
+        지하철역_생성_요청(관리자_토큰, "역삼역");
 
         // when
         ExtractableResponse<Response> stationResponse = RestAssured.given().log().all()
@@ -71,11 +71,12 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteStation() {
         // given
-        ExtractableResponse<Response> createResponse = 지하철역_생성_요청("강남역");
+        ExtractableResponse<Response> createResponse = 지하철역_생성_요청(관리자_토큰, "강남역");
 
         // when
         String location = createResponse.header("location");
         RestAssured.given().log().all()
+            .auth().oauth2(관리자_토큰)
                 .when()
                 .delete(location)
                 .then().log().all()
@@ -84,6 +85,7 @@ public class StationAcceptanceTest extends AcceptanceTest {
         // then
         List<String> stationNames =
                 RestAssured.given().log().all()
+                    .auth().oauth2(관리자_토큰)
                         .when().get("/stations")
                         .then().log().all()
                         .extract().jsonPath().getList("name", String.class);
