3fe6073116e60983986a05e832620778d1a25eb1
imcool2551 <imcool2551@gmail.com>
2022-08-02 16:01:17 +0900

    refactor: 관리자, 일반사용자 이메일/비밀번호 상수화

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -10,6 +10,11 @@ import java.util.List;
 @Component
 public class DataLoader {
 
+    public static final String ADMIN_EMAIL = "admin@email.com";
+    public static final String MEMBER_EMAIL = "member@email.com";
+    public static final String PASSWORD = "password";
+    public static final Integer AGE = 20;
+
     private final MemberRepository memberRepository;
 
     public DataLoader(MemberRepository memberRepository) {
@@ -17,7 +22,7 @@ public class DataLoader {
     }
 
     public void loadData() {
-        memberRepository.save(new Member("admin@email.com", "password", 20, List.of(RoleType.ROLE_ADMIN.name())));
-        memberRepository.save(new Member("member@email.com", "password", 20, List.of(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(ADMIN_EMAIL, PASSWORD, AGE, List.of(RoleType.ROLE_ADMIN.name())));
+        memberRepository.save(new Member(MEMBER_EMAIL, PASSWORD, AGE, List.of(RoleType.ROLE_MEMBER.name())));
     }
 }
