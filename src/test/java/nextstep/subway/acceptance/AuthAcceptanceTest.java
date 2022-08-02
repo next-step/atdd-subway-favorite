3e1a96207a566f1b74ab2c3c1fab0147bf57b7be
Choi Seon Kang <42162127+zzangoobrother@users.noreply.github.com>
2022-08-02 12:06:15 +0900

    Step1 - 인증 기반 인수 테스트 (#271)
    * test : 인증 테스트 코드 구현
    
    * feat : 인증 기능 코드 구현
    
    * test : 저장, 수정, 삭제 권한 추가
    
    * fix : 기능 코드 저장, 수정, 삭제 권한 추가
    
    * refactor : 테스트 코드 리팩토링

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,12 +1,18 @@
 package nextstep.subway.acceptance;
 
+import static nextstep.subway.acceptance.MemberSteps.PASSWORD_FIELD;
+import static nextstep.subway.acceptance.MemberSteps.USERNAME_FIELD;
+import static nextstep.subway.acceptance.MemberSteps.베이직_인증으로_내_회원_정보_조회_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회됨;
+
+import io.restassured.RestAssured;
+import io.restassured.authentication.FormAuthConfig;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static nextstep.subway.acceptance.MemberSteps.*;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
@@ -16,6 +22,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Basic Auth")
     @Test
     void myInfoWithBasicAuth() {
+
         ExtractableResponse<Response> response = 베이직_인증으로_내_회원_정보_조회_요청(EMAIL, PASSWORD);
 
         회원_정보_조회됨(response, EMAIL, AGE);
@@ -32,18 +39,31 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
-        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
-
-        ExtractableResponse<Response> response = 베어러_인증으로_내_회원_정보_조회_요청(accessToken);
+        ExtractableResponse<Response> response = 베어러_인증으로_내_회원_정보_조회_요청(관리자토큰);
 
         회원_정보_조회됨(response, EMAIL, AGE);
     }
 
     private ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
-        return null;
+        return RestAssured.given().log().all()
+            .auth().form(email, password, new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .get("/members/me")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 
     private ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
-        return null;
+        return CommonAuthRestAssured.given(accessToken)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .get("/members/me")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 }
