aaadedd24defc1ca01dc5d6b99c42b199d12e557
MoonHKLee <mf311110@gmail.com>
2022-08-03 00:16:41 +0900

    test : Bearer / Session 기반 인증 테스트코드 작성.

--- src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -1,6 +1,9 @@
 package nextstep.subway.acceptance;
 
+import static nextstep.subway.acceptance.MemberSteps.로그인_되어_있음;
+
 import io.restassured.RestAssured;
+import nextstep.DataLoader;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +14,32 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
+
+    protected static final String ADMIN_EMAIL = "admin@email.com";
+    protected static final String ADMIN_PASSWORD = "password";
+    protected static final Integer ADMIN_AGE = 20;
+
+    protected static final String MEMBER_EMAIL = "member@email.com";
+    protected static final String MEMBER_PASSWORD = "password";
+    protected static final Integer MEMBER_AGE = 29;
     @LocalServerPort
     int port;
 
     @Autowired
     private DatabaseCleanup databaseCleanup;
 
+    @Autowired
+    private DataLoader dataLoader;
+
+    protected String ADMIN;
+    protected String MEMBER;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
         databaseCleanup.execute();
+        dataLoader.loadData();
+        ADMIN = 로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD);
+        MEMBER = 로그인_되어_있음(MEMBER_EMAIL, MEMBER_PASSWORD);
     }
 }
--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,49 +1,62 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.authentication.FormAuthConfig;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
 
 
 class AuthAcceptanceTest extends AcceptanceTest {
-    private static final String EMAIL = "admin@email.com";
-    private static final String PASSWORD = "password";
-    private static final Integer AGE = 20;
 
     @DisplayName("Basic Auth")
     @Test
     void myInfoWithBasicAuth() {
-        ExtractableResponse<Response> response = 베이직_인증으로_내_회원_정보_조회_요청(EMAIL, PASSWORD);
+        ExtractableResponse<Response> response = 베이직_인증으로_내_회원_정보_조회_요청(ADMIN_EMAIL, ADMIN_PASSWORD);
 
-        회원_정보_조회됨(response, EMAIL, AGE);
+        회원_정보_조회됨(response, ADMIN_EMAIL, ADMIN_AGE);
     }
 
     @DisplayName("Session 로그인 후 내 정보 조회")
     @Test
     void myInfoWithSession() {
-        ExtractableResponse<Response> response = 폼_로그인_후_내_회원_정보_조회_요청(EMAIL, PASSWORD);
+        ExtractableResponse<Response> response = 폼_로그인_후_내_회원_정보_조회_요청(ADMIN_EMAIL, ADMIN_PASSWORD);
 
-        회원_정보_조회됨(response, EMAIL, AGE);
+        회원_정보_조회됨(response, ADMIN_EMAIL, ADMIN_AGE);
     }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
-        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
+        String accessToken = 로그인_되어_있음(ADMIN_EMAIL, ADMIN_PASSWORD);
 
         ExtractableResponse<Response> response = 베어러_인증으로_내_회원_정보_조회_요청(accessToken);
 
-        회원_정보_조회됨(response, EMAIL, AGE);
+        회원_정보_조회됨(response, ADMIN_EMAIL, ADMIN_AGE);
     }
 
     private ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
-        return null;
+        return RestAssured.given().log().all()
+            .auth().form(email, password, new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/members/me")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 
     private ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
-        return null;
+        return RestAssured.given().log().all()
+            .auth().oauth2(accessToken)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/members/me")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 }
