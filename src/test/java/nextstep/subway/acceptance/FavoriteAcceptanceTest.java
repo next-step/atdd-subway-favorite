a65e13baa069000aee373bba5b6564b02e778527
jiwoo <zbqmgldjfh@gmail.com>
2022-08-02 15:31:36 +0900

    test: 즐겨찾기 조회 기능 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
@@ -1,10 +1,15 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.exception.NotFoundStationException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +22,8 @@ import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철
 import static nextstep.subway.acceptance.MemberSteps.관리자_로그인_되어_있음;
 import static nextstep.subway.acceptance.MemberSteps.유저_로그인_되어_있음;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class FavoriteAcceptanceTest extends AcceptanceTest {
 
@@ -80,6 +87,29 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
 
         // then
         즐겨찾기_생성_실패_확인(response, HttpStatus.NOT_FOUND, NotFoundStationException.MESSAGE);
+    }
+
+    @DisplayName("모든 즐겨찾기를 성공적으로 조회한다")
+    @Test
+    public void search_all_favorites() {
+        // given
+        즐겨찾기_생성_요청(accessToken, 교대역, 강남역);
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/favorites")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getList("$..name")).containsExactly("교대역", "강남역")
+        );
     }
 
     private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, Integer distance) {
