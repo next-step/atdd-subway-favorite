8891a51ec86b62d43272381a838816e9b3586a98
imcool2551 <imcool2551@gmail.com>
2022-08-02 15:34:28 +0900

    test: 지하철역 제거시 관리자 권한 필요

--- src/main/java/nextstep/subway/ui/StationController.java
+++ src/main/java/nextstep/subway/ui/StationController.java
@@ -1,5 +1,6 @@
 package nextstep.subway.ui;
 
+import nextstep.auth.secured.Secured;
 import nextstep.subway.applicaion.StationService;
 import nextstep.subway.applicaion.dto.StationRequest;
 import nextstep.subway.applicaion.dto.StationResponse;
@@ -29,6 +30,7 @@ public class StationController {
         return ResponseEntity.ok().body(stationService.findAllStations());
     }
 
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/stations/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         stationService.deleteStationById(id);
--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -3,18 +3,33 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import nextstep.subway.applicaion.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+import static nextstep.subway.acceptance.MemberSteps.로그인_되어_있음;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
-public class StationAcceptanceTest extends AcceptanceTest {
+class StationAcceptanceTest extends AcceptanceTest {
+
+    private static String adminToken;
+    private static String userToken;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        adminToken = 로그인_되어_있음("admin@email.com", "password");
+        userToken = 로그인_되어_있음("member@email.com", "password");
+    }
 
     /**
      * When 지하철역을 생성하면
@@ -62,12 +77,7 @@ public class StationAcceptanceTest extends AcceptanceTest {
         assertThat(stations).hasSize(2);
     }
 
-    /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-     */
-    @DisplayName("지하철역을 제거한다.")
+    @DisplayName("관리자는 지하철역을 제거할 수 있다.")
     @Test
     void deleteStation() {
         // given
@@ -75,9 +85,8 @@ public class StationAcceptanceTest extends AcceptanceTest {
 
         // when
         String location = createResponse.header("location");
-        RestAssured.given().log().all()
-                .when()
-                .delete(location)
+        given(adminToken)
+                .when().delete(location)
                 .then().log().all()
                 .extract();
 
@@ -88,5 +97,26 @@ public class StationAcceptanceTest extends AcceptanceTest {
                         .then().log().all()
                         .extract().jsonPath().getList("name", String.class);
         assertThat(stationNames).doesNotContain("강남역");
+    }
+
+    @DisplayName("일반 사용자는 지하철역을 제거할 수 없다.")
+    @Test
+    void deleteStation_Exception() {
+        // given
+        ExtractableResponse<Response> createResponse = 지하철역_생성_요청("강남역");
+
+        // when
+        String location = createResponse.header("location");
+        ExtractableResponse<Response> deleteResponse = given(userToken)
+                .when().delete(location)
+                .then().log().all()
+                .extract();
+
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    private static RequestSpecification given(String token) {
+        return RestAssured.given().log().all()
+                .auth().oauth2(token);
     }
 }
