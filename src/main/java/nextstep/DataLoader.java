74ee09f17fa6ae4996f86cda8ab41ac64d08bf8d
realrains <real.longrain@gmail.com>
2022-08-02 23:09:21 +0900

    feature: DataLoader > 관리자, 일반 멤버 저장

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,43 @@
 package nextstep;
 
+import java.util.List;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader {
+
+    private final MemberRepository memberRepository;
+
+    public DataLoader(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public void loadData() {
+        memberRepository.save(admin());
+        memberRepository.save(member());
+    }
+
+    private Member admin() {
+        return new Member(
+                "admin@subway.go.kr",
+                "1q2w3e4r!",
+                20,
+                List.of(
+                        RoleType.ROLE_MEMBER.name(),
+                        RoleType.ROLE_ADMIN.name()
+                )
+        );
+    }
+
+    private Member member() {
+        return new Member(
+                "user@subway.go.kr",
+                "qhdks!12",
+                22,
+                List.of(RoleType.ROLE_MEMBER.name())
+        );
     }
 }
