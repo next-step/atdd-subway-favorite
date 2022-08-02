5f71f7369d25adff77be37809d7e77cf1db95c2c
urpark <parkuram12@gmail.com>
2022-08-02 22:22:07 +0900

    feat: session 기반 인증 필터와 bearer 기반 인증 필터 구현

--- src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContext;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.auth.token.JwtTokenProvider;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
     private JwtTokenProvider jwtTokenProvider;
@@ -15,7 +19,19 @@ public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            String token = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
+
+            String userName = jwtTokenProvider.getPrincipal(token);
+            List<String> roles = jwtTokenProvider.getRoles(token);
+
+            Authentication authentication = new Authentication(userName, roles);
+            SecurityContext securityContext = new SecurityContext(authentication);
+            SecurityContextHolder.setContext(securityContext);
+            return true;
+        } catch (Exception e) {
+            return true;
+        }
+
     }
 }
