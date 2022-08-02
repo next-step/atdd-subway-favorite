819794e408a0211e4123759d036bd853000a149d
chosundeveloper <iddaumid@naver.com>
2022-08-02 23:52:57 +0900

    feat : UsernamePasswordAuthenticationFilter 구현

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,6 +1,10 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +19,28 @@ public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            AuthenticationToken token = new AuthenticationToken(username, password);
+
+            LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+
+            if (ObjectUtils.isEmpty(loginMember)) {
+                throw new AuthenticationException();
+            }
+
+            if (!loginMember.checkPassword(token.getCredentials())) {
+                throw new AuthenticationException();
+            }
+
+            Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return true;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
