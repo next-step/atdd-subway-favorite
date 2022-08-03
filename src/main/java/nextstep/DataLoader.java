3a4310ae7b02fb5584dc39a0844310a02359eed9
Shin seongsu <tlstjdtn321@naver.com>
2022-08-03 23:24:48 +0900

    refactor:

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,8 +1,10 @@
 package nextstep;
 
+import static nextstep.member.domain.RoleType.ROLE_ADMIN;
+import static nextstep.member.domain.RoleType.ROLE_MEMBER;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import nextstep.member.application.MemberService;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Component;
@@ -14,7 +16,8 @@ public class DataLoader {
     private final MemberRepository memberRepository;
 
     public void loadData() {
-        Member member = new Member("admin", "password", 30, List.of("ROLE_ADMIN", "ROLE_MEMBER"));
-        memberRepository.save(member);
+        Member admin = new Member("admin@email.com", "password", 20, List.of(ROLE_ADMIN.name()));
+        Member member = new Member("member@email.com", "password", 20, List.of(ROLE_MEMBER.name()));
+        memberRepository.saveAll(List.of(admin, member));
     }
 }
