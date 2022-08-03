9d1c54cb3de4df844984c114eacbc453fc65205e
imcool2551 <imcool2551@gmail.com>
2022-08-03 16:24:32 +0900

    refactor: 인수테스트
    반복 사용되는 문자열 상수화, 인수 조건이 더 잘 들어나도록 리팩토링

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -19,14 +19,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("지하철역 관련 기능")
 class StationAcceptanceTest extends AcceptanceTest {
 
+    private static final String 강남역 = "강남역";
+    private static final String 역삼역 = "역삼역";
+
     @DisplayName("관리자 권한으로 지하철역을 생성한다.")
     @Test
     void createStation() {
         // when
-        지하철역_생성("강남역");
+        지하철역_생성(강남역);
 
         // then
-        지하철역들이_존재한다("강남역");
+        지하철역들이_존재한다(강남역);
     }
 
     @DisplayName("일반 사용자는 지하철역을 생성할 수 없다.")
@@ -43,31 +46,31 @@ class StationAcceptanceTest extends AcceptanceTest {
     @Test
     void getStations() {
         // when
-        지하철역_생성("강남역");
-        지하철역_생성("역삼역");
+        지하철역_생성(강남역);
+        지하철역_생성(역삼역);
 
         // then
-        지하철역들이_존재한다("강남역", "역삼역");
+        지하철역들이_존재한다(강남역, 역삼역);
     }
 
     @DisplayName("관리자는 지하철역을 제거할 수 있다.")
     @Test
     void deleteStation() {
         // given
-        var createResponse = 지하철역_생성("강남역");
+        var createResponse = 지하철역_생성(강남역);
 
         // when
         지하철역_제거(createResponse.header("location"));
 
         // then
-        지하철역이_존재하지_않는다("강남역");
+        지하철역이_존재하지_않는다(강남역);
     }
 
     @DisplayName("일반 사용자는 지하철역을 제거할 수 없다.")
     @Test
     void deleteStation_Exception() {
         // given
-        var createResponse = 지하철역_생성("강남역");
+        var createResponse = 지하철역_생성(강남역);
 
         // when
         var deleteResponse = 일반사용자_권한으로_지하철역_제거(createResponse.header("location"));
@@ -78,7 +81,7 @@ class StationAcceptanceTest extends AcceptanceTest {
 
     private ExtractableResponse<Response> 일반사용자_권한으로_지하철역_생성() {
         return givenUserRole()
-                .body(Map.of("name", "강남역"))
+                .body(Map.of("name", 강남역))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/stations")
                 .then().log().all()
