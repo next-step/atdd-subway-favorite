24e2c82560f4bedbf21fa0c01c934030b6c9b3a2
chkim <cndghks0213@gmail.com>
2022-08-03 20:28:49 +0900

    refactor: UserDetailsService를 통해 auth -> member 의존 제거

--- src/main/java/nextstep/member/application/LoginMemberService.java
+++ src/main/java/nextstep/member/application/LoginMemberService.java
@@ -1,18 +1,20 @@
 package nextstep.member.application;
 
+import nextstep.auth.UserDetailsService;
 import nextstep.member.domain.LoginMember;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoginMemberService {
+public class LoginMemberService implements UserDetailsService {
     private MemberRepository memberRepository;
 
     public LoginMemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Override
     public LoginMember loadUserByUsername(String email) {
         Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
         return LoginMember.of(member);
