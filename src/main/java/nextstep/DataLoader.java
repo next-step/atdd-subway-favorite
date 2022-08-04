ad191e74af71a49b5eace23d7da43b36b59d593e
waterdragon <tndyd5390@naver.com>
2022-08-04 16:55:22 +0900

    feat: DataLoader구현

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,24 @@
 package nextstep;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
 
 @Component
 public class DataLoader {
+    private final String ADMIN_PASSWORD = "password";
+    private final String ADMIN_EMAIL = "admin@admin.com";
+    private final MemberRepository memberRepository;
+
+    public DataLoader(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public void loadData() {
+        memberRepository.save(new Member(ADMIN_EMAIL, ADMIN_PASSWORD, 30, List.of(RoleType.ROLE_ADMIN.name())));
     }
 }
