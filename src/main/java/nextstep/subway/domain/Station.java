23fec3d41762180868e20fb4f8aaf1ada0a8244b
lsj8367 <lsj8367@naver.com>
2022-08-05 22:35:24 +0900

    :sparkles: 날짜 auditing 설정

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -2,13 +2,16 @@ package nextstep.subway.domain;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Station {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
