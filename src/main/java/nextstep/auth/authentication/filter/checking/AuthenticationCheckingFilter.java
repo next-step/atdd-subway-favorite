2b90dce98876805454c8f366a3380e1151405d8c
realrains <real.longrain@gmail.com>
2022-08-05 17:58:50 +0900

    feature: AuthenticationCheckingFilter 정의

--- src/main/java/nextstep/auth/authentication/filter/checking/AuthenticationCheckingFilter.java
+++ src/main/java/nextstep/auth/authentication/filter/checking/AuthenticationCheckingFilter.java
@@ -1,14 +1,15 @@
-package nextstep.auth.authentication.filter.processing;
+package nextstep.auth.authentication.filter.checking;
 
 import nextstep.auth.authentication.AuthenticationToken;
 import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public abstract class AuthenticationProcessingFilter implements HandlerInterceptor {
+public abstract class AuthenticationCheckingFilter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -16,18 +17,15 @@ public abstract class AuthenticationProcessingFilter implements HandlerIntercept
             var authenticationToken = convert(request);
             var authentication = authenticate(authenticationToken);
 
-            processing(authentication, response);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
         } catch (Exception e) {
-            return false;
+            return true;
         }
-        return false;
+        return true;
     }
 
     protected abstract AuthenticationToken convert(HttpServletRequest request) throws IOException;
 
     protected abstract Authentication authenticate(AuthenticationToken authenticationToken);
-
-    protected abstract void processing(Authentication authentication, HttpServletResponse response) throws Exception;
-
 }
