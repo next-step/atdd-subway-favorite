913b2e9283fe5b24f642cbefbf35395d4c34576b
lsj8367 <lsj8367@naver.com>
2022-08-05 22:48:38 +0900

    :sparkles: 전체 필드 생성자 구현

--- src/main/java/nextstep/subway/applicaion/dto/FavoriteRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/FavoriteRequest.java
@@ -5,6 +5,14 @@ public class FavoriteRequest {
     private Long source;
     private Long target;
 
+    public FavoriteRequest() {
+    }
+
+    public FavoriteRequest(final Long source, final Long target) {
+        this.source = source;
+        this.target = target;
+    }
+
     public Long getSource() {
         return source;
     }
--- src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
@@ -3,6 +3,13 @@ package nextstep.subway.applicaion.dto;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {
+    }
+
+    public StationRequest(final String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
