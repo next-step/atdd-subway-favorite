ce5edf542fd95c0f283137495a8b1dc056f739cd
lsj8367 <lsj8367@naver.com>
2022-08-05 22:07:33 +0900

    :sparkles: auditing 구현

---
+++ src/main/java/nextstep/subway/config/JpaConfiguration.java
@@ -0,0 +1,10 @@
+package nextstep.subway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+
+}
