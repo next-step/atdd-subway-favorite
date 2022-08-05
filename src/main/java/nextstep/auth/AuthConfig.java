f0f2df0626ac38f42eb40de720f0ac936ffbf181
Shin seongsu <tlstjdtn321@naver.com>
2022-08-05 21:45:02 +0900

    fix: 필요없는 filter 삭제

--- src/main/java/nextstep/auth/AuthConfig.java
+++ src/main/java/nextstep/auth/AuthConfig.java
@@ -2,14 +2,14 @@ package nextstep.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import nextstep.auth.authentication.BasicAuthenticationFilter;
-import nextstep.auth.authentication.BearerTokenAuthenticationFilter;
-import nextstep.auth.authentication.UsernamePasswordAuthenticationFilter;
 import nextstep.auth.authorization.AuthenticationPrincipalArgumentResolver;
 import nextstep.auth.context.SecurityContextPersistenceFilter;
+import nextstep.auth.interceptor.BasicAuthenticationFilter;
+import nextstep.auth.interceptor.BearerTokenAuthenticationFilter;
+import nextstep.auth.interceptor.TokenAuthenticationInterceptor;
+import nextstep.auth.interceptor.UsernamePasswordAuthenticationFilter;
 import nextstep.auth.token.JwtTokenProvider;
-import nextstep.auth.token.TokenAuthenticationInterceptor;
-import nextstep.member.application.LoginMemberService;
+import nextstep.auth.user.UserDetailsService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,20 +20,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthConfig implements WebMvcConfigurer {
 
-    private final LoginMemberService loginMemberService;
+    private final UserDetailsService userDetailsService;
     private final JwtTokenProvider jwtTokenProvider;
     private final ObjectMapper objectMapper;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecurityContextPersistenceFilter());
-        registry.addInterceptor(new UsernamePasswordAuthenticationFilter(loginMemberService))
+        registry.addInterceptor(new UsernamePasswordAuthenticationFilter(userDetailsService))
             .addPathPatterns("/login/form");
         registry.addInterceptor(
-                new TokenAuthenticationInterceptor(loginMemberService, jwtTokenProvider, objectMapper))
+                new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider, objectMapper))
             .addPathPatterns("/login/token");
-        registry.addInterceptor(new BasicAuthenticationFilter(loginMemberService));
-        registry.addInterceptor(new BearerTokenAuthenticationFilter(jwtTokenProvider));
+        registry.addInterceptor(new BasicAuthenticationFilter(userDetailsService));
+        registry.addInterceptor(new BearerTokenAuthenticationFilter(jwtTokenProvider, userDetailsService));
     }
 
     @Override
