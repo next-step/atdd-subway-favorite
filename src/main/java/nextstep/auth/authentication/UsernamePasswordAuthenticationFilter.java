72269c11cf3720890c37e95266bf8c898b495d5a
최시온 <sochoi@miridih.com>
2022-08-06 13:24:20 +0900

    feature: UsernamePasswordAuthenticationFilter 구현

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,21 +1,53 @@
 package nextstep.auth.authentication;
 
+import lombok.RequiredArgsConstructor;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
+@RequiredArgsConstructor
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
-    private LoginMemberService loginMemberService;
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
 
-    public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
-        this.loginMemberService = loginMemberService;
-    }
+    private final LoginMemberService loginMemberService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            Map<String, String[]> paramMap = request.getParameterMap();
+            String userName = paramMap.get(USERNAME)[0];
+            String password = paramMap.get(PASSWORD)[0];
+
+            AuthenticationToken token = new AuthenticationToken(userName, password);
+
+            LoginMember loginMember = loginMemberService.loadUserByUsername(token.getPrincipal());
+
+            if (loginMember == null) {
+                throw new AuthenticationException();
+            }
+
+            checkPassword(loginMember, token.getPrincipal());
+
+            Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private void checkPassword(LoginMember loginMember, String password) {
+        if (!loginMember.checkPassword(password)) {
+            throw new AuthenticationException();
+        }
     }
 }
