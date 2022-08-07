935b1806bb2128d535436e93d0f4e670097ae432
uram <parkuram12@gmail.com>
2022-08-07 21:52:47 +0900

    modify: AuthConfig

--- src/main/java/nextstep/auth/AuthConfig.java
+++ src/main/java/nextstep/auth/AuthConfig.java
@@ -1,12 +1,15 @@
 package nextstep.auth;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.auth.authentication.AuthenticationToken;
 import nextstep.auth.authentication.nonchain.UsernamePasswordAuthenticationFilter;
 import nextstep.auth.authentication.chain.AuthenticationChainFilter;
+import nextstep.auth.authentication.provider.AuthenticationProvider;
 import nextstep.auth.authorization.AuthenticationPrincipalArgumentResolver;
 import nextstep.auth.context.SecurityContextPersistenceFilter;
 import nextstep.auth.token.JwtTokenProvider;
 import nextstep.auth.authentication.nonchain.TokenAuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,15 +19,17 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 public class AuthConfig implements WebMvcConfigurer {
-    private final UserDetailsService userDetailsService;
     private final JwtTokenProvider jwtTokenProvider;
     private final List<AuthenticationChainFilter> chainFilters;
+
+    @Qualifier("defaultAuthenticationProvider")
+    private final AuthenticationProvider<AuthenticationToken> authenticationProvider;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecurityContextPersistenceFilter());
-        registry.addInterceptor(new UsernamePasswordAuthenticationFilter(userDetailsService)).addPathPatterns("/login/form");
-        registry.addInterceptor(new TokenAuthenticationInterceptor(userDetailsService, jwtTokenProvider)).addPathPatterns("/login/token");
+        registry.addInterceptor(new UsernamePasswordAuthenticationFilter(authenticationProvider)).addPathPatterns("/login/form");
+        registry.addInterceptor(new TokenAuthenticationInterceptor(jwtTokenProvider, authenticationProvider)).addPathPatterns("/login/token");
         chainFilters.forEach(registry::addInterceptor);
     }
 
