f1bbf298f073ab68c0df6893a215894039b7e938
uram <parkuram12@gmail.com>
2022-08-07 15:27:17 +0900

    refactor: UsernamePasswordAuthenticationFilter
    - 테스트가 가능한 단위로 분리

--- src/main/java/nextstep/auth/authentication/nonchain/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/nonchain/UsernamePasswordAuthenticationFilter.java
@@ -3,6 +3,8 @@ package nextstep.auth.authentication.nonchain;
 import lombok.RequiredArgsConstructor;
 import nextstep.auth.User;
 import nextstep.auth.authentication.AuthenticationException;
+import nextstep.auth.authentication.AuthenticationToken;
+import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContextMapper;
 import nextstep.auth.UserDetailsService;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,16 +18,27 @@ public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
-        String userEmail = request.getParameter("username");
-        String password = request.getParameter("password");
-        User userDetails = userDetailsService.loadUserByUsername(userEmail);
+        AuthenticationToken authenticationToken = convert(request);
 
-        if(!userDetails.checkPassword(password)) {
-            throw new AuthenticationException();
-        }
+        Authentication authenticate = authenticate(authenticationToken);
 
-        SecurityContextMapper.setContext(userDetails.getUsername(), userDetails.getAuthorities());
+        SecurityContextMapper.setContext(authenticate.getPrincipal().toString(), authenticate.getAuthorities());
         return false;
     }
 
+    public AuthenticationToken convert(HttpServletRequest request) {
+        String userEmail = request.getParameter("username");
+        String password = request.getParameter("password");
+        return new AuthenticationToken(userEmail, password);
+    }
+
+    public Authentication authenticate(AuthenticationToken authenticationToken) {
+        User user = userDetailsService.loadUserByUsername(authenticationToken.getPrincipal());
+
+        if(!user.checkPassword(authenticationToken.getCredentials())) {
+            throw new AuthenticationException();
+        }
+
+        return new Authentication(user.getUsername(),user.getAuthorities());
+    }
 }
