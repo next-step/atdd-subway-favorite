62ae329b2ed753693094dd29ef5f2d0f0274c3be
MRSTK90 <dkgell13@gmail.com>
2022-08-08 02:10:50 +0900

    new : 생성/수정 시간 자동 업데이트를 위한 클래스 설정

---
+++ src/main/java/nextstep/favorite/domain/BaseTimeEntity.java
@@ -0,0 +1,36 @@
+package nextstep.favorite.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,12 +1,14 @@
 package nextstep.subway.domain;
 
+import nextstep.favorite.domain.BaseTimeEntity;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Station {
+public class Station extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
