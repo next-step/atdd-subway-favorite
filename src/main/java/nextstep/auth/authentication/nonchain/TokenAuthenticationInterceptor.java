49913e28699dd4f1bfa841e4b011245bc7281012
uram <parkuram12@gmail.com>
2022-08-07 14:49:41 +0900

    refactor: TokenAuthenticationInterceptor 테스트가 가능한 단위로 쪼개기

--- src/main/java/nextstep/auth/authentication/nonchain/TokenAuthenticationInterceptor.java
+++ src/main/java/nextstep/auth/authentication/nonchain/TokenAuthenticationInterceptor.java
@@ -2,8 +2,10 @@ package nextstep.auth.authentication.nonchain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import nextstep.auth.CustomUserDetails;
+import nextstep.auth.User;
 import nextstep.auth.authentication.AuthenticationException;
+import nextstep.auth.authentication.AuthenticationToken;
+import nextstep.auth.context.Authentication;
 import nextstep.auth.token.JwtTokenProvider;
 import nextstep.auth.token.TokenRequest;
 import nextstep.auth.token.TokenResponse;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -22,19 +25,11 @@ public class TokenAuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        TokenRequest tokenRequest = new ObjectMapper().readValue(content, TokenRequest.class);
+        AuthenticationToken authenticationToken = convert(request);
 
-        String principal = tokenRequest.getEmail();
-        String credentials = tokenRequest.getPassword();
+        Authentication authenticate = authenticate(authenticationToken);
 
-        CustomUserDetails userDetails = userDetailsService.loadUserByUsername(principal);
-
-        if (!userDetails.checkPassword(credentials)) {
-            throw new AuthenticationException();
-        }
-
-        String token = jwtTokenProvider.createToken(userDetails.getUsername(), userDetails.getAuthorities());
+        String token = jwtTokenProvider.createToken(authenticate.getPrincipal().toString(), authenticate.getAuthorities());
         TokenResponse tokenResponse = new TokenResponse(token);
 
         String responseToClient = new ObjectMapper().writeValueAsString(tokenResponse);
@@ -43,5 +38,21 @@ public class TokenAuthenticationInterceptor implements HandlerInterceptor {
         response.getOutputStream().print(responseToClient);
 
         return false;
+    }
+
+    public AuthenticationToken convert(HttpServletRequest request) throws IOException {
+        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        TokenRequest tokenRequest = new ObjectMapper().readValue(content, TokenRequest.class);
+        return new AuthenticationToken(tokenRequest.getEmail(), tokenRequest.getPassword());
+    }
+
+    public Authentication authenticate(AuthenticationToken authenticationToken) {
+        User user = userDetailsService.loadUserByUsername(authenticationToken.getPrincipal());
+
+        if (!user.checkPassword(authenticationToken.getCredentials())) {
+            throw new AuthenticationException();
+        }
+
+        return new Authentication(user.getUsername(), user.getAuthorities());
     }
 }
