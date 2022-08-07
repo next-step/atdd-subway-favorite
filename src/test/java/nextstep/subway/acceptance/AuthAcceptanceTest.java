9962af2aa950e6b6069d69b20ffb55f9cdaa3897
beadoer1 <beadoer1@gmail.com>
2022-08-07 16:23:30 +0900

    test: 폼 로그인 회원 정보 조회 테스트 fail 작성

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,16 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
 
@@ -47,7 +53,16 @@ class AuthAcceptanceTest extends AcceptanceTest {
     }
 
     private ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
-        return null;
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        return RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .params(params)
+                .when().get("/members/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
     }
 
     private ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
