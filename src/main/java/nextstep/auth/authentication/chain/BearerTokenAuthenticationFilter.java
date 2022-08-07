761cfd4a3a67c5310563809478aa8846726f2d54
uram <parkuram12@gmail.com>
2022-08-07 19:42:09 +0900

    refactor: BearerTokenAuthenticationFilter 테스트가 가능한 단위로 메서드 추출

--- src/main/java/nextstep/auth/authentication/chain/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/chain/BearerTokenAuthenticationFilter.java
@@ -2,8 +2,10 @@ package nextstep.auth.authentication.chain;
 
 
 import lombok.RequiredArgsConstructor;
+import nextstep.auth.authentication.AuthenticationException;
 import nextstep.auth.authentication.AuthorizationExtractor;
 import nextstep.auth.authentication.AuthorizationType;
+import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContextMapper;
 import nextstep.auth.token.JwtTokenProvider;
 import org.springframework.stereotype.Component;
@@ -22,13 +24,20 @@ public class BearerTokenAuthenticationFilter implements AuthenticationChainFilte
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String token = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
 
-        if(jwtTokenProvider.validateToken(token)) {
-            String userName = jwtTokenProvider.getPrincipal(token);
-            List<String> roles = jwtTokenProvider.getRoles(token);
-            SecurityContextMapper.setContext(userName, roles);
+        if(!jwtTokenProvider.validateToken(token)) {
+            throw new AuthenticationException();
         }
+
+        Authentication authenticate = authenticate(token);
+
+        SecurityContextMapper.setContext(authenticate.getPrincipal().toString(), authenticate.getAuthorities() );
 
         return true;
     }
 
+    public Authentication authenticate(String token) {
+        String userName = jwtTokenProvider.getPrincipal(token);
+        List<String> roles = jwtTokenProvider.getRoles(token);
+        return new Authentication(userName, roles);
+    }
 }
