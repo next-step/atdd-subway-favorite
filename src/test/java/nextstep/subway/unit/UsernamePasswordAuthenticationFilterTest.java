b4b3d811063b2a5ef5a35bc7a2e54a92be5e4494
uram <parkuram12@gmail.com>
2022-08-07 21:49:13 +0900

    test: UsernamePasswordAuthenticationFilter

--- src/test/java/nextstep/subway/unit/UsernamePasswordAuthenticationFilterTest.java
+++ src/test/java/nextstep/subway/unit/UsernamePasswordAuthenticationFilterTest.java
@@ -1,13 +1,12 @@
 package nextstep.subway.unit;
 
-import nextstep.auth.User;
-import nextstep.auth.UserDetailsService;
 import nextstep.auth.authentication.AuthenticationToken;
 import nextstep.auth.authentication.nonchain.UsernamePasswordAuthenticationFilter;
+import nextstep.auth.authentication.provider.AuthenticationProvider;
 import nextstep.auth.context.Authentication;
 import nextstep.auth.context.SecurityContext;
 import nextstep.auth.context.SecurityContextHolder;
-import nextstep.member.domain.LoginMember;
+import nextstep.member.domain.Member;
 import nextstep.member.domain.RoleType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,46 +16,21 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UsernamePasswordAuthenticationFilterTest {
 
     @Mock
-    UserDetailsService userDetailsService;
-
-    LoginMember loginMember = new LoginMember(
-            "parkuram12@gmail.com",
-            "password"
-            ,List.of(RoleType.ROLE_MEMBER.name())
-    );
-
+    AuthenticationProvider<AuthenticationToken> authenticationProvider;
 
     @InjectMocks
     UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter;
 
-    @Test
-    void preHandle() {
-        //given
-        when(userDetailsService.loadUserByUsername(anyString()))
-                .thenReturn(new User(loginMember.getUsername(), loginMember.getPassword(), loginMember.getAuthorities()));
-        MockHttpServletRequest mockRequest = createMockRequest();
-        MockHttpServletResponse mockResponse = createMockResponse();
+    Member user = Member.createUser("user1@gmail.com", "pass" , 23);
 
-        //when
-        boolean isChain = usernamePasswordAuthenticationFilter.preHandle(mockRequest, mockResponse, new Object());
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-
-        //then
-        assertThat(isChain).isFalse();
-        assertThat(authentication.getPrincipal()).isEqualTo(loginMember.getUsername());
-        assertThat(authentication.getAuthorities()).contains(RoleType.ROLE_MEMBER.name());
-    }
 
     @Test
     void convert() {
@@ -67,36 +41,51 @@ class UsernamePasswordAuthenticationFilterTest {
         AuthenticationToken authenticationToken = usernamePasswordAuthenticationFilter.convert(mockRequest);
 
         //then
-        assertThat(authenticationToken.getPrincipal()).isEqualTo(loginMember.getUsername());
-        assertThat(authenticationToken.getCredentials()).isEqualTo(loginMember.getPassword());
+        assertThat(authenticationToken.getPrincipal()).isEqualTo(user.getEmail());
+        assertThat(authenticationToken.getCredentials()).isEqualTo(user.getPassword());
     }
 
     @Test
     void authenticate() {
         //given
-        when(userDetailsService.loadUserByUsername(anyString()))
-                .thenReturn(new User(loginMember.getUsername(), loginMember.getPassword(), loginMember.getAuthorities()));
+        when(authenticationProvider.authenticate(any()))
+                .thenReturn(new Authentication(user.getEmail(), user.getRoles()));
 
         //when
-        Authentication authenticate = usernamePasswordAuthenticationFilter.authenticate(
-                new AuthenticationToken(loginMember.getUsername(), loginMember.getPassword())
+        Authentication authenticate = usernamePasswordAuthenticationFilter.authentication(
+                new AuthenticationToken(user.getEmail(), user.getPassword())
         );
 
         //then
-        assertThat(authenticate.getPrincipal()).isEqualTo(loginMember.getUsername());
+        assertThat(authenticate.getPrincipal()).isEqualTo(user.getEmail());
         assertThat(authenticate.getAuthorities()).contains(RoleType.ROLE_MEMBER.name());
     }
 
+    @Test
+    void preHandle() throws Exception {
+        //given
+        when(authenticationProvider.authenticate(any()))
+                .thenReturn(new Authentication(user.getEmail(), user.getRoles()));
+
+        boolean isChain = usernamePasswordAuthenticationFilter.preHandle(createMockRequest(), createMockResponse(), new Object());
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+
+        assertThat(isChain).isFalse();
+        assertThat(authentication.getPrincipal()).isEqualTo(user.getEmail());
+        assertThat(authentication.getAuthorities()).contains(RoleType.ROLE_MEMBER.name());
+    }
+
+
     private MockHttpServletRequest createMockRequest() {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setParameter("username", loginMember.getUsername());
-        request.setParameter("password", loginMember.getPassword());
+        request.setParameter("username", user.getEmail());
+        request.setParameter("password", user.getPassword());
         return request;
     }
 
-    private MockHttpServletResponse createMockResponse() {
+    private MockHttpServletResponse createMockResponse(){
         return new MockHttpServletResponse();
     }
-
 
 }
