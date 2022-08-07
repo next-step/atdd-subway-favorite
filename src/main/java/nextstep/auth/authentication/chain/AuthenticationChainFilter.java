dba068cd2f7cbeb7145dacae5614a111b35d6b2e
uram <parkuram12@gmail.com>
2022-08-07 21:51:13 +0900

    refactor: AuthenticationNonChainFilter, AuthenticationChainFilter 추상 클래스로 분리

--- src/main/java/nextstep/auth/authentication/chain/AuthenticationChainFilter.java
+++ src/main/java/nextstep/auth/authentication/chain/AuthenticationChainFilter.java
@@ -1,6 +1,29 @@
 package nextstep.auth.authentication.chain;
 
+import nextstep.auth.authentication.AuthenticationToken;
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextMapper;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public interface AuthenticationChainFilter extends HandlerInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class AuthenticationChainFilter implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        try {
+            AuthenticationToken token = convert(request);
+
+            Authentication authentication = authentication(token);
+
+            SecurityContextMapper.setContext(authentication.getPrincipal().toString(), authentication.getAuthorities());
+            return true;
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    public abstract AuthenticationToken convert(HttpServletRequest request);
+    public abstract Authentication authentication(AuthenticationToken authenticationToken);
 }
