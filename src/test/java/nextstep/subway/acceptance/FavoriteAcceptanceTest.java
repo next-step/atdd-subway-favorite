c782710e7e3ba445bae666c75ad0610f50f8f046
sangnyyy <dltkdals2202@gmail.com>
2022-08-08 02:01:44 +0900

    test : 즐겨찾기 삭제 시 인증되지 못할 때 에러 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.acceptance.FavoriteSteps.*;
+import static nextstep.subway.acceptance.MemberSteps.로그인_되어_있음;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,7 +46,7 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("즐겨찾기 추가 시 미 인증 에러")
     @Test
-    void createFavoriteUnauthorizedException() {
+    void createFavoriteAuthenticateException() {
         //given
         long 교대역 = 지하철역_생성_요청("교대역", ACCESS_TOKEN).jsonPath().getLong("id");
         long 남부터미널역 = 지하철역_생성_요청("남부터미널역", ACCESS_TOKEN).jsonPath().getLong("id");
@@ -54,6 +55,29 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
         //when
         final String weirdToken = "abcd";
         ExtractableResponse<Response> createdResponse = 즐겨찾기_생성_요청(교대역, 양재역, weirdToken);
+
+        //then
+        assertThat(createdResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+
+    }
+
+
+    /**
+     * Given 역을 미리 추가해놓고
+     * When 권한이 없는 계정으로 즐겨찾기를 추가 시
+     * Then 에러가 발생한다
+     */
+    @DisplayName("즐겨찾기 추가 시 권한이 없을 때")
+    @Test
+    void createFavoriteUnauthorizedException() {
+        //given
+        long 교대역 = 지하철역_생성_요청("교대역", ACCESS_TOKEN).jsonPath().getLong("id");
+        long 남부터미널역 = 지하철역_생성_요청("남부터미널역", ACCESS_TOKEN).jsonPath().getLong("id");
+        long 양재역 = 지하철역_생성_요청("양재역", ACCESS_TOKEN).jsonPath().getLong("id");
+
+        //when
+        String accessToken = 로그인_되어_있음("member@email.com", "password");
+        ExtractableResponse<Response> createdResponse = 즐겨찾기_생성_요청(교대역, 양재역, accessToken);
 
         //then
         assertThat(createdResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
@@ -111,6 +135,31 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
 
         ExtractableResponse<Response> getResponse = 즐겨찾기_목록_조회_요청();
         assertThat(getResponse.jsonPath().getList("")).isEmpty();
+    }
+
+
+    /**
+     * Given 역과 즐겨찾기를 추가해놓고
+     * When 즐겨찾기를 삭제 시 인증되지 않으면
+     * Then 에러가 발생한다
+     */
+    @DisplayName("즐겨찾기 삭제")
+    @Test
+    void deleteFavoriteAuthenticationException() {
+        //given
+        long 교대역 = 지하철역_생성_요청("교대역", ACCESS_TOKEN).jsonPath().getLong("id");
+        long 남부터미널역 = 지하철역_생성_요청("남부터미널역", ACCESS_TOKEN).jsonPath().getLong("id");
+        long 양재역 = 지하철역_생성_요청("양재역", ACCESS_TOKEN).jsonPath().getLong("id");
+
+
+        Long 즐겨찾기 = 즐겨찾기_생성_요청(교대역, 양재역, ACCESS_TOKEN).jsonPath().getLong("id");
+
+        //when
+        final String weirdToken = "abcd";
+        ExtractableResponse<Response> deletedResponse = 즐겨찾기_목록_삭제_요청(즐겨찾기, weirdToken);
+
+        //then
+        assertThat(deletedResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
 }
