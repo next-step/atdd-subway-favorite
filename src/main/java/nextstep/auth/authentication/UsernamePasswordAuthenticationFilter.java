891ae8e6997abc7109820e7ac1b4b5b9026fa73f
nara1030 <nara1030@naver.com>
2022-08-08 15:05:30 +0900

    feat/test] Form 기반 로그인

--- src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/UsernamePasswordAuthenticationFilter.java
@@ -1,12 +1,19 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.member.application.LoginMemberService;
+import nextstep.member.domain.LoginMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor {
+    public static final String USERNAME_FIELD = "username";
+    public static final String PASSWORD_FIELD = "password";
+
     private LoginMemberService loginMemberService;
 
     public UsernamePasswordAuthenticationFilter(LoginMemberService loginMemberService) {
@@ -15,7 +22,31 @@ public class UsernamePasswordAuthenticationFilter implements HandlerInterceptor 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            Map<String, String[]> paramMap = request.getParameterMap();
+            String username = paramMap.get(USERNAME_FIELD)[0];
+            String password = paramMap.get(PASSWORD_FIELD)[0];
+
+            AuthenticationToken token = new AuthenticationToken(username, password);
+
+            String principal = token.getPrincipal();
+            LoginMember loginMember = loginMemberService.loadUserByUsername(principal);
+
+            if (loginMember == null) {
+                throw new AuthenticationException();
+            }
+
+            if (!loginMember.checkPassword(token.getCredentials())) {
+                throw new AuthenticationException();
+            }
+
+            Authentication authentication = new Authentication(loginMember.getEmail(), loginMember.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return false;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
