efeeaea57d83b5cfe06ce0e44f4bc5bbb3656dbb
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-09 17:15:32 +0900

    Test : FavoriteServiceMockTest 구현

--- src/main/java/nextstep/subway/applicaion/dto/FavoriteRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/FavoriteRequest.java
@@ -4,6 +4,14 @@ public class FavoriteRequest {
     private long source;
     private long target;
 
+    public FavoriteRequest() {
+    }
+
+    public FavoriteRequest(long source, long target) {
+        this.source = source;
+        this.target = target;
+    }
+
     public long getSource() {
         return source;
     }
---
+++ src/test/java/nextstep/subway/unit/FavoriteServiceMockTest.java
@@ -0,0 +1,117 @@
+package nextstep.subway.unit;
+
+import nextstep.auth.authentication.AuthenticationException;
+import nextstep.auth.member.LoginMember;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
+import nextstep.subway.applicaion.FavoriteService;
+import nextstep.subway.applicaion.dto.FavoriteRequest;
+import nextstep.subway.applicaion.dto.FavoritesResponse;
+import nextstep.subway.domain.Favorite;
+import nextstep.subway.domain.FavoriteRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FavoriteServiceMockTest {
+
+    public static final LoginMember LOGIN_MEMBER = new LoginMember("user@email.com", "password", List.of(RoleType.ROLE_MEMBER.name()));
+    public static final Member MEMBER = new Member(1L, "user@email.com", "password", 20, List.of(RoleType.ROLE_MEMBER.name()));
+    public static final long 강남역_ID = 1L;
+    public static final long 역삼역_ID = 2L;
+    public static final Favorite 즐겨찾기 = new Favorite(1L, MEMBER.getId(), 강남역_ID, 역삼역_ID);
+    public static final Favorite 다른_회원의_즐겨찾기 = new Favorite(1L, 999L, 강남역_ID, 역삼역_ID);
+    public static final Station 강남역 = new Station("강남역");
+    public static final Station 역삼역 = new Station("역삼역");
+    @Mock
+    FavoriteRepository favoriteRepository;
+    @Mock
+    MemberRepository memberRepository;
+    @Mock
+    StationRepository stationRepository;
+
+    @Autowired
+    FavoriteService favoriteService;
+
+    @BeforeEach
+    void setUp() {
+        favoriteService = new FavoriteService(favoriteRepository, memberRepository, stationRepository);
+    }
+
+    @Test
+    @DisplayName("로그인한 사용자의 정보와 출발역, 그리고 도착역을 가져와 즐겨찾기를 등록합니다.")
+    void saveFavorite() {
+        // given
+        FavoriteRequest 요청 = new FavoriteRequest(강남역_ID, 역삼역_ID);
+
+        // when
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.of(MEMBER));
+        when(stationRepository.findById(강남역_ID)).thenReturn(Optional.of(강남역));
+        when(stationRepository.findById(역삼역_ID)).thenReturn(Optional.of(역삼역));
+        when(favoriteRepository.save(any())).thenReturn(즐겨찾기);
+
+        // then
+        assertDoesNotThrow(
+            () -> favoriteService.saveFavorite(LOGIN_MEMBER, 요청)
+        );
+    }
+
+    @Test
+    @DisplayName("로그인한 사용자의 정보를 가져와 사용자의 즐겨찾기 목록을 조회합니다.")
+    void findFavorites() {
+        // when
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.of(MEMBER));
+        when(stationRepository.findById(강남역_ID)).thenReturn(Optional.of(강남역));
+        when(stationRepository.findById(역삼역_ID)).thenReturn(Optional.of(역삼역));
+        when(favoriteRepository.findAllByMemberId(MEMBER.getId())).thenReturn(List.of(즐겨찾기));
+
+        // then
+        FavoritesResponse favorites = favoriteService.findFavorites(LOGIN_MEMBER);
+        assertThat(favorites.getFavorites()).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("로그인한 사용자의 정보와 즐겨찾기의 식별자를 가져와 즐겨찾기를 삭제합니다.")
+    void deleteFavorite() {
+        // when
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.of(MEMBER));
+        when(favoriteRepository.findById(즐겨찾기.getId())).thenReturn(Optional.of(즐겨찾기));
+        doNothing().when(favoriteRepository).delete(즐겨찾기);
+
+        // then
+        assertDoesNotThrow(
+            () -> favoriteService.deleteFavorite(LOGIN_MEMBER, 즐겨찾기.getId())
+        );
+    }
+
+    @Test
+    @DisplayName("등록한 사용자가 아닌 경우 Unauthorized 예외가 발생합니다.")
+    void deleteFavorite_invalid_memberId() {
+        // when
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.of(MEMBER));
+        when(favoriteRepository.findById(즐겨찾기.getId())).thenReturn(Optional.of(다른_회원의_즐겨찾기));
+
+        // then
+        assertThatThrownBy(
+            () -> favoriteService.deleteFavorite(LOGIN_MEMBER, 즐겨찾기.getId())
+        ).isInstanceOf(AuthenticationException.class);
+    }
+}
