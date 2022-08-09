2b30c0387b0031b4e5ed6ad1793e7f442e16ac20
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-09 16:38:18 +0900

    Feat : Favorite 도메인 구현

---
+++ src/main/java/nextstep/subway/domain/Favorite.java
@@ -0,0 +1,35 @@
+package nextstep.subway.domain;
+
+public class Favorite {
+    private Long id;
+    private Long memberId;
+    private Long sourceId;
+    private Long targetId;
+
+    public Favorite(Long id, Long memberId, Long sourceId, Long targetId) {
+        this.id = id;
+        this.memberId = memberId;
+        this.sourceId = sourceId;
+        this.targetId = targetId;
+    }
+
+    public Favorite(Long memberId, Long sourceId, Long targetId) {
+        this(null, memberId, sourceId, targetId);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Long getSourceId() {
+        return sourceId;
+    }
+
+    public Long getTargetId() {
+        return targetId;
+    }
+}
