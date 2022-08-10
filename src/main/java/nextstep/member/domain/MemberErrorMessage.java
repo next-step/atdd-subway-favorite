8b9fe17fa45528b3792d2253421c064df97a7bd6
uram <parkuram12@gmail.com>
2022-08-10 14:07:19 +0900

    feat: RoleAuthenticationException 핸들러 추가

--- src/main/java/nextstep/member/domain/MemberErrorMessage.java
+++ src/main/java/nextstep/member/domain/MemberErrorMessage.java
@@ -9,5 +9,5 @@ public enum MemberErrorMessage {
     UNAUTHORIZED("접근 권한이 없습니다."),
     NOT_FOUND_FAVORITE("즐겨찾기를 찾을 수 없습니다.");
 
-    private String message;
+    private final String message;
 }
--- src/main/java/nextstep/member/ui/MemberGlobalControllerExceptionHandler.java
+++ src/main/java/nextstep/member/ui/MemberGlobalControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package nextstep.member.ui;
 
-import nextstep.member.ui.exception.ErrorResponse;
+import nextstep.auth.secured.RoleAuthenticationException;
+import nextstep.member.domain.MemberErrorMessage;
+import nextstep.common.exception.ErrorResponse;
 import nextstep.member.ui.exception.FavoriteOwnerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +16,14 @@ public class MemberGlobalControllerExceptionHandler {
     public ResponseEntity<ErrorResponse> favoriteOwnerException(FavoriteOwnerException e) {
         ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), e.getMessage());
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(RoleAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> roleAuthenticationException(RoleAuthenticationException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(
+                HttpStatus.UNAUTHORIZED.value(),
+                MemberErrorMessage.UNAUTHORIZED.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 }
