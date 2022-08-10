bf24cdd26265ab7e09af15e77068cf13669e7591
uram <parkuram12@gmail.com>
2022-08-10 14:06:54 +0900

    feat: MemberExceptionMessage Enum 추가

---
+++ src/main/java/nextstep/member/domain/MemberErrorMessage.java
@@ -0,0 +1,13 @@
+package nextstep.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum MemberErrorMessage {
+    UNAUTHORIZED("접근 권한이 없습니다."),
+    NOT_FOUND_FAVORITE("즐겨찾기를 찾을 수 없습니다.");
+
+    private String message;
+}
