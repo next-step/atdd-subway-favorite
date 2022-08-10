8f38f43ab7ebef4a3fcaa8ab23f4a384417e7a6f
MRSTK90 <dkgell13@gmail.com>
2022-08-10 15:31:17 +0900

    fix : 정적 팩토리 메서드 삭제

--- src/main/java/nextstep/favorite/application/dto/FavoriteResponse.java
+++ src/main/java/nextstep/favorite/application/dto/FavoriteResponse.java
@@ -3,8 +3,6 @@ package nextstep.favorite.application.dto;
 import nextstep.favorite.domain.Favorite;
 import nextstep.subway.domain.Station;
 
-import java.time.LocalDateTime;
-
 public class FavoriteResponse {
     private Long id;
     private Station source;
@@ -14,18 +12,10 @@ public class FavoriteResponse {
         this.id = id;
     }
 
-    public FavoriteResponse(Long id, Station source, Station target, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.id = id;
-        this.source = source;
-        this.target = target;
-        this.source.setCreatedDate(createdDate);
-        this.target.setCreatedDate(createdDate);
-        this.source.setModifiedDate(modifiedDate);
-        this.target.setModifiedDate(modifiedDate);
-    }
-
-    public static FavoriteResponse of(Favorite favorite) {
-        return new FavoriteResponse(favorite.getId(), favorite.getSource(), favorite.getTarget(), favorite.getCreatedDate(), favorite.getModifiedDate());
+    public FavoriteResponse(Favorite favorite) {
+        this.id = favorite.getId();
+        this.source = favorite.getSource();
+        this.target = favorite.getTarget();
     }
 
     public Long getId() {
