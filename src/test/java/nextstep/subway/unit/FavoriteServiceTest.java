f64eb0fc0a72964a4f1f731d89836889fbe42354
MRSTK90 <dkgell13@gmail.com>
2022-08-10 16:17:29 +0900

    new : 서비스 예외처리 내용 추가

--- src/test/java/nextstep/subway/unit/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/unit/FavoriteServiceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.unit;
 import nextstep.favorite.application.FavoriteService;
 import nextstep.favorite.application.dto.FavoriteRequest;
 import nextstep.favorite.application.dto.FavoriteResponse;
+import nextstep.favorite.exception.FavoriteException;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberDetails;
 import nextstep.member.domain.MemberRepository;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -25,6 +27,8 @@ public class FavoriteServiceTest {
     FavoriteRequest request;
     Member member;
     MemberDetails memberDetails;
+    Member anotherMember;
+    MemberDetails anotherMemberDetails;
     @Autowired
     private FavoriteService favoriteService;
     @Autowired
@@ -33,15 +37,21 @@ public class FavoriteServiceTest {
     private StationRepository stationRepository;
     private Station magok;
     private Station balsan;
+    private Station woojangsan;
 
     @BeforeEach
     void setUp() {
         magok = stationRepository.save(new Station("magok"));
         balsan = stationRepository.save(new Station("balsan"));
+        woojangsan = stationRepository.save(new Station("woojangsan"));
         request = new FavoriteRequest(magok.getId(), balsan.getId());
 
         member = memberRepository.save(new Member("user@email.com", "password", 22));
         memberDetails = new MemberDetails(member.getEmail(), member.getPassword(), List.of("ROLE_MEMBER"));
+
+        anotherMember = memberRepository.save(new Member("another@email.com", "password", 30));
+        anotherMemberDetails = new MemberDetails(anotherMember.getEmail(), anotherMember.getPassword(), List.of("ROLE_MEMBER"));
+
 
     }
 
@@ -86,5 +96,38 @@ public class FavoriteServiceTest {
         //then
         assertThat(favoriteService.getFavorites(memberDetails)).hasSize(0);
     }
+
+
+    @DisplayName("즐겨찾기 등록하지 않은 상태로 즐겨찾기 조회")
+    @Test
+    void canNotGetFavoriteWithNotRegistMember() {
+        //when then
+        assertThatThrownBy(() -> favoriteService.getFavorite(memberDetails, request))
+                .isInstanceOf(FavoriteException.class);
+    }
+
+    @DisplayName("다른사람이 등록한 즐겨찾기 조회")
+    @Test
+    void canNotGetOtherFavorite() {
+        //given
+        favoriteService.createFavorite(memberDetails, request);
+
+        //when then
+        assertThatThrownBy(() -> favoriteService.getFavorite(anotherMemberDetails, request))
+                .isInstanceOf(FavoriteException.class);
+    }
+
+    @DisplayName("본인이 등록하지 않은 역에 대한 정보로 조회하기")
+    @Test
+    void canNotgetFavoriteWithIllegalStationInfo() {
+        //given
+        favoriteService.createFavorite(memberDetails, request);
+        FavoriteRequest otherRequest = new FavoriteRequest(balsan.getId(), woojangsan.getId());
+
+        //when then
+        assertThatThrownBy(() -> favoriteService.getFavorite(memberDetails, otherRequest))
+                .isInstanceOf(FavoriteException.class);
+    }
+
 
 }
