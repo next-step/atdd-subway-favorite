01856b2f443c3bc43226a55ac3a2180a652d9342
Shin seongsu <tlstjdtn321@naver.com>
2022-08-10 18:30:41 +0900

    fix: 테스트로 만든 회원 삭제

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -2,7 +2,6 @@ package nextstep;
 
 import static nextstep.member.domain.RoleType.ROLE_ADMIN;
 import static nextstep.member.domain.RoleType.ROLE_MEMBER;
-import static nextstep.member.domain.RoleType.ROLE_NOT_LOGIN;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +18,6 @@ public class DataLoader {
     public void loadData() {
         Member admin = new Member("admin@email.com", "password", 20, List.of(ROLE_ADMIN.name()));
         Member member = new Member("member@email.com", "password", 20, List.of(ROLE_MEMBER.name()));
-        Member user = new Member("user@email.com","password", 20, List.of(ROLE_NOT_LOGIN.name()));
-        memberRepository.saveAll(List.of(admin, member, user));
+        memberRepository.saveAll(List.of(admin, member));
     }
 }
--- src/main/java/nextstep/member/domain/RoleType.java
+++ src/main/java/nextstep/member/domain/RoleType.java
@@ -2,6 +2,5 @@ package nextstep.member.domain;
 
 public enum RoleType {
     ROLE_ADMIN,
-    ROLE_MEMBER,
-    ROLE_NOT_LOGIN
+    ROLE_MEMBER;
 }
