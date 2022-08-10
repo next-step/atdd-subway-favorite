604ddb5d8c32ffe0b56c37aa1c854a2a0cd7af49
MRSTK90 <dkgell13@gmail.com>
2022-08-10 16:17:10 +0900

    new : getFavorite Response 스펙 변경을 적용하기 위한 클래스 생성

---
+++ src/main/java/nextstep/favorite/application/dto/MemberFavoriteResponse.java
@@ -0,0 +1,13 @@
+package nextstep.favorite.application.dto;
+
+public class MemberFavoriteResponse {
+    private Long id;
+
+    public MemberFavoriteResponse(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
