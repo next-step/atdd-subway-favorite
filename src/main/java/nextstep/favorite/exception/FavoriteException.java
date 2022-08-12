c7ca7c59d0126951ae851f6854700a282b695f2c
MRSTK90 <dkgell13@gmail.com>
2022-08-12 19:58:15 +0900

    fix : Stauts Code 수정

--- src/main/java/nextstep/favorite/exception/FavoriteException.java
+++ src/main/java/nextstep/favorite/exception/FavoriteException.java
@@ -3,7 +3,7 @@ package nextstep.favorite.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class FavoriteException extends RuntimeException {
     public FavoriteException(String message) {
         super(message);
