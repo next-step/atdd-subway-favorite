bf6f801741e5f1eb449545a9c4ba7eb8600b1e9a
김현수 <hsu.kim@sk.com>
2022-08-15 21:46:39 +0900

    refactor: 메서드 위치 변경

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -43,24 +43,4 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
         회원_정보_조회됨(response, EMAIL, AGE);
     }
-
-    public static ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
-        return RestAssured
-            .given().log().all()
-            .auth().form(email, password, new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().get("/members/me")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value()).extract();
-    }
-
-    public static ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
-        return RestAssured.given().log().all()
-                          .auth().oauth2(accessToken)
-                          .accept(MediaType.APPLICATION_JSON_VALUE)
-                          .when().get("/members/me")
-                          .then().log().all()
-                          .statusCode(HttpStatus.OK.value())
-                          .extract();
-    }
 }
