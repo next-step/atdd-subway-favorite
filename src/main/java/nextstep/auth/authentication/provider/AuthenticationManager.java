a7c0499cd96b5ae00b685b40954882e999493efc
epicarts <0505zxc@gmail.com>
2022-08-16 18:14:39 +0900

    feat: Spring Security 구조로 리펙토링

--- src/main/java/nextstep/auth/authentication/provider/AuthenticationManager.java
+++
@@ -1,8 +0,0 @@
-package nextstep.auth.authentication.provider;
-
-import nextstep.auth.authentication.AuthenticationException;
-import nextstep.auth.context.Authentication;
-
-public interface AuthenticationManager {
-    Authentication authenticate(Authentication authentication) throws AuthenticationException;
-}
