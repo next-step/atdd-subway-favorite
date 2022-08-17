86b18b3238fd1208b2375ef85afbd5c844dff439
epicarts <0505zxc@gmail.com>
2022-08-17 17:56:57 +0900

    refactor: FavoriteException 이 IllegalArgumentException를 상속 받도록 수정함

--- src/main/java/nextstep/member/domain/exception/FavoriteException.java
+++ src/main/java/nextstep/member/domain/exception/FavoriteException.java
@@ -1,6 +1,6 @@
 package nextstep.member.domain.exception;
 
-public class FavoriteException extends RuntimeException {
+public class FavoriteException extends IllegalArgumentException {
     public static final String SAME_SOURCE_AND_TARGET = "즐겨찾기의 출발점과 도착역이 같을 수 없습니다.";
     public static final String DUPLICATION_FAVORITE = "이미 등록되어있는 즐겨찾기입니다.";
 
