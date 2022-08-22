2d72c7cab9d90fd63507523356ca73dd63997fce
younlll <youn.lee96@gmail.com>
2022-08-22 13:30:08 +0900

    test: 토큰을 통한 권한 확인 테스트

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.acceptance;
 import static nextstep.subway.acceptance.MemberSteps.로그인_되어_있음;
 import static nextstep.subway.acceptance.MemberSteps.베이직_인증으로_내_회원_정보_조회_요청;
 import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회됨;
+import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청_토큰_포함;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
 import io.restassured.authentication.FormAuthConfig;
@@ -19,6 +21,9 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
     private static final Integer AGE = 20;
+    private static final String MEMBER_EMAIL = "member@email.com";
+    private static final String MEMBER_PASSWORD = "memberPassword";
+    private static final Integer MEMBER_AGE = 23;
 
     @DisplayName("Basic Auth")
     @Test
@@ -44,6 +49,45 @@ class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 베어러_인증으로_내_회원_정보_조회_요청(accessToken);
 
         회원_정보_조회됨(response, EMAIL, AGE);
+    }
+
+    @DisplayName("권한 확인 성공")
+    @Test
+    void adminAuth() {
+        // given
+        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_생성_요청_토큰_포함("신논현역", accessToken);
+
+        // then
+        assertThat(response.response().statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("권한이 존재하지 않는 경우 지하철역 생성 실패")
+    @Test
+    void invalidToken() {
+        // given
+        String accessToken = "Bearer failToken";
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_생성_요청_토큰_포함("신논현역", accessToken);
+
+        // then
+        assertThat(response.response().statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("관리자 권한이 아닌 토큰인 경우, 지하철역 생성 불가")
+    @Test
+    void notAdminAuth() {
+        // given
+        String accessToken = 로그인_되어_있음(MEMBER_EMAIL, MEMBER_PASSWORD);
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_생성_요청_토큰_포함("신논현역", accessToken);
+
+        // then
+        assertThat(response.response().statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private ExtractableResponse<Response> 폼_로그인_후_내_회원_정보_조회_요청(String email, String password) {
--- src/test/java/nextstep/subway/acceptance/StationSteps.java
+++ src/test/java/nextstep/subway/acceptance/StationSteps.java
@@ -3,16 +3,29 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.springframework.http.MediaType;
-
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.MediaType;
 
 public class StationSteps {
     public static ExtractableResponse<Response> 지하철역_생성_요청(String name) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
         return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철역_생성_요청_토큰_포함(String name, String token) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured.given().log().all()
+                .auth().oauth2(token)
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
