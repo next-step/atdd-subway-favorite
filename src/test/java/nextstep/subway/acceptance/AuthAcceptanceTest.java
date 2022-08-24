6aaf7498f750ebcd91d75d6865f97ff5ae1ce3b4
younlll <youn.lee96@gmail.com>
2022-08-25 03:05:43 +0900

    refactor: 중복 코드 제거

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -17,12 +17,20 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 
 class AuthAcceptanceTest extends AcceptanceTest {
+    private String accessToken;
+
+    @BeforeEach
+    void init() {
+        accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
+    }
+
     @DisplayName("Basic Auth")
     @Test
     void myInfoWithBasicAuth() {
@@ -42,8 +50,6 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
-        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
-
         ExtractableResponse<Response> response = 베어러_인증으로_내_회원_정보_조회_요청(accessToken);
 
         회원_정보_조회됨(response, EMAIL, AGE);
@@ -52,9 +58,6 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("권한 확인 성공")
     @Test
     void adminAuth() {
-        // given
-        String accessToken = 로그인_되어_있음(EMAIL, PASSWORD);
-
         // when
         ExtractableResponse<Response> response = 지하철역_생성_요청_토큰_포함("신논현역", accessToken);
 
@@ -65,9 +68,6 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("권한이 존재하지 않는 경우 지하철역 생성 실패")
     @Test
     void invalidToken() {
-        // given
-        String accessToken = "Bearer failToken";
-
         // when
         ExtractableResponse<Response> response = 지하철역_생성_요청_토큰_포함("신논현역", accessToken);
 
@@ -78,9 +78,6 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("관리자 권한이 아닌 토큰인 경우, 지하철역 생성 불가")
     @Test
     void notAdminAuth() {
-        // given
-        String accessToken = 로그인_되어_있음(MEMBER_EMAIL, MEMBER_PASSWORD);
-
         // when
         ExtractableResponse<Response> response = 지하철역_생성_요청_토큰_포함("신논현역", accessToken);
 
