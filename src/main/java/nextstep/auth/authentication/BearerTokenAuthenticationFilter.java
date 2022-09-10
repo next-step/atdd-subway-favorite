ed93a8e8d1f6f756f1c0335aea2a39f44d6f8325
cafitac <cafitac99@gmail.com>
2022-09-10 22:26:09 +0900

    feat: 베어러 인증에 대한 필터를 작성합니다

--- src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/nextstep/auth/authentication/BearerTokenAuthenticationFilter.java
@@ -1,10 +1,13 @@
 package nextstep.auth.authentication;
 
+import nextstep.auth.context.Authentication;
+import nextstep.auth.context.SecurityContextHolder;
 import nextstep.auth.token.JwtTokenProvider;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
     private JwtTokenProvider jwtTokenProvider;
@@ -15,7 +18,23 @@ public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // TODO: 구현하세요.
-        return true;
+        try {
+            String authCredentials = AuthorizationExtractor.extract(request, AuthorizationType.BEARER);
+
+            if (!jwtTokenProvider.validateToken(authCredentials)) {
+                throw new AuthenticationException();
+            }
+
+            final String principal = jwtTokenProvider.getPrincipal(authCredentials);
+            final List<String> authorities = jwtTokenProvider.getRoles(authCredentials);
+
+            Authentication authentication = new Authentication(principal, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return true;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
