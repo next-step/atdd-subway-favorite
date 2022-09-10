692c68ba2ab274ee3db1151e4d53f4f81504664c
cafitac <cafitac99@gmail.com>
2022-09-10 22:22:09 +0900

    feat: 테스트에 필요한 기본 유저 데이터 설정

--- src/main/java/nextstep/DataLoader.java
+++ src/main/java/nextstep/DataLoader.java
@@ -1,9 +1,21 @@
 package nextstep;
 
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DataLoader {
+
+    private MemberRepository memberRepository;
+
+    public DataLoader(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
     public void loadData() {
+        memberRepository.save(new Member("admin@email.com", "password", 20));
     }
 }
