91d8c97399165f184ba5f71a74e7f1a8b043f963
김현수 <hsu.kim@sk.com>
2022-09-27 02:31:14 +0900

    feat: 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/support/AcceptanceTestSteps.java
+++ src/test/java/nextstep/subway/acceptance/support/AcceptanceTestSteps.java
@@ -4,12 +4,12 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
 public class AcceptanceTestSteps {
-    static RequestSpecification given() {
+    public static RequestSpecification given() {
         return RestAssured
             .given().log().all();
     }
 
-    static RequestSpecification given(String token) {
+    public static RequestSpecification given(String token) {
         return RestAssured
             .given().log().all()
             .auth().oauth2(token);
---
+++ src/test/java/nextstep/subway/acceptance/support/FavoritesSteps.java
@@ -0,0 +1,36 @@
+package nextstep.subway.acceptance.support;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static nextstep.subway.acceptance.support.AcceptanceTestSteps.given;
+
+public class FavoritesSteps {
+    public static ExtractableResponse<Response> 즐겨찾기_생성_요청(String token, Long sourceId, Long targetId) {
+        Map<String, Long> params = new HashMap<>();
+        params.put("source", sourceId);
+        params.put("target", targetId);
+        return given(token)
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/favorites")
+            .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_조회_요청(String token) {
+        return given(token)
+            .when().get("/favorites")
+            .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_삭제_요청(String token, Long favoritesId) {
+        return given(token)
+            .delete("/favorites/{favoritesId}", favoritesId)
+            .then().log().all()
+            .extract();
+    }
+}
