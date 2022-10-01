d1b97b5773346276e1040bbbd5f27383a7086161
김현수 <hsu.kim@sk.com>
2022-10-02 03:03:08 +0900

    feat: 트랜잭션 추가

--- src/main/java/nextstep/subway/applicaion/FavoritesService.java
+++ src/main/java/nextstep/subway/applicaion/FavoritesService.java
@@ -7,11 +7,13 @@ import nextstep.subway.domain.Favorite;
 import nextstep.subway.domain.FavoritesRepository;
 import nextstep.subway.domain.Station;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class FavoritesService {
     private final FavoritesRepository favoritesRepository;
@@ -27,6 +29,7 @@ public class FavoritesService {
         return new FavoriteResponse(favorite);
     }
 
+    @Transactional(readOnly = true)
     public List<FavoriteResponse> getFavorites() {
         List<Favorite> favorites = favoritesRepository.findAll();
 
