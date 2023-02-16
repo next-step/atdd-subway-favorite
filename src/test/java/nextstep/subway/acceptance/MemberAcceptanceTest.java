3c6f85ced709845da88277bff89efbf1c66e97d9
hiblue02 <hiblue02@gmail.com>
2023-02-17 01:22:48 +0900

    test(Member): 토큰 테스트 추가

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -2,8 +2,7 @@ package nextstep.subway.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
@@ -13,6 +12,7 @@ class MemberAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "email@email.com";
     public static final String PASSWORD = "password";
     public static final int AGE = 20;
+
 
     @DisplayName("회원가입을 한다.")
     @Test
@@ -24,48 +24,82 @@ class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("회원 정보를 조회한다.")
-    @Test
-    void getMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+    @Nested
+    @DisplayName("회원 정보")
+    class members {
+        ExtractableResponse<Response> createResponse;
 
-        // when
-        ExtractableResponse<Response> response = 회원_정보_조회_요청(createResponse);
+        @BeforeEach
+        void setUp() {
+            // given
+            createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+        }
 
-        // then
-        회원_정보_조회됨(response, EMAIL, AGE);
+        @DisplayName("회원 정보를 조회한다.")
+        @Test
+        void getMember() {
+            // when
+            ExtractableResponse<Response> response = 회원_정보_조회_요청(createResponse);
 
+            // then
+            회원_정보_조회됨(response, EMAIL, AGE);
+
+        }
+
+        @DisplayName("회원 정보를 수정한다.")
+        @Test
+        void updateMember() {
+            // when
+            ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        }
+
+        @DisplayName("회원 정보를 삭제한다.")
+        @Test
+        void deleteMember() {
+            // when
+            ExtractableResponse<Response> response = 회원_삭제_요청(createResponse);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        }
     }
 
-    @DisplayName("회원 정보를 수정한다.")
-    @Test
-    void updateMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
 
-        // when
-        ExtractableResponse<Response> response = 회원_정보_수정_요청(createResponse, "new" + EMAIL, "new" + PASSWORD, AGE);
+    @Nested
+    @DisplayName("내 회원 정보 조회")
+    class MyInfo {
+        ExtractableResponse<Response> createResponse;
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
+        @BeforeEach
+        void setUp() {
+            // given
+            createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+        }
 
-    @DisplayName("회원 정보를 삭제한다.")
-    @Test
-    void deleteMember() {
-        // given
-        ExtractableResponse<Response> createResponse = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+        @Test
+        @DisplayName("토큰으로 회원 정보를 조회한다.")
+        void getMyInfoByToken() {
+            //when 토큰으로 내 회원 정보를 조회한다.
+            String accessToken = 베어러_인증_로그인_요청(EMAIL, PASSWORD).jsonPath().getString("accessToken");
+            ExtractableResponse<Response> response = 토큰으로_내_회원_정보_조회_요청(accessToken);
+            //then
+            Assertions.assertAll(
+                    () -> assertThat(response.jsonPath().getString("email")).isEqualTo(EMAIL),
+                    () -> assertThat(response.jsonPath().getInt("age")).isEqualTo(AGE)
+            );
+        }
 
-        // when
-        ExtractableResponse<Response> response = 회원_삭제_요청(createResponse);
+        @Test
+        @DisplayName("잘못된 토큰으로 회원 정보를 조회한다.")
+        void getMyInfoByNoneToken() {
+            //when 토큰없이 내 회원 정보를 조회한다.
+            ExtractableResponse<Response> response = 토큰으로_내_회원_정보_조회_요청("0000");
+            //then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-    }
-
-    @DisplayName("내 정보를 조회한다.")
-    @Test
-    void getMyInfo() {
     }
 }
