ef28c530e6b0b2b3f7ab8274bd8960c52992986f
Yunsik-Choi <choiys0311@gmail.com>
2023-02-17 17:05:45 +0900

    ⚡feat : DataLoader 설정

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,16 +1,27 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.DataLoader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
+
+    @Autowired
+    private DataLoader dataLoader;
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        dataLoader.loadData();
+    }
 
     @DisplayName("Bearer Auth")
     @Test
