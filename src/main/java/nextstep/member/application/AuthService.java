fdb2a4e068f89c9608829bbfeeededead256b277
yunhwan <pyh8618@gmail.com>
2023-02-18 17:09:34 +0900

    feat : 로그인정보를 통해 token을 생성하는 서비스 기능 추가

--- src/main/java/nextstep/member/application/AuthService.java
+++ src/main/java/nextstep/member/application/AuthService.java
@@ -1,18 +1,39 @@
 package nextstep.member.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.member.application.dto.MemberResponse;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
+import nextstep.member.domain.Member;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthService {
+    private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
-    public AuthService(JwtTokenProvider jwtTokenProvider) {
+    public AuthService(MemberService memberService, JwtTokenProvider jwtTokenProvider, ObjectMapper objectMapper) {
+        this.memberService = memberService;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.objectMapper = objectMapper;
     }
 
     public TokenResponse loginMember(TokenRequest tokenRequest) {
-        return new TokenResponse();
+        Member member = memberService.findMemberByEmail(tokenRequest.getEmail());
+        member.checkPassword(tokenRequest.getPassword());
+        String token = jwtTokenProvider.createToken(convertObjectAsString(MemberResponse.of(member)), member.getRoles());
+        return new TokenResponse(token);
+    }
+
+    private String convertObjectAsString(MemberResponse memberResponse) {
+        try {
+            return objectMapper.writeValueAsString(memberResponse);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/nextstep/subway/unit/AuthServiceTest.java
+++ src/test/java/nextstep/subway/unit/AuthServiceTest.java
@@ -3,14 +3,13 @@ package nextstep.subway.unit;
 import nextstep.member.application.AuthService;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -19,8 +18,9 @@ class AuthServiceTest {
     private AuthService authService;
 
     @Test
+    @DisplayName("이메일, 패스워드가 일치하는 멤버가 존재한다면 토큰반환")
     void createToken() {
-        TokenRequest tokenRequest = new TokenRequest("test@email.com", "password");
+        TokenRequest tokenRequest = new TokenRequest("admin@email.com", "password");
         TokenResponse tokenResponse = authService.loginMember(tokenRequest);
         assertThat(tokenResponse).isNotNull();
         assertThat(tokenResponse.getAccessToken()).isNotBlank();
