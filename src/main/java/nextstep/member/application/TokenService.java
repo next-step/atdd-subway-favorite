fd0aff21f957fc9a663b88b81c52b749114ec5ad
seondongpyo <seondongpyo@gmail.com>
2023-02-18 16:26:34 +0900

    refactor: 주석 제거

--- src/main/java/nextstep/member/application/TokenService.java
+++ src/main/java/nextstep/member/application/TokenService.java
@@ -26,17 +26,9 @@ public class TokenService {
     }
 
     public TokenResponse createGitHubToken(String code) {
-        // 1. 권한증서(code)로 GitHub 에서 Access Token 발급
         String accessTokenFromGithub = gitHubClient.getAccessTokenFromGitHub(code);
-
-        // 2. Access Token 으로 GitHub 의 Resource Server 에서 사용자 프로필 조회
         GitHubProfileResponse githubProfile = gitHubClient.getGithubProfileFromGithub(accessTokenFromGithub);
-
-        // 3. 사용자 프로필의 이메일 주소로 회원을 조회
-        // Option. 조회된 회원이 없으면 해당 이메일 주소로 회원 가입
         Member member = memberService.findOrCreateMember(githubProfile.getEmail());
-
-        // 4. 회원 정보로 토큰 발급
         String token = jwtTokenProvider.createToken(String.valueOf(member.getId()), member.getRoles());
         return new TokenResponse(token);
     }
