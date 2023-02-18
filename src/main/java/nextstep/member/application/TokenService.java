1194e0ae707761f75f8997caf692688026bf9f44
seondongpyo <seondongpyo@gmail.com>
2023-02-18 15:34:38 +0900

    test: MemberService 단위 테스트 작성
    - 로그인 시도, 이메일로 회원 조회 시 없으면 회원을 생성하는 메서드에 대한 단위 테스트 작성
    - 기본적인 CRUD에 대한 단위 테스트는 생략

--- src/main/java/nextstep/member/application/TokenService.java
+++ src/main/java/nextstep/member/application/TokenService.java
@@ -2,6 +2,7 @@ package nextstep.member.application;
 
 import org.springframework.stereotype.Service;
 
+import nextstep.member.application.dto.GitHubProfileResponse;
 import nextstep.member.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
 
@@ -29,9 +30,11 @@ public class TokenService {
         String accessTokenFromGithub = gitHubClient.getAccessTokenFromGitHub(code);
 
         // 2. Access Token 으로 GitHub 의 Resource Server 에서 사용자 프로필 조회
+        GitHubProfileResponse githubProfile = gitHubClient.getGithubProfileFromGithub(accessTokenFromGithub);
 
         // 3. 사용자 프로필의 이메일 주소로 회원을 조회
         // Option. 조회된 회원이 없으면 해당 이메일 주소로 회원 가입
+        Member member = memberService.findOrCreateMember(githubProfile.getEmail());
 
         // 4. 회원 정보로 토큰 발급
         return new TokenResponse(accessTokenFromGithub);
