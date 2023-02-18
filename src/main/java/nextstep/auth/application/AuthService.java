715f2f74009a4fb3a0802e0cf6e23558663614eb
michol.lee <lovethefeel@gmail.com>
2023-02-19 02:29:53 +0900

    refactor: Oauth2.0 프로세스 참고하여 로그인 처리

--- src/main/java/nextstep/auth/application/AuthService.java
+++ src/main/java/nextstep/auth/application/AuthService.java
@@ -1,18 +1,15 @@
 package nextstep.auth.application;
 
-import nextstep.auth.application.dto.GithubAccessTokenResponse;
-import nextstep.auth.application.dto.GithubAccessTokenRequest;
+import nextstep.auth.application.dto.*;
 import nextstep.auth.config.exception.MissingTokenException;
 import nextstep.auth.domain.LoginUserInfo;
-import nextstep.auth.infra.GithubResponses;
+import nextstep.auth.infra.GithubClient;
 import nextstep.auth.infra.JwtTokenProvider;
 import nextstep.member.application.MemberService;
-import nextstep.auth.application.dto.TokenRequest;
-import nextstep.auth.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 import static nextstep.auth.config.message.AuthError.NOT_MISSING_TOKEN;
 import static nextstep.auth.config.message.AuthError.NOT_VALID_TOKEN;
@@ -22,28 +19,27 @@ public class AuthService {
 
     private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final GithubClient githubClient;
 
-    public AuthService(final MemberService memberService, final JwtTokenProvider jwtTokenProvider) {
+    public AuthService(final MemberService memberService, final JwtTokenProvider jwtTokenProvider, final GithubClient githubClient) {
         this.memberService = memberService;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.githubClient = githubClient;
     }
 
     public TokenResponse login(final TokenRequest request) {
         final Member member = memberService.findByEmail(request.getEmail());
         member.validatePassword(request.getPassword());
 
-        final String token = jwtTokenProvider.createToken(request.getEmail(), member.getRoles());
+        final String token = createToken(request.getEmail(), member.getRoles());
         return new TokenResponse(token);
     }
 
-    public GithubAccessTokenResponse loginByGithub(final GithubAccessTokenRequest request) {
-        final String code = request.getCode();
-        try {
-            final GithubResponses githubResponses = GithubResponses.find(code);
-            return new GithubAccessTokenResponse(githubResponses.getAccessToken());
-        } catch (IllegalArgumentException ie) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+    public GithubAccessTokenResponse loginByGithub(final GithubTokenRequest request) {
+        final String accessToken = githubClient.getAccessTokenFromGithub(request.getCode());
+        final GithubProfileResponse response = githubClient.getGithubProfileFromGithub(accessToken);
+        final Member findMember = memberService.findByEmail(response.getEmail());
+        return new GithubAccessTokenResponse(createToken(findMember));
     }
 
     public LoginUserInfo findMemberByToken(final String credentials) {
@@ -56,5 +52,13 @@ public class AuthService {
         final String email = jwtTokenProvider.getPrincipal(credentials);
         final Member member = memberService.findByEmail(email);
         return LoginUserInfo.from(member);
+    }
+
+    private String createToken(final Member member) {
+        return jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
+    }
+
+    private String createToken(final String email, final List<String> roles) {
+        return jwtTokenProvider.createToken(email, roles);
     }
 }
