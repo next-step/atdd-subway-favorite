573e3bbaf291ec0b78f2931a1bd3d9b638345ce3
Choonghee Lee <choonghee.lee@hotmail.com>
2023-02-19 14:43:28 +0900

    test(AuthAcceptance): 베어러 토큰 요청

---
+++ src/main/java/nextstep/member/application/AuthService.java
@@ -0,0 +1,30 @@
+package nextstep.member.application;
+
+import nextstep.member.application.dto.TokenRequest;
+import nextstep.member.application.dto.TokenResponse;
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider, MemberRepository memberRepository) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberRepository = memberRepository;
+    }
+
+    public TokenResponse login(TokenRequest request) {
+        Member member = authenticate(request.getEmail(), request.getPassword());
+        String token = jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
+        return new TokenResponse(token);
+    }
+
+    private Member authenticate(String email, String password) {
+        return memberRepository.findByEmail(email)
+                .filter(it -> it.checkPassword(password))
+                .orElseThrow(RuntimeException::new);
+    }
+}
