c1bfff341182962865070a920bb1f74ce96ded25
hungrytech <xorals9448@gmail.com>
2023-02-19 16:43:08 +0900

    feat : 서버통신 예외발생시 응답 메세지 전달하도록 추가

--- src/main/java/nextstep/member/application/exception/MemberExceptionHandler.java
+++ src/main/java/nextstep/member/application/exception/MemberExceptionHandler.java
@@ -1,37 +1,46 @@
 package nextstep.member.application.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import nextstep.member.application.dto.MemberErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import lombok.extern.slf4j.Slf4j;
-import nextstep.member.application.dto.MemberErrorResponse;
 
 @Slf4j
 @RestControllerAdvice
 public class MemberExceptionHandler {
 
 
-	@ExceptionHandler(InvalidTokenException.class)
-	public ResponseEntity<MemberErrorResponse> handleInvalidTokenException(InvalidTokenException exception) {
-		ErrorCode errorCode = exception.getErrorCode();
-		log.info("인증예외 이유: {} 코드: {}", errorCode.getMessage(), errorCode.getCode());
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<MemberErrorResponse> handleInvalidTokenException(InvalidTokenException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        log.info("인증예외 이유: {} 코드: {}", errorCode.getMessage(), errorCode.getCode());
 
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-			.body(MemberErrorResponse.of(errorCode));
-	}
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(MemberErrorResponse.of(errorCode));
+    }
 
-	@ExceptionHandler(NotFoundMemberException.class)
-	public ResponseEntity<MemberErrorResponse> handleNotFoundMemberException(NotFoundMemberException exception) {
-		ErrorCode errorCode = exception.getErrorCode();
-		loggingMemberException(errorCode);
+    @ExceptionHandler(NotFoundMemberException.class)
+    public ResponseEntity<MemberErrorResponse> handleNotFoundMemberException(NotFoundMemberException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        loggingMemberException(errorCode);
 
-		return ResponseEntity.badRequest()
-			.body(MemberErrorResponse.of(errorCode));
-	}
+        return ResponseEntity.badRequest()
+                .body(MemberErrorResponse.of(errorCode));
+    }
 
-	private void loggingMemberException(ErrorCode errorCode) {
-		log.info("회원 예외 발생 메세지: {}, 코드: {}", errorCode.getMessage(), errorCode.getCode());
-	}
+    @ExceptionHandler(GithubOauthConnectionException.class)
+    public ResponseEntity<MemberErrorResponse> handleGithubOauthConnectionException(
+            GithubOauthConnectionException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        loggingMemberException(errorCode);
+
+        return ResponseEntity.internalServerError()
+                .body(MemberErrorResponse.of(errorCode));
+    }
+
+    private void loggingMemberException(ErrorCode errorCode) {
+        log.info("회원 예외 발생 메세지: {}, 코드: {}", errorCode.getMessage(), errorCode.getCode());
+    }
 }
