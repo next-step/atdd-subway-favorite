210e8b28caa714e36bd6849a867581a92fca1b5d
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-21 01:41:18 +0900

    test(토큰 생성): 인수 조건 예외 테스트 작성

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -3,8 +3,11 @@ package nextstep.member.acceptance;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.AcceptanceTest;
+import nextstep.DataLoader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,11 +16,35 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
 
+    @Autowired
+    private DataLoader dataLoader;
+
+    @Override
+    public void setUp() {
+        super.setUp();
+
+        dataLoader.loadData();
+    }
+
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
         ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
 
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    @Test
+    void wrongPassword() {
+        ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, "틀린 암호");
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void notExistEmail() {
+        ExtractableResponse<Response> response = 베어러_인증_로그인_요청("존재하지 않는 이메일", PASSWORD);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 }
