06d6077ca7aa3e1dfd7906361384071d22aec389
cafitac <cafitac99@gmail.com>
2023-02-20 16:43:44 +0900

    test: Basic 인증 인터셉터에 대한 테스트 코드를 작성합니다

---
+++ src/test/java/nextstep/subway/unit/BasicAuthorizationInterceptorTest.java
@@ -0,0 +1,48 @@
+package nextstep.subway.unit;
+
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
+import nextstep.member.filter.BasicAuthorizationInterceptor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.util.Base64;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class BasicAuthorizationInterceptorTest {
+
+    private static final String USERNAME = "test@email.com";
+    private static final String PASSWORD = "password";
+    private static final MockHttpServletRequest request = new MockHttpServletRequest();
+    private static final MockHttpServletResponse response = new MockHttpServletResponse();
+    @Autowired
+    private BasicAuthorizationInterceptor basicAuthorizationInterceptor;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.save(new Member("test@email.com", "password", 20, List.of(RoleType.ROLE_MEMBER.name())));
+    }
+
+    @Test
+    void authorize() throws Exception {
+        // given
+        final String token = new String(Base64.getEncoder().encode((USERNAME + ":" + PASSWORD).getBytes()));
+        request.addHeader("Authorization", "Basic " + token);
+
+        // when
+        final boolean success = basicAuthorizationInterceptor.preHandle(request, response, null);
+
+        // then
+        assertThat(success).isTrue();
+    }
+}
