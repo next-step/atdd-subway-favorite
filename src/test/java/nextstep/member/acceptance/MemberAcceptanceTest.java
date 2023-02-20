6eacb6a6c08b80927d25b40e0f0d4cf63c2c38b8
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-20 15:45:44 +0900

    style: member test 패키지 분리

--- src/test/java/nextstep/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/MemberAcceptanceTest.java
@@ -1,16 +1,17 @@
-package nextstep.subway.acceptance;
+package nextstep.member.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import static nextstep.subway.acceptance.MemberSteps.회원_삭제_요청;
-import static nextstep.subway.acceptance.MemberSteps.회원_생성_요청;
-import static nextstep.subway.acceptance.MemberSteps.회원_정보_수정_요청;
-import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회_요청;
-import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회됨;
+import static nextstep.member.acceptance.MemberSteps.회원_삭제_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_정보_수정_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_정보_조회_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_정보_조회됨;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberAcceptanceTest extends AcceptanceTest {
