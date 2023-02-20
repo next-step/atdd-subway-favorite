b18eebae17301084cb3db65a47e0ca748067e2e9
ji-one <rlawldnjs8@gmail.com>
2023-02-21 01:15:52 +0900

    test: AuthAcceptanceTest 인수 조건 작성

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -12,6 +12,10 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
 
+    /**
+     * When 베어러 인증 로그인 요청을 하면
+     * Then Access Token을 발급 받는다.
+     */
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
