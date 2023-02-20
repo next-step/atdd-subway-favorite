dffc4403df3975636d388448b2f020755819732d
cafitac <cafitac99@gmail.com>
2023-02-20 16:57:34 +0900

    feat: Basic 인증 인터셉터를 InterceptorRegistry 에 등록합니다

---
+++ src/main/java/nextstep/common/configuration/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package nextstep.common.configuration;
+
+import lombok.RequiredArgsConstructor;
+import nextstep.member.application.MemberService;
+import nextstep.member.filter.BasicAuthorizationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final MemberService memberService;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new BasicAuthorizationInterceptor(memberService))
+                .excludePathPatterns("/login");
+    }
+}
