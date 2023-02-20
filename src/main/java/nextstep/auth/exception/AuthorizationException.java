fc080a28492226071af67aaf86a62533c581a560
wu2ee <gulb1602@gmail.com>
2023-02-21 02:08:50 +0900

    feat : 인증 & 인가 커스텀 예외 추가

---
+++ src/main/java/nextstep/auth/exception/AuthorizationException.java
@@ -0,0 +1,14 @@
+package nextstep.auth.exception;
+
+public class AuthorizationException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public AuthorizationException() {
+        super(ErrorCode.FORBIDDEN.getMessage());
+        this.errorCode = ErrorCode.FORBIDDEN;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
