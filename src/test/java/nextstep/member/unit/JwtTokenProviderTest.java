030132ade840ca9a2992a6120ccd7a2933955304
cyPark95 <cas15987@gmail.com>
2023-02-21 14:09:28 +0900

    test: 토큰 생성 단위 테스트 작성

---
+++ src/test/java/nextstep/member/unit/JwtTokenProviderTest.java
@@ -0,0 +1,31 @@
+package nextstep.member.unit;
+
+import nextstep.member.application.JwtTokenProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class JwtTokenProviderTest {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private final String EMAIL = "email";
+    private final List<String> ROLES = List.of("ROLE_MEMBER");
+
+    @Test
+    @DisplayName("토큰 생성")
+    void createToken() {
+        // when
+        final String token = jwtTokenProvider.createToken(EMAIL, ROLES);
+
+        // then
+        assertThat(token).isNotBlank();
+    }
+}
