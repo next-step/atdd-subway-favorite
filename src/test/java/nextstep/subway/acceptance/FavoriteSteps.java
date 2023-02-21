57143455786d506d09e87b9c55daafd51f387ed6
doorisopen <doorwasopen@gmail.com>
2023-02-21 23:57:37 +0900

    test: 즐겨찾기 인수 테스트 작성

---
+++ src/test/java/nextstep/subway/acceptance/FavoriteSteps.java
@@ -0,0 +1,26 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.utils.Request;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FavoriteSteps {
+
+    public static ExtractableResponse<Response> 즐겨찾기_생성_요청(String token, Long target, Long source) {
+        Map<String, String> body = new HashMap<>();
+        body.put("target", target + "");
+        body.put("source", source + "");
+        return Request.oauthPost(token, "/favorites", body);
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_목록_조회_요청(String token) {
+        return Request.oauthGet(token, "/favorites");
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_삭제_요청(String token, Long id) {
+        return Request.oauthDelete(token, "/favorites/" + id);
+    }
+}
--- src/test/java/nextstep/utils/Request.java
+++ src/test/java/nextstep/utils/Request.java
@@ -5,9 +5,20 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Request {
+    public static ExtractableResponse<Response> oauthGet(final String token, final String path) {
+        return oauthGet(token, path, MediaType.APPLICATION_JSON_VALUE, new HashMap<>());
+    }
+
+    public static ExtractableResponse<Response> oauthGet(final String token,
+                                                         final String path,
+                                                         final Map<String, ?> params) {
+        return oauthGet(token, path, MediaType.APPLICATION_JSON_VALUE, params);
+    }
+
     public static ExtractableResponse<Response> oauthGet(final String token,
                                                          final String contentType,
                                                          final String path,
@@ -16,13 +27,28 @@ public class Request {
                 .given().log().all()
                 .auth().oauth2(token)
                 .contentType(contentType)
-                .body(params)
+                .queryParams(params)
                 .when().get(path)
                 .then().log().all().extract();
     }
 
-    public static ExtractableResponse<Response> post(final String path, final Map<String, Object> params) {
-        return post(Map.of(), path, MediaType.APPLICATION_JSON_VALUE, params);
+    public static ExtractableResponse<Response> oauthPost(final String token, final String path, final Map<String, ?> body) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post(path)
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> oauthDelete(final String token, final String path) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete(path)
+                .then().log().all().extract();
     }
 
     public static ExtractableResponse<Response> post(final Map<String, ?> headers,
@@ -35,14 +61,6 @@ public class Request {
                 .contentType(contentType)
                 .body(params)
                 .when().post(path)
-                .then().log().all().extract();
-    }
-
-    public static ExtractableResponse<Response> get(final String path, final Map<String, Object> params) {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get(path)
                 .then().log().all().extract();
     }
 }
