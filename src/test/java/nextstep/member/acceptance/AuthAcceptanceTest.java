d0f375cc53d95e3d0488e31f628123c11131bb6d
cyPark95 <cas15987@gmail.com>
2023-02-21 14:48:06 +0900

    feat: 초기 데이터 설정

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -1,13 +1,19 @@
 package nextstep.member.acceptance;
 
+import nextstep.DataLoader;
 import nextstep.common.acceptance.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static nextstep.member.acceptance.AuthSteps.토큰_발급_검증;
 import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청;
 
 class AuthAcceptanceTest extends AcceptanceTest {
+
+    @Autowired
+    private DataLoader dataLoader;
+
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
 
@@ -18,6 +24,9 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @Test
     @DisplayName("로그인을 통한 토큰 발급")
     void bearerAuth() {
+        // given
+        dataLoader.loadData();
+
         // when
         var response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
 
