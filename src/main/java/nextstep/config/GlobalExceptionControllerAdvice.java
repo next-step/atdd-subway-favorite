3da25602692a9dbd79d32d10d223c6f26b7f758f
bada.kang <kangbada0728@gmail.com>
2023-02-22 21:34:02 +0900

    add: GlobalExceptionControllerAdvice 추가

---
+++ src/main/java/nextstep/config/GlobalExceptionControllerAdvice.java
@@ -0,0 +1,23 @@
+package nextstep.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionControllerAdvice {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> noSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
+
+}
