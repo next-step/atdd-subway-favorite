e0f68582689fec525aae4bbe7a1fb49683a53ab8
bada.kang <kangbada0728@gmail.com>
2023-02-22 21:35:42 +0900

    add: 토큰 요청 실패 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -12,7 +12,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
 
-    @DisplayName("Bearer Auth")
+    @DisplayName("Bearer Auth : 정상")
     @Test
     void bearerAuth() {
         MemberSteps.회원_생성_요청(EMAIL, PASSWORD, 3);
@@ -20,4 +20,32 @@ class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
     }
+
+    @DisplayName("Bearer Auth : invalid email")
+    @Test
+    void test1() {
+        // given
+        MemberSteps.회원_생성_요청(EMAIL, PASSWORD, 3);
+
+        // when
+        ExtractableResponse<Response> response = 베어러_인증_로그인_요청("invalid", PASSWORD);
+
+        // then
+        assertThat(response.body().asString()).contains("존재하지 않는 Email");
+    }
+
+    @DisplayName("Bearer Auth : invalid password")
+    @Test
+    void test2() {
+        // given
+        MemberSteps.회원_생성_요청(EMAIL, PASSWORD, 3);
+
+        // when
+        ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, "invalid");
+
+        // then
+        assertThat(response.body().asString()).contains("잘못된 비밀번호");
+    }
+
+
 }
