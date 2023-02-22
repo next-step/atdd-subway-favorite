574d2a4d2de6d64adf061f3ad1aad1da2dbbf8ac
yunhwan <pyh8618@gmail.com>
2023-02-22 23:58:36 +0900

    feat : AuthService github로그인 메서드 추가

--- src/main/java/nextstep/member/application/AuthService.java
+++ src/main/java/nextstep/member/application/AuthService.java
@@ -1,6 +1,7 @@
 package nextstep.member.application;
 
 import nextstep.exception.member.PasswordNotEqualException;
+import nextstep.member.application.dto.GithubAccessTokenRequest;
 import nextstep.member.application.dto.MemberResponse;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
@@ -12,10 +13,12 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final GithubClient githubClient;
 
-    public AuthService(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
+    public AuthService(MemberService memberService, JwtTokenProvider jwtTokenProvider, GithubClient githubClient) {
         this.memberService = memberService;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.githubClient = githubClient;
     }
 
     public TokenResponse loginMember(TokenRequest tokenRequest) {
@@ -25,5 +28,9 @@ public class AuthService {
         }
         String token = jwtTokenProvider.createToken(ObjectStringMapper.convertObjectAsString(MemberResponse.of(member)), member.getRoles());
         return new TokenResponse(token);
+    }
+
+    public String loginGithub(String code) {
+        return githubClient.getAccessTokenFromGithub(code);
     }
 }
