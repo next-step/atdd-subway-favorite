dbb3081eb9696f68d1c7aab37787d7dd9a98514c
bada.kang <kangbada0728@gmail.com>
2023-02-22 22:40:28 +0900

    refactor: Test 최적화

--- src/test/java/nextstep/subway/unit/LoginServiceTest.java
+++ src/test/java/nextstep/subway/unit/LoginServiceTest.java
@@ -1,22 +1,36 @@
 package nextstep.subway.unit;
 
+import nextstep.DataLoader;
 import nextstep.member.application.JwtTokenProvider;
 import nextstep.member.application.LoginService;
 import nextstep.member.application.dto.TokenRequest;
+import nextstep.member.domain.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
+@Import({JwtTokenProvider.class, DataLoader.class})
+@DataJpaTest
 public class LoginServiceTest {
     @Autowired
-    LoginService loginService;
-    @Autowired
     JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    DataLoader dataLoader;
+    LoginService loginService;
+
+    @BeforeEach
+    void setUp() {
+        loginService = new LoginService(memberRepository, jwtTokenProvider);
+        dataLoader.loadData();
+    }
 
     @Test
     @DisplayName("토큰 생성 테스트 : 존재하지 않는 이메일")
