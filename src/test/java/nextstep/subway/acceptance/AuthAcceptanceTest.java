5da22ae3454fdc5b449f5e2e32ba5bb51a3bc5b9
sm9171 <sm9171@nate.com>
2023-02-22 09:47:06 +0900

    test: 깃헙 로그인 요청 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -2,18 +2,21 @@ package nextstep.subway.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.utils.GithubResponses;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.subway.acceptance.AuthSteps.깃헙_로그인_요청;
 import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static nextstep.subway.acceptance.MemberSteps.회원_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthAcceptanceTest extends AcceptanceTest {
-    private static final String EMAIL = "admin@email.com";
-    private static final String PASSWORD = "password";
-    public static final int AGE = 20;
+    private static final String 이메일 = "email@email.com";
+    private static final String 비밀번호 = "password";
+    public static final int 나이 = 20;
+    public static final String 깃헙코드 = GithubResponses.사용자1.getCode();
 
     /**
      * 회원을 생성하고
@@ -21,13 +24,22 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @BeforeEach
     public void setUp() {
         super.setUp();
-        회원_생성_요청(EMAIL, PASSWORD, AGE);
+        회원_생성_요청(이메일, 비밀번호, 나이);
     }
 
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
-        ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
+        ExtractableResponse<Response> response = 베어러_인증_로그인_요청(이메일, 비밀번호);
+
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    @DisplayName("Github Auth")
+    @Test
+    void githubAuth() {
+
+        ExtractableResponse<Response> response = 깃헙_로그인_요청(깃헙코드);
 
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
     }
