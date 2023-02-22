b9f4652cdc205c9801758e2739df63c51bfad0dd
Yunsik-Choi <choiys0311@gmail.com>
2023-02-22 16:29:10 +0900

    ⛑️ test : 깃헙 로그인 인수 테스트 작성

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceSteps.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceSteps.java
@@ -3,9 +3,12 @@ package nextstep.member.acceptance;
 import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class AuthAcceptanceSteps {
 
@@ -13,5 +16,22 @@ public class AuthAcceptanceSteps {
         ExtractableResponse<Response> response = 베어러_인증_로그인_요청(email, password);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    public static void Github_로그인_검증(final ExtractableResponse<Response> response) {
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    public static void Github에_가입이_되어있지_않은_경우_예외_처리_검증(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    public static ExtractableResponse<Response> Github_로그인_요청(final Map<String, String> params) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/login/github")
+                .then().log().all()
+                .extract();
     }
 }
--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -1,16 +1,22 @@
 package nextstep.member.acceptance;
 
+import static nextstep.member.acceptance.AuthAcceptanceSteps.Github_로그인_검증;
+import static nextstep.member.acceptance.AuthAcceptanceSteps.Github_로그인_요청;
+import static nextstep.member.acceptance.AuthAcceptanceSteps.Github에_가입이_되어있지_않은_경우_예외_처리_검증;
 import static nextstep.member.acceptance.AuthAcceptanceSteps.베어러_인증_로그인에_실패하면_예외_처리한다;
 import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.Map;
 import java.util.UUID;
 import nextstep.DataLoader;
 import nextstep.utils.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @DisplayName("인증 관련 기능")
@@ -43,5 +49,36 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @Test
     void bearerAuthLoginFail() {
         베어러_인증_로그인에_실패하면_예외_처리한다(EMAIL, PASSWORD + UUID.randomUUID());
+    }
+
+    /**
+     * given Github에서 발급받은 권한증서로
+     * when 로그인을 요청하면
+     * then accessToken을 발급한다.
+     */
+    @DisplayName("Github에서 발급받은 권한증서로 로그인을 요청하면 accessToken을 발급한다.")
+    @ParameterizedTest(name = "{0}")
+    @ValueSource(strings = {"832ovnq039hfjn", "mkfo0aFa03m", "a3hnfnoew92", "nvci383mciq0oq"})
+    void githubAuth(final String code) {
+        Map<String, String> params = Map.of("code", code);
+
+        var response = Github_로그인_요청(params);
+
+        Github_로그인_검증(response);
+    }
+
+    /**
+     * given Github에서 발급받지 않은 권한증서로
+     * when 로그인을 요청하면
+     * then 예외 처리한다.
+     */
+    @DisplayName("Github에 가입이 되어있지 않은 경우 회원 가입을 먼저 진행해야 한다.")
+    @Test
+    void githubAuth() {
+        Map<String, String> params = Map.of("code", "code");
+
+        var response = Github_로그인_요청(params);
+
+        Github에_가입이_되어있지_않은_경우_예외_처리_검증(response);
     }
 }
