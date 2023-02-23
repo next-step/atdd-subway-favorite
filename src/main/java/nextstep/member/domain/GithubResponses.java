e9fe1df925d3cd25051f4c118985ca7b83c67b5d
seungwoo.song <seungwoo.song@nhnpayco.com>
2023-02-23 21:12:58 +0900

    refactor : 메서드 추출

--- src/main/java/nextstep/member/domain/GithubResponses.java
+++ src/main/java/nextstep/member/domain/GithubResponses.java
@@ -4,33 +4,30 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 @Getter
 @RequiredArgsConstructor
 public enum GithubResponses {
-    사용자1("832ovnq039hfjn", "access_token_1", "admin@email.com"),
+    사용자1("832ovnq039hfjn", "access_token_1", "admin@email.com");
 
-    사용자2("mkfo0aFa03m", "access_token_2", "email2@email.com"),
-
-    사용자3("m-a3hnfnoew92", "access_token_3", "email3@email.com"),
-
-    사용자4("nvci383mciq0oq", "access_token_4", "email4@email.com");
-
+    public static final String NOT_FOUND_USER_MESSAGE = "사용자를 찾을 수 없습니다.";
     private final String code;
     private final String accessToken;
     private final String email;
 
     public static GithubResponses ofCode(String code) {
-        return Arrays.stream(values())
-                .filter(e -> e.code.equals(code))
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자 입니다. " + code));
+        return findUser(e -> e.code.equals(code));
     }
 
     public static GithubResponses ofAccessToken(String accessToken) {
+        return findUser(e -> e.accessToken.equals(accessToken));
+    }
+
+    private static GithubResponses findUser(Predicate<GithubResponses> predicate) {
         return Arrays.stream(values())
-                .filter(e -> e.accessToken.equals(accessToken))
+                .filter(predicate)
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자 입니다. " + accessToken));
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_USER_MESSAGE));
     }
 }
