b991be65085d354a3c439cbd74081430b865e9ce
Jaepil Yu <jaepilyu1324@gmail.com>
2023-02-23 19:18:51 +0900

    test: github 로그인 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,21 +1,31 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static nextstep.subway.acceptance.MemberSteps.*;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
-import static org.assertj.core.api.Assertions.assertThat;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
 class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
+    private static final String CODE = "code";
 
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
         ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
+
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    @DisplayName("Github Auth")
+    @Test
+    void githubAuth() {
+        ExtractableResponse<Response> response = 깃허브_인증_로그인_요청(CODE);
 
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
     }
