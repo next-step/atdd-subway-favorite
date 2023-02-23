51becfe214a3030fb4060f8e3ba6d4a1f6897825
dhlee3994 <dhlee3994@gmail.com>
2023-02-23 23:21:57 +0900

    Test: 깃허브 권한증서로 얻은 액세스 토큰으로 회원정보 조회하는 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -8,6 +8,8 @@ import static nextstep.subway.acceptance.AcceptanceUtils.응답코드_400을_반
 import static nextstep.subway.acceptance.MemberSteps.깃허브_인증_로그인_요청;
 import static nextstep.subway.acceptance.MemberSteps.깃허브_인증_로그인_요청하고_토큰_반환;
 import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청하고_토큰_반환;
+import static nextstep.subway.acceptance.MemberSteps.토근_인증으로_내_회원_정보_조회_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회됨;
 import static nextstep.subway.utils.FakeGithubTokenResponse.사용자1;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -63,5 +65,25 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
         // then
         응답코드_400을_반환한다(response);
+    }
+
+
+    /**
+     * given: 깃허브 로그인 요청을 통해 얻은 액세스 토큰으로
+     * when : 회원 정보 조회 요청을 하면
+     * then : 회원 정보를 반환한다.
+     */
+    @DisplayName("깃허브 로그인 요청으로 얻은 토큰으로 회원 정보를 조회할 수 있다.")
+    @Test
+    void githubAuthAndGetInfo() {
+        // given
+        final String code = 사용자1.getCode();
+        var token = 깃허브_인증_로그인_요청하고_토큰_반환(code);
+
+        // when
+        final var response = 토근_인증으로_내_회원_정보_조회_요청(token);
+
+        // then
+        회원_정보_조회됨(response, 사용자1.getEmail());
     }
 }
