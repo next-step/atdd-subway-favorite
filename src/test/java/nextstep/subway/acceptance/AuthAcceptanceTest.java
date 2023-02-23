5b00cb1cb00b2d92704a3f4e7d344a0a4ae04eeb
dhlee3994 <dhlee3994@gmail.com>
2023-02-23 23:20:05 +0900

    Test: 인수테스트에 맞게 시나리오 형태로 테스트 수정

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -1,12 +1,12 @@
 package nextstep.subway.acceptance;
 
-import nextstep.subway.utils.FakeGithubTokenResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static nextstep.subway.acceptance.MemberSteps.깃허브_인증_로그인_요청하고_토큰_반환;
 import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청하고_토큰_반환;
+import static nextstep.subway.utils.FakeGithubTokenResponse.사용자1;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthAcceptanceTest extends AcceptanceTest {
@@ -26,11 +26,21 @@ class AuthAcceptanceTest extends AcceptanceTest {
         assertThat(token).isNotBlank();
     }
 
-    @DisplayName("Github Auth")
+    /**
+     * given: 깃허브에서 받은 권한증서를 이용하여
+     * when : 로그인 요청을 하면
+     * then : 액세스 토큰을 발급 받는다
+     */
+    @DisplayName("깃허브 권한증서로 로그인 요청을하면 액세스 토큰을 발급받는다.")
     @Test
     void githubAuth() {
-        var token = 깃허브_인증_로그인_요청하고_토큰_반환(FakeGithubTokenResponse.사용자1.getCode());
+        // given
+        final String code = 사용자1.getCode();
 
+        // when
+        var token = 깃허브_인증_로그인_요청하고_토큰_반환(code);
+
+        // then
         assertThat(token).isNotBlank();
     }
 }
