21aa7332e73d692348979f915e5f37768fd4545c
joel-you <ruew12@gmail.com>
2023-02-23 18:20:19 +0900

    feature: AuthAcceptanceTest 누락된 인수테스트 추가

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package nextstep.subway.ui;
 
+import nextstep.member.domain.exception.InvalidUserInfoException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,5 +16,10 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Void> handleIllegalArgsException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(InvalidUserInfoException.class)
+    public ResponseEntity<Void> handleInvalidUserInfoException(InvalidUserInfoException e) {
+        return ResponseEntity.unprocessableEntity().build();
     }
 }
