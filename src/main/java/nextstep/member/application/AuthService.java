63bcc9ee2aef40072599191c99408198929c03fe
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-02-24 18:53:51 +0900

    refactor: LoginController의 비지니스로직 AuthService로 위임

--- src/main/java/nextstep/member/application/AuthService.java
+++ src/main/java/nextstep/member/application/AuthService.java
@@ -1,12 +1,27 @@
 package nextstep.member.application;
 
 import nextstep.member.application.dto.TokenResponse;
+import nextstep.member.domain.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
+        this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public TokenResponse authByBasic(String email, String password) {
+        Member member = memberService.authenticate(email, password);
+        String token = jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
+        return new TokenResponse(token);
+    }
 
     public TokenResponse authByGithub(String code) {
+        
         return new TokenResponse();
     }
 }
--- src/main/java/nextstep/member/ui/LoginController.java
+++ src/main/java/nextstep/member/ui/LoginController.java
@@ -1,12 +1,9 @@
 package nextstep.member.ui;
 
 import nextstep.member.application.AuthService;
-import nextstep.member.application.JwtTokenProvider;
-import nextstep.member.application.MemberService;
 import nextstep.member.application.dto.GithubTokenRequest;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
-import nextstep.member.domain.Member;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,25 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final AuthService authService;
-    private final MemberService memberService;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    public LoginController(AuthService authService, MemberService memberService, JwtTokenProvider jwtTokenProvider) {
+    public LoginController(AuthService authService) {
         this.authService = authService;
-        this.memberService = memberService;
-        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest tokenRequest) {
-        Member member = memberService.authenticate(tokenRequest.getEmail(), tokenRequest.getPassword());
-        String token = jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
-        return ResponseEntity.ok().body(new TokenResponse(token));
+        TokenResponse token = authService.authByBasic(tokenRequest.getEmail(), tokenRequest.getPassword());
+
+        return ResponseEntity.ok().body(token);
     }
-    
+
     @PostMapping("/github")
     public ResponseEntity<TokenResponse> loginByGithub(@RequestBody GithubTokenRequest githubTokenRequest) {
         TokenResponse token = authService.authByGithub(githubTokenRequest.getCode());
+
         return ResponseEntity.ok().body(token);
     }
 }
