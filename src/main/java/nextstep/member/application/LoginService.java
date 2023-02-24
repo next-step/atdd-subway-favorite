fdf900552410463110872ae3478dc3a85c9d3a15
bada.kang <kangbada0728@gmail.com>
2023-02-24 23:14:34 +0900

    refactor: Message를 상수로 변경

--- src/main/java/nextstep/member/application/LoginService.java
+++ src/main/java/nextstep/member/application/LoginService.java
@@ -3,6 +3,7 @@ package nextstep.member.application;
 import lombok.RequiredArgsConstructor;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
+import nextstep.member.application.message.Message;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,9 @@ public class LoginService {
 
     public TokenResponse createToken(TokenRequest tokenRequest) {
         Member member = memberRepository.findByEmail(tokenRequest.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Email 입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(Message.NOT_EXIST_EAMIL));
         if (!member.arePasswordsSame(tokenRequest.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호 입니다.");
+            throw new IllegalArgumentException(Message.INVALID_PASSWORD);
         }
         return new TokenResponse(jwtTokenProvider.createToken(member.getEmail(), member.getRoles()));
     }
