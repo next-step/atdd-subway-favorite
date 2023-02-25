f3a7ccb54857ea0e7eec759784d0b189bbe187b3
cyPark95 <cas15987@gmail.com>
2023-02-26 01:06:34 +0900

    test: 깃허브 로그인 인수 테스트 수정

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -1,9 +1,11 @@
 package nextstep.member.acceptance;
 
-import nextstep.DataLoader;
 import nextstep.common.acceptance.AcceptanceTest;
+import nextstep.common.utils.DataLoader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static nextstep.member.acceptance.AuthSteps.토큰_발급_검증;
@@ -39,11 +41,15 @@ class AuthAcceptanceTest extends AcceptanceTest {
      * When 깃허브 권한증서를 통해 로그인 요청하면
      * Then 토큰을 발급받는다.
      */
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"832ovnq039hfjn", "mkfo0aFa03m", "m-a3hnfnoew92", "nvci383mciq0oq"})
     @DisplayName("깃허브 권한증서를 통한 토큰 발급")
-    void githubAuth() {
+    void githubAuth(final String code) {
+        // given
+        dataLoader.loadData();
+
         // when
-        var response = 깃허브_권한증서_로그인_요청("code");
+        var response = 깃허브_권한증서_로그인_요청(code);
 
         // then
         토큰_발급_검증(response);
