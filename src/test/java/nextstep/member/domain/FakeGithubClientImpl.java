fb9806bea0fa2c94bf1835458a1467ee823ccb5b
cyPark95 <cas15987@gmail.com>
2023-02-25 16:38:06 +0900

    test: 깃허브 로그인 액세스 토큰 조회 테스트 페이크 클래스 작성

---
+++ src/test/java/nextstep/member/domain/FakeGithubClientImpl.java
@@ -0,0 +1,26 @@
+package nextstep.member.domain;
+
+import nextstep.auth.domain.GithubProfileResponse;
+import nextstep.auth.domain.Oauth2Client;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import static nextstep.common.constants.ErrorConstant.INVALID_AUTHENTICATION_INFO;
+
+@Component
+@Primary
+public class FakeGithubClientImpl implements Oauth2Client {
+
+    private GithubResponses response;
+
+    @Override
+    public String getAccessToken(final String code) {
+        try {
+            response = GithubResponses.ofCode(code);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(INVALID_AUTHENTICATION_INFO);
+        }
+
+        return response.getAccessToken();
+    }
+}
