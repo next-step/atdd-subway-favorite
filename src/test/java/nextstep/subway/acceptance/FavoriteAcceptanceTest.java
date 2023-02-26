05717eea3e7559c07b919935480605ec7e6afa9a
Jaepil Yu <jaepilyu1324@gmail.com>
2023-02-26 20:01:42 +0900

    test: 즐겨찾기 추가 인수 테스트 추가

---
+++ src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
@@ -0,0 +1,53 @@
+package nextstep.subway.acceptance;
+
+import static nextstep.subway.acceptance.FavoriteSteps.*;
+import static nextstep.subway.acceptance.MemberSteps.*;
+import static nextstep.subway.acceptance.StationSteps.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class FavoriteAcceptanceTest extends AcceptanceTest {
+    private static final String EMAIL = "admin@email.com";
+    private static final String PASSWORD = "password";
+
+    private Long 신논현역;
+    private Long 강남역;
+    private Long 양재역;
+    private String accessToken;
+
+    @BeforeEach
+    void setup() {
+        신논현역 = 지하철역_생성_요청("신논현역").jsonPath().getLong("id");
+        강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
+        양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
+        accessToken = 베어러_인증_로그인_요청(EMAIL, PASSWORD).jsonPath().getString("accessToken");
+    }
+
+    @Test
+    @DisplayName("로그인 후 즐겨찾기 추가")
+    void addFavorite() {
+        ExtractableResponse<Response> response = 즐겨찾기_추가(accessToken, 신논현역, 강남역);
+
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+            () -> assertThat(response.header("Location")).isNotBlank()
+        );
+
+    }
+
+    @Test
+    @DisplayName("로그인 없이 즐겨찾기 추가할 경우 에러")
+    void addFavoriteWithoutLogin() {
+        accessToken = "";
+        assertThat(즐겨찾기_추가(accessToken, 신논현역, 강남역).statusCode()).isNotEqualTo(HttpStatus.CREATED.value());
+
+    }
+}
---
+++ src/test/java/nextstep/subway/acceptance/FavoriteSteps.java
@@ -0,0 +1,25 @@
+package nextstep.subway.acceptance;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class FavoriteSteps {
+    public static ExtractableResponse<Response> 즐겨찾기_추가(String accessToken, Long source, Long target) {
+        Map<String, String> params = new HashMap<>();
+        params.put("source", String.valueOf(source));
+        params.put("color", String.valueOf(target));
+        return RestAssured
+            .given().log().all()
+            .auth().oauth2(accessToken)
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/favorites")
+            .then().log().all().extract();
+    }
+}
