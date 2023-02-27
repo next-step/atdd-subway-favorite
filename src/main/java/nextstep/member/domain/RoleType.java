4e9b2115624c49be333f3238ea3c6ba563eedf68
wkwon <rnjsdntjr26@daum.net>
2023-02-27 19:35:08 +0900

    3주차 1단계: 미사용 메서드 제거 (리팩터링)

--- src/main/java/nextstep/member/domain/RoleType.java
+++ src/main/java/nextstep/member/domain/RoleType.java
@@ -5,12 +5,4 @@ import java.util.List;
 public enum RoleType {
     ROLE_ADMIN,
     ROLE_MEMBER;
-
-    public static List<String> getAdminRole(){
-        return List.of(ROLE_MEMBER.name(), ROLE_ADMIN.name());
-    }
-
-    public static List<String> getMemberRole(){
-        return List.of(ROLE_MEMBER.name());
-    }
 }
