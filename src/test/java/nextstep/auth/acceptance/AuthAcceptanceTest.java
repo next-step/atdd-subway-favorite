6c653c0262edfb533a49153f497c2c0c1cb2b62b
wu2ee <gulb1602@gmail.com>
2023-02-27 22:59:38 +0900

    test : 깃헙 로그인 인수 테스트 추가
    - fake input / output 추가

--- src/test/java/nextstep/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/auth/acceptance/AuthAcceptanceTest.java
@@ -3,10 +3,18 @@ package nextstep.auth.acceptance;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.AcceptanceTest;
+import nextstep.auth.acceptance.fake.FakeGithubResponses;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static nextstep.auth.acceptance.AuthSteps.깃헙_로그인_요청_성공;
+import static nextstep.auth.acceptance.AuthSteps.깃헙_로그인_요청_실패;
 import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청_성공;
 import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청_실패;
 import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
@@ -68,5 +76,62 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
         // when & then
         베어러_인증_로그인_요청_실패(EMAIL, invalidPassword);
+    }
+
+    /**
+     * given Github 에서 받은, 서버에 이미 등록된 유저의 권한증서(code)를 가지고
+     * when Github 로 로그인 요청하면
+     * then 우리 서버의 토큰을 발급받을 수 있다.
+     */
+    @DisplayName("Github Auth : Github 에서 받은 이미 등록된 유저의 권한증서(code)를 이용하여 로그인 요청 하면 액세스 토큰을 받는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"사용자1", "사용자2", "사용자3", "사용자4"})
+    void githubAuthLoginAlreadyMember(FakeGithubResponses fakeGithubResponses) {
+        // given
+        회원_생성_요청(fakeGithubResponses.getEmail(), PASSWORD, AGE);
+        Map<String, String> params = new HashMap<>();
+        params.put("code", fakeGithubResponses.getCode());
+
+        // when
+        ExtractableResponse<Response> response = 깃헙_로그인_요청_성공(params);
+
+        // then
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    /**
+     * given Github 에서 받은, 서버에 등록되지 않은 유저의 권한증서(code)를 가지고
+     * when Github 로 로그인 요청하면
+     * then 우리 서버의 토큰을 발급받을 수 있다.
+     */
+    @DisplayName("Github Auth : Github 에서 받은 이미 등록된 유저의 권한증서(code)를 이용하여 로그인 요청 하면 액세스 토큰을 받는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"사용자1", "사용자2", "사용자3", "사용자4"})
+    void githubAuthLoginCreateMember(FakeGithubResponses fakeGithubResponses) {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("code", fakeGithubResponses.getCode());
+
+        // when
+        ExtractableResponse<Response> response = 깃헙_로그인_요청_성공(params);
+
+        // then
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    /**
+     * given Github 에 없는 권한증서(code)를 가지고
+     * when Github 로 로그인 요청하면
+     * then 우리 서버의 토큰을 발급받을 수 없다.
+     */
+    @DisplayName("Github Auth : Github 에 없는 권한증서(code)로 로그인 요청하면 토큰을 발급받을 수 없다.")
+    @Test
+    void githubAuthLoginExceptionNotContainsCode() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("code", "code");
+
+        // when & then
+        깃헙_로그인_요청_실패(params);
     }
 }
