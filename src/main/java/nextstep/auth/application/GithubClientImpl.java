d7ac4b90600fcc529f50fd857dc0877965e06dc9
wu2ee <gulb1602@gmail.com>
2023-02-27 23:02:30 +0900

    feat : GithubClient 인터페이스 & 구현체 추가
    - 프로덕션 코드 -> 실제 github 요청 client 구현
    - 테스트 코드 -> fake 객체 이용

---
+++ src/main/java/nextstep/auth/application/GithubClientImpl.java
@@ -0,0 +1,73 @@
+package nextstep.auth.application;
+
+import nextstep.auth.dto.GithubAccessTokenRequest;
+import nextstep.auth.dto.GithubAccessTokenResponse;
+import nextstep.auth.dto.GithubProfileResponse;
+import nextstep.auth.exception.AuthenticationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+@Component
+public class GithubClientImpl implements GithubClient {
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.url.access-token}")
+    private String tokenUrl;
+    @Value("${github.url.profile}")
+    private String profileUrl;
+
+    @Override
+    public String getAccessTokenFromGithub(String code) {
+        GithubAccessTokenRequest githubAccessTokenRequest = new GithubAccessTokenRequest(
+                code,
+                clientId,
+                clientSecret
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity(
+                githubAccessTokenRequest, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        String accessToken = Objects.requireNonNull(restTemplate
+                        .exchange(tokenUrl, HttpMethod.POST, httpEntity, GithubAccessTokenResponse.class)
+                        .getBody())
+                .getAccessToken();
+        if (accessToken == null) {
+            throw new AuthenticationException();
+        }
+        return accessToken;
+    }
+
+    @Override
+    public GithubProfileResponse getGithubProfileFromGithub(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "token " + accessToken);
+
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            return restTemplate
+                    .exchange(profileUrl, HttpMethod.GET, httpEntity, GithubProfileResponse.class)
+                    .getBody();
+        } catch (HttpClientErrorException e) {
+            throw new AuthenticationException();
+        }
+    }
+}
