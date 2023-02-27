ee65661dd2c7480a09897ce8896ec0134b7575e0
Sangmin <sm9171@nate.com>
2023-02-27 17:39:45 +0900

    refactor(auth): FavoriteResponse 객체 수정 * 외부 엔티티 객체 -> 내부클래스 객체로 수정

--- src/main/java/nextstep/member/application/dto/FavoriteResponse.java
+++ src/main/java/nextstep/member/application/dto/FavoriteResponse.java
@@ -1,35 +1,70 @@
 package nextstep.member.application.dto;
 
 import nextstep.member.domain.Favorite;
-import nextstep.subway.domain.Station;
 
 public class FavoriteResponse {
-    private Long memberId;
-    private Station source;
-    private Station target;
+	private Long memberId;
+	private Source source;
+	private Target target;
 
-    protected FavoriteResponse() {
-    }
+	protected FavoriteResponse() {
+	}
 
-    public FavoriteResponse(Long memberId, Station source, Station target) {
-        this.memberId = memberId;
-        this.source = source;
-        this.target = target;
-    }
+	public FavoriteResponse(Long memberId, Source source, Target target) {
+		this.memberId = memberId;
+		this.source = source;
+		this.target = target;
+	}
 
-    public static FavoriteResponse of(Favorite favorite) {
-        return new FavoriteResponse(favorite.getMemberId(), favorite.getSource(), favorite.getTarget());
-    }
+	public static FavoriteResponse of(Favorite favorite) {
+		return new FavoriteResponse(favorite.getMemberId(), new Source(favorite.getSource().getId(), favorite.getSource().getName()), new Target(favorite.getTarget().getId(), favorite.getTarget().getName()));
+	}
 
-    public Long getMemberId() {
-        return memberId;
-    }
+	public Long getMemberId() {
+		return memberId;
+	}
 
-    public Station getSource() {
-        return source;
-    }
+	public Source getSource() {
+		return source;
+	}
 
-    public Station getTarget() {
-        return target;
-    }
+	public Target getTarget() {
+		return target;
+	}
+
+	static class Source {
+		private Long id;
+		private String name;
+
+		public Source(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+
+	static class Target {
+		private Long id;
+		private String name;
+
+		public Target(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
 }
