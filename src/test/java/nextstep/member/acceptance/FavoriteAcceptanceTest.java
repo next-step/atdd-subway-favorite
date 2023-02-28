4213cf8fd15246c276e2f9b0a58ca234c5987d4b
cyPark95 <cas15987@gmail.com>
2023-02-28 16:09:43 +0900

    test: 즐겨찾기 조회 인수 테스트 작성

--- src/test/java/nextstep/member/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/FavoriteAcceptanceTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import static nextstep.common.constants.ErrorConstant.INVALID_AUTHENTICATION_INFO;
-import static nextstep.member.acceptance.MemberSteps.*;
+import static nextstep.member.acceptance.AuthSteps.권한없는_요청_검증;
+import static nextstep.member.acceptance.FavoriteSteps.*;
+import static nextstep.member.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class FavoriteAcceptanceTest extends AcceptanceTest {
 
@@ -77,17 +80,52 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
      * When 비로그인 상태에서 즐겨찾기 조회를 요청하면
      * Then 예외가 발생한다.
      */
+    @Test
+    @DisplayName("비로그인 상태에서 즐겨찾기 조회")
+    void showFavorites_notLogin() {
+        // when
+        var response = 비로그인_즐겨찾기_조회_요청();
+
+        // then
+        권한없는_요청_검증(response, INVALID_AUTHENTICATION_INFO);
+    }
 
     /**
      * When 유효하지 않은 토큰으로 즐겨찾기 조회를 요청하면
      * Then 예외가 발생한다.
      */
+    @Test
+    @DisplayName("유효하지 않은 토큰으로 즐겨찾기 조회")
+    void showFavorites_invalidToken() {
+        // when
+        var response = 토큰으로_즐겨찾기_조회_요청("invalidToken");
+
+        // then
+        권한없는_요청_검증(response, INVALID_AUTHENTICATION_INFO);
+    }
 
     /**
      * Given 즐겨찾기를 등록하고
      * When 즐겨찾기 조회를 요청하면
      * Then 즐겨찾기 목록이 조회된다.
      */
+    @Test
+    @DisplayName("즐겨찾기 조회")
+    void showFavorites() {
+        // given
+        경로_즐겨찾기_등록(관리자, 강남역, 선릉역);
+
+        // when
+        var response = 토큰으로_즐겨찾기_조회_요청(관리자);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertAll(
+                () -> assertThat(response.jsonPath().getLong("[0].id")).isEqualTo(1L),
+                () -> assertThat(response.jsonPath().getLong("[0].source.id")).isEqualTo(강남역),
+                () -> assertThat(response.jsonPath().getLong("[0].target.id")).isEqualTo(선릉역)
+        );
+    }
 
     /**
      * When 비로그인 상태에서 즐겨찾기 삭제를 요청하면
--- src/test/java/nextstep/member/acceptance/FavoriteSteps.java
+++ src/test/java/nextstep/member/acceptance/FavoriteSteps.java
@@ -3,10 +3,13 @@ package nextstep.member.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FavoriteSteps {
 
@@ -27,6 +30,27 @@ public class FavoriteSteps {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(params)
                 .when().post("/favorites")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 경로_즐겨찾기_등록(final String token,
+                                  final Long sourceId,
+                                  final Long targetId) {
+        ExtractableResponse<Response> response = 경로_즐겨찾기_등록_요청(token, sourceId, targetId);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    public static ExtractableResponse<Response> 비로그인_즐겨찾기_조회_요청() {
+        return 토큰으로_즐겨찾기_조회_요청("");
+    }
+
+    public static ExtractableResponse<Response> 토큰으로_즐겨찾기_조회_요청(final String token) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/favorites")
                 .then().log().all()
                 .extract();
     }
