afa9c11d2a43c5dabfd326d63a7135f7b975b370
cafitac <cafitac99@gmail.com>
2023-02-28 17:40:09 +0900

    test: Github 소셜 로그인에 대한 인수 테스트 코드를 작성합니다

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.subway.acceptance.MemberSteps.깃헙_소셜_로그인_요청;
 import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,5 +19,19 @@ class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
 
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    /**
+     * When Github 권한증서(code) 를 이용하여 로그인을 요청하면
+     * Then 내부 서버 사용자에 대한 인증 토큰이 발급된다.
+     */
+    @DisplayName("Github OAuth")
+    @Test
+    void githubOAuth() {
+        // when
+        final ExtractableResponse<Response> 깃헙_소셜_로그인_응답 = 깃헙_소셜_로그인_요청("testCode");
+
+        // then
+        assertThat(깃헙_소셜_로그인_응답.jsonPath().getString("accessToken")).isNotBlank();
     }
 }
--- src/test/java/nextstep/subway/acceptance/MemberSteps.java
+++ src/test/java/nextstep/subway/acceptance/MemberSteps.java
@@ -26,6 +26,18 @@ public class MemberSteps {
                 .statusCode(HttpStatus.OK.value()).extract();
     }
 
+    public static ExtractableResponse<Response> 깃헙_소셜_로그인_요청(final String code) {
+        Map<String, String> params = new HashMap<>();
+        params.put("code", code);
+
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/login/github")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
+    }
+
     public static ExtractableResponse<Response> 회원_생성_요청(String email, String password, Integer age) {
         Map<String, String> params = new HashMap<>();
         params.put("email", email);
