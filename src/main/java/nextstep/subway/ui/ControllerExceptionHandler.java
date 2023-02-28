e90a36518bd8f640034c1644d4b42379f53604c9
dongho.geum <dongho.geum@myrealtrip.com>
2023-02-28 17:08:20 +0900

    feat: 권한이 없는 경우 401 Unauthorized 응답 기능

--- src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/nextstep/subway/ui/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package nextstep.subway.ui;
 
+import nextstep.member.domain.exception.IllegalAccessTokenException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,4 +18,11 @@ public class ControllerExceptionHandler {
     public ResponseEntity<Void> handleIllegalArgsException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().build();
     }
+
+    @ExceptionHandler(IllegalAccessTokenException.class)
+    public ResponseEntity<Void> handleIllegalAccessTokenException(IllegalAccessTokenException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+
 }
