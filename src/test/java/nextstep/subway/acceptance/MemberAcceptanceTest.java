0a328faa176093db42c8c8cb5439013de3fed826
JohnPrk <uo3641493@gmail.com>
2023-02-28 15:29:18 +0900

    feat : 토큰을 이용해 내 정보 조회 기능 인수 테스트 완성

--- src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/MemberAcceptanceTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.member.application.dto.TokenResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -10,9 +11,9 @@ import static nextstep.subway.acceptance.MemberSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberAcceptanceTest extends AcceptanceTest {
-    public static final String EMAIL = "email@email.com";
-    public static final String PASSWORD = "password";
-    public static final int AGE = 20;
+    private static final String EMAIL = "email@email.com";
+    private static final String PASSWORD = "password";
+    private static final int AGE = 20;
 
     @DisplayName("회원가입을 한다.")
     @Test
@@ -64,8 +65,24 @@ class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
+    /**
+     * given : 회원 데이터 생성 하고
+     * when : 인증 로그인 요청을 하고 토큰을 받아서
+     * then : 회원 정보를 조회할 수 있다.
+     */
     @DisplayName("내 정보를 조회한다.")
     @Test
     void getMyInfo() {
+        //given
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
+
+        //when
+        ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
+        TokenResponse tokenResponse = response.as(TokenResponse.class);
+        String accessToken = tokenResponse.getAccessToken();
+        ExtractableResponse<Response> resultResponse = 토큰으로_내_회원_정보_조회_요청(accessToken);
+
+        //then
+        회원_정보_조회됨(resultResponse, EMAIL, AGE);
     }
 }
