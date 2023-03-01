28eaa5db085851b093ac5947139265a939ba8741
bactoria <bactoria@gmail.com>
2023-03-01 21:42:24 +0900

    test:: 즐겨찾기 조회 인수테스트 추가

--- src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
@@ -15,11 +15,14 @@ import java.util.Map;
 import static nextstep.subway.acceptance.MemberSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class FavoriteAcceptanceTest extends AcceptanceTest {
     private Long 강남역;
     private Long 양재역;
-    
+    private Long 양재시민의숲역;
+    private Long 청계산입구역;
+
     private String accessToken;
 
     /**
@@ -32,6 +35,8 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
 
         강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
         양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
+        양재시민의숲역 = 지하철역_생성_요청("양재시민의숲역").jsonPath().getLong("id");
+        청계산입구역 = 지하철역_생성_요청("청계산입구역").jsonPath().getLong("id");
 
         회원_생성_요청("bactoria@gmail.com", "qwe123", 20);
         var 베어러_인증_로그인_응답 = 베어러_인증_로그인_요청("bactoria@gmail.com", "qwe123");
@@ -45,13 +50,49 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
     @DisplayName("즐겨찾기 추가")
     @Test
     void addFavorite() {
+        // when
+        var 즐겨찾기_추가_응답 = 즐겨찾기_추가_요청(강남역, 양재역);
+
+        // then
+        assertThat(즐겨찾기_추가_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    /**
+     * GIVEN 로그인한 회원이 즐겨찾기를 2번 하고
+     * WHEN 로그인한 회원이 즐겨찾기 조회하는 경우 <br>
+     * THEN 2개의 즐겨찾기 목록이 조회된다 <br>
+     */
+    @DisplayName("즐겨찾기 조회")
+    @Test
+    void readFavorite() {
         // given
-        Map<String, String> params = new HashMap<>();
-        params.put("source", 강남역 + "");
-        params.put("target", 양재역 + "");
+        즐겨찾기_추가_요청(강남역, 양재역);
+        즐겨찾기_추가_요청(양재시민의숲역, 청계산입구역);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/favorites")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(() -> {
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            assertThat(response.body().jsonPath().getList("source.id", Long.class)).containsExactly(강남역, 양재시민의숲역);
+            assertThat(response.body().jsonPath().getList("target.id", Long.class)).containsExactly(양재역, 청계산입구역);
+        });
+    }
+
+    private ExtractableResponse<Response> 즐겨찾기_추가_요청(Long source, Long target) {
+        Map<String, String> params = new HashMap<>();
+        params.put("source", source + "");
+        params.put("target", target + "");
+
+        // when
+        return RestAssured.given().log().all()
                 .body(params)
                 .auth().oauth2(accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -59,9 +100,5 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
                 .post("/favorites")
                 .then().log().all()
                 .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
-
 }
