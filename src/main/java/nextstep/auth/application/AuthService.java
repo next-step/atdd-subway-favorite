4c667e48839813ca35b056e0f4ab431ca34cf015
cafitac <cafitac99@gmail.com>
2023-03-01 22:32:12 +0900

    feat: GithubLogin 에 대한 서비스 코드를 작성합니다

--- src/main/java/nextstep/auth/application/AuthService.java
+++ src/main/java/nextstep/auth/application/AuthService.java
@@ -1,10 +1,13 @@
 package nextstep.auth.application;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.auth.application.dto.GithubLoginRequest;
 import nextstep.auth.application.dto.TokenRequest;
 import nextstep.auth.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
+import nextstep.member.infra.GithubClient;
+import nextstep.member.infra.dto.GithubProfileResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,7 @@ public class AuthService {
 
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final GithubClient githubClient;
 
     public TokenResponse login(final TokenRequest tokenRequest) {
         final Member member = memberRepository.findByEmail(tokenRequest.getEmail())
@@ -20,6 +24,15 @@ public class AuthService {
         if (!member.checkPassword(tokenRequest.getPassword())) {
             throw new IllegalArgumentException();
         }
+        final String token = jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
+        return new TokenResponse(token);
+    }
+
+    public TokenResponse githubLogin(final GithubLoginRequest githubLoginRequest) {
+        final String accessToken = githubClient.getAccessTokenFromGithub(githubLoginRequest.getCode());
+        final GithubProfileResponse githubProfileResponse = githubClient.getGithubProfileFromGithub(accessToken);
+        final Member member = memberRepository.findByEmail(githubProfileResponse.getEmail())
+                .orElseThrow(IllegalArgumentException::new);
         final String token = jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
         return new TokenResponse(token);
     }
