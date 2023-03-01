4dbb4125bf69c6a13413f51dfa0412a8b6a16dbb
cafitac <cafitac99@gmail.com>
2023-03-01 22:31:34 +0900

    test: GithubOAuth 에 대한 테스트 코드가 잘못 작성되어있어 수정합니다

--- src/test/java/nextstep/subway/unit/AuthServiceTest.java
+++ src/test/java/nextstep/subway/unit/AuthServiceTest.java
@@ -7,12 +7,13 @@ import nextstep.auth.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import nextstep.member.domain.RoleType;
+import nextstep.subway.GithubClientTestContextConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Import(GithubClientTestContextConfiguration.class)
 @Transactional
 public class AuthServiceTest {
 
@@ -52,7 +54,7 @@ public class AuthServiceTest {
     @Test
     void githubOAuth() {
         // given
-        final GithubLoginRequest githubLoginRequest = new GithubLoginRequest(GithubResponses.사용자1.getEmail());
+        final GithubLoginRequest githubLoginRequest = new GithubLoginRequest(GithubResponses.사용자1.getCode());
 
         // when
         final TokenResponse tokenResponse = authService.githubLogin(githubLoginRequest);
