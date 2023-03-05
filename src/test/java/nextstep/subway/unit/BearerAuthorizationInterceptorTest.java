952ab942ff0967e3d85a22963aa41dcc0e69303a
cafitac <cafitac99@gmail.com>
2023-03-05 18:55:59 +0900

    test: Bearer 인증에 대한 단위 테스트 코드를 작성합니다

--- src/test/java/nextstep/subway/unit/BearerAuthorizationInterceptorTest.java
+++ src/test/java/nextstep/subway/unit/BearerAuthorizationInterceptorTest.java
@@ -1,45 +1,47 @@
 package nextstep.subway.unit;
 
-import nextstep.auth.filter.BasicAuthorizationInterceptor;
+import nextstep.auth.application.JwtTokenProvider;
+import nextstep.auth.filter.BearerAuthorizationInterceptor;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import nextstep.member.domain.RoleType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import java.util.Base64;
-import java.util.Enumeration;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BasicAuthorizationInterceptorTest extends SpringTest {
+public class BearerAuthorizationInterceptorTest extends SpringTest {
 
-    private static final String USERNAME = "test@email.com";
-    private static final String PASSWORD = "password";
     private static final MockHttpServletRequest request = new MockHttpServletRequest();
     private static final MockHttpServletResponse response = new MockHttpServletResponse();
     @Autowired
-    private BasicAuthorizationInterceptor basicAuthorizationInterceptor;
-    @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    private BearerAuthorizationInterceptor bearerAuthorizationInterceptor;
+    private Member member;
 
     @BeforeEach
     void setUp() {
-        memberRepository.save(new Member("test@email.com", "password", 20, List.of(RoleType.ROLE_MEMBER.name())));
+        member = memberRepository.save(new Member("test@email.com", "password", 20, List.of(RoleType.ROLE_MEMBER.name())));
     }
 
+    @DisplayName("베어러 토큰 인증")
     @Test
     void authorize() throws Exception {
         // given
-        final String token = new String(Base64.getEncoder().encode((USERNAME + ":" + PASSWORD).getBytes()));
-        request.addHeader("Authorization", "Basic " + token);
+        final String token = jwtTokenProvider.createToken(member.getEmail(), member.getRoles());
+        request.addHeader("Authorization", "Bearer " + token);
 
         // when
-        basicAuthorizationInterceptor.preHandle(request, response, null);
+        bearerAuthorizationInterceptor.preHandle(request, response, null);
 
         // then
         assertThat(request.getAttribute("user")).isNotNull();
