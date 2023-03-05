8cbae10d1ba4679f554dd34d875cca70a3f86fe6
cafitac <cafitac99@gmail.com>
2023-03-05 19:07:24 +0900

    feat: Bearer 인증 인터셉터를 등록합니다

--- src/main/java/nextstep/auth/WebMvcConfig.java
+++ src/main/java/nextstep/auth/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package nextstep.auth;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.auth.application.JwtTokenProvider;
+import nextstep.auth.filter.BearerAuthorizationInterceptor;
 import nextstep.member.application.MemberService;
 import nextstep.auth.filter.BasicAuthorizationInterceptor;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new BasicAuthorizationInterceptor(memberService))
-                .addPathPatterns("/members/me");
+                .addPathPatterns("/members/me")
+                .addPathPatterns("/favorites");
+        registry.addInterceptor(new BearerAuthorizationInterceptor(memberService, jwtTokenProvider))
+                .addPathPatterns("/members/me")
+                .addPathPatterns("/favorites");
     }
 }
