8d8e5d60437444ec23140450b84969c524b91728
hoo <tbnsok40@gmail.com>
2023-03-21 17:38:33 +0900

    test: 로그인 및 회원가입 인수테스트

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -2,8 +2,13 @@ package nextstep.subway.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.DataLoader;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static nextstep.subway.acceptance.MemberSteps.*;
 import static org.assertj.core.api.Assertions.*;
@@ -12,6 +17,9 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "admin@email.com";
     private static final String PASSWORD = "password";
     private static final int AGE = 30;
+
+    @Autowired
+    private DataLoader dataLoader;
 
     @DisplayName("Bearer Auth : Bearer 인증 테스트")
     @Test
@@ -22,10 +30,47 @@ class AuthAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
     }
 
-    @DisplayName("Bearer Auth : Bearer 인증 실패 테스트")
     @Test
+    @DisplayName("Bearer Auth : Bearer 인증 실패 테스트")
     void bearerAuthFailure() {
         회원_생성_요청(EMAIL, PASSWORD, AGE);
         assertThatThrownBy(() -> 베어러_인증_로그인_요청(PASSWORD, EMAIL), new NullPointerException().getMessage());
+    }
+
+    /**
+     * given Github 의 회원일 때
+     * when Github 로그인을 하면
+     * then 토큰을 발급 받을 수 있다.
+     */
+    @ParameterizedTest
+    @DisplayName("로그인 시 토큰 발급")
+    @ValueSource(strings = {"1231@3123", "3123@1231"})
+    void singInTest(String code) {
+        // given
+        dataLoader.loadGithubMemberData();
+
+        // when
+        String accessToken = 깃허브_로그인(code).jsonPath().get("accessToken");
+
+        // then
+        assertThat(accessToken).isNotBlank();
+    }
+
+    /**
+     * given Github 의 회원일 때
+     * when Github 로그인을 하면
+     * then 토큰을 발급 받을 수 있다.
+     */
+    @ParameterizedTest
+    @DisplayName("회원가입 진행 후 토큰 발급")
+    @ValueSource(strings = {"1231@3123", "3123@1231"})
+    void singUpTest(String code) {
+        // given
+
+        // when
+        String accessToken = 깃허브_로그인(code).jsonPath().get("accessToken");
+
+        // then
+        assertThat(accessToken).isNotBlank();
     }
 }
--- src/test/java/nextstep/subway/acceptance/MemberSteps.java
+++ src/test/java/nextstep/subway/acceptance/MemberSteps.java
@@ -97,6 +97,21 @@ public class MemberSteps {
                 .extract();
     }
 
+    public static ExtractableResponse<Response> 깃허브_로그인(String code) {
+        Map<String, String> params = new HashMap<>();
+        params.put("code", code);
+
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+            .when()
+                .post("/login/github")
+            .then()
+                .log()
+                .all()
+            .statusCode(HttpStatus.OK.value()).extract();
+    }
+
     public static void 회원_정보_조회됨(ExtractableResponse<Response> response, String email, int age) {
         assertThat(response.jsonPath().getString("id")).isNotNull();
         assertThat(response.jsonPath().getString("email")).isEqualTo(email);
