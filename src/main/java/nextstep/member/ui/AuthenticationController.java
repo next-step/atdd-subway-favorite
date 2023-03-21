0ab4982c1307ddaa5dc7dbd6ffc7c98668cea605
hoo <tbnsok40@gmail.com>
2023-03-21 17:37:11 +0900

    feature: github login Controller

--- src/main/java/nextstep/member/ui/AuthenticationController.java
+++ src/main/java/nextstep/member/ui/AuthenticationController.java
@@ -1,15 +1,16 @@
 package nextstep.member.ui;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import nextstep.member.application.dto.GithubAccessTokenResponse;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
 import nextstep.member.application.service.AuthenticationService;
+import nextstep.member.application.dto.GithubLoginRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +20,13 @@ public class AuthenticationController {
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
-        TokenResponse response = authenticationService.login(request);
+        TokenResponse response = authenticationService.basicLogin(request);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login/github")
+    public ResponseEntity<GithubAccessTokenResponse> githubLogin(@RequestBody GithubLoginRequest request) {
+        GithubAccessTokenResponse githubAccessTokenResponse = authenticationService.githubLogin(request.getCode());
+        return ResponseEntity.ok(githubAccessTokenResponse);
     }
 }
--- src/main/java/nextstep/member/ui/MemberController.java
+++ src/main/java/nextstep/member/ui/MemberController.java
@@ -1,6 +1,6 @@
 package nextstep.member.ui;
 
-import nextstep.member.application.MemberService;
+import nextstep.member.application.service.MemberService;
 import nextstep.member.application.dto.MemberRequest;
 import nextstep.member.application.dto.MemberResponse;
 import org.springframework.http.ResponseEntity;
