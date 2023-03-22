9287469c6c4b2d7583aa43340c21c511d066f516
hoo <tbnsok40@gmail.com>
2023-03-22 11:27:53 +0900

    feature: 인수 테스트 케이스 생성

---
+++ src/test/java/nextstep/subway/acceptance/FavoriteAcceptanceTest.java
@@ -0,0 +1,32 @@
+package nextstep.subway.acceptance;
+
+import org.junit.jupiter.api.Test;
+
+public class FavoriteAcceptanceTest {
+
+    @Test
+    void 즐겨찾기_생성_테스트() {
+
+    }
+
+    @Test
+    void 즐겨찾기_생성_예외_테스트() {
+
+    }
+
+    @Test
+    void 즐겨찾기_조회_테스트() {
+
+    }
+
+    @Test
+    void 즐겨찾기_삭제_테스트() {
+
+    }
+
+    @Test
+    void 비인증_유저_즐겨찾기_예외_테스트() {
+
+    }
+
+}
