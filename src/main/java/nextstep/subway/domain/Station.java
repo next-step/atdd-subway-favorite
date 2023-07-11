240871d02b1208193277549debfe477676e91857
boorownie <probitanima11@gmail.com>
2023-07-11 23:36:51 +0900

    7th default sample

---
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -0,0 +1,29 @@
+package nextstep.subway.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Station() {
+    }
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
