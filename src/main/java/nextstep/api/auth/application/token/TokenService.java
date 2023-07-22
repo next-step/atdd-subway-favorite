1aaab02235ad56747ce187385d9a8aca6bdce820
sinb57 <sinb57@naver.com>
2023-07-23 02:04:25 +0900

    refactor: TokenService 메서드 리팩터링

--- src/main/java/nextstep/api/auth/application/token/TokenService.java
+++ src/main/java/nextstep/api/auth/application/token/TokenService.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import nextstep.api.auth.AuthenticationException;
 import nextstep.api.auth.application.token.dto.TokenResponse;
+import nextstep.api.auth.application.token.oauth2.OAuth2User;
 import nextstep.api.auth.application.token.oauth2.OAuth2UserService;
 import nextstep.api.auth.application.token.oauth2.github.GithubClient;
+import nextstep.api.auth.application.userdetails.UserDetails;
 import nextstep.api.auth.application.userdetails.UserDetailsService;
 import nextstep.api.auth.support.JwtTokenProvider;
 
@@ -19,23 +21,28 @@ public class TokenService {
     private final GithubClient githubClient;
 
     public TokenResponse createToken(final String email, final String password) {
+        final var userDetails = loadUser(email, password);
+        final var token = jwtTokenProvider.createToken(userDetails.getUsername(), userDetails.getRole());
+        return new TokenResponse(token);
+    }
+
+    private UserDetails loadUser(final String email, final String password) {
         final var userDetails = userDetailsService.loadUserByUsername(email);
         if (!userDetails.getPassword().equals(password)) {
             throw new AuthenticationException();
         }
-
-        final var token = jwtTokenProvider.createToken(userDetails.getUsername(), userDetails.getRole());
-
-        return new TokenResponse(token);
+        return userDetails;
     }
 
     public TokenResponse createTokenFromGithub(String code) {
-        final var accessTokenFromGithub = githubClient.getAccessTokenFromGithub(code);
-        final var githubProfile = githubClient.getGithubProfileFromGithub(accessTokenFromGithub);
-
-        final var oAuth2User = oAuth2UserService.loadUser(githubProfile);
-
+        final var oAuth2User = loadUserFromGithub(code);
         final var token = jwtTokenProvider.createToken(oAuth2User.getUsername(), oAuth2User.getRole());
         return new TokenResponse(token);
+    }
+
+    private OAuth2User loadUserFromGithub(final String code) {
+        final var accessTokenFromGithub = githubClient.getAccessTokenFromGithub(code);
+        final var githubProfile = githubClient.getGithubProfileFromGithub(accessTokenFromGithub);
+        return oAuth2UserService.loadUser(githubProfile);
     }
 }
