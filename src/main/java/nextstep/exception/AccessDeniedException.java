47950c11f0a14439af0a1c02ad8a4f53501141c9
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-07-26 21:45:14 +0900

    refactor: 예외 메시지 접근 제어자 public에서 private로 수정

--- src/main/java/nextstep/exception/AccessDeniedException.java
+++ src/main/java/nextstep/exception/AccessDeniedException.java
@@ -1,7 +1,7 @@
 package nextstep.exception;
 
 public class AccessDeniedException extends RuntimeException {
-    public static final String ERROR_MESSAGE = "접근 권한이 없습니다.";
+    private static final String ERROR_MESSAGE = "접근 권한이 없습니다.";
 
     public AccessDeniedException() {
         super(ERROR_MESSAGE);
--- src/main/java/nextstep/exception/notfoundexception/FavoriteNotFoundException.java
+++ src/main/java/nextstep/exception/notfoundexception/FavoriteNotFoundException.java
@@ -1,7 +1,7 @@
 package nextstep.exception.notfoundexception;
 
 public class FavoriteNotFoundException extends NotFoundException {
-    public static final String ERROR_MESSAGE = "Favorite 이 존재하지 않습니다.";
+    private static final String ERROR_MESSAGE = "Favorite 이 존재하지 않습니다.";
 
     public FavoriteNotFoundException() {
         super(ERROR_MESSAGE);
