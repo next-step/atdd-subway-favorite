08212f7dcdf1cf8d94f3cb232d4a85399c8b355f
Hyeongmin Park <thalsal@naver.com>
2023-07-27 11:52:29 +0900

    test : 멤버조회 인수테스트

--- src/test/java/nextstep/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/MemberAcceptanceTest.java
@@ -1,12 +1,19 @@
 package nextstep.member.acceptance;
 
+import static nextstep.member.acceptance.MemberSteps.내정보_조회_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_로그인_요청_후_token_추출;
+import static nextstep.member.acceptance.MemberSteps.회원_삭제_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_정보_수정_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_정보_조회_요청;
+import static nextstep.member.acceptance.MemberSteps.회원_정보_조회됨;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import nextstep.utils.AcceptanceTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-
-import static nextstep.member.acceptance.MemberSteps.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "email@email.com";
@@ -63,15 +70,26 @@ class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
-    /**
-     * Given 회원 가입을 생성하고
-     * And 로그인을 하고
-     * When 토큰을 통해 내 정보를 조회하면
-     * Then 내 정보를 조회할 수 있다
-     */
     @DisplayName("내 정보를 조회한다.")
     @Test
     void getMyInfo() {
 
+        //given
+        var createResponse = 회원_정보_조회_요청(회원_생성_요청(EMAIL, PASSWORD, AGE));
+
+        String accessToken = 회원_로그인_요청_후_token_추출(EMAIL, PASSWORD);
+
+        //when
+         var response = 내정보_조회_요청(accessToken);
+
+         //then
+        Assertions.assertAll(
+            () -> assertThat(response.jsonPath().getString("id"))
+                .isEqualTo(createResponse.jsonPath().getString("id")),
+            () -> assertThat(response.jsonPath().getString("email"))
+                .isEqualTo(createResponse.jsonPath().getString("email")),
+            () -> assertThat(response.jsonPath().getInt("age"))
+                .isEqualTo(createResponse.jsonPath().getInt("age"))
+        );
     }
 }
--- src/test/java/nextstep/member/acceptance/MemberSteps.java
+++ src/test/java/nextstep/member/acceptance/MemberSteps.java
@@ -3,6 +3,7 @@ package nextstep.member.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
@@ -63,5 +64,30 @@ public class MemberSteps {
         assertThat(response.jsonPath().getString("id")).isNotNull();
         assertThat(response.jsonPath().getString("email")).isEqualTo(email);
         assertThat(response.jsonPath().getInt("age")).isEqualTo(age);
+    }
+
+    public static String 회원_로그인_요청_후_token_추출(String email, String password) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(params)
+            .when().post("/login/token")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value()).extract()
+            .jsonPath().getString("accessToken");
+    }
+
+    public static  ExtractableResponse<Response> 내정보_조회_요청(String accessToken) {
+
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .header("authorization", "Bearer " + accessToken)
+            .when().get("/members/me")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
     }
 }
