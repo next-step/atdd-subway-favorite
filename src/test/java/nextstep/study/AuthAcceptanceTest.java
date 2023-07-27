a41a535ae655e8b4db930ba06cce7de969652ec8
james.jin <james.jin@kakaocorp.com>
2023-07-27 14:20:47 +0900

    2단계 - 깃헙 로그인 인수 테스트 - 피드백 -- MemberSteps의 메서드를 활용해서 회원을 생성

--- src/test/java/nextstep/study/AuthAcceptanceTest.java
+++ src/test/java/nextstep/study/AuthAcceptanceTest.java
@@ -3,18 +3,16 @@ package nextstep.study;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import nextstep.member.domain.Member;
-import nextstep.member.domain.MemberRepository;
 import nextstep.utils.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
 import static nextstep.utils.GithubTestController.CODE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,13 +21,10 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     public static final String PASSWORD = "password";
     public static final Integer AGE = 20;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
-        memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
 
         Map<String, String> params = new HashMap<>();
         params.put("email", EMAIL);
@@ -48,7 +43,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Github Auth")
     @Test
     void githubAuth() {
-        memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
 
         Map<String, String> params = new HashMap<>();
         params.put("code", CODE);
