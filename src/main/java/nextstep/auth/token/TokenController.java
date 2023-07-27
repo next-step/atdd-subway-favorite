d1e8ca3334c81edf32339bbb791b521f4c998358
Hyeongmin Park <thalsal@naver.com>
2023-07-27 11:52:55 +0900

    feat : 내 정보 조회 기능 구현

--- src/main/java/nextstep/auth/token/TokenController.java
+++ src/main/java/nextstep/auth/token/TokenController.java
@@ -14,6 +14,7 @@ public class TokenController {
         this.tokenService = tokenService;
     }
 
+    //todo. 이거 왜 Post 지?
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> createToken(@RequestBody TokenRequest request) {
         TokenResponse response = tokenService.createToken(request.getEmail(), request.getPassword());
