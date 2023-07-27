1f0a833f952e91f160c71eab53359f4b426e89b3
김설영 <kimsy8979@mrblue.com>
2023-07-27 10:11:58 +0900

    test: github 로그인 후 정보 조회 인수테스트 시나리오 작성

--- src/test/java/nextstep/study/AuthAcceptanceTest.java
+++ src/test/java/nextstep/study/AuthAcceptanceTest.java
@@ -1,20 +1,28 @@
 package nextstep.study;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.HashMap;
-import java.util.Map;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import nextstep.utils.AcceptanceTest;
+import nextstep.utils.GithubTestRepository;
+import nextstep.utils.GithubTestUser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static nextstep.auth.acceptance.step.TokenSteps.깃허브_로그인_요청;
+import static nextstep.auth.acceptance.step.TokenSteps.토큰_추출;
+import static nextstep.member.acceptance.step.MemberSteps.내_정보_조회_요청;
+import static nextstep.member.acceptance.step.MemberSteps.이메일_추출;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "admin@email.com";
@@ -23,6 +31,17 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private GithubTestRepository githubTestRepository;
+
+    private final String userCode = "code1";
+    private final String userEmail = "email1";
+
+    @BeforeEach
+    void githubSetUp() {
+        githubTestRepository.addUser(userCode, new GithubTestUser(userEmail, 10));
+    }
 
     @DisplayName("Bearer Auth")
     @Test
@@ -44,23 +63,38 @@ class AuthAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * # 요구사항
-     * - GitHubClient의 요청이 Github이 아닌 GithubTestController에서 처리하게 하기
-     * - code에 해당하는 사용자 정보를 이용하여 AccessToken 응답받기
+     * When : Github 로그인을 요청하면
+     * Then : AccessToken이 발급된다
      */
     @DisplayName("Github Auth")
     @Test
     void githubAuth() {
-        Map<String, String> params = new HashMap<>();
-        params.put("code", "code1");
+        // when
+        ExtractableResponse<Response> githubLoginResponse = 깃허브_로그인_요청(userCode);
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(params)
-                .when().post("/login/github")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value()).extract();
+        // then
+        assertThat(토큰_추출(githubLoginResponse)).isNotBlank();
+    }
 
-        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    /**
+     * Given : Github 로그인을 요청하고
+     * And : AccessToken을 발급받은 후
+     * When : 내 정보 조회를 요청하면
+     * Then : 조회가 성공한다.
+     */
+    @DisplayName("Github 인증 후 토큰으로 내정보 요청")
+    @Test
+    void githubAuth2() {
+        // given
+        String accessToken = 토큰_추출(깃허브_로그인_요청(userCode));
+
+        // when
+        ExtractableResponse<Response> response = 내_정보_조회_요청(accessToken);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        String email = 이메일_추출(response);
+        assertThat(email).isEqualTo(userEmail);
     }
 }
