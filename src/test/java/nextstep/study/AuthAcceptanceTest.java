14bc2bd0d0e6e48278ef80d6055375721de1d5fd
kakao-jamesjin <136402208+kakao-jamesjin@users.noreply.github.com>
2023-07-28 12:45:12 +0900

    2단계 - 깃헙 로그인 인수 테스트 (#492)
    * 7th default sample
    
    * 2단계 - 깃헙 로그인 인수 테스트
    - AuthAcceptanceTest.githubAuth() 테스트 및 관련 코드 구현
    
    * 2단계 - 깃헙 로그인 인수 테스트
    - 피드백
    -- MemberSteps의 메서드를 활용해서 회원을 생성
    
    * 2단계 - 깃헙 로그인 인수 테스트
    - 피드백 반영
    -- 불필요한 코드 제거
    
    * 2단계 - 깃헙 로그인 인수 테스트
    - 피드백 반영
    -- 상수 별도 관리
    
    ---------
    
    Co-authored-by: boorownie <probitanima11@gmail.com>

--- src/test/java/nextstep/study/AuthAcceptanceTest.java
+++ src/test/java/nextstep/study/AuthAcceptanceTest.java
@@ -3,32 +3,30 @@ package nextstep.study;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import nextstep.member.domain.Member;
-import nextstep.member.domain.MemberRepository;
 import nextstep.utils.AcceptanceTest;
+import nextstep.utils.GithubResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthAcceptanceTest extends AcceptanceTest {
-    public static final String EMAIL = "admin@email.com";
-    public static final String PASSWORD = "password";
-    public static final Integer AGE = 20;
+    private static final String EMAIL = "admin@email.com";
+    private static final String PASSWORD = "password";
+    private static final Integer AGE = 20;
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private GithubResponse githubResponse = GithubResponse.사용자;
 
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
-        memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
 
         Map<String, String> params = new HashMap<>();
         params.put("email", EMAIL);
@@ -47,8 +45,10 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Github Auth")
     @Test
     void githubAuth() {
+        회원_생성_요청(EMAIL, PASSWORD, AGE);
+
         Map<String, String> params = new HashMap<>();
-        params.put("code", "code");
+        params.put("code", githubResponse.getCode());
 
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
--- src/test/java/nextstep/utils/GithubTestController.java
+++ src/test/java/nextstep/utils/GithubTestController.java
@@ -1,23 +1,44 @@
 package nextstep.utils;
 
+import nextstep.auth.token.oauth2.github.GithubAccessTokenRequest;
 import nextstep.auth.token.oauth2.github.GithubAccessTokenResponse;
 import nextstep.auth.token.oauth2.github.GithubProfileResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class GithubTestController {
+class GithubTestController {
+    private static final String TOKEN_TYPE = "token_type";
+    private static final String SCOPE = "scope";
+    private static final String BEARER = "bearer";
+    private static final String TOKEN = "TOKEN";
+
+    private GithubResponse githubResponse = GithubResponse.사용자;
 
     @PostMapping("/github/login/oauth/access_token")
-    public ResponseEntity<GithubAccessTokenResponse> accessToken() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GithubAccessTokenResponse> accessToken(@RequestBody GithubAccessTokenRequest request) {
+        if (githubResponse.getCode().equals(request.getCode())) {
+            return ResponseEntity.ok(new GithubAccessTokenResponse(
+                    githubResponse.getAccessToken(), TOKEN_TYPE, SCOPE, BEARER));
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/github/user")
-    public ResponseEntity<GithubProfileResponse> user() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GithubProfileResponse> user(@RequestHeader("Authorization") String authorizationHeader) {
+        if (!isValid(authorizationHeader)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(new GithubProfileResponse(githubResponse.getEmail(), githubResponse.getAge()));
+    }
+
+    private boolean isValid(String authorizationHeader) {
+        String[] split = authorizationHeader.split(StringUtils.SPACE);
+        if (split.length != 2) {
+            return false;
+        }
+        return TOKEN.equalsIgnoreCase(split[0]);
     }
 }
 
