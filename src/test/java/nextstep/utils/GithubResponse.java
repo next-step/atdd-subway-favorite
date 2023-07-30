efcd308b372d8f9e8c9caf2cbe26e611500a26c0
wade.song <wade.song@kakaocorp.com>
2023-07-30 13:35:55 +0900

    [step3] 컨트롤러 개행 변경

--- src/test/java/nextstep/utils/GithubResponse.java
+++ src/test/java/nextstep/utils/GithubResponse.java
@@ -3,12 +3,7 @@ package nextstep.utils;
 import java.util.Arrays;
 
 public enum GithubResponse {
-    사용자1("aofijeowifjaoief", "access_token_1", "email1@email.com", 12),
-    사용자2("fau3nfin93dmn", "access_token_2", "email2@email.com", 24),
-    사용자3("afnm93fmdodf", "access_token_3", "email3@email.com", 32),
-    사용자4("fm04fndkaladmd", "access_token_4", "email4@email.com", 45),
-    잘못된_토큰("asdfl892e23dss", "wrong-token", null, 0)
-    ;
+    사용자1("aofijeowifjaoief", "access_token_1", "email1@email.com", 12), 사용자2("fau3nfin93dmn", "access_token_2", "email2@email.com", 24), 사용자3("afnm93fmdodf", "access_token_3", "email3@email.com", 32), 사용자4("fm04fndkaladmd", "access_token_4", "email4@email.com", 45), 잘못된_토큰("asdfl892e23dss", "wrong-token", null, 0);
 
     private String code;
     private String accessToken;
@@ -39,10 +34,16 @@ public enum GithubResponse {
     }
 
     public static GithubResponse fromCode(String code) {
-        return Arrays.stream(values()).filter(response -> response.getCode().equals(code)).findFirst().orElseThrow(() -> new RuntimeException("code가 잘못되었습니다."));
+        return Arrays.stream(values())
+                .filter(response -> response.getCode().equals(code))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("code가 잘못되었습니다."));
     }
 
     public static GithubResponse fromToken(String accessToken) {
-        return Arrays.stream(values()).filter(response -> response.getAccessToken().equals(accessToken)).findFirst().orElseThrow(() -> new RuntimeException("accessToken이 잘못되었습니다."));
+        return Arrays.stream(values())
+                .filter(response -> response.getAccessToken().equals(accessToken))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("accessToken이 잘못되었습니다."));
     }
 }
