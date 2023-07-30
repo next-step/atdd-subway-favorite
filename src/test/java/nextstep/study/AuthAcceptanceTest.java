7af5d8bcb551c1f5e6f251beb5015475b08b42bb
Minsuk kim <alstjrdl951@gmail.com>
2023-07-30 19:45:39 +0900

    test: 깃허브에 토큰 & 리소스 조회 요청 인수 테스트 인수 조건 작성

--- src/test/java/nextstep/study/AuthAcceptanceTest.java
+++ src/test/java/nextstep/study/AuthAcceptanceTest.java
@@ -59,4 +59,22 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
     }
+
+    /**
+     * Given {code, cliend_id, client_secret} json body 값과 함께
+     * When Accept: application/json 형식으로
+     * And POST 요청을 https://github.com/login/oauth/access_token에 보내면
+     * Then access_token, scope, token_type이 json으로 body에 담겨 응답이 온다.
+     * Ref. https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
+     * */
+
+    /**
+     * Given POST 요청 https://github.com/login/oauth/access_token으로 깃허브로 부터 받은 accessToken이 있을 때
+     * When GET 요청을 https://api.github.com/user에 아래 헤더와 함께 보내면,
+     *   -H "Accept: application/vnd.github+json" \
+     *   -H "Authorization: Bearer <YOUR-TOKEN>" \
+     *   -H "X-GitHub-Api-Version: 2022-11-28"
+     * Then email 값이 json으로 body에 담겨 응답이 온다.
+     * Ref. https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-the-authenticated-user
+     * */
 }
