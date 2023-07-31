d89a94f0631c6bd98c82481e97451e94f6ed0645
Hyeongmin Park <thalsal@naver.com>
2023-07-31 12:26:03 +0900

    test : access token 전역변수 추출

--- src/test/java/nextstep/utils/AcceptanceTest.java
+++ src/test/java/nextstep/utils/AcceptanceTest.java
@@ -1,5 +1,8 @@
 package nextstep.utils;
 
+import static nextstep.member.acceptance.MemberSteps.회원_로그인_요청_후_token_추출;
+import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +11,11 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AcceptanceTest {
+
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final int AGE = 20;
+    public static String ACCESS_TOKEN = null;
 
     @Autowired
     private DatabaseCleanup databaseCleanup;
@@ -18,5 +26,15 @@ public class AcceptanceTest {
     public void setUp() {
         databaseCleanup.execute();
         dataLoader.loadData();
+    }
+
+    public static String getAccessToken() {
+
+        if (ACCESS_TOKEN == null) {
+            회원_생성_요청(EMAIL, PASSWORD, AGE);
+            ACCESS_TOKEN = 회원_로그인_요청_후_token_추출(EMAIL, PASSWORD);
+        }
+
+        return ACCESS_TOKEN;
     }
 }
