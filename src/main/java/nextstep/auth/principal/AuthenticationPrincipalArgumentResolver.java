351792c3479567622255c90965e879d8a8b0a7e6
ALbertIM0427 <guang7226961@naver.com>
2023-08-02 07:15:10 +0900

    style: 매직리터릴을 상수로 처리

--- src/main/java/nextstep/auth/principal/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/auth/principal/AuthenticationPrincipalArgumentResolver.java
@@ -3,6 +3,7 @@ package nextstep.auth.principal;
 import nextstep.auth.AuthenticationException;
 import nextstep.auth.token.JwtTokenProvider;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -10,8 +11,11 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
 
-    public static final int TOKEN_KEY = 0;
-    public static final int TOKEN_INDEX = 1;
+    private static final int TOKEN_KEY = 0;
+    private static final int TOKEN_INDEX = 1;
+    private static final String TOKEN_DELIMITER = " ";
+    private static final int TOKEN_SIZE = 2;
+    private static final String BEARER_KEY = "bearer";
     private final JwtTokenProvider jwtTokenProvider;
 
     public AuthenticationPrincipalArgumentResolver(JwtTokenProvider jwtTokenProvider) {
@@ -26,12 +30,12 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
             NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String authorization = webRequest.getHeader("Authorization");
+        String authorization = webRequest.getHeader(HttpHeaders.AUTHORIZATION);
         if (authorization == null) {
             throw new AuthenticationException();
         }
-        String[] tokenInfo = authorization.split(" ");
-        if (tokenInfo.length == 1 || !"bearer".equalsIgnoreCase(tokenInfo[TOKEN_KEY])) {
+        String[] tokenInfo = authorization.split(TOKEN_DELIMITER);
+        if (tokenInfo.length != TOKEN_SIZE || !isBearerToken(tokenInfo)) {
             throw new AuthenticationException();
         }
         String token = tokenInfo[TOKEN_INDEX];
@@ -40,5 +44,9 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
         String role = jwtTokenProvider.getRoles(token);
 
         return new UserPrincipal(username, role);
+    }
+
+    private static boolean isBearerToken(String[] tokenInfo) {
+        return BEARER_KEY.equalsIgnoreCase(tokenInfo[TOKEN_KEY]);
     }
 }
