67cd81b381c9c623747c30010b5937a2d69c6485
Kade <37147899+Kade-Yoo@users.noreply.github.com>
2023-08-01 19:17:47 +0900

    2단계 - 깃헙 로그인 인수 테스트 (#523)
    - GithubResponses 추가(테스트를 위한 Response 하드코딩 값)
    - GithubTestController 응답 값 추가
    - 인수테스트 code값 변경
    - application-test.properties Github 정보 추가(test용)
    - 상수 분리

--- src/test/java/nextstep/utils/GithubTestController.java
+++ src/test/java/nextstep/utils/GithubTestController.java
@@ -1,23 +1,34 @@
 package nextstep.utils;
 
+import nextstep.auth.AuthenticationException;
 import nextstep.auth.token.oauth2.github.GithubAccessTokenResponse;
 import nextstep.auth.token.oauth2.github.GithubProfileResponse;
+import nextstep.auth.token.oauth2.github.GithubTokenRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GithubTestController {
 
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String SPACE = " ";
+    private static final int TOKEN_VALUE_INDEX = 1;
+
     @PostMapping("/github/login/oauth/access_token")
-    public ResponseEntity<GithubAccessTokenResponse> accessToken() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GithubAccessTokenResponse> accessToken(@RequestBody GithubTokenRequest request) {
+        String accessToken = GithubResponses.findAccessTokenByCode(request.getCode());
+        return ResponseEntity.ok(new GithubAccessTokenResponse(accessToken));
     }
 
     @GetMapping("/github/user")
-    public ResponseEntity<GithubProfileResponse> user() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GithubProfileResponse> user(@RequestHeader(AUTHORIZATION) String token) {
+        String[] tokenArray = token.split(SPACE);
+        String emailByAccessToken = GithubResponses.findEmailByAccessToken(tokenArray[TOKEN_VALUE_INDEX]);
+        return ResponseEntity.ok(new GithubProfileResponse(emailByAccessToken));
     }
 }
 
