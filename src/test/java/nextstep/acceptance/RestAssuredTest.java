30700259840107818fffc217835c26d74b1b00da
kimsan <cloudhat17@gmail.com>
2023-08-01 12:28:07 +0900

    step1 : 이전 주차의 코드로 초기화

--- src/test/java/nextstep/acceptance/RestAssuredTest.java
+++ src/test/java/nextstep/acceptance/RestAssuredTest.java
@@ -32,47 +32,4 @@ public class RestAssuredTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
-    static class AuthAcceptanceTest extends AcceptanceTest {
-        public static final String EMAIL = "admin@email.com";
-        public static final String PASSWORD = "password";
-        public static final Integer AGE = 20;
-
-        @Autowired
-        private MemberRepository memberRepository;
-
-        @DisplayName("Bearer Auth")
-        @Test
-        void bearerAuth() {
-            memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
-
-            Map<String, String> params = new HashMap<>();
-            params.put("email", EMAIL);
-            params.put("password", PASSWORD);
-
-            ExtractableResponse<Response> response = RestAssured.given().log().all()
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .body(params)
-                    .when().post("/login/token")
-                    .then().log().all()
-                    .statusCode(HttpStatus.OK.value()).extract();
-
-            assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
-        }
-
-        @DisplayName("Github Auth")
-        @Test
-        void githubAuth() {
-            Map<String, String> params = new HashMap<>();
-            params.put("code", "code");
-
-            ExtractableResponse<Response> response = RestAssured.given().log().all()
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .body(params)
-                    .when().post("/login/github")
-                    .then().log().all()
-                    .statusCode(HttpStatus.OK.value()).extract();
-
-            assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
-        }
-    }
 }
