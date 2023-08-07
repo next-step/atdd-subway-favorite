fca3a864512f16c7b6c98db613b0cd0d3978277f
Minsuk kim <alstjrdl951@gmail.com>
2023-08-07 10:51:12 +0900

    feat: Favorite을 통해 Station 정보가 바뀌는 것 방지

--- src/main/java/nextstep/subway/domain/entity/Favorite.java
+++ src/main/java/nextstep/subway/domain/entity/Favorite.java
@@ -18,11 +18,11 @@ public class Favorite {
     @Column(name = "member_id")
     Long memberId;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "source_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     Station source;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "target_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     Station target;
 
