d19e19aff6773fe961a349507ed55d6e41e39d0c
SeokyungKim <kskyung0624@gmail.com>
2023-08-08 17:08:17 +0900

    refactor : 무의미하게 반복되는 부분에 대해 제거한다

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -43,16 +43,6 @@ public class SubwayMap {
                 graph.setEdgeWeight(sectionEdge, it.getDistance());
             });
 
-        // 지하철 역의 연결 정보(간선)을 등록
-        lines.stream()
-            .flatMap(it -> it.getSections().stream())
-            .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance()))
-            .forEach(it -> {
-                SectionEdge sectionEdge = SectionEdge.of(it);
-                graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                graph.setEdgeWeight(sectionEdge, it.getDistance());
-            });
-
         // 다익스트라 최단 경로 찾기
         DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
         GraphPath<Station, SectionEdge> result = dijkstraShortestPath.getPath(source, target);
