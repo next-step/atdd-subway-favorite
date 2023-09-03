f6bcc99b4b2167cb3b719b68d7f8a93a44a7593d
hellonayeon <hellonykwon@gmail.com>
2023-09-03 13:56:11 +0900

    Feat: 즐겨찾기 생성 전 경로 조회 로직 추가

--- src/main/java/nextstep/subway/applicaion/FavoriteService.java
+++ src/main/java/nextstep/subway/applicaion/FavoriteService.java
@@ -3,6 +3,7 @@ package nextstep.subway.applicaion;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import nextstep.subway.applicaion.dto.FavoriteRequest;
+import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Favorite;
 import nextstep.subway.domain.FavoriteRepository;
@@ -15,6 +16,7 @@ public class FavoriteService {
 
     private final FavoriteRepository favoriteRepository;
     private final StationService stationService;
+    private final PathService pathService;
 
 
     public List<Favorite> getFavorites(Long memberId) {
@@ -27,6 +29,8 @@ public class FavoriteService {
     }
 
     public Favorite createFavorite(Long memberId, FavoriteRequest request) {
+        pathService.findPath(request.getSource(), request.getTarget());
+
         Station source = stationService.findById(request.getSource());
         Station target = stationService.findById(request.getTarget());
         return favoriteRepository.save(new Favorite(source, target, memberId));
