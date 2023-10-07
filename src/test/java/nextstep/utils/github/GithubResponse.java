2cf7ecc7d2051fec16eb72b25677c89119404f08
hoon25 <cchoon95@hanmail.net>
2023-10-08 03:41:33 +0900

    feat(github_profile): 기능구현(step2)

--- src/test/java/nextstep/utils/github/GithubResponse.java
+++ src/test/java/nextstep/utils/github/GithubResponse.java
@@ -3,20 +3,23 @@ package nextstep.utils.github;
 import java.util.Arrays;
 
 public enum GithubResponse {
-    사용자1("s1emn12lkm", "access_token_1", "email1@email.com"),
-    사용자2("asdfavvw3122", "access_token_2", "email2@email.com"),
-    사용자3("fefef2121212", "access_token_3", "email3@email.com"),
-    사용자4("dfdhhtgrwcxz", "access_token_4", "email4@email.com"),
+    사용자1("s1emn12lkm", "access_token_1", "email1@email.com", 10),
+    사용자2("asdfavvw3122", "access_token_2", "email2@email.com", 12),
+    사용자3("fefef2121212", "access_token_3", "email3@email.com", 13),
+    사용자4("dfdhhtgrwcxz", "access_token_4", "email4@email.com", 15),
     ;
     private String code;
     private String accessToken;
     private String email;
+    private Integer age;
 
-    GithubResponse(String code, String accessToken, String email) {
+    GithubResponse(String code, String accessToken, String email, Integer age) {
         this.code = code;
         this.accessToken = accessToken;
         this.email = email;
+        this.age = age;
     }
+
     public static GithubResponse valueOfCode(String code) {
         return Arrays.stream(values())
                 .filter(value -> value.code.equals(code))
@@ -24,10 +27,26 @@ public enum GithubResponse {
                 .orElse(null);
     }
 
+    public static GithubResponse valueOfAccessToken(String accessToken) {
+        return Arrays.stream(values())
+                .filter(value -> value.accessToken.equals(accessToken))
+                .findAny()
+                .orElse(null);
+    }
+
     public String getCode() {
         return code;
     }
+
     public String getAccessToken() {
         return accessToken;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 }
--- src/test/java/nextstep/utils/github/GithubTestController.java
+++ src/test/java/nextstep/utils/github/GithubTestController.java
@@ -4,10 +4,7 @@ import nextstep.auth.token.oauth2.github.GithubAccessTokenRequest;
 import nextstep.auth.token.oauth2.github.GithubAccessTokenResponse;
 import nextstep.auth.token.oauth2.github.GithubProfileResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GithubTestController {
@@ -15,13 +12,16 @@ public class GithubTestController {
     @PostMapping("/github/login/oauth/access_token")
     public ResponseEntity<GithubAccessTokenResponse> accessToken(@RequestBody GithubAccessTokenRequest request) {
         String accessToken = GithubResponse.valueOfCode(request.getCode()).getAccessToken();
-        GithubAccessTokenResponse githubAccessTokenResponse = new GithubAccessTokenResponse(accessToken, "github", "profile", "bearer");
-        return ResponseEntity.ok().body(githubAccessTokenResponse);
+        GithubAccessTokenResponse response = new GithubAccessTokenResponse(accessToken, "github", "profile", "bearer");
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/github/user")
-    public ResponseEntity<GithubProfileResponse> user() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GithubProfileResponse> user(@RequestHeader("Authorization") String accessTokenHeader) {
+        String accessToken = accessTokenHeader.split(" ")[1];
+        GithubResponse githubResponse = GithubResponse.valueOfAccessToken(accessToken);
+        GithubProfileResponse response = new GithubProfileResponse(githubResponse.getEmail(), githubResponse.getAge());
+        return ResponseEntity.ok().body(response);
     }
 }
 
