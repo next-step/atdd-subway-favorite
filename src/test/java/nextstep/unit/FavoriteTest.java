bbfddf5cfff7ec0cb15c1c2ef2cde1cc64ba51d1
hoon25 <cchoon95@hanmail.net>
2023-10-08 06:22:26 +0900

    test(favorite): 본인의 즐겨찾기인지 검증 tdd

--- src/test/java/nextstep/unit/FavoriteTest.java
+++ src/test/java/nextstep/unit/FavoriteTest.java
@@ -1,5 +1,42 @@
 package nextstep.unit;
 
+import nextstep.auth.AuthenticationException;
+import nextstep.member.domain.Favorite;
+import nextstep.member.domain.Member;
+import nextstep.subway.domain.Station;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class FavoriteTest {
+    Station 역삼역;
+    Station 선릉역;
+    Member 유저_1번;
+    Member 유저_2번;
+    Favorite 유저_1번의_즐겨찾기;
+
+    @BeforeEach()
+    public void setup() {
+        역삼역 = new Station("역삼역");
+        선릉역 = new Station("선릉역");
+        유저_1번 = new Member("email@email.com", "asdfasdf", 12);
+        유저_2번 = new Member("email2@email.com", "asdfasdf", 12);
+        유저_1번의_즐겨찾기 = new Favorite(유저_1번, 역삼역, 선릉역);
+    }
+
+    @Test
+    @DisplayName("즐겨찾기의 유저가 동일하면 통과한다.")
+    void checkOwnerSuccess() {
+        // when, then
+        유저_1번의_즐겨찾기.checkOwner(유저_1번);
+    }
+
+    @Test
+    @DisplayName("즐겨찾기의 유저가 다르면 실패한다.")
+    void checkOwnerFailure() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> 유저_1번의_즐겨찾기.checkOwner(유저_2번)).isInstanceOf(AuthenticationException.class);
+    }
 
 }
