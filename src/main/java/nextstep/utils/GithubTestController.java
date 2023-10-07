8318a255c3b5bdb990cb80627fddb899640bd3c6
hoon25 <cchoon95@hanmail.net>
2023-10-08 02:46:44 +0900

    test(githubClient): githubClient Fake 생성(step2)

---
+++ src/main/java/nextstep/utils/GithubTestController.java
@@ -0,0 +1,21 @@
+package nextstep.utils;
+
+import nextstep.auth.token.oauth2.github.GithubAccessTokenResponse;
+import nextstep.auth.token.oauth2.github.GithubProfileResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GithubTestController {
+    @PostMapping("/github/login/ouath/access_token")
+    public ResponseEntity<GithubAccessTokenResponse> accessToken() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/github/user")
+    public ResponseEntity<GithubProfileResponse> user() {
+        return ResponseEntity.ok().build();
+    }
+}
