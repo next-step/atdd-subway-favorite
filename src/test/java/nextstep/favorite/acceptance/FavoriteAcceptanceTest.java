a7034fd148d6874bd0e8eaecfa0f487c8c958f98
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-12 19:22:23 +0900

    fix: 즐겨찾기 실패 시나리오 인수테스트 추가

--- src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
@@ -2,7 +2,10 @@ package nextstep.favorite.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import nextstep.common.api.*;
+import nextstep.common.api.FavoriteApiHelper;
+import nextstep.common.api.LineApiHelper;
+import nextstep.common.api.SectionApiHelper;
+import nextstep.common.api.StationApiHelper;
 import nextstep.core.AcceptanceTest;
 import nextstep.core.AcceptanceTestAuthBase;
 import nextstep.core.RestAssuredHelper;
@@ -18,19 +21,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("즐겨찾기 관련 기능")
 @AcceptanceTest
 public class FavoriteAcceptanceTest extends AcceptanceTestAuthBase {
-    private final int 교대역_강남역_distance = 5;
-    private final int 강남역_양재역_distance = 10;
-    private final int 교대역_남부터미널_distance = 2;
-    private final int 남부터미널_양재역_distance = 3;
     private Long 강남역_Id;
     private Long 교대역_Id;
     private Long 양재역_Id;
     private Long 남부터미널역_Id;
     private Long 사당역_Id;
     private Long 서울역_Id;
-    private Long 이호선_Id;
-    private Long 신분당선_Id;
-    private Long 삼호선_Id;
 
     /**
      * Given 지하철 노선을 생성하고
@@ -44,15 +40,16 @@ public class FavoriteAcceptanceTest extends AcceptanceTestAuthBase {
         남부터미널역_Id = RestAssuredHelper.getIdFromBody(StationApiHelper.createStation("남부터미널역"));
         서울역_Id = RestAssuredHelper.getIdFromBody(StationApiHelper.createStation("서울역"));
         사당역_Id = RestAssuredHelper.getIdFromBody(StationApiHelper.createStation("사당역"));
-        이호선_Id = RestAssuredHelper.getIdFromBody((LineApiHelper.createLine("2호선", "green", 교대역_Id, 강남역_Id, 교대역_강남역_distance)));
-        신분당선_Id = RestAssuredHelper.getIdFromBody((LineApiHelper.createLine("신분당선", "red", 강남역_Id, 양재역_Id, 강남역_양재역_distance)));
-        삼호선_Id = RestAssuredHelper.getIdFromBody((LineApiHelper.createLine("3호선", "orange", 교대역_Id, 남부터미널역_Id, 교대역_남부터미널_distance)));
-        SectionApiHelper.createSection(삼호선_Id, 남부터미널역_Id, 양재역_Id, 남부터미널_양재역_distance);
+        RestAssuredHelper.getIdFromBody((LineApiHelper.createLine("2호선", "green", 교대역_Id, 강남역_Id, 5)));
+        RestAssuredHelper.getIdFromBody((LineApiHelper.createLine("신분당선", "red", 강남역_Id, 양재역_Id, 10)));
+        final Long 삼호선_Id = RestAssuredHelper.getIdFromBody((LineApiHelper.createLine("3호선", "orange", 교대역_Id, 남부터미널역_Id, 2)));
+        SectionApiHelper.createSection(삼호선_Id, 남부터미널역_Id, 양재역_Id, 3);
     }
 
 
     /**
-     * When 로그인 정보와 함께 출발역과 도착역을 통해 경로를 즐겨찾기에 추가 하면
+     * Given 로그인 정보와 함께
+     * When 출발역과 도착역을 통해 경로를 즐겨찾기에 추가 하면
      * Then 즐겨찾기 목록 조회 시 생성한 즐겨찾기를 찾을 수 있다
      */
     @DisplayName("출발역과 도착역을 통해 즐겨찾기를 추가 할 수 있다.")
@@ -66,6 +63,57 @@ public class FavoriteAcceptanceTest extends AcceptanceTestAuthBase {
 
         // then
         즐겨찾기_목록_조회_시_생성한_즐겨찾기를_찾을_수_있다(response);
+    }
+
+    /**
+     * Given 로그인 정보 없이
+     * When 출발역과 도착역을 통해 경로를 즐겨찾기에 추가 하면
+     * Then 에러가 난다
+     */
+    @DisplayName("로그인이 되어있지 않으면 즐겨찾기를 추가 할 수 없다.")
+    @Test
+    void 로그인_안된_상태는_즐겨찾기_추가에_실패한다() {
+        // when
+        final ExtractableResponse<Response> response = 즐겨찾기_목록_조회_요청_Without_로그인(강남역_Id, 남부터미널역_Id);
+
+        // then
+        즐겨찾기_추가_요청이_거부된다(response);
+    }
+
+    /**
+     * Given 로그인 정보와 함께
+     * When 출발역과 도착역을 통해 경로를 즐겨찾기에 추가 하는데
+     * When 존재하지 않는 경로인 경우
+     * Then 에러가 난다
+     */
+    @DisplayName("존재하지 않는 경로는 즐겨찾기에 추가 할 수 없다.")
+    @Test
+    void 존재하지_않는_경로는_즐겨찾기_추가에_실패한다() {
+        // when
+        final ExtractableResponse<Response> response = 즐겨찾기_추가_요청_With_로그인(사당역_Id, 서울역_Id);
+
+        // then
+        즐겨찾기_추가_요청이_실패한다(response);
+    }
+
+    /**
+     * Given 로그인 정보와 함께
+     * When 출발역과 도착역을 통해 경로를 즐겨찾기에 추가 하는데
+     * When 이미 추가된 즐겨찾기 경로라면
+     * Then 에러가 난다
+     */
+    @DisplayName("이미 추가된 즐겨찾기 경로라면 즐겨찾기에 추가 할 수 없다.")
+    @Test
+    void 이미_추가된_즐겨찾기_경로라면_즐겨찾기_추가에_실패한다() {
+        // given
+        final ExtractableResponse<Response> firstResponse = 즐겨찾기_추가_요청_With_로그인(강남역_Id, 남부터미널역_Id);
+        즐겨찾기_추가_요청이_성공한다(firstResponse);
+
+        // when
+        final ExtractableResponse<Response> secondResponse = 즐겨찾기_추가_요청_With_로그인(강남역_Id, 남부터미널역_Id);
+
+        // then
+        즐겨찾기_추가_요청이_실패한다(secondResponse);
     }
 
     private void 즐겨찾기_목록_조회_시_생성한_즐겨찾기를_찾을_수_있다(final ExtractableResponse<Response> response) {
@@ -83,8 +131,20 @@ public class FavoriteAcceptanceTest extends AcceptanceTestAuthBase {
         return FavoriteApiHelper.addFavorite(accessToken, sourceId, targetId);
     }
 
+    private ExtractableResponse<Response> 즐겨찾기_목록_조회_요청_Without_로그인(final Long sourceId, final Long targetId) {
+        return FavoriteApiHelper.addFavorite(accessToken, sourceId, targetId);
+    }
+
     private void 즐겨찾기_추가_요청이_성공한다(final ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private void 즐겨찾기_추가_요청이_거부된다(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    private void 즐겨찾기_추가_요청이_실패한다(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
 }
