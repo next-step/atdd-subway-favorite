b362ab0a93b8d479e7b1fe732efa13a060c02601
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-12 19:02:00 +0900

    fix: AcceptanceTestAuthBase 클래스 추가
    - AuthAcceptanceTest 로그인 로직 상위 클래스로 이관

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -3,49 +3,23 @@ package nextstep.member.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.core.AcceptanceTestAuthBase;
 import nextstep.core.AcceptanceTest;
-import nextstep.member.domain.Member;
-import nextstep.member.domain.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @AcceptanceTest
-class AuthAcceptanceTest {
-    public static final String EMAIL = "admin@email.com";
-    public static final String PASSWORD = "password";
-    public static final Integer AGE = 20;
-
-    @Autowired
-    private MemberRepository memberRepository;
+class AuthAcceptanceTest extends AcceptanceTestAuthBase {
 
     @DisplayName("Bearer Auth")
     @Test
     void bearerAuth() {
-        memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
-
-        Map<String, String> params = new HashMap<>();
-        params.put("email", EMAIL);
-        params.put("password", PASSWORD);
-
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(params)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value()).extract();
-
-        String accessToken = response.jsonPath().getString("accessToken");
         assertThat(accessToken).isNotBlank();
 
-        ExtractableResponse<Response> response2 = RestAssured.given().log().all()
+        final ExtractableResponse<Response> response2 = RestAssured.given().log().all()
                 .auth().oauth2(accessToken)
                 .when().get("/members/me")
                 .then().log().all()
