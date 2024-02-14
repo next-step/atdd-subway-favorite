0ab656398719e8ecdd12a9f4b367a90ed280a146
jikimee64 <jikimee64@gmail.com>
2024-02-15 00:36:40 +0900

    fix: 글로벌 익셉션 인증 실패 Exception 정의 추가

--- src/main/java/nextstep/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/exception/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package nextstep.exception;
 
+import nextstep.member.AuthenticationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +14,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         return ResponseEntity.status(BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<Object> handleAuthenticationException(AuthenticationException ex) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message("인증에 실패했습니다.")
+                .build();
+        return ResponseEntity.status(UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(ApplicationException.class)
