4e8e2f7affbcc439afbb5f625a436b94b594ef89
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-15 21:13:55 +0900

    fix: FavoriteNotExistException 메시지 수정

--- src/main/java/nextstep/favorite/exception/FavoriteNotExistException.java
+++ src/main/java/nextstep/favorite/exception/FavoriteNotExistException.java
@@ -4,6 +4,6 @@ import nextstep.common.exception.NotFoundException;
 
 public class FavoriteNotExistException extends NotFoundException {
     public FavoriteNotExistException(final Long id) {
-        super(String.format("Station is not exist - id : %s", id));
+        super(String.format("Favorite is not exist - id : %s", id));
     }
 }
