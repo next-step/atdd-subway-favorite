5af7c5755ed94917d431d86237c4e948f765a9da
Lee seung chan <dltmdcks702@gmail.com>
2024-02-17 15:58:14 +0900

    test: add test for create favorite

--- src/main/java/nextstep/favorite/domain/Favorite.java
+++ src/main/java/nextstep/favorite/domain/Favorite.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import nextstep.member.domain.Member;
+import nextstep.subway.domain.Station;
 
 @Entity
 public class Favorite {
@@ -11,4 +15,40 @@ public class Favorite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "sourceStationId")
+    private Station sourceStation;
+
+    @ManyToOne
+    @JoinColumn(name = "targetStationId")
+    private Station targetStation;
+
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    public Favorite() {
+    }
+
+    public Favorite(Station sourceStation, Station targetStation, Member member) {
+        this.sourceStation = sourceStation;
+        this.targetStation = targetStation;
+        this.member = member;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getSourceStation() {
+        return sourceStation;
+    }
+
+    public Station getTargetStation() {
+        return targetStation;
+    }
+
+    public Member getMember() {
+        return member;
+    }
 }
--- src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
@@ -2,7 +2,48 @@ package nextstep.favorite.acceptance;
 
 import nextstep.utils.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("즐겨찾기 관련 기능")
 public class FavoriteAcceptanceTest extends AcceptanceTest {
+
+    /**
+     * when: 즐겨 찾기를 생성하면
+     * then: 즐겨 찾기가 생성된다.
+     */
+    @Test
+    void 즐겨_찾기_생성() {
+        // when
+        // then
+    }
+
+    /**
+     * given: 존재하지 않는 역 id에 대해서
+     * when: 즐겨 찾기를 생성하면
+     * then: 즐겨 찾기 생성에 실패한다.
+     */
+    @Test
+    void  즐겨_찾기_요청_시_source와_target역_모두_존재해야_한다() {
+        // when
+        // then
+    }
+
+    /**
+     * given: source와 target이 같은 역 id에 대해서
+     * when: 즐겨 찾기를 생성하면
+     * then: 즐겨 찾기 생성에 실패한다.
+     */
+    @Test
+    void 즐겨_찾기_요청_시_source와_target역이_같으면_안된다() {
+    }
+
+    /**
+     * given: 유효하지 않은 bearer token에 대해서
+     * when: 즐겨 찾기를 생성하면
+     * then: 즐겨 찾기 생성에 실패한다.
+     */
+    @Test
+    void  즐겨_찾기_요청_시_bearer_token이_유효하지_않으면_안된다() {
+
+    }
 }
