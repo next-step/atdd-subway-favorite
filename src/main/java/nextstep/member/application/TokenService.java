fa928f8ed866524fb17ce5dad129cd9ee2e0e5b1
jikimee64 <jikimee64@gmail.com>
2024-02-18 02:22:41 +0900

    feat: 깃허브 로그인 구현

--- src/main/java/nextstep/member/application/TokenService.java
+++ src/main/java/nextstep/member/application/TokenService.java
@@ -1,19 +1,21 @@
 package nextstep.member.application;
 
+import lombok.RequiredArgsConstructor;
 import nextstep.exception.AuthenticationException;
+import nextstep.member.application.dto.GithubAccessTokenResponse;
+import nextstep.member.application.dto.GithubEmailResponse;
 import nextstep.member.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRegister;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TokenService {
-    private MemberService memberService;
-    private JwtTokenProvider jwtTokenProvider;
-
-    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
-        this.memberService = memberService;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final MemberService memberService;
+    private final MemberRegister memberRegister;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final GithubClient githubClient;
 
     public TokenResponse createToken(String email, String password) {
         Member member = memberService.findMemberByEmail(email);
@@ -21,6 +23,16 @@ public class TokenService {
             throw new AuthenticationException();
         }
 
+        String token = jwtTokenProvider.createToken(member.getEmail());
+
+        return new TokenResponse(token);
+    }
+
+    public TokenResponse createTokenGithub(String code) {
+        GithubAccessTokenResponse tokenResponse = githubClient.requestAccessToken(code);
+        GithubEmailResponse emailResponse = githubClient.requestGithubEmail(tokenResponse.getAccessToken());
+
+        Member member = memberRegister.findOrCreate(emailResponse.getEmail());
         String token = jwtTokenProvider.createToken(member.getEmail());
 
         return new TokenResponse(token);
