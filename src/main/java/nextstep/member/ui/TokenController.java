8197125ef5dbeb02c95b38a8ac925314d3ae4361
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-17 10:50:01 +0900

    fix: 깃헙 로그인 요청 Controller 구성

--- src/main/java/nextstep/member/ui/TokenController.java
+++ src/main/java/nextstep/member/ui/TokenController.java
@@ -1,6 +1,7 @@
 package nextstep.member.ui;
 
 import nextstep.member.application.TokenService;
+import nextstep.member.application.dto.GithubTokenRequest;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TokenController {
-    private TokenService tokenService;
+    private final TokenService tokenService;
 
-    public TokenController(TokenService tokenService) {
+    public TokenController(final TokenService tokenService) {
         this.tokenService = tokenService;
     }
 
     @PostMapping("/login/token")
-    public ResponseEntity<TokenResponse> createToken(@RequestBody TokenRequest request) {
-        TokenResponse response = tokenService.createToken(request.getEmail(), request.getPassword());
+    public ResponseEntity<TokenResponse> createToken(@RequestBody final TokenRequest request) {
+        final TokenResponse response = tokenService.createToken(request.getEmail(), request.getPassword());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login/github")
+    public ResponseEntity<TokenResponse> createTokenForGithub(@RequestBody final GithubTokenRequest request) {
+        final TokenResponse response = tokenService.createTokenForGithub(request.getCode());
 
         return ResponseEntity.ok(response);
     }
