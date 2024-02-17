e61adaea3147d1159a7f778f0aba488529d006e4
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-18 00:00:57 +0900

    fix: AcceptanceTest @ActiveProfiles("test") 추가

--- src/test/java/nextstep/core/AcceptanceTest.java
+++ src/test/java/nextstep/core/AcceptanceTest.java
@@ -2,6 +2,7 @@ package nextstep.core;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.lang.annotation.ElementType;
@@ -14,5 +15,6 @@ import java.lang.annotation.Target;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = TestConfig.class)
 @ExtendWith(AcceptanceTestExtension.class)
+@ActiveProfiles("test")
 public @interface AcceptanceTest {
 }
