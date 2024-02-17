62b6bb72a43532d34f33c97b7e8c0cecefa6a411
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-17 23:36:54 +0900

    fix: TokenServiceTest createTokenForGithub 테스트 추가

---
+++ src/test/java/nextstep/member/application/TokenServiceTest.java
@@ -0,0 +1,26 @@
+package nextstep.member.application;
+
+import nextstep.member.application.dto.TokenResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+class TokenServiceTest {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Test
+    @DisplayName("createTokenForGithub 를 통해 accessToken 을 발급 받을 수 있다.")
+    void createTokenForGithubTest() {
+        final TokenResponse code = tokenService.createTokenForGithub("code");
+
+        assertThat(code.getAccessToken()).isNotBlank();
+    }
+}
