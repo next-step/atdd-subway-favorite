ebe4a1c9b0099d1af601e78806b29c1340ca6f00
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-17 11:05:10 +0900

    fix: GithubTestController 작성

---
+++ src/test/java/nextstep/member/application/GithubTestController.java
@@ -0,0 +1,24 @@
+package nextstep.member.application;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GithubTestController {
+    @PostMapping("/github/login/oauth/access_token")
+    public ResponseEntity<GithubAccessTokenResponse> accessToken(
+            @RequestBody final GithubAccessTokenRequest tokenRequest) {
+        final String accessToken = "github_access_token";
+        final GithubAccessTokenResponse response = new GithubAccessTokenResponse(accessToken, "", "", "");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/github/user")
+    public ResponseEntity<GithubProfileResponse> user(
+            @RequestHeader("Authorization") final String authorization) {
+        final String accessToken = authorization.split(" ")[1];
+        final GithubProfileResponse response = new GithubProfileResponse("email@email.com", 20);
+        return ResponseEntity.ok(response);
+    }
+
+}
