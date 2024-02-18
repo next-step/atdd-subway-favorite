617fac91fa24ade696b9929db7adc549372d221d
g0rani <pjw0405@naver.com>
2024-02-18 17:24:52 +0900

    test: 즐겨 찾기 등록시 같은 출발역과 도착역 일때 테스트 코드 작성

--- src/test/java/nextstep/subway/favorite/domain/FavoriteTest.java
+++ src/test/java/nextstep/subway/favorite/domain/FavoriteTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.favorite.domain;
 
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.testhelper.fixture.StationFixture;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,15 @@ class FavoriteTest {
 
     @Test
     @DisplayName("즐겨찾기를 등록 할 수 있다")
-    void createFavorite() {
+    void createFavorite1() {
         Favorite favorite = new Favorite(StationFixture.강남역, StationFixture.잠실역, new Member());
+    }
+
+    @Test
+    @DisplayName("즐겨찾기를 등록 시 출발역과 도착역이 같을 경우 에러 발생")
+    void createFavorite2() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Favorite(StationFixture.강남역,
+                StationFixture.강남역, new Member()));
     }
 
 }
