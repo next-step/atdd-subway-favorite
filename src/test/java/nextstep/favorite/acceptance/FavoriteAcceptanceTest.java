f73a5849ad0b757ad4902a881dba1108e5d2a419
Lee seung chan <dltmdcks702@gmail.com>
2024-02-18 20:05:07 +0900

    feat: add test for fetch

--- src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
@@ -78,4 +78,38 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
         // then
         ResponseUtils.응답의_STATUS_검증(response, HttpStatus.BAD_REQUEST);
     }
+
+    /**
+     * given: 즐겨 찾기 생성 시
+     * when: 즐겨 찾기를 조회하면
+     * then: 즐겨 찾기가 조회된다.
+     */
+    @Test
+    void 즐겨_찾기_조회() {
+        // given
+        FavoriteSteps.지하철_좋아요_생성(강남역, 역삼역, accessToken);
+
+        // when
+        final ExtractableResponse<Response> favoriteResponse = FavoriteSteps.지하철_좋아요_조회(accessToken);
+
+        // then
+        assertThat(favoriteResponse).isNotNull();
+        ResponseUtils.응답의_STATUS_검증(favoriteResponse, HttpStatus.OK);
+    }
+
+    /**
+     *  given: 즐겨 찾기가 없을 때
+     *  when: 즐겨 찾기를 조회하면
+     *  then: 빈 어레이가 리턴된다
+     */
+    @Test
+    void 즐겨_찾기_조회__없으면_빈_어레이가_리턴된다() {
+        // when
+        final ExtractableResponse<Response> favoriteResponse = FavoriteSteps.지하철_좋아요_조회(accessToken);
+
+        // then
+        assertThat(favoriteResponse).isNotNull();
+        ResponseUtils.응답의_STATUS_검증(favoriteResponse, HttpStatus.OK);
+        assertThat(favoriteResponse.jsonPath().getList(".", FavoriteResponse.class)).isEmpty();
+    }
 }
