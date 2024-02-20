e1694c6bd746c8761d88c84b5628e7857f52b0fd
ian <xonic789@naver.com>
2024-02-20 09:50:18 +0900

    이 전 미션 코드 가져오기

---
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -0,0 +1,13 @@
+package nextstep.subway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwayApplication.class, args);
+    }
+
+}
