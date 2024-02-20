4a05d58c59302e52bd997624c005696f5ab32b87
qwe5507 <apvmffkdls@gmail.com>
2024-02-20 23:58:48 +0900

    refactor: member 패키지 안의 인증 로직에 대한 테스트 삭제

--- src/test/java/nextstep/auth/application/GithubServiceTest.java
+++ src/test/java/nextstep/auth/application/GithubServiceTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static nextstep.member.acceptance.AuthSteps.createToken;
+import static nextstep.auth.acceptance.AuthSteps.createToken;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
