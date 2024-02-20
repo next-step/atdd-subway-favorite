d1317f32cb2bc2e62470e3bb2a3e26fa6a60c149
qwe5507 <apvmffkdls@gmail.com>
2024-02-20 14:49:01 +0900

    test: step2피드백 반영 memberservice findMemberOrCreate() 테스트 작성

--- src/test/java/nextstep/member/unit/MemberServiceTest.java
+++ src/test/java/nextstep/member/unit/MemberServiceTest.java
@@ -3,41 +3,46 @@ package nextstep.member.unit;
 
 import nextstep.member.application.MemberService;
 import nextstep.member.application.dto.GithubProfileResponse;
+import nextstep.member.application.dto.MemberResponse;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import nextstep.utils.GithubResponses;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Optional;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class MemberServiceTest {
     @Autowired
     private MemberService memberService;
-    @MockBean
+    @Autowired
     private MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+        memberRepository.deleteAll();
+    }
 
     @DisplayName("회원이 등록되어 있지 않을 때, 회원이 등록된다.")
     @Test
     void findMemberOrCreate_unRegisterMember() {
         final String email = GithubResponses.사용자1.email();
         final Integer age = 31;
-        final Member member = new Member(email, "", age);
-        when(memberRepository.findByEmail(email)).thenReturn(Optional.empty());
-        when(memberRepository.save(member)).thenReturn(member);
+        assertThatThrownBy(() -> {memberService.findMemberByEmail(email);})
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("등록된 회원이 아닙니다.");
 
         memberService.findMemberOrCreate(new GithubProfileResponse(email, age));
 
-        verify(memberRepository).save(member);
+        MemberResponse result = memberService.findMemberByEmail(email);
+        assertThat(result.getEmail()).isEqualTo(email);
     }
 
     @DisplayName("회원이 등록되어 있을 때")
@@ -46,10 +51,11 @@ public class MemberServiceTest {
         final String email = GithubResponses.사용자1.email();
         final Integer age = 31;
         final Member member = new Member(email, "", age);
-        when(memberRepository.findByEmail(email)).thenReturn(Optional.ofNullable(member));
+        memberRepository.save(member);
 
         memberService.findMemberOrCreate(new GithubProfileResponse(email, age));
 
-        verify(memberRepository).save(member);
+        MemberResponse result = memberService.findMemberByEmail(email);
+        assertThat(result.getEmail()).isEqualTo(email);
     }
 }
