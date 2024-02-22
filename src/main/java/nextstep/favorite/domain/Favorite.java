88e0c8d4f8be1d9d309186ec7b610b6b09290ed0
ian <xonic789@naver.com>
2024-02-22 09:43:23 +0900

    즐겨찾기 인수테스트 작성 및 API 구현

--- src/main/java/nextstep/favorite/domain/Favorite.java
+++ src/main/java/nextstep/favorite/domain/Favorite.java
@@ -7,8 +7,27 @@ import javax.persistence.Id;
 
 @Entity
 public class Favorite {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long memberId;
+
+    private Long source;
+
+    private Long target;
+
+    private Favorite() {
+    }
+
+    public Favorite(Long memberId, Long source, Long target) {
+        this.memberId = memberId;
+        this.source = source;
+        this.target = target;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
