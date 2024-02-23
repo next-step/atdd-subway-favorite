106b036ea8da71e82d2f12a7248ca9e554c6ef75
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-24 02:02:22 +0900

    test: 토큰을 통해 회원 정보를 조회하는 기능에 대한 인수 테스트 추가

--- src/test/java/nextstep/core/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/core/member/acceptance/MemberAcceptanceTest.java
@@ -1,10 +1,13 @@
 package nextstep.core.member.acceptance;
 
 import nextstep.common.annotation.AcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
+import static nextstep.core.member.step.AuthSteps.성공하는_토큰_발급_요청;
 import static nextstep.core.member.step.MemberSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,7 +37,7 @@ class MemberAcceptanceTest {
         var response = 회원_정보_조회_요청(createResponse);
 
         // then
-        회원_정보_조회됨(response, EMAIL, AGE);
+        회원_정보_확인(response, EMAIL, AGE);
 
     }
 
@@ -64,15 +67,80 @@ class MemberAcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
-    /**
-     * Given 회원 가입을 생성하고
-     * And 로그인을 하고
-     * When 토큰을 통해 내 정보를 조회하면
-     * Then 내 정보를 조회할 수 있다
-     */
-    @DisplayName("내 정보를 조회한다.")
-    @Test
-    void getMyInfo() {
+    @Nested
+    class 내_정보_조회 {
 
+        @Nested
+        class 성공 {
+            /**
+             * Given 회원을 생성한다.
+             * Given 생성된 회원 정보를 통해 토큰을 발급받는다.
+             * When  토큰을 통해 내 정보를 조회하면
+             * Then  내 정보를 조회할 수 있다
+             */
+            @Test
+            void 정상적인_토큰으로_조회() {
+                // given
+                var 이메일 = "admin@email.com";
+                var 비밀번호 = "password";
+                var 나이 = 20;
+                회원_생성_요청(이메일, 비밀번호, 나이);
+
+                var 발급된_토큰 = 성공하는_토큰_발급_요청(이메일, 비밀번호);
+
+                // when
+                var 토큰으로_전달받은_회원_정보 = 성공하는_토큰으로_회원정보_요청(발급된_토큰);
+
+                // then
+                회원_정보_확인(토큰으로_전달받은_회원_정보, 이메일, 나이);
+            }
+
+        }
+
+        @Nested
+        class 실패 {
+            String 이메일;
+            String 비밀번호;
+            int 나이;
+
+            @BeforeEach
+            void 사전_회원_생성() {
+                이메일 = "admin@email.com";
+                비밀번호 = "password";
+                나이 = 20;
+
+                회원_생성_요청(이메일, 비밀번호, 나이);
+            }
+
+            /**
+             * Given 회원을 생성하고, 저장소에 저장한다.
+             * Given 생성된 회원 정보를 통해 토큰을 발급받는다.
+             * When  비어있는 토큰으로 내 정보를 조회하면
+             * Then  내 정보를 조회할 수 없다.
+             */
+            @Test
+            void 비어있는_토큰을_전달한_경우() {
+                // given
+                var 비어있는_토큰 = "";
+
+                // when
+                실패하는_토큰으로_회원정보_요청(비어있는_토큰);
+            }
+
+            /**
+             * Given 회원을 생성하고, 저장소에 저장한다.
+             * Given 생성된 회원 정보를 통해 토큰을 발급받는다.
+             * When  발급받은 토큰을 임의로 수정하고 내 정보를 조회하면
+             * Then  내 정보를 조회할 수 없다.
+             */
+            @Test
+            void 비정상적인_토큰을_전달한_경우() {
+                // given
+                var 임의로_수정된_토큰 = "Changed" + 성공하는_토큰_발급_요청(이메일, 비밀번호);
+
+                // when
+                실패하는_토큰으로_회원정보_요청(임의로_수정된_토큰);
+            }
+        }
     }
 }
