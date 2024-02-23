2642f7ffcf65111d4ebc6dc12f3c3072b25306da
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-23 15:39:29 +0900

    feat: 즐겨찾기 조회 요구사항에 대한 기능 구현

--- src/main/java/nextstep/core/favorite/application/dto/FavoriteResponse.java
+++ src/main/java/nextstep/core/favorite/application/dto/FavoriteResponse.java
@@ -1,7 +1,17 @@
 package nextstep.core.favorite.application.dto;
 
+import nextstep.core.station.application.dto.StationResponse;
+
 /**
  * TODO: StationResponse를 포함하는 클래스로 만듭니다.
  */
 public class FavoriteResponse {
+
+    private final StationResponse sourceStation;
+    private final StationResponse targetStation;
+
+    public FavoriteResponse(StationResponse sourceStation, StationResponse targetStation) {
+        this.sourceStation = sourceStation;
+        this.targetStation = targetStation;
+    }
 }
