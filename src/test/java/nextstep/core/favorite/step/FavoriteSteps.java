ec9b79bc2ba6a4b7d3c066ed80433419c9bedc76
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-24 13:05:52 +0900

    refactor: 즐겨찾기 추가와 관련된 인수 테스트 메서드 리팩토링
    - refactor: 회원생성 후 토큰을 발급을 요청하고 토큰을 반환하는 RestAssured 메서드 분리

---
+++ src/test/java/nextstep/core/favorite/step/FavoriteSteps.java
@@ -0,0 +1,93 @@
+package nextstep.core.favorite.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.core.favorite.application.dto.FavoriteRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FavoriteSteps {
+
+
+    public static void 성공하는_즐겨찾기_추가_요청(FavoriteRequest 즐겨찾기_요청_정보, String 토큰) {
+        ExtractableResponse<Response> 즐겨찾기_추가_요청_응답 = RestAssured
+                .given().log().all()
+                .header("Authorization", 토큰)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(즐겨찾기_요청_정보)
+                .when()
+                .post("/favorites")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+    }
+
+    public static void 잘못된_토큰으로_즐겨찾기_추가_요청(FavoriteRequest 즐겨찾기_요청_정보, String 토큰) {
+        ExtractableResponse<Response> 즐겨찾기_추가_요청_응답 = RestAssured
+                .given().log().all()
+                .header("Authorization", 토큰)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(즐겨찾기_요청_정보)
+                .when()
+                .post("/favorites")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .extract();
+    }
+
+    public static void 토큰없이_즐겨찾기_추가_요청(FavoriteRequest 즐겨찾기_요청_정보) {
+        ExtractableResponse<Response> 즐겨찾기_추가_요청_응답 = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(즐겨찾기_요청_정보)
+                .when()
+                .post("/favorites")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .extract();
+    }
+
+    public static void 실패하는_즐겨찾기_추가_요청(FavoriteRequest 즐겨찾기_요청_정보, String 토큰) {
+        ExtractableResponse<Response> 즐겨찾기_추가_요청_응답 = RestAssured
+                .given().log().all()
+                .header("Authorization", 토큰)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(즐겨찾기_요청_정보)
+                .when()
+                .post("/favorites")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract();
+    }
+
+    public static void 특정_회원의_즐겨찾기_목록_검증(FavoriteRequest 확인할_즐겨찾기_정보, String 토큰) {
+        ExtractableResponse<Response> 즐겨찾기_조회_요청_응답 = RestAssured
+                .given().log().all()
+                .header("Authorization", 토큰)
+                .when()
+                .get("/favorites")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+
+        assertThat(즐겨찾기_조회_요청_응답.jsonPath().getList("source.id", Long.class)).containsExactly(확인할_즐겨찾기_정보.getSource());
+        assertThat(즐겨찾기_조회_요청_응답.jsonPath().getList("target.id", Long.class)).containsExactly(확인할_즐겨찾기_정보.getTarget());
+    }
+
+    public static void 특정_회원의_즐겨찾기_목록_없음_검증(FavoriteRequest 확인할_즐겨찾기_정보, String 토큰) {
+        ExtractableResponse<Response> 즐겨찾기_조회_요청_응답 = RestAssured
+                .given().log().all()
+                .header("Authorization", 토큰)
+                .when()
+                .get("/favorites")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+
+        assertThat(즐겨찾기_조회_요청_응답.jsonPath().getList("source.id", Long.class)).doesNotContain(확인할_즐겨찾기_정보.getSource());
+        assertThat(즐겨찾기_조회_요청_응답.jsonPath().getList("target.id", Long.class)).doesNotContain(확인할_즐겨찾기_정보.getTarget());
+    }
+}
