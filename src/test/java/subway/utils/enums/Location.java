6b332437369676a275851d9ae96ec5f0f5530068
yu <yu@reverselab.co.kr>
2024-02-24 16:09:43 +0900

    Location MEMBERS, TOKENS 추가

--- src/test/java/subway/utils/enums/Location.java
+++ src/test/java/subway/utils/enums/Location.java
@@ -8,7 +8,9 @@ public enum Location {
 	LINES("/lines", path -> UriComponentsBuilder.fromPath("/lines/").path(path)),
 	STATIONS("/stations", path -> UriComponentsBuilder.fromPath("/stations/").path(path)),
 	PATHS("/paths", path -> UriComponentsBuilder.fromPath("/paths/").path(path)),
-	FAVORITES("/favorites", path -> UriComponentsBuilder.fromPath("/favorites/").path(path));
+	FAVORITES("/favorites", path -> UriComponentsBuilder.fromPath("/favorites/").path(path)),
+	MEMBERS("/members", path -> UriComponentsBuilder.fromPath("/members/").path(path)),
+	TOKENS("/login/token", path -> UriComponentsBuilder.fromPath("/login/token/").path(path));
 
 	private final String uri;
 	private final Function<String, UriComponentsBuilder> expression;
