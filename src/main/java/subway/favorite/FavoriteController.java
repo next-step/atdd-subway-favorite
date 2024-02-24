b533d1ab110189d939c16a40383acfbaa7b65122
yu <yu@reverselab.co.kr>
2024-02-24 15:41:01 +0900

    인수 테스트 oauth2 적용하여 초안 작성

--- src/main/java/subway/favorite/FavoriteController.java
+++ src/main/java/subway/favorite/FavoriteController.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +22,10 @@ public class FavoriteController {
 	}
 
 	@PostMapping
-	public ResponseEntity<Void> save(@AuthenticationPrincipal LoginMember member, @RequestBody FavoriteRequest request) {
+	public ResponseEntity<Void> save(
+		@AuthenticationPrincipal LoginMember member,
+		@RequestBody FavoriteRequest request
+	) {
 		Long savedId = favoriteService.save(member.getEmail(), request);
 		return ResponseEntity.created(URI.create("/favorites/" + savedId)).build();
 	}
--- src/test/java/subway/acceptance/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/subway/acceptance/favorite/FavoriteAcceptanceTest.java
@@ -1,16 +1,32 @@
 package subway.acceptance.favorite;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import subway.acceptance.AcceptanceTest;
-import subway.utils.enums.Location;
-import subway.utils.rest.Rest;
+import subway.dto.favorite.FavoriteRequest;
+import subway.member.Member;
+import subway.member.MemberRepository;
 
 @DisplayName("즐겨찾기 인수 테스트")
 class FavoriteAcceptanceTest extends AcceptanceTest {
+	public static final String EMAIL = "admin@email.com";
+	public static final String PASSWORD = "password";
+	public static final Integer AGE = 20;
+
+	@Autowired
+	private MemberRepository memberRepository;
 
 	/**
 	 * given 로그인된 회원인지 검사한다.
@@ -20,10 +36,30 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
 	@DisplayName("즐겨찾기를 생성한다.")
 	@Test
 	void saveFavorite() {
-		HashMap<String, String> params = new HashMap<>();
-		Rest.builder()
-			.uri(Location.FAVORITES.path())
+		FavoriteRequest favoriteRequest = new FavoriteRequest(1L, 3L);
+		memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
+
+		Map<String, String> params = new HashMap<>();
+		params.put("email", EMAIL);
+		params.put("password", PASSWORD);
+
+		ExtractableResponse<Response> response = RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.body(params)
-			.post();
+			.when().post("/login/token")
+			.then().log().all()
+			.statusCode(HttpStatus.OK.value()).extract();
+
+		String accessToken = response.jsonPath().getString("accessToken");
+		assertThat(accessToken).isNotBlank();
+
+		ExtractableResponse<Response> extract = RestAssured.given().log().all()
+			.auth().oauth2(accessToken)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(favoriteRequest)
+			.when().post("/favorites")
+			.then().log().all()
+			.statusCode(HttpStatus.CREATED.value())
+			.extract();
 	}
 }
