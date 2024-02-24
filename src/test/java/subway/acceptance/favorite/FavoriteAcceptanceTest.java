5b1b925a9aa72eee83d2a431b9c725422e584494
yu <yu@reverselab.co.kr>
2024-02-24 16:08:15 +0900

    즐겨 찾기 인수 테스트 oauth2 적용하여 해피케이스 완성

--- src/test/java/subway/acceptance/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/subway/acceptance/favorite/FavoriteAcceptanceTest.java
@@ -1,13 +1,9 @@
 package subway.acceptance.favorite;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import static subway.fixture.acceptance.MemberAcceptanceSteps.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -16,18 +12,11 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import subway.acceptance.AcceptanceTest;
 import subway.dto.favorite.FavoriteRequest;
-import subway.member.Member;
-import subway.member.MemberRepository;
+import subway.dto.station.StationResponse;
+import subway.fixture.acceptance.StationAcceptanceSteps;
 
 @DisplayName("즐겨찾기 인수 테스트")
 class FavoriteAcceptanceTest extends AcceptanceTest {
-	public static final String EMAIL = "admin@email.com";
-	public static final String PASSWORD = "password";
-	public static final Integer AGE = 20;
-
-	@Autowired
-	private MemberRepository memberRepository;
-
 	/**
 	 * given 로그인된 회원인지 검사한다.
 	 * when 즐겨찾기를 생성한다.
@@ -36,25 +25,14 @@ class FavoriteAcceptanceTest extends AcceptanceTest {
 	@DisplayName("즐겨찾기를 생성한다.")
 	@Test
 	void saveFavorite() {
-		FavoriteRequest favoriteRequest = new FavoriteRequest(1L, 3L);
-		memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
+		멤버_생성();
+		Long 불광역 = StationAcceptanceSteps.정류장_생성("불광역").as(StationResponse.class).getId();
+		Long 양재역 = StationAcceptanceSteps.정류장_생성("양재역").as(StationResponse.class).getId();
 
-		Map<String, String> params = new HashMap<>();
-		params.put("email", EMAIL);
-		params.put("password", PASSWORD);
-
-		ExtractableResponse<Response> response = RestAssured.given().log().all()
-			.contentType(MediaType.APPLICATION_JSON_VALUE)
-			.body(params)
-			.when().post("/login/token")
-			.then().log().all()
-			.statusCode(HttpStatus.OK.value()).extract();
-
-		String accessToken = response.jsonPath().getString("accessToken");
-		assertThat(accessToken).isNotBlank();
+		FavoriteRequest favoriteRequest = new FavoriteRequest(불광역, 양재역);
 
 		ExtractableResponse<Response> extract = RestAssured.given().log().all()
-			.auth().oauth2(accessToken)
+			.auth().oauth2(로그인())
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.body(favoriteRequest)
 			.when().post("/favorites")
