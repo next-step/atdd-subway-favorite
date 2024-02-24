bcc7d0f6850459ff555cab1ae32e000ad5e3f13c
hhhhhwi <aab555586@gmail.com>
2024-02-24 16:43:08 +0900

    feat: 즐겨찾기 관련 도메인 Favorite 생성

--- src/main/java/nextstep/subway/domain/entity/Favorite.java
+++ src/main/java/nextstep/subway/domain/entity/Favorite.java
@@ -1,9 +1,7 @@
 package nextstep.subway.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Favorite {
@@ -11,4 +9,42 @@ public class Favorite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long source;
+
+    private Long target;
+
+    protected Favorite() {
+    }
+
+    public Favorite(Long source, Long target) {
+        this.source = source;
+        this.target = target;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getSource() {
+        return source;
+    }
+
+    public Long getTarget() {
+        return target;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Favorite) {
+            Favorite favorite = (Favorite) obj;
+            return Objects.equals(id, favorite.getId());
+        }
+
+        return false;
+    }
 }
