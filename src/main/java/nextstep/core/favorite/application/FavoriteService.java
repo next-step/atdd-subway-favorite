f52f0231242426f1fbabd0e48c786b93de1d1442
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-24 16:35:21 +0900

    refactor: 즐겨찾기 서비스 레이어 리팩토링 및 Converter 로직 분리

--- src/main/java/nextstep/core/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/core/favorite/application/FavoriteService.java
@@ -7,15 +7,15 @@ import nextstep.core.favorite.domain.FavoriteRepository;
 import nextstep.core.member.domain.Member;
 import nextstep.core.member.exception.NonMatchingMemberException;
 import nextstep.core.pathFinder.application.PathFinderService;
-import nextstep.core.pathFinder.application.dto.PathFinderRequest;
 import nextstep.core.station.application.StationService;
-import nextstep.core.station.application.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
+
+import static nextstep.core.favorite.application.FavoriteConverter.convertToFavoriteResponses;
+import static nextstep.core.pathFinder.application.converter.PathFinderConverter.convertToPathFinderRequest;
 
 @Transactional(readOnly = true)
 @Service
@@ -34,29 +34,14 @@ public class FavoriteService {
 
     @Transactional
     public Favorite createFavorite(FavoriteRequest request, Member member) {
-        if(!pathFinderService.isValidPath(new PathFinderRequest(request.getSource(), request.getTarget()))) {
+        if (!pathFinderService.isValidPath(convertToPathFinderRequest(request))) {
             throw new IllegalArgumentException("출발역과 도착역을 잇는 경로가 없습니다.");
         }
-
-        Favorite favorite = new Favorite(
-                stationService.findStation(request.getSource()),
-                stationService.findStation(request.getTarget()),
-                member);
-        return favoriteRepository.save(favorite);
+        return favoriteRepository.save(createFavoriteEntity(request, member));
     }
 
     public List<FavoriteResponse> findFavorites(Member member) {
-        List<Favorite> favorites = favoriteRepository.findByMember(member);
-
-        List<FavoriteResponse> favoriteResponses = new ArrayList<>();
-
-        favorites.forEach(favorite -> {
-            favoriteResponses.add(new FavoriteResponse(
-                    new StationResponse(favorite.getSourceStation().getId(), favorite.getSourceStation().getName()),
-                    new StationResponse(favorite.getTargetStation().getId(), favorite.getTargetStation().getName())));
-        });
-
-        return favoriteResponses;
+        return convertToFavoriteResponses(favoriteRepository.findByMember(member));
     }
 
     @Transactional
@@ -69,5 +54,12 @@ public class FavoriteService {
 
     private Favorite findFavoriteById(Long id) {
         return favoriteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("즐겨찾기 번호에 해당하는 즐겨찾기가 없습니다."));
+    }
+
+    private Favorite createFavoriteEntity(FavoriteRequest request, Member member) {
+        return new Favorite(
+                stationService.findStation(request.getSource()),
+                stationService.findStation(request.getTarget()),
+                member);
     }
 }
