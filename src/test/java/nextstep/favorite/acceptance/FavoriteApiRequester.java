6440e90fc2b58f8d3fda18426761b353e4557cef
seonje1 <re552@naver.com>
2024-02-26 01:21:00 +0900

    핒드백 반영

--- src/test/java/nextstep/favorite/acceptance/FavoriteApiRequester.java
+++ src/test/java/nextstep/favorite/acceptance/FavoriteApiRequester.java
@@ -5,18 +5,18 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.favorite.application.dto.FavoriteRequest;
 import nextstep.fixture.MockTokenFactory;
+import org.apache.http.HttpHeaders;
 
 import static io.restassured.RestAssured.given;
 
 public class FavoriteApiRequester {
 
     private static final String URL = "/favorites";
-    private static final String AUTHORIZATION = "Authorization";
 
     public static ExtractableResponse<Response> createFavoriteApiCall(FavoriteRequest request) {
         String accessToken = MockTokenFactory.getAccessToken();
         return given().log().all()
-                .header(AUTHORIZATION, accessToken)
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .contentType(ContentType.JSON)
                 .body(request)
                 .when().post(URL)
@@ -27,7 +27,7 @@ public class FavoriteApiRequester {
     public static ExtractableResponse<Response> findFavoritesApiCall() {
         String accessToken = MockTokenFactory.getAccessToken();
         return given().log().all()
-                .header(AUTHORIZATION, accessToken)
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .when().get(URL)
                 .then().log().all()
                 .extract();
@@ -36,7 +36,7 @@ public class FavoriteApiRequester {
     public static ExtractableResponse<Response> deleteFavoriteApiCall(Long id) {
         String accessToken = MockTokenFactory.getAccessToken();
         return given().log().all()
-                .header(AUTHORIZATION, accessToken)
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .pathParam("id", id)
                 .when().delete(URL + "/{id}")
                 .then().log().all()
