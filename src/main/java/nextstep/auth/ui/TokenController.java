84e5119d749ffc42c46f05232c6564627c9a34f3
domodazzi <domodazzi0@gmail.com>
2024-02-27 00:14:42 +0900

    test : AuthService 테스트 작성

--- src/main/java/nextstep/auth/ui/TokenController.java
+++ src/main/java/nextstep/auth/ui/TokenController.java
@@ -1,5 +1,6 @@
 package nextstep.auth.ui;
 
+import lombok.RequiredArgsConstructor;
 import nextstep.member.application.TokenService;
 import nextstep.auth.application.dto.GithubLoginRequest;
 import nextstep.auth.application.dto.TokenRequest;
@@ -9,13 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class TokenController {
-    private TokenService tokenService;
 
-    public TokenController(TokenService tokenService) {
-        this.tokenService = tokenService;
-    }
+    private final TokenService tokenService;
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> createToken(@RequestBody TokenRequest request) {
--- src/test/java/nextstep/auth/application/AuthServiceTest.java
+++ src/test/java/nextstep/auth/application/AuthServiceTest.java
@@ -1,9 +1,13 @@
 package nextstep.auth.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nextstep.member.acceptance.MemberSteps;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest
 class AuthServiceTest {
@@ -14,10 +18,32 @@ class AuthServiceTest {
   @DisplayName("이메일과 패스워드로 토큰을 생성한다.")
   @Test
   void createToken() {
+    // given
+    var email = "domodazzi@gmail.com";
+    var age = 20;
+    MemberSteps.회원_생성_요청(email, "password", age);
+
+    // when
+    var token = authService.createToken("domodazzi", "password");
+
+    // then
+    var response = MemberSteps.회원_정보_조회_요청(token.getAccessToken());
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    assertThat(response.jsonPath().getString("email")).isEqualTo(email);
+    assertThat(response.jsonPath().getInt("age")).isEqualTo(age);
   }
 
   @DisplayName("깃헙 액세스 코드로 토큰을 생성한다.")
   @Test
   void createTokenFromGithub() {
+    // given
+    var code = "domodazzi";
+
+    // when
+    var token = authService.createTokenFromGithub(code);
+
+    // then
+    var response = MemberSteps.회원_정보_조회_요청(token.getAccessToken());
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
   }
 }
