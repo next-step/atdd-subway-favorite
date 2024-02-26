851dc2472644aeb4447e69173d833c509ed38e7d
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-26 14:09:10 +0900

    test: 실제 GitHub의 인증과 리소스 서버를 대체하는 Fake Controller 추가

---
+++ src/test/java/nextstep/common/utils/TestGitHubController.java
@@ -0,0 +1,26 @@
+package nextstep.common.utils;
+
+import nextstep.core.member.application.GithubProfileResponse;
+import nextstep.core.member.application.dto.GithubAccessTokenRequest;
+import nextstep.core.member.application.dto.GithubAccessTokenResponse;
+import nextstep.core.member.fixture.TokenFixture;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Profile("test")
+@RestController
+public class TestGitHubController {
+
+    @PostMapping("/github/login/oauth/access_token")
+    public ResponseEntity<GithubAccessTokenResponse> createToken(@RequestBody GithubAccessTokenRequest request) {
+        GithubAccessTokenResponse githubAccessTokenResponse = new GithubAccessTokenResponse(TokenFixture.findToken(
+                request.getCode()), "repo,gist", "bearer");
+        return ResponseEntity.ok().body(githubAccessTokenResponse);
+    }
+
+    @GetMapping("/github/user")
+    public ResponseEntity<GithubProfileResponse> findAuthenticateUser(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok().body(new GithubProfileResponse(TokenFixture.findMemberInfo(token)));
+    }
+}
