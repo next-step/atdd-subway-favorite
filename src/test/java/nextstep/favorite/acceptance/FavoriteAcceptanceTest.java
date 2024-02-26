172653a8584097d22dcde035394c9b391552808f
문희원 <munhw1408@gmail.com>
2024-02-27 03:20:59 +0900

    test: Favorite 인수테스트 리팩토링

--- src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/favorite/acceptance/FavoriteAcceptanceTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
+import static nextstep.utils.api.FavoriteApi.*;
+import static nextstep.utils.api.FavoriteApi.즐겨찾기를_삭제한다;
 import static nextstep.utils.api.LineApi.*;
 import static nextstep.utils.api.MemberApi.*;
 import static nextstep.utils.api.StationApi.지하철역_생성요청;
@@ -26,21 +25,23 @@ import java.util.Map;
 
 @DisplayName("즐겨찾기 관련 기능")
 public class FavoriteAcceptanceTest extends AcceptanceTest {
-    final String 회원_이메일 = "munhw1408@gmail.com";
+    final String 회원_이메일 = "test@test.com";
     final String 회원_비밀번호 = "test1234!!";
-    final Map<String, Long> 추가할_즐겨찾기 = new HashMap<>();
-    final int testMemberAge = 18;
+    final Map<String, Long> 강남역에서_신논현역_즐겨찾기 = new HashMap<>();
 
     // XXX: dataLoader로 이동?
     @BeforeEach
     void setUpMember() {
         // given
-        회원_생성_요청(회원_이메일, 회원_비밀번호, testMemberAge);
+        회원_생성_요청(회원_이메일, 회원_비밀번호, 18);
         Long 강남역ID = 지하철역_생성요청(강남역).getLong("id");
         Long 논현역ID = 지하철역_생성요청(논현역).getLong("id");
         Long 신논현역ID = 지하철역_생성요청(신논현역).getLong("id");
         Long 신분당선ID = 노선생성요청(신분당선_생성_바디(강남역ID, 논현역ID)).getLong("id");
         구간생성요청(신분당선ID, 추가구간_생성_바디(논현역ID, 신논현역ID));
+
+        강남역에서_신논현역_즐겨찾기.put("source", 강남역ID);
+        강남역에서_신논현역_즐겨찾기.put("target", 신논현역ID);
     }
 
     @Nested
@@ -58,28 +59,13 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             String 인증_토큰 = response.jsonPath().getString("accessToken");
 
             // When
-            추가할_즐겨찾기.put("source", 1L);
-            추가할_즐겨찾기.put("target", 2L);
-
-            ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            assertThat(즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰).statusCode()).isEqualTo(201);
 
             // Then
-            assertThat(즐겨찾기를_추가한다.statusCode()).isEqualTo(201);
-            
-            ExtractableResponse<Response> 즐겨찾기_목록을_조회한다 = RestAssured.given().log().all()
-                .auth().oauth2(인증_토큰)
-                .when().get("/favorites")
-                .then().log().all()
-                .extract();
+            ExtractableResponse<Response> 즐겨찾기_조회_결과 = 즐겨찾기_목록을_조회한다(인증_토큰);
 
-            assertThat(즐겨찾기_목록을_조회한다.jsonPath().getString("[0].source.name")).isEqualTo("강남역");
-            assertThat(즐겨찾기_목록을_조회한다.jsonPath().getString("[0].target.name")).isEqualTo("논현역");
+            assertThat(즐겨찾기_조회_결과.jsonPath().getString("[0].source.name")).isEqualTo("강남역");
+            assertThat(즐겨찾기_조회_결과.jsonPath().getString("[0].target.name")).isEqualTo("신논현역");
         }
 
         /**
@@ -90,20 +76,9 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
         @Test
         @DisplayName("로그인하지 않은 사용자는 즐겨찾기를 추가할 수 없다.")
         void failForNotLogin() {
-          // Given
           // When
-          추가할_즐겨찾기.put("source", 1L);
-          추가할_즐겨찾기.put("target", 3L);
-
-          ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                  .body(추가할_즐겨찾기)
-                  .contentType(MediaType.APPLICATION_JSON_VALUE)
-                  .when().post("/favorites")
-                  .then().log().all()
-                  .extract();
-
           // Then
-          assertThat(즐겨찾기를_추가한다.statusCode()).isEqualTo(401);
+          assertThat(즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기).statusCode()).isEqualTo(401);
         }
 
         /**
@@ -117,36 +92,13 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             // Given
             ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
             String 인증_토큰 = response.jsonPath().getString("accessToken");
-            추가할_즐겨찾기.put("source", 1L);
-            추가할_즐겨찾기.put("target", 2L);
-
-            ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰);
 
             // When
-            ExtractableResponse<Response> 즐겨찾기를_다시_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            assertThat(즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰).statusCode()).isEqualTo(409);
 
             // Then
-            assertThat(즐겨찾기를_다시_추가한다.statusCode()).isEqualTo(409);
-
-            ExtractableResponse<Response> 즐겨찾기_목록을_조회한다 = RestAssured.given().log().all()
-                    .auth().oauth2(인증_토큰)
-                    .when().get("/favorites")
-                    .then().log().all()
-                    .extract();
-
-            assertThat(즐겨찾기_목록을_조회한다.jsonPath().getList("source.id")).hasSize(1);
+            assertThat(즐겨찾기_목록을_조회한다(인증_토큰).jsonPath().getList(".")).hasSize(1);
         }
 
         /**
@@ -160,27 +112,21 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             // Given
             ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
             String 인증_토큰 = response.jsonPath().getString("accessToken");
-            추가할_즐겨찾기.put("source", 1L);
-            추가할_즐겨찾기.put("target", 100000L);
+
+            Map<String, Long> 추가할_즐겨찾기 = Map.of("source", 1L, "target", 404L);
 
             // When
-            ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            assertThat(즐겨찾기를_추가한다(추가할_즐겨찾기, 인증_토큰).statusCode()).isEqualTo(404);
 
             // Then
-            assertThat(즐겨찾기를_추가한다.statusCode()).isEqualTo(404);
+            assertThat(즐겨찾기_목록을_조회한다(인증_토큰).jsonPath().getList(".")).hasSize(0);
         }
     }
 
     @Nested
     class FindFavorites {
         /**
-         * Given 사용자가 로그인 되어있을 때
+         * Given 사용자가 로그인 되어있고, 즐겨찾기를 추가한 뒤
          * When 즐겨찾기 목록을 조회하면
          * Then 즐겨찾기 목록이 조회된다.
          */
@@ -190,28 +136,16 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             // given
             ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
             String 인증_토큰 = response.jsonPath().getString("accessToken");
-            추가할_즐겨찾기.put("source", 1L);
-            추가할_즐겨찾기.put("target", 2L);
+
+            즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰);
 
             // when
-            ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            ExtractableResponse<Response> 즐겨찾기_목록_조회 = 즐겨찾기_목록을_조회한다(인증_토큰);
             
             // then
-            ExtractableResponse<Response> 즐겨찾기_목록을_조회한다 = RestAssured.given().log().all()
-                    .auth().oauth2(인증_토큰)
-                    .when().get("/favorites")
-                    .then().log().all()
-                    .extract();
-            
-            assertThat(즐겨찾기_목록을_조회한다.statusCode()).isEqualTo(200);
-            assertThat(즐겨찾기_목록을_조회한다.jsonPath().getString("[0].source.name")).isEqualTo("강남역");
-            assertThat(즐겨찾기_목록을_조회한다.jsonPath().getString("[0].target.name")).isEqualTo("논현역");
+            assertThat(즐겨찾기_목록_조회.statusCode()).isEqualTo(200);
+            assertThat(즐겨찾기_목록_조회.jsonPath().getString("[0].source.name")).isEqualTo("강남역");
+            assertThat(즐겨찾기_목록_조회.jsonPath().getString("[0].target.name")).isEqualTo("신논현역");
         }
 
         /**
@@ -222,22 +156,16 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
         @Test
         @DisplayName("로그인하지 않은 사용자는 즐겨찾기 목록을 조회할 수 없다.")
         void failForNotLogin() {
-            // given
             // when
-            ExtractableResponse<Response> 즐겨찾기_목록을_조회한다 = RestAssured.given().log().all()
-                    .when().get("/favorites")
-                    .then().log().all()
-                    .extract();
-            
             // then
-            assertThat(즐겨찾기_목록을_조회한다.statusCode()).isEqualTo(401);
+            assertThat(즐겨찾기_목록을_조회한다().statusCode()).isEqualTo(401);
         }
     }
 
     @Nested
     class RemoveFavorite {
       /**
-       * Given 사용자가 로그인 되어있을 때
+       * Given 사용자가 로그인 되어있고, 즐겨찾기를 추가한 뒤
        * When 즐겨찾기를 삭제하면
        * Then 즐겨찾기 목록에서 삭제된다.
        */
@@ -247,34 +175,13 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             // Given
             ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
             String 인증_토큰 = response.jsonPath().getString("accessToken");
-            추가할_즐겨찾기.put("source", 1L);
-            추가할_즐겨찾기.put("target", 2L);
 
-            ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰);
 
             // When
-            ExtractableResponse<Response> 즐겨찾기를_삭제한다 = RestAssured.given().log().all()
-                    .auth().oauth2(인증_토큰)
-                    .when().delete("/favorites/1")
-                    .then().log().all()
-                    .extract();
-
             // Then
-            assertThat(즐겨찾기를_삭제한다.statusCode()).isEqualTo(204);
-
-            ExtractableResponse<Response> 즐겨찾기_목록을_조회한다 = RestAssured.given().log().all()
-                    .auth().oauth2(인증_토큰)
-                    .when().get("/favorites")
-                    .then().log().all()
-                    .extract();
-
-            assertThat(즐겨찾기_목록을_조회한다.jsonPath().getList(".")).isEmpty();
+            assertThat(즐겨찾기를_삭제한다(1L, 인증_토큰).statusCode()).isEqualTo(204);
+            assertThat(즐겨찾기_목록을_조회한다(인증_토큰).jsonPath().getList(".")).isEmpty();
         }
 
         /**
@@ -286,14 +193,18 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
         @DisplayName("로그인하지 않은 사용자는 즐겨찾기를 삭제할 수 없다.")
         void failForNotLogin() {
             // Given
-            // When
-            ExtractableResponse<Response> 즐겨찾기를_삭제한다 = RestAssured.given().log().all()
-                    .when().delete("/favorites/1")
-                    .then().log().all()
-                    .extract();
+            ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
+            String 인증_토큰 = response.jsonPath().getString("accessToken");
 
+            즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰);
+
+            // When
             // Then
-            assertThat(즐겨찾기를_삭제한다.statusCode()).isEqualTo(401);
+            assertThat(즐겨찾기를_삭제한다(1L).statusCode()).isEqualTo(401);
+
+            ExtractableResponse<Response> 즐겨찾기_목록을_조회_확인 = 즐겨찾기_목록을_조회한다(인증_토큰);
+            assertThat(즐겨찾기_목록을_조회_확인.jsonPath().getList("source.name")).contains("강남역");
+            assertThat(즐겨찾기_목록을_조회_확인.jsonPath().getList("target.name")).contains("신논현역");
         }
 
         /**
@@ -308,15 +219,11 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
             String 인증_토큰 = response.jsonPath().getString("accessToken");
 
-            // When
-            ExtractableResponse<Response> 즐겨찾기를_삭제한다 = RestAssured.given().log().all()
-                    .auth().oauth2(인증_토큰)
-                    .when().delete("/favorites/404")
-                    .then().log().all()
-                    .extract();
+            즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰);
 
+            // When
             // Then
-            assertThat(즐겨찾기를_삭제한다.statusCode()).isEqualTo(404);
+            assertThat(즐겨찾기를_삭제한다(404L, 인증_토큰).statusCode()).isEqualTo(404);
         }
 
         /**
@@ -330,31 +237,23 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
             // Given
             ExtractableResponse<Response> response = 로그인을_시도한다(회원_이메일, 회원_비밀번호);
             String 인증_토큰 = response.jsonPath().getString("accessToken");
-            추가할_즐겨찾기.put("source", 1L);
-            추가할_즐겨찾기.put("target", 2L);
 
-            ExtractableResponse<Response> 즐겨찾기를_추가한다 = RestAssured.given().log().all()
-                    .body(추가할_즐겨찾기)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .auth().oauth2(인증_토큰)
-                    .when().post("/favorites")
-                    .then().log().all()
-                    .extract();
+            즐겨찾기를_추가한다(강남역에서_신논현역_즐겨찾기, 인증_토큰);
 
-            회원_생성_요청("hello@gmail.com", "test1234!!", 18);
-
-            ExtractableResponse<Response> 다른_사용자_로그인_요청 = 로그인을_시도한다("hello@gmail.com", "test1234!!");
+            회원_생성_요청("sabotage@test.com", "test1234!!", 18);
+            ExtractableResponse<Response> 다른_사용자_로그인_요청 = 로그인을_시도한다(
+                    "sabotage@test.com", "test1234!!"
+            );
             String 다른_사용자_인증_토큰 = 다른_사용자_로그인_요청.jsonPath().getString("accessToken");
 
             // When
-            ExtractableResponse<Response> 즐겨찾기를_삭제한다 = RestAssured.given().log().all()
-                    .auth().oauth2(다른_사용자_인증_토큰)
-                    .when().delete("/favorites/1")
-                    .then().log().all()
-                    .extract();
-            
+
+            assertThat(즐겨찾기를_삭제한다(1L, 다른_사용자_인증_토큰).statusCode()).isEqualTo(403);
+
             // Then
-            assertThat(즐겨찾기를_삭제한다.statusCode()).isEqualTo(403);
+            ExtractableResponse<Response> 즐겨찾기_목록을_조회_확인 = 즐겨찾기_목록을_조회한다(인증_토큰);
+            assertThat(즐겨찾기_목록을_조회_확인.jsonPath().getList("source.name")).contains("강남역");
+            assertThat(즐겨찾기_목록을_조회_확인.jsonPath().getList("target.name")).contains("신논현역");
         }
     }
 
