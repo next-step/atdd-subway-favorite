a666aa8ea4ec59a772eff37c5a4bdddba0567796
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-02-28 21:27:05 +0900

    fix: 패키지 리팩토링 과정에서 수정하지 못한 Token Service 레이어 테스트 코드와 테스트 시나리오 수정

--- src/test/java/nextstep/core/auth/application/TokenServiceMockTest.java
+++ src/test/java/nextstep/core/auth/application/TokenServiceMockTest.java
@@ -1,6 +1,6 @@
 package nextstep.core.auth.application;
 
-import nextstep.core.auth.application.dto.TokenResponse;
+import nextstep.core.auth.application.dto.GithubProfileResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,8 +8,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static nextstep.core.member.fixture.MemberFixture.브라운;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TokenServiceMockTest {
@@ -28,32 +29,26 @@ public class TokenServiceMockTest {
     }
 
     @Nested
-    class 깃허브부터_토큰_발급 {
+    class 깃허브_프로필_요청 {
         @Nested
         class 성공 {
 
             final String code = "Temp Code";
-            final String token = "Bearer TOKEN ";
-
-            final String email = "test@test.com";
-            final String password = "PASSWORD";
-            final int age = 20;
 
             /**
-             * When  코드를 통해 깃허브에 회원정보를 요청한다.
-             * When     응답받은 회원정보에 일치하는 회원을 조회한다.
-             * Then  정상적으로 토큰이 발급된다.
+             * When  코드를 통해 깃허브 프로필을 요청할 경우
+             * Then  정상적으로 프로필을 전달받는다.
              */
             @Test
-            void 깃허브로_회원가입된_회원의_토큰_발급_요청() {
+            void 코드로_깃허브_프로필_요청() {
                 // given
-                when(jwtTokenProvider.createToken(email)).thenReturn(token);
+                when(githubClient.requestGithubProfile(code)).thenReturn(new GithubProfileResponse(브라운.이메일));
 
                 // when
-                TokenResponse tokenResponse = tokenService.createTokenByGithub(email);
+                GithubProfileResponse githubProfileResponse = tokenService.requestGithubProfile(code);
 
                 // then
-                assertThat(tokenResponse.getAccessToken()).isNotBlank();
+                assertThat(githubProfileResponse.getEmail()).isEqualTo(브라운.이메일);
             }
         }
     }
--- src/test/java/nextstep/core/auth/application/TokenServiceTest.java
+++ src/test/java/nextstep/core/auth/application/TokenServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static nextstep.core.member.fixture.MemberFixture.브라운;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("토큰 서비스 레이어 테스트")
@@ -31,16 +30,19 @@ public class TokenServiceTest {
 
     @Nested
     class 토큰_발급 {
+
+        final String value = "Temp Value";
+
         @Nested
         class 성공 {
             /**
-             * When  생성된 회원 정보로 토큰 발급을 요청할 경우
+             * When  토큰 발급을 요청할 경우
              * Then  정상적으로 토큰이 발급된다.
              */
             @Test
-            void 저장된_회원으로_토큰_발급_요청() {
+            void 토큰_발급_요청() {
                 // when
-                TokenResponse tokenResponse = tokenService.createToken(브라운.이메일);
+                TokenResponse tokenResponse = tokenService.createToken(value);
 
                 // then
                 assertThat(tokenResponse.getAccessToken()).isNotBlank();
