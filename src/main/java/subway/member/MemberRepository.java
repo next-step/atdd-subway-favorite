df1e9e6e42a18479f47752c5b9f27aac50c37940
yu <yu@reverselab.co.kr>
2024-02-28 13:13:11 +0900

    불필요 메서드 제거

--- src/main/java/subway/member/MemberRepository.java
+++ src/main/java/subway/member/MemberRepository.java
@@ -5,7 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
-
-    void deleteByEmail(String email);
+	Optional<Member> findByEmail(String email);
 }
