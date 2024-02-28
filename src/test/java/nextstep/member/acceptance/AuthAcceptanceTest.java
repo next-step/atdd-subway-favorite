dbd071cad78c775704dc2527df816eccc8121f96
o3ozzvb <ou1112@naver.com>
2024-02-28 10:44:40 +0900

    test : 깃헙 로그인, 정보 조회 인수 테스트 작성

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -3,12 +3,14 @@ package nextstep.member.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.favorite.application.dto.GithubResponses;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import nextstep.utils.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -17,6 +19,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AuthAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "admin@email.com";
     public static final String PASSWORD = "password";
@@ -51,5 +54,47 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 .statusCode(HttpStatus.OK.value()).extract();
 
         assertThat(response2.jsonPath().getString("email")).isEqualTo(EMAIL);
+    }
+
+    @DisplayName("Github Auth")
+    @Test
+    void githubAuth() {
+        Map<String, String> params = new HashMap<>();
+        params.put("code", "code");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/github/login/oauth/access_token")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
+
+        assertThat(response.jsonPath().getString("code")).isNotBlank();
+    }
+
+    @DisplayName("Github Login")
+    @Test
+    void githubLogin() {
+        Map<String, String> params = new HashMap<>();
+        params.put("code", "code");
+
+        ExtractableResponse<Response> tokenResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/github/login/oauth/access_token")
+                .then().log().all().extract();
+
+        // then
+        assertThat(tokenResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // token 을 이용해서 정보 조회를 했을 때
+        ExtractableResponse<Response> profileResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/github/user")
+                .then().log().all().extract();
+
+        String token = profileResponse.jsonPath().getString(".");
+        assertThat(profileResponse.jsonPath().get("email").toString()).isEqualTo("email");
     }
 }
