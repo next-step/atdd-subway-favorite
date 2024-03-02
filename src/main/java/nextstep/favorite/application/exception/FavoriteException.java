1ea7b67edc35917820e224c18c46fe654d7090cf
Lee SeungWoo <64122884+xonic789@users.noreply.github.com>
2024-03-02 14:07:05 +0900

    Update src/main/java/nextstep/favorite/application/exception/FavoriteException.java
    Co-authored-by: testrace <odetsuo@gmail.com>

--- src/main/java/nextstep/favorite/application/exception/FavoriteException.java
+++ src/main/java/nextstep/favorite/application/exception/FavoriteException.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public class FavoriteException extends ApplicationException {
 
-    private final static String NOT_FOUND = "즐겨찾기를 찾을 수 없습니다.";
-    private final static String NOT_CREATED = "즐겨찾기 추가에 실패했습니다.";
+    private static final String NOT_FOUND = "즐겨찾기를 찾을 수 없습니다.";
+    private static final String NOT_CREATED = "즐겨찾기 추가에 실패했습니다.";
 
     public FavoriteException(HttpStatus status, String message) {
         super(status, message);
