a460c22a2dc0326017a82656dbb862c24d05d895
KIM SAN <devmtn30@gmail.com>
2024-03-02 13:13:41 +0900

    🚀 1단계 - 즐겨찾기 기능 완성 (#633)
    * 8th init - sample
    
    * feat: 실습
    
    * feat 즐겨 찾기 기능 인수 테스트 작성
    
    * test: Favorite unit test
    
    * feat: 🚀 1단계 - 즐겨찾기 기능 완성
    
    * test: 예외처리 상황 인수테스트
    
    ---------
    
    Co-authored-by: boorownie <probitanima11@gmail.com>

--- src/main/java/nextstep/favorite/domain/Favorite.java
+++ src/main/java/nextstep/favorite/domain/Favorite.java
@@ -1,9 +1,14 @@
 package nextstep.favorite.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import nextstep.member.domain.Member;
+import nextstep.subway.domain.Station;
 
 @Entity
 public class Favorite {
@@ -11,4 +16,60 @@ public class Favorite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "source")
+    private Station source;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "target")
+    private Station target;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "create_user")
+    private Member createUser;
+
+    public Favorite() {
+    }
+
+    public Favorite(Station source, Station target, Member createUser) {
+        this.source = source;
+        this.target = target;
+        this.createUser = createUser;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Station getSource() {
+        return source;
+    }
+
+    public void setSource(Station source) {
+        this.source = source;
+    }
+
+    public Station getTarget() {
+        return target;
+    }
+
+    public void setTarget(Station target) {
+        this.target = target;
+    }
+
+    public Member getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Member createUser) {
+        this.createUser = createUser;
+    }
+
+    public boolean isCreateUser(Member loginMember) {
+        return loginMember.equals(createUser);
+    }
 }
--- src/main/java/nextstep/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/favorite/ui/FavoriteController.java
@@ -3,6 +3,8 @@ package nextstep.favorite.ui;
 import nextstep.favorite.application.FavoriteService;
 import nextstep.favorite.application.dto.FavoriteRequest;
 import nextstep.favorite.application.dto.FavoriteResponse;
+import nextstep.member.domain.LoginMember;
+import nextstep.member.ui.AuthenticationPrincipal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,22 +20,24 @@ public class FavoriteController {
     }
 
     @PostMapping("/favorites")
-    public ResponseEntity createFavorite(@RequestBody FavoriteRequest request) {
-        favoriteService.createFavorite(request);
+    public ResponseEntity createFavorite(@RequestBody FavoriteRequest request,
+        @AuthenticationPrincipal LoginMember member) {
+        favoriteService.createFavorite(request, member);
         return ResponseEntity
                 .created(URI.create("/favorites/" + 1L))
                 .build();
     }
 
     @GetMapping("/favorites")
-    public ResponseEntity<List<FavoriteResponse>> getFavorites() {
-        List<FavoriteResponse> favorites = favoriteService.findFavorites();
+    public ResponseEntity<List<FavoriteResponse>> getFavorites(@AuthenticationPrincipal LoginMember member) {
+        List<FavoriteResponse> favorites = favoriteService.findFavorites(member);
         return ResponseEntity.ok().body(favorites);
     }
 
     @DeleteMapping("/favorites/{id}")
-    public ResponseEntity deleteFavorite(@PathVariable Long id) {
-        favoriteService.deleteFavorite(id);
+    public ResponseEntity deleteFavorite(@PathVariable Long id,
+        @AuthenticationPrincipal LoginMember member) {
+        favoriteService.deleteFavorite(id, member);
         return ResponseEntity.noContent().build();
     }
 }
