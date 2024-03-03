ef8b772f768fcff2dd0b0bfe1bf26e0d47d202c9
o3ozzvb <ou1112@naver.com>
2024-03-03 21:43:26 +0900

    test : TokenServiceMockTest 수정

--- src/test/java/nextstep/auth/application/TokenServiceMockTest.java
+++ src/test/java/nextstep/auth/application/TokenServiceMockTest.java
@@ -1,9 +1,9 @@
-package nextstep.member.application;
+package nextstep.auth.application;
 
-import nextstep.favorite.application.GithubClient;
-import nextstep.favorite.application.dto.GithubProfileResponse;
-import nextstep.member.application.dto.TokenResponse;
+import nextstep.auth.application.dto.GithubProfileResponse;
+import nextstep.auth.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,9 +18,9 @@ public class TokenServiceMockTest {
     @Mock
     private GithubClient githubClient;
     @Mock
-    private MemberService memberService;
-    @Mock
     private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private UserDetailsService userDetailsService;
 
     @DisplayName("github token 발급 테스트")
     @Test
@@ -28,22 +28,21 @@ public class TokenServiceMockTest {
         String code = "code";
         String githubAccessToken = "github_access_token";
         GithubProfileResponse githubProfileResponse = new GithubProfileResponse("test@test.com", 20);
-        Member member = createMemberFrom(githubProfileResponse);
+        UserDetails userDetails = createUserDetailsFrom(githubProfileResponse);
         String accessToken = "access_token";
 
         when(githubClient.requestGithubToken(code)).thenReturn(githubAccessToken);
         when(githubClient.requestGithubProfile(githubAccessToken)).thenReturn(githubProfileResponse);
-        when(memberService.findOrCreateMember(githubProfileResponse)).thenReturn(member);
-        when(jwtTokenProvider.createToken(member.getEmail())).thenReturn(accessToken);
+        when(userDetailsService.findOrCreateMember(githubProfileResponse.getEmail(), githubProfileResponse.getAge())).thenReturn(userDetails);
+        when(jwtTokenProvider.createToken(githubProfileResponse.getEmail())).thenReturn(accessToken);
 
-        TokenService tokenService = new TokenService(memberService, jwtTokenProvider, githubClient);
+        TokenService tokenService = new TokenService(jwtTokenProvider, githubClient, userDetailsService);
         TokenResponse tokenResponse = tokenService.createGithubToken(code);
 
         assertThat(tokenResponse.getAccessToken()).isEqualTo(accessToken);
     }
 
-    private Member createMemberFrom(final GithubProfileResponse githubProfileResponse) {
-        final Member member = new Member(1L, githubProfileResponse.getEmail(), "", githubProfileResponse.getAge());
-        return member;
+    private UserDetails createUserDetailsFrom(final GithubProfileResponse githubProfileResponse) {
+        return new UserDetails(1L, githubProfileResponse.getEmail(), "", githubProfileResponse.getAge());
     }
 }
