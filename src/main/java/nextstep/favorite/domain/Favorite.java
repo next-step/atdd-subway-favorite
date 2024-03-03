67a6d3c38d5d14dbef8385d05e8568b3094685fe
cjsrhd94 <cjsrhd94@gmail.com>
2024-03-03 21:16:06 +0900

    feat: 즐겨찾기 생성, 목록조회, 삭제 기능 구현

--- src/main/java/nextstep/favorite/domain/Favorite.java
+++ src/main/java/nextstep/favorite/domain/Favorite.java
@@ -10,5 +10,48 @@ public class Favorite {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long memberId;
+    private Long sourceStationId;
+    private Long targetStationId;
 
+    public Favorite() {}
+
+    public Favorite(Long memberId, Long sourceStationId, Long targetStationId) {
+        this.memberId = memberId;
+        this.sourceStationId = sourceStationId;
+        this.targetStationId = targetStationId;
+    }
+
+    public Favorite(Long id, Long memberId, Long sourceStationId, Long targetStationId) {
+        this.id = id;
+        this.memberId = memberId;
+        this.sourceStationId = sourceStationId;
+        this.targetStationId = targetStationId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Long getSourceStationId() {
+        return sourceStationId;
+    }
+
+    public Long getTargetStationId() {
+        return targetStationId;
+    }
+
+    public void validate(Long memberId) {
+        if (!isOwner(memberId)) {
+            throw new IllegalArgumentException("해당 즐겨찾기 생성자가 아닙니다.");
+        }
+    }
+
+    private boolean isOwner(Long memberId) {
+        return this.memberId.equals(memberId);
+    }
 }
