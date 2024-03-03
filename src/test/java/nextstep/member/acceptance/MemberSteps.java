6c02f05b0ad33eb55d420f1f4092863d31e1cb44
cjsrhd94 <cjsrhd94@gmail.com>
2024-03-03 21:14:55 +0900

    test: favorite 인수테스트 작성

--- src/test/java/nextstep/member/acceptance/MemberSteps.java
+++ src/test/java/nextstep/member/acceptance/MemberSteps.java
@@ -3,6 +3,8 @@ package nextstep.member.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.member.application.dto.TokenRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
@@ -63,5 +65,14 @@ public class MemberSteps {
         assertThat(response.jsonPath().getString("id")).isNotNull();
         assertThat(response.jsonPath().getString("email")).isEqualTo(email);
         assertThat(response.jsonPath().getInt("age")).isEqualTo(age);
+    }
+
+    public static ExtractableResponse<Response> 회원_로그인_요청(String email, String password) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest(email, password))
+                .when().post("/login/token")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
     }
 }
