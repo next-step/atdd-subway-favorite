787f1852708f3b8e7794efb2c574c8ca5a8ef3b7
o3ozzvb <ou1112@naver.com>
2024-03-03 21:32:46 +0900

    refactor: 기존 코드 제거

--- src/main/java/nextstep/auth/application/TokenService.java
+++ src/main/java/nextstep/auth/application/TokenService.java
@@ -1,40 +1,25 @@
 package nextstep.auth.application;
 
-import nextstep.favorite.application.GithubClient;
-import nextstep.favorite.application.dto.GithubProfileResponse;
 import nextstep.auth.AuthenticationException;
 import nextstep.auth.application.dto.TokenResponse;
-import nextstep.member.application.MemberService;
-import nextstep.member.domain.Member;
+import nextstep.favorite.application.GithubClient;
+import nextstep.favorite.application.dto.GithubProfileResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 @Service
 public class TokenService {
-    private MemberService memberService;
     private JwtTokenProvider jwtTokenProvider;
     private GithubClient githubClient;
     private UserDetailsService userDetailsService;
 
-    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider, GithubClient githubClient, UserDetailsService userDetailsService) {
-        this.memberService = memberService;
+    public TokenService(JwtTokenProvider jwtTokenProvider, GithubClient githubClient, UserDetailsService userDetailsService) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.githubClient = githubClient;
         this.userDetailsService = userDetailsService;
     }
 
     public TokenResponse createToken(String email, String password) {
-        Member member = memberService.findMemberByEmail(email);
-        if (!member.getPassword().equals(password)) {
-            throw new AuthenticationException();
-        }
-
-        String token = jwtTokenProvider.createToken(member.getEmail());
-
-        return new TokenResponse(token);
-    }
-
-    public TokenResponse createToken_new(String email, String password) {
         UserDetails userDetails = userDetailsService.findMemberByEmail(email);
         if (!userDetails.getPassword().equals(password)) {
             throw new AuthenticationException();
@@ -46,18 +31,6 @@ public class TokenService {
     }
 
     public TokenResponse createGithubToken(final String code) {
-        String githubToken = githubClient.requestGithubToken(code);
-        if (StringUtils.isEmpty(githubToken)) {
-            throw new AuthenticationException();
-        }
-        GithubProfileResponse githubProfileResponse = githubClient.requestGithubProfile(githubToken);
-        Member member = memberService.findOrCreateMember(githubProfileResponse);
-        String token = jwtTokenProvider.createToken(member.getEmail());
-
-        return new TokenResponse(token);
-    }
-
-    public TokenResponse createGithubToken_new(final String code) {
         String githubToken = githubClient.requestGithubToken(code);
         if (StringUtils.isEmpty(githubToken)) {
             throw new AuthenticationException();
