a2481952ac033f653577a7bef057cd555ab60f72
cjsrhd94 <cjsrhd94@gmail.com>
2024-03-04 22:35:50 +0900

    refactor: 즐겨찾기 삭제 로직 리팩토링
    - 즐겨찾기를 삭제할 수 있는지 확인하는 검증로직을 repository 단계로 이관

--- src/main/java/nextstep/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/favorite/domain/FavoriteRepository.java
@@ -3,7 +3,9 @@ package nextstep.favorite.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findAllByMemberId(Long memberId);
+    Optional<Favorite> findByIdAndMemberId(Long id, Long memberId);
 }
