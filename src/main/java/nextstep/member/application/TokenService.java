95bad39428715861facd278e121ec34b1ee22c2e
cjsrhd94 <cjsrhd94@gmail.com>
2024-03-05 16:40:13 +0900

    feat: github 로그인 기능 구현

--- src/main/java/nextstep/member/application/TokenService.java
+++ src/main/java/nextstep/member/application/TokenService.java
@@ -1,18 +1,23 @@
 package nextstep.member.application;
 
 import nextstep.member.AuthenticationException;
+import nextstep.member.application.dto.OAuth2ProfileResponse;
+import nextstep.member.application.dto.OAuth2LoginRequest;
 import nextstep.member.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class TokenService {
-    private MemberService memberService;
-    private JwtTokenProvider jwtTokenProvider;
+    private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final GithubClient githubClient;
 
-    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
+    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider, GithubClient githubClient) {
         this.memberService = memberService;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.githubClient = githubClient;
     }
 
     public TokenResponse createToken(String email, String password) {
@@ -20,6 +25,20 @@ public class TokenService {
         if (!member.getPassword().equals(password)) {
             throw new AuthenticationException();
         }
+
+        String token = jwtTokenProvider.createToken(member.getEmail());
+
+        return new TokenResponse(token);
+    }
+
+    public TokenResponse createTokenByGithubLogin(OAuth2LoginRequest request) {
+        String accessToken = githubClient.requestGithubToken(request.getCode());
+        if (!StringUtils.hasText(accessToken)) {
+            throw new AuthenticationException();
+        }
+
+        OAuth2ProfileResponse profileResponse = githubClient.requestGithubProfile(accessToken);
+        Member member = memberService.findOrCreateMember(profileResponse);
 
         String token = jwtTokenProvider.createToken(member.getEmail());
 
--- src/main/java/nextstep/member/ui/TokenController.java
+++ src/main/java/nextstep/member/ui/TokenController.java
@@ -1,6 +1,7 @@
 package nextstep.member.ui;
 
 import nextstep.member.application.TokenService;
+import nextstep.member.application.dto.OAuth2LoginRequest;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TokenController {
-    private TokenService tokenService;
+    private final TokenService tokenService;
 
     public TokenController(TokenService tokenService) {
         this.tokenService = tokenService;
@@ -19,6 +20,13 @@ public class TokenController {
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> createToken(@RequestBody TokenRequest request) {
         TokenResponse response = tokenService.createToken(request.getEmail(), request.getPassword());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login/github")
+    public ResponseEntity<TokenResponse> getAccessToken(@RequestBody OAuth2LoginRequest request) {
+        TokenResponse response = tokenService.createTokenByGithubLogin(request);
 
         return ResponseEntity.ok(response);
     }
