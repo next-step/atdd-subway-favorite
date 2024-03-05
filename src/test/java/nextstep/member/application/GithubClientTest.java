120a55899aafa5f736420d2bd43e432e3d3a0d19
sunga <eert2260@naver.com>
2024-03-06 00:20:27 +0900

    test : 깃헙 사용자 정보 반환 테스트

--- src/test/java/nextstep/member/application/GithubClientTest.java
+++ src/test/java/nextstep/member/application/GithubClientTest.java
@@ -1,5 +1,6 @@
 package nextstep.member.application;
 
+import nextstep.member.application.dto.GithubProfileResponse;
 import nextstep.utils.GithubResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -23,6 +25,18 @@ public class GithubClientTest {
 
         assertThat(githubToken).isNotBlank();
         assertThat(githubToken).isEqualTo(GithubResponse.회원.getAccessToken());
+    }
+
+    @Test
+    void 사용자_정보_반환() {
+        String accessToken = GithubResponse.회원.getAccessToken();
+
+        GithubProfileResponse response = githubClient.requestGithubProfile(accessToken);
+
+        assertAll(
+                () -> assertThat(response.getEmail()).isEqualTo(GithubResponse.회원.getEmail()),
+                () -> assertThat(response.getAge()).isEqualTo(GithubResponse.회원.getAge())
+        );
     }
 
 }
