f53eed930b59ebd344691cd43f23361b76a753a2
ian <xonic789@naver.com>
2024-03-07 00:33:52 +0900

    피드백 반영 - GithubResponses 리팩터링

--- src/main/java/nextstep/test/GithubResponses.java
+++ src/main/java/nextstep/test/GithubResponses.java
@@ -1,5 +1,7 @@
 package nextstep.test;
 
+import java.util.Arrays;
+
 public enum GithubResponses {
     사용자1("aofijeowifjaoief", "access_token_1", "email1@email.com"),
     사용자2("fau3nfin93dmn", "access_token_2", "email2@email.com"),
@@ -30,36 +32,16 @@ public enum GithubResponses {
     }
 
     public static GithubResponses from(String code) {
-        switch (code) {
-            case "aofijeowifjaoief":
-                return GithubResponses.사용자1;
-            case "fau3nfin93dmn":
-                return GithubResponses.사용자2;
-            case "afnm93fmdodf":
-                return GithubResponses.사용자3;
-            case "fm04fndkaladmd":
-                return GithubResponses.사용자4;
-            case "code":
-                return GithubResponses.이미_가입된_사용자;
-            default:
-                throw new IllegalArgumentException("존재하지 않는 코드입니다.");
-        }
+        return Arrays.stream(GithubResponses.values())
+            .filter(githubResponses -> githubResponses.code.equals(code))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 코드입니다."));
     }
 
     public static GithubResponses fromAccessToken(String accessToken) {
-        switch (accessToken) {
-            case "access_token_1":
-                return GithubResponses.사용자1;
-            case "access_token_2":
-                return GithubResponses.사용자2;
-            case "access_token_3":
-                return GithubResponses.사용자3;
-            case "access_token_4":
-                return GithubResponses.사용자4;
-            case "access_token5":
-                return GithubResponses.이미_가입된_사용자;
-            default:
-                throw new IllegalArgumentException("존재하지 않는 액세스 토큰입니다.");
-        }
+        return Arrays.stream(GithubResponses.values())
+            .filter(githubResponses -> githubResponses.accessToken.equals(accessToken))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 액세스 토큰입니다."));
     }
 }
