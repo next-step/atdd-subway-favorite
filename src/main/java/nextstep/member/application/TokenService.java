e43433644de9f572c8b612556b8bc034386453ae
10015387 <sunga08@wjtb.net>
2024-03-06 12:22:54 +0900

    feat : 비회원 로그인 시 회원 가입 진행 로직 추가

--- src/main/java/nextstep/member/application/TokenService.java
+++ src/main/java/nextstep/member/application/TokenService.java
@@ -4,6 +4,7 @@ import nextstep.member.AuthenticationException;
 import nextstep.member.application.dto.GithubProfileResponse;
 import nextstep.member.application.dto.TokenResponse;
 import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,13 @@ public class TokenService {
     private MemberService memberService;
     private JwtTokenProvider jwtTokenProvider;
     private GithubClient githubClient;
+    private MemberRepository memberRepository;
 
-    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider, GithubClient githubClient) {
+    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider, GithubClient githubClient, MemberRepository memberRepository) {
         this.memberService = memberService;
         this.jwtTokenProvider = jwtTokenProvider;
         this.githubClient = githubClient;
+        this.memberRepository = memberRepository;
     }
 
     public TokenResponse createToken(String email, String password) {
@@ -32,6 +35,11 @@ public class TokenService {
     public TokenResponse githubLogin(String code) {
         String accessToken = githubClient.requestGithubToken(code);
         GithubProfileResponse profile = githubClient.requestGithubProfile(accessToken);
+
+        Member member = memberRepository.findByEmail(profile.getEmail()).orElse(null);
+        if(member == null) {
+            memberRepository.save(new Member(profile.getEmail(), "", profile.getAge()));
+        }
 
         String token = jwtTokenProvider.createToken(profile.getEmail());
         return new TokenResponse(token);
