ddd16ab5db253b0d2496fa070fa90e4739ee0d01
Hyeon9mak <jinha3507@gmail.com>
2024-03-10 15:38:19 +0900

    test: 즐겨찾기 등록시 경로를 찾을 수 없거나 연결되지 않는 등 경로 조회가 불가능한 조회의 경우 테스트 작성

--- src/test/java/nextstep/favorite/unit/AddFavoriteTest.java
+++ src/test/java/nextstep/favorite/unit/AddFavoriteTest.java
@@ -24,6 +24,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("즐겨찾기 생성 테스트")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -52,6 +53,7 @@ public class AddFavoriteTest {
 
     private Station 강남역;
     private Station 역삼역;
+    private Station 선릉역;
     private Member 최현구;
 
     @BeforeEach
@@ -64,6 +66,7 @@ public class AddFavoriteTest {
 
         강남역 = stationRepository.save(new Station("강남역"));
         역삼역 = stationRepository.save(new Station("역삼역"));
+        선릉역 = stationRepository.save(new Station("선릉역"));
 
         lineRepository.save(new Line("2호선", "green", 강남역, 역삼역, 10L));
 
@@ -78,14 +81,11 @@ public class AddFavoriteTest {
         @DisplayName("즐겨찾기에 지하철역을 등록한다.")
         @Test
         void addFavorite() {
-            // given
             LoginMember loginMember = new LoginMember("jinha3507@gmail.com");
             FavoriteRequest request = new FavoriteRequest(강남역.getId(), 역삼역.getId());
 
-            // when
             addFavoriteService.addFavorite(loginMember, request);
 
-            // then
             List<Favorite> results = favoriteRepository.findAll();
             assertThat(results).hasSize(1);
 //            assertThat(results.get(0).getId()).isNotNull();
@@ -97,25 +97,24 @@ public class AddFavoriteTest {
         @DisplayName("경로를 찾을 수 없거나 연결되지 않는 등 경로 조회가 불가능한 조회의 경우 즐겨찾기로 등록할 수 없다.")
         @Test
         void addFavoriteWithInvalidPath() {
-            // given
-            // when
-            // then
+            LoginMember loginMember = new LoginMember("jinha3507@gmail.com");
+            FavoriteRequest request = new FavoriteRequest(강남역.getId(), 선릉역.getId());
+
+            assertThatThrownBy(() -> addFavoriteService.addFavorite(loginMember, request))
+                    .isExactlyInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("도착역이 존재하지 않습니다. stationId: " + 선릉역.getId());
         }
 
         @DisplayName("존재하지 않는 역을 즐겨찾기로 등록할 수 없다.")
         @Test
         void addFavoriteWithInvalidStation() {
-            // given
-            // when
-            // then
+
         }
 
         @DisplayName("즐겨찾기에 이미 등록된 역을 다시 등록할 수 없다.")
         @Test
         void addFavoriteWithDuplicateStation() {
-            // given
-            // when
-            // then
+
         }
     }
 }
