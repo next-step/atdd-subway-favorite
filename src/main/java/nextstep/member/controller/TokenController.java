3532304b986dbd6b5f9cb6d00b2a9753e87a4a81
Lee-KyungSeok <leeseok8347@gmail.com>
2024-08-03 18:35:55 +0900

    TokenController 로직 작성 및 인수테스트 완료

--- src/main/java/nextstep/member/controller/TokenController.java
+++ src/main/java/nextstep/member/controller/TokenController.java
@@ -1,31 +1,30 @@
 package nextstep.member.controller;
 
+import lombok.RequiredArgsConstructor;
 import nextstep.member.controller.dto.OAuthCodeRequest;
-import nextstep.member.domain.command.TokenCommander;
 import nextstep.member.controller.dto.TokenRequest;
 import nextstep.member.controller.dto.TokenResponse;
+import nextstep.member.domain.command.TokenCommander;
+import nextstep.member.domain.command.authenticator.SocialOAuthProvider;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TokenController {
     private final TokenCommander tokenCommander;
 
-    public TokenController(TokenCommander tokenCommander) {
-        this.tokenCommander = tokenCommander;
-    }
-
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> createToken(@RequestBody TokenRequest request) {
-        TokenResponse response = new TokenResponse(tokenCommander.createToken(request.getEmail(), request.getPassword()));
-        return ResponseEntity.ok(response);
+        String token = tokenCommander.createToken(request.getEmail(), request.getPassword());
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 
     @PostMapping("/login/github")
     public ResponseEntity<TokenResponse> createToken(@RequestBody OAuthCodeRequest request) {
-        TokenResponse response = new TokenResponse("토큰토큰");
-        return ResponseEntity.ok(response);
+        String token = tokenCommander.authenticateSocialOAuth(request.toCommand(SocialOAuthProvider.GITHUB));
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 }
--- src/main/java/nextstep/member/controller/dto/OAuthCodeRequest.java
+++ src/main/java/nextstep/member/controller/dto/OAuthCodeRequest.java
@@ -1,8 +1,18 @@
 package nextstep.member.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nextstep.member.domain.command.authenticator.AuthenticateSocialOAuthCommand;
+import nextstep.member.domain.command.authenticator.SocialOAuthProvider;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OAuthCodeRequest {
     private String code;
+
+    public AuthenticateSocialOAuthCommand.ByAuthCode toCommand(SocialOAuthProvider provider) {
+        return new AuthenticateSocialOAuthCommand.ByAuthCode(provider, code);
+    }
 }
