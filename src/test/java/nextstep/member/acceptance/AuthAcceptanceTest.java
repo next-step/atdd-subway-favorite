57ef2fa0acb79582db808e5a7a070967f1465d08
danny <gryb809@gmail.com>
2024-08-04 21:09:31 +0900

    feat: 인증 관련 step 생성

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
+import static nextstep.member.acceptance.AuthSteps.로그인_토큰_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AuthAcceptanceTest extends AcceptanceTest {
@@ -30,17 +31,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
     void bearerAuth() {
         memberRepository.save(new Member(EMAIL, PASSWORD, AGE));
 
-        Map<String, String> params = new HashMap<>();
-        params.put("email", EMAIL);
-        params.put("password", PASSWORD);
-
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(params)
-                .when().post("/login/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value()).extract();
-
+        ExtractableResponse<Response> response = 로그인_토큰_요청(EMAIL, PASSWORD);
         String accessToken = response.jsonPath().getString("accessToken");
         assertThat(accessToken).isNotBlank();
 
--- src/test/java/nextstep/member/acceptance/AuthSteps.java
+++ src/test/java/nextstep/member/acceptance/AuthSteps.java
@@ -3,25 +3,24 @@ package nextstep.member.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class AuthSteps {
-    public static ExtractableResponse<Response> 회원_생성_요청(String email, String password, Integer age) {
+    public static ExtractableResponse<Response> 로그인_토큰_요청(String email, String password) {
         Map<String, String> params = new HashMap<>();
         params.put("email", email);
         params.put("password", password);
-        params.put("age", age + "");
 
-        return RestAssured
-                .given().log().all()
+
+        return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(params)
-                .when().post("/members")
-                .then().log().all().extract();
+                .when().post("/login/token")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
     }
 }
