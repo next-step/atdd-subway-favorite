00df9cbcaadddfe502dfb444e9a9ea638f742275
jiyeong-dt <imjee02@gmail.com>
2024-08-04 20:53:09 +0900

    feat: ExceptionAdvice에 ResponseStatus 파싱 기능 추가

--- src/main/java/nextstep/exceptions/ExceptionAdvice.java
+++ src/main/java/nextstep/exceptions/ExceptionAdvice.java
@@ -2,6 +2,7 @@ package nextstep.exceptions;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
@@ -11,6 +12,12 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<Map<String, String>> baseException(BaseException ex) {
+        if (ex.getClass().isAnnotationPresent(ResponseStatus.class)) {
+            ResponseStatus responseStatus = ex.getClass().getAnnotation(ResponseStatus.class);
+            return ResponseEntity.status(responseStatus.value())
+                    .body(Map.of("message", ex.getMessage()));
+        }
+
         return ResponseEntity.badRequest().body(Map.of("message", ex.getMessage()));
     }
 
