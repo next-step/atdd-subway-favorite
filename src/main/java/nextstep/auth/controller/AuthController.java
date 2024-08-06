6f63fd314da70bef9d9733446abb7d332cca054e
Lee-KyungSeok <leeseok8347@gmail.com>
2024-08-06 22:05:49 +0900

    TokenController -> AuthController

--- src/main/java/nextstep/auth/controller/AuthController.java
+++ src/main/java/nextstep/auth/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TokenController {
+public class AuthController {
     private final EmailPasswordAuthenticator emailPasswordAuthenticator;
     private final SocialOAuthUserAuthenticator socialOAuthUserAuthenticator;
 
