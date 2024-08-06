d9bf6336ec37e09fab75f70fb7e042c56d8a48f4
jiyeong-dt <imjee02@gmail.com>
2024-08-07 00:55:12 +0900

    feat: redirect 요청을 받을 accessToken 엔드포인트 생성

---
+++ src/main/java/nextstep/member/ui/OAuth2Controller.java
@@ -0,0 +1,22 @@
+package nextstep.member.ui;
+
+import nextstep.member.application.GithubOAuth2Service;
+import nextstep.member.application.dto.TokenResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuth2Controller {
+
+    private final GithubOAuth2Service githubOAuth2Service;
+
+    public OAuth2Controller(final GithubOAuth2Service githubOAuth2Service) {
+        this.githubOAuth2Service = githubOAuth2Service;
+    }
+
+    @GetMapping("/login/code/github")
+    public ResponseEntity<TokenResponse> createToken(String code) {
+        return ResponseEntity.ok(githubOAuth2Service.createToken(code));
+    }
+}
