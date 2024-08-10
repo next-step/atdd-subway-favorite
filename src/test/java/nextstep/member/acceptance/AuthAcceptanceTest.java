76588f00545bcf8e142838c78a87bbc36e8b90aa
danny <gryb809@gmail.com>
2024-08-10 15:22:25 +0900

    fix: 테스트 실행 오류 수정

--- src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/member/acceptance/AuthAcceptanceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import static nextstep.member.acceptance.AuthSteps.Github_로그인_토큰_요청;
@@ -15,7 +14,6 @@ import static nextstep.member.acceptance.AuthSteps.로그인_토큰_요청;
 import static nextstep.member.acceptance.MemberSteps.본인_정보_조회;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
 @DisplayName("인증 토큰 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Sql(scripts = "classpath:truncate-tables.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
