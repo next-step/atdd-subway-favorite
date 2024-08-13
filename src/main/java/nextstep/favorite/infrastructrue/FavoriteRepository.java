5c0509caf340a9976bc0730082142dbfbebe35d3
m5s3 <devmyoungsoo2@gmail.com>
2024-08-14 08:49:40 +0900

    refactor(Favorite): 미사용 메소드 제거

--- src/main/java/nextstep/favorite/infrastructrue/FavoriteRepository.java
+++ src/main/java/nextstep/favorite/infrastructrue/FavoriteRepository.java
@@ -7,7 +7,6 @@ import nextstep.favorite.domain.Favorite;
 public interface FavoriteRepository {
 
     Favorite save(Favorite favorite);
-    List<Favorite> findAll();
     Optional<Favorite> findById(Long id);
     void deleteById(Long id);
     List<Favorite> findByMemberId(Long memberId);
