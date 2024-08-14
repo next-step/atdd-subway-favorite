f0930d5c3885a6e9b8ad4d222cd905d4bc6870f9
Jiyoung Won <jiyoung928@gmail.com>
2024-08-14 15:31:37 +0900

    step2 : 깃헙 로그인 구현

--- src/main/java/nextstep/member/ui/TokenController.java
+++ src/main/java/nextstep/member/ui/TokenController.java
@@ -1,6 +1,9 @@
 package nextstep.member.ui;
 
+import nextstep.member.application.GithubClient;
 import nextstep.member.application.TokenService;
+import nextstep.member.application.dto.GithubAccessTokenRequest;
+import nextstep.member.application.dto.GithubAccessTokenResponse;
 import nextstep.member.application.dto.TokenRequest;
 import nextstep.member.application.dto.TokenResponse;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +22,12 @@ public class TokenController {
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> createToken(@RequestBody TokenRequest request) {
         TokenResponse response = tokenService.createToken(request.getEmail(), request.getPassword());
-
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login/github")
+    public ResponseEntity<GithubAccessTokenResponse> createGithubToken(@RequestBody GithubAccessTokenRequest request) {
+        GithubAccessTokenResponse response  = tokenService.creteGithubAccessToken(request.getCode());
+        return ResponseEntity.ok(response );
     }
 }
