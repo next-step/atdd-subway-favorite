0ad2e135f35447978b1ddc5c8b071dab9658651c
m5s3 <devmyoungsoo2@gmail.com>
2024-08-19 12:59:31 +0900

    refactor(RestTemplateUtils): `RestTemplate` 을 재사용 하도록 수정

--- src/main/java/nextstep/utils/RestTemplateUtils.java
+++ src/main/java/nextstep/utils/RestTemplateUtils.java
@@ -6,20 +6,19 @@ import org.springframework.web.client.RestTemplate;
 
 public final class RestTemplateUtils {
 
+    private static final RestTemplate restTemplate = new RestTemplate();
+
     private RestTemplateUtils() {
         throw new IllegalStateException("Utility class");
     }
 
     public static <T> T post(HttpEntity<?> httpEntity, String uri, Class<T> responseClass) {
-        RestTemplate restTemplate = new RestTemplate();
         return restTemplate
                 .postForEntity(uri, httpEntity, responseClass)
                 .getBody();
     }
 
     public static <T> T getWithHeaders(HttpEntity<?> httpEntity, String uri, Class<T> responseClass) {
-        RestTemplate restTemplate = new RestTemplate();
-
         return restTemplate
                 .exchange(uri, HttpMethod.GET, httpEntity, responseClass)
                 .getBody();
