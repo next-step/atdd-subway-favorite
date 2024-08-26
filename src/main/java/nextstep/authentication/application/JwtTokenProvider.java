bf6094f6bbe09e730c450c8a1468f5ac5cf0b8e7
jungwoonson <jungwoon727@gmail.com>
2024-08-26 14:41:13 +0900

    refactor: 상수화로 가독성 향상

--- src/main/java/nextstep/authentication/application/JwtTokenProvider.java
+++ src/main/java/nextstep/authentication/application/JwtTokenProvider.java
@@ -8,6 +8,9 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+
+    private static final String ID = "id";
+
     @Value("${security.jwt.token.secret-key}")
     private String secretKey;
     @Value("${security.jwt.token.expire-length}")
@@ -15,7 +18,7 @@ public class JwtTokenProvider {
 
     public String createToken(String principal, Long id) {
         Claims claims = Jwts.claims().setSubject(principal);
-        claims.put("id", id);
+        claims.put(ID, id);
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
 
@@ -28,17 +31,25 @@ public class JwtTokenProvider {
     }
 
     public String getPrincipal(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public Long getId(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("id", Long.class);
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .get(ID, Long.class);
     }
 
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             return false;
--- src/main/java/nextstep/authentication/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/authentication/ui/AuthenticationPrincipalArgumentResolver.java
@@ -12,6 +12,13 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String SPACE = " ";
+    private static final String BEARER = "bearer";
+    private static final int TOKEN_INDEX = 1;
+    private static final int BEARER_INDEX = 0;
+
     private JwtTokenProvider jwtTokenProvider;
 
     public AuthenticationPrincipalArgumentResolver(JwtTokenProvider jwtTokenProvider) {
@@ -37,15 +44,15 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     }
 
     private String getToken(NativeWebRequest webRequest) {
-        String authorization = webRequest.getHeader("Authorization");
+        String authorization = webRequest.getHeader(AUTHORIZATION);
         if (isUnauthorized(authorization)) {
             throw new AuthenticationException();
         }
 
-        return authorization.split(" ")[1];
+        return authorization.split(SPACE)[TOKEN_INDEX];
     }
 
     private boolean isUnauthorized(String authorization) {
-        return authorization == null || !"bearer".equalsIgnoreCase(authorization.split(" ")[0]);
+        return authorization == null || !BEARER.equalsIgnoreCase(authorization.split(SPACE)[BEARER_INDEX]);
     }
 }
